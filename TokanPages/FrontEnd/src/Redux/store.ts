f0ee2f748b8927ac5107edfe6a187d94d4e63119
Tomasz Kandula <tomasz.kandula@gmail.com>
2020-10-31 19:13:22 +0100

    fix: fix naming, use camelCase for files and PascalCase for folders

---
+++ TokanPages/FrontEnd/src/Redux/store.ts
@@ -0,0 +1,34 @@
+﻿import { applyMiddleware, combineReducers, compose, createStore } from "redux";
+import thunk from "redux-thunk";
+import { connectRouter, routerMiddleware } from "connected-react-router";
+import { History } from "history";
+
+export default function configureStore(history: History) 
+{
+    
+    const middleware = 
+    [
+        thunk,
+        routerMiddleware(history)
+    ];
+
+    const rootReducer = combineReducers(
+    {
+        // ... other reducers here,
+        router: connectRouter(history)
+    });
+
+    const enhancers = [];
+    const windowIfDefined = typeof window === 'undefined' ? null : window as any;
+    
+    if (windowIfDefined && windowIfDefined.__REDUX_DEVTOOLS_EXTENSION__) {
+        enhancers.push(windowIfDefined.__REDUX_DEVTOOLS_EXTENSION__());
+    }
+
+    return createStore(
+        rootReducer,
+        // ... initial app state here,
+        compose(applyMiddleware(...middleware), ...enhancers)
+    );
+
+}
