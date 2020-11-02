023988267f14801c3521bb2367d2f62366b7b164
Tomasz Kandula <tomasz.kandula@gmail.com>
2020-11-02 16:27:18 +0100

    style: add links to footer, change colors

--- TokanPages/FrontEnd/src/Components/footer.tsx
+++ TokanPages/FrontEnd/src/Components/footer.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
 import Box from "@material-ui/core/Box";
@@ -6,11 +7,9 @@ import Typography from "@material-ui/core/Typography";
 import IconButton from "@material-ui/core/IconButton";
 import GitHubIcon from "@material-ui/icons/GitHub";
 import LinkedInIcon from "@material-ui/icons/LinkedIn";
-import InstagramIcon from "@material-ui/icons/Instagram";
 
 const useStyles = makeStyles((theme) => (
 {
-
 	root: 
   	{
 		[theme.breakpoints.down("md")]: 
@@ -19,51 +18,72 @@ const useStyles = makeStyles((theme) => (
 		},
 		backgroundColor: "#FFFFFF"
 	},
-	  
 	iconsBoxRoot: 
   	{
-    	[theme.breakpoints.down("md")]: 
+		[theme.breakpoints.down("md")]: 
     	{
       		width: "100%",
-      		marginBottom: theme.spacing(2),
+      		marginBottom: theme.spacing(0),
     	}
-  	},
-  
+  	}, 
 	copy: 
   	{
-    	[theme.breakpoints.down("md")]: 
+		[theme.breakpoints.down("md")]: 
     	{
       		width: "100%",
       		order: 12,
     	}
-  	}
-}
-));
+	},
+	links:
+	{
+		color: "#757575",
+		textDecoration: "none"
+	}	  
+}));
 
 export default function Footer(props: any) 
 {
   
 	const classes = useStyles();
 
+	const SetTermsLink = () => 
+	{
+
+		return (
+			<Link to="/terms" className={classes.links}>
+				Terms of use
+			</Link>			
+		);
+
+	}
+
+	const SetPolicyLink = () => 
+	{
+
+		return (
+			<Link to="/policy" className={classes.links}>
+				Privacy policy
+			</Link>			
+		);
+
+	}
+	
   	return (
     	<footer className={classes.root}>
       		<Container maxWidth="lg">
-        		<Box py={6} display="flex" flexWrap="wrap" alignItems="center">
-          			<Typography color="textSecondary" component="p" gutterBottom={false} className={classes.copy}>
-						  © 2020 Tomasz Kandula. All rights reserved.
+				<Box py={6} display="flex" flexWrap="wrap" alignItems="center">
+					<Typography color="textSecondary" component="p" gutterBottom={false} className={classes.copy}>
+						© 2020 Tomasz Kandula. All rights reserved. <SetTermsLink />. <SetPolicyLink />.
 					</Typography>
-          			<Box ml="auto" className={classes.iconsBoxRoot}>
-            			<IconButton color="default" aria-label="GitHub" href="https://github.com/TomaszKandula" target="_blank">
-              				<GitHubIcon />
-            			</IconButton>
-            			<IconButton color="default" aria-label="LinkedIn" href="https://www.linkedin.com/in/tomaszkandula/" target="_blank">
-              				<LinkedInIcon />
-            			</IconButton>
-            			<IconButton color="default" aria-label="Instagram" href="https://www.instagram.com/tomkandula/" target="_blank">
-              				<InstagramIcon />
-            			</IconButton>
-          			</Box>
-        		</Box>
+					<Box ml="auto" className={classes.iconsBoxRoot}>
+						<IconButton color="default" aria-label="GitHub" href="https://github.com/TomaszKandula" target="_blank">
+							<GitHubIcon />
+						</IconButton>
+						<IconButton color="default" aria-label="LinkedIn" href="https://www.linkedin.com/in/tomaszkandula/" target="_blank">
+		  					<LinkedInIcon />
+						</IconButton>
+  					</Box>
+				</Box>
       		</Container>
     	</footer>
   );
