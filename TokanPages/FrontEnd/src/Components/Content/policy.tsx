7e7b8940feb8f1b543707bf55b98f8d068cee907
Tomasz 'Tom' Kandula <tomasz.kandula@gmail.com>
2020-11-03 13:33:36 +0100

    feat: add back buttons (#16)

--- TokanPages/FrontEnd/src/Components/Content/policy.tsx
+++ TokanPages/FrontEnd/src/Components/Content/policy.tsx
@@ -1,12 +1,25 @@
 import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import Container from "@material-ui/core/Container";
 import Box from "@material-ui/core/Box";
 import Typography from "@material-ui/core/Typography";
+import { Divider, IconButton, makeStyles } from "@material-ui/core";
+import { ArrowBack } from "@material-ui/icons";
 import ReactHtmlParser from 'react-html-parser';
 import axios from "axios";
 
+const useStyles = makeStyles((theme) => (
+{
+    divider:
+    {
+        marginBottom: "30px"
+    }
+}));    
+
 export default function Content(props: { content: any; }) 
 {
+
+	const classes = useStyles();
 
     const [ policy, setPolicy ] = useState("Fetching content...");
     const fetchTerms = async () => 
@@ -21,9 +34,15 @@ export default function Content(props: { content: any; })
     	<section>
       		<Container maxWidth="sm">       
 		        <Box py={12}>
-                <Typography variant="overline" component="span">
-                    {ReactHtmlParser(policy)}
-                </Typography>
+                    <Link to="/">
+                        <IconButton>
+                            <ArrowBack/>
+                        </IconButton>        		
+                    </Link> 
+                    <Divider className={classes.divider} />
+                    <Typography variant="overline" component="span">
+                        {ReactHtmlParser(policy)}
+                    </Typography>
         		</Box>
 			</Container>
     	</section>
