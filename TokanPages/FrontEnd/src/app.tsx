edcae92f749b3c6c6104b340acb1de58e62fac3d
Tomasz Kandula <tomasz.kandula@gmail.com>
2020-11-04 17:54:02 +0100

    feat: add page for list of articles

--- TokanPages/FrontEnd/src/app.tsx
+++ TokanPages/FrontEnd/src/app.tsx
@@ -7,6 +7,7 @@ import Fab from "@material-ui/core/Fab";
 import KeyboardArrowUpIcon from "@material-ui/icons/KeyboardArrowUp";
 import MainPage from "./Pages/mainPage";
 import StoryPage from "./Pages/storyPage"; 
+import ArticlesPage from "./Pages/articlesPage";
 import TermsPage from "./Pages/termsPage";
 import PolicyPage from "./Pages/policyPage";
 import SigninPage from "./Pages/signinPage";
@@ -71,6 +72,7 @@ export default function App()
                 <Switch>
                   <Route exact path="/"><MainPage /></Route>
                   <Route exact path="/mystory"><StoryPage /></Route>
+                  <Route exact path="/articles"><ArticlesPage /></Route>
                   <Route exact path="/terms"><TermsPage /></Route>
                   <Route exact path="/policy"><PolicyPage /></Route>
                   <Route exact path="/signin"><SigninPage /></Route>
