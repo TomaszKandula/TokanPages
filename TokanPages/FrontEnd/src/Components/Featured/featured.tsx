802a4f9c74314e182539d2293033a5d754efaf42
Tomasz Kandula <tomasz.kandula@gmail.com>
2020-11-04 17:14:52 +0100

    refactor: update text and articles order

--- TokanPages/FrontEnd/src/Components/Featured/featured.tsx
+++ TokanPages/FrontEnd/src/Components/Featured/featured.tsx
@@ -34,7 +34,7 @@ export default function Featured(props: any)
     	      			<Box textAlign="center" mb={5}>
         	    			<Typography variant="h4" component="h2" gutterBottom={true}>Featured</Typography>
             				<Typography variant="subtitle1" color="textSecondary">
-								Below you can read various articles (either mine or where I'm featured) that has been published on different portals like Medium.com or LinkedIn.
+								My pick of three articles that I wrote and published elsewhere or articles where I'm featured. So far I can be find on Medium.com, LinkedIn and JustJoinIT.
 							</Typography>
 	          			</Box>
     	    		</Container>
@@ -44,24 +44,24 @@ export default function Featured(props: any)
               					<Grid item xs={12} md={4}>
 	                				<Card>
     	              					<CardActionArea href="http://geek.justjoin.it/wysokie-widelki-clickbait-devdebata" target="_blank">
-	    	                			<CardMedia className={classes.media} image="https://maindbstorage.blob.core.windows.net/tokanpages/images/article3.jpg" />
-    	    	            			<CardHeader title="Just Geek IT" subheader="Widełki płacowe stanowią standard w branży..." titleTypographyProps={{gutterBottom: true}}/>
+	    		                			<CardMedia className={classes.media} image="https://maindbstorage.blob.core.windows.net/tokanpages/images/article1.jpg" />
+        	        				    	<CardHeader title="Stored Procedures" subheader="I explain why I do not need them that much..." titleTypographyProps={{gutterBottom: true}} />
         	    	      				</CardActionArea>
             	    				</Card>
               					</Grid>
 		              			<Grid item xs={12} md={4}>
 		                			<Card>
 		        		    	    	<CardActionArea href="https://medium.com/@tomasz.kandula/sql-injection-1bde8bb76ebc" target="_blank">
-	        		        			<CardMedia className={classes.media} image="https://maindbstorage.blob.core.windows.net/tokanpages/images/article2.jpg" />
-			        		            <CardHeader title="SQL Injection" subheader="This article will explore the issue in greater detail..." titleTypographyProps={{gutterBottom: true}} />
+		        		        			<CardMedia className={classes.media} image="https://maindbstorage.blob.core.windows.net/tokanpages/images/article2.jpg" />
+				        		            <CardHeader title="SQL Injection" subheader="This article will explore the issue in greater detail..." titleTypographyProps={{gutterBottom: true}} />
         			        			</CardActionArea>
             	    				</Card>
               					</Grid>
               					<Grid item xs={12} md={4}>
                 					<Card>
 					        	        <CardActionArea href="https://medium.com/@tomasz.kandula/i-said-goodbye-to-stored-procedures-539d56350486" target="_blank">
-				                	    <CardMedia className={classes.media} image="https://maindbstorage.blob.core.windows.net/tokanpages/images/article1.jpg" />
-                				    	<CardHeader title="Stored Procedures" subheader="I explain why I do not need them that much..." titleTypographyProps={{gutterBottom: true}} />
+					                	    <CardMedia className={classes.media} image="https://maindbstorage.blob.core.windows.net/tokanpages/images/article3.jpg" />
+    		    	            			<CardHeader title="Just Geek IT" subheader="Widełki płacowe stanowią standard w branży..." titleTypographyProps={{gutterBottom: true}}/>
 	                    				</CardActionArea>
     	                			</Card>
         	          			</Grid>
