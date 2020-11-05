9c7da1a63a4d34c7f7f64081cce8f511bdabc873
Tomasz 'Tom' Kandula <tomasz.kandula@gmail.com>
2020-11-05 23:00:10 +0100

    style: change card layout for articles (#36)

--- TokanPages/FrontEnd/src/Components/Articles/articleList.tsx
+++ TokanPages/FrontEnd/src/Components/Articles/articleList.tsx
@@ -7,6 +7,10 @@ import ArticleCard from "./articleCard";
 
 const useStyles = makeStyles((theme) => (
 {
+	container:
+	{
+		maxWidth: "700px"
+	},
 	divider:
 	{
 		marginBottom: "30px"
@@ -17,12 +21,12 @@ export default function ArticleList()
 {
 
 	const classes = useStyles();
-	// Temp. mock
+	// Temp. mock replace with API call for JSON data
 	const content =  JSON.parse('{"articles": [{"uid":"a8db7e28-2d47-463c-9c38-c17706056f72","title": "abc","desc":"mnbvcxz lkjhgfdsa poiuytrewq"},{"uid":"d2dc8a0d-1167-412b-86bd-1cf406f1ec71","title": "def","desc":"poiuytrewq lkjhgfdsa mnbvcxz"}]}');
 
 	return (
     	<section>
-      		<Container maxWidth="xs">       
+      		<Container className={classes.container}>       
 			  	<Box pt={12} pb={8}>             
 				  	<Link to="/">
                         <IconButton>
