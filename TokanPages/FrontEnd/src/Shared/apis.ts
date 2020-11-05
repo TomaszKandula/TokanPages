007d41276957f1b6cae45b838bd8d03d5320c1ab
Tomasz 'Tom' Kandula <tomasz.kandula@gmail.com>
2020-11-05 18:27:53 +0100

    refactor: update api urls (#33)

--- TokanPages/FrontEnd/src/Shared/apis.ts
+++ TokanPages/FrontEnd/src/Shared/apis.ts
@@ -1,5 +1,5 @@
 export const APP_URL     = "http://localhost:3000";
 export const STORAGE_URL = "https://maindbstorage.blob.core.windows.net/tokanpages";
-export const STORY_URL   = "http://localhost:3000/static/mystory.html";
-export const TERMS_URL   = "http://localhost:3000/static/terms.html";
-export const POLICY_URL  = "http://localhost:3000/static/policy.html";
+export const STORY_URL   = `${APP_URL}/static/mystory.html`;
+export const TERMS_URL   = `${APP_URL}/static/terms.html`;
+export const POLICY_URL  = `${APP_URL}/static/policy.html`;
