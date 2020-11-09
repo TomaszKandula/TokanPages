3bd0a1794d2b4ef2e11616c5f6b279b0cae2a5cc
Tomasz 'Tom' Kandula <tomasz.kandula@gmail.com>
2020-11-09 15:58:12 +0100

    feat: add serilog config to entry point (#46)

--- TokanPages/Program.cs
+++ TokanPages/Program.cs
@@ -1,5 +1,9 @@
+using System;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Events;
 
 namespace TokanPages
 {
@@ -7,16 +11,44 @@ namespace TokanPages
     public class Program
     {
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+        private static IWebHostBuilder CreateWebHostBuilder(string[] ARgs) =>
+             WebHost.CreateDefaultBuilder(ARgs)
+                 .UseStartup<Startup>()
+                 .UseSerilog();
 
-        public static void Main(string[] args)
+        public static int Main(string[] ARgs)
         {
-            CreateHostBuilder(args).Build().Run();
+
+            var LAppPath = AppDomain.CurrentDomain.BaseDirectory;
+
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Information()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Error)
+                .Enrich.FromLogContext()
+                .WriteTo.File(
+                    LAppPath + "\\logs\\log-.txt",
+                    outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {Message:lj}{NewLine}{Exception}",
+                    rollingInterval: RollingInterval.Day,
+                    rollOnFileSizeLimit: true,
+                    retainedFileCountLimit: null)
+                .CreateLogger();
+
+            try
+            {
+                Log.Information("Starting WebHost...");
+                CreateWebHostBuilder(ARgs).Build().Run();
+                return 0;
+            }
+            catch (Exception LException)
+            {
+                Log.Fatal(LException, "WebHost has been terminated unexpectedly.");
+                return 1;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+
         }
 
     }
