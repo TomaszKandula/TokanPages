8acbd29ee91f2234e294cb01ab672dd28f5dd51b
Tomasz 'Tom' Kandula <tomasz.kandula@gmail.com>
2020-11-10 12:20:28 +0100

    feat: add cosmos db (#50)

---
+++ TokanPages/BackEnd/Database/CosmosDbService.cs
@@ -0,0 +1,64 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Microsoft.Azure.Cosmos;
+using TokanPages.BackEnd.Database.Model;
+
+namespace TokanPages.BackEnd.Database
+{
+
+    public class CosmosDbService : ICosmosDbService
+    {
+
+        private readonly Container FContainer;
+
+        public CosmosDbService(CosmosClient ACosmosClient, string ADatabaseName, string AContainerName)
+        {
+            FContainer = ACosmosClient.GetContainer(ADatabaseName, AContainerName);
+        }
+
+        public async Task<Item> GetItemAsync(string AUid)
+        {
+            try
+            {
+                ItemResponse<Item> LResponse = await FContainer.ReadItemAsync<Item>(AUid, new PartitionKey(AUid));
+                return LResponse.Resource;
+            }
+            catch (CosmosException Exception) when (Exception.StatusCode == System.Net.HttpStatusCode.NotFound)
+            {
+                return null;
+            }
+        }
+
+        public async Task<IEnumerable<Item>> GetItemsAsync(string AQueryString)
+        {
+            var LQuery = FContainer.GetItemQueryIterator<Item>(new QueryDefinition(AQueryString));
+            var LResults = new List<Item>();
+
+            while (LQuery.HasMoreResults)
+            {
+                var LResponse = await LQuery.ReadNextAsync();
+                LResults.AddRange(LResponse.ToList());
+            }
+
+            return LResults;
+        }
+
+        public async Task AddItemAsync(Item AItem)
+        {
+            await FContainer.CreateItemAsync(AItem, new PartitionKey(AItem.Uid));
+        }
+
+        public async Task UpdateItemAsync(string AUid, Item AItem)
+        {
+            await FContainer.UpsertItemAsync(AItem, new PartitionKey(AUid));
+        }
+
+        public async Task DeleteItemAsync(string AUid)
+        {
+            await FContainer.DeleteItemAsync<Item>(AUid, new PartitionKey(AUid));
+        }
+
+    }
+
+}
---
+++ TokanPages/BackEnd/Database/ICosmosDbService.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using System.Collections.Generic;
+using TokanPages.BackEnd.Database.Model;
+
+namespace TokanPages.BackEnd.Database
+{
+
+    public interface ICosmosDbService
+    {
+
+        Task<IEnumerable<Item>> GetItemsAsync(string query);
+
+        Task<Item> GetItemAsync(string id);
+
+        Task AddItemAsync(Item item);
+
+        Task UpdateItemAsync(string id, Item item);
+
+        Task DeleteItemAsync(string id);
+
+    }
+
+}
