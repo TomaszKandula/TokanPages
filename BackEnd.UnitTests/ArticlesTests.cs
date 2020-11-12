0b7f90d6f406d3f19d849f02a869621c776a2a1f
Tomasz 'Tom' Kandula <tomasz.kandula@gmail.com>
2020-11-12 13:46:59 +0100

    test: add new test (#63)

--- BackEnd.UnitTests/ArticlesTests.cs
+++ BackEnd.UnitTests/ArticlesTests.cs
@@ -1,15 +1,10 @@
-﻿using Moq;
-using Xunit;
+﻿using Xunit;
 using FluentAssertions;
-using System;
 using System.Linq;
-using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.Azure.Documents;
-using Microsoft.Azure.Documents.Client;
-using TokanPages.BackEnd.Database.Model;
 using BackEnd.UnitTests.Mock;
-using BackEnd.UnitTests.Configuration;
+using BackEnd.UnitTests.Database;
+using TokanPages.BackEnd.Logic.Articles;
 
 namespace BackEnd.UnitTests
 {
@@ -18,38 +13,41 @@ namespace BackEnd.UnitTests
     {
 
         [Fact]
-        public async Task GetAllArticles_Test() 
+        public async Task GetAllArticles_Test()
         {
 
-            //Arrange
-            var LResponse = new FeedResponse<Article>(DummyData.ReturnDummyArticles());
+            // Arrange
+            var LFakeCosmosService = new FakeCosmosDbService
+            {
+                DummyArticles = DummyData.ReturnDummyArticles()
+            };
 
-            var LMockDocumentQuery = new Mock<IFakeDocumentQuery<Article>>();
-            LMockDocumentQuery
-                .SetupSequence(Query => Query.HasMoreResults)
-                .Returns(true)
-                .Returns(false);
+            // Act
+            var LArticles = new Articles(LFakeCosmosService);
+            var LResult = await LArticles.GetAllArticles();
 
-            LMockDocumentQuery
-                .Setup(Query => Query.ExecuteNextAsync<Article>(It.IsAny<CancellationToken>()))
-                .ReturnsAsync(LResponse);
-
-            var LDocumentClient = new Mock<IDocumentClient>();
-
-            LDocumentClient
-                .Setup(Client => Client.CreateDocumentQuery<Article>(It.IsAny<Uri>(), It.IsAny<SqlQuerySpec>(), It.IsAny<FeedOptions>()))
-                .Returns(LMockDocumentQuery.Object);
-
-            var LCosmosDatabase = string.Empty;
-
-            var LDocumentsRepository = new DocumentDbRepository<Article>(LDocumentClient.Object);
-
-            //Act
-            var LQuery = LDocumentsRepository.GetQueryable("select * from c"); 
-            var LResult = await LDocumentsRepository.ExecuteQueryAsync(LQuery);
-
-            //Assert
+            // Assert
             LResult.Count().Should().Be(6);
+
+        }
+
+        [Fact]
+        public async Task GetSingleArticle_Test() 
+        {
+
+            // Arrange
+            var LFakeCosmosService = new FakeCosmosDbService
+            {
+                DummyArticles = DummyData.ReturnDummyArticles()
+            };
+
+            // Act
+            var LArticles = new Articles(LFakeCosmosService);
+            var LResult = await LArticles.GetSingleArticle("f29306f9-36fe-4935-8f86-fb448a21019c");
+
+            // Assert
+            LResult.Should().NotBeNull();
+            LResult.Title.Should().Be("ZXC");
 
         }
 
