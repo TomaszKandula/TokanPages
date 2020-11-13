5b2ff37796a39ab4ef12a9024a90dbf830713af7
Tomasz 'Tom' Kandula <tomasz.kandula@gmail.com>
2020-11-13 21:25:47 +0100

    feat: add azure storage (#79)
    * feat: add azure storage configuration
    * feat: adding azure storage implementation

---
+++ TokanPages/BackEnd/Settings/AzureStorage.cs
@@ -0,0 +1,11 @@
+﻿namespace TokanPages.BackEnd.Settings
+{
+
+    public class AzureStorage
+    {
+        public string AccountName { get; set; }
+        public string AccountKey { get; set; }
+        public string BasicUrl { get; set; }
+    }
+
+}
