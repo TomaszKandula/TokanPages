197517856036786f6fb943c0e84bfccbb4b864f8
Tomasz Kandula <tomasz.kandula@gmail.com>
2020-11-14 18:24:11 +0100

    test: add missing integration tests

---
+++ BackEnd.IntegrationTests/CosmosDbServiceTest.cs
@@ -0,0 +1,62 @@
+﻿using Xunit;
+using FluentAssertions;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.Azure.Cosmos;
+using Microsoft.Extensions.Configuration;
+using TokanPages.BackEnd.Settings;
+using TokanPages.BackEnd.Database;
+
+namespace BackEnd.IntegrationTests
+{
+
+    public class CosmosDbServiceTest
+    {
+
+        private readonly CosmosDb FCosmosDb;
+
+        public CosmosDbServiceTest() 
+        {
+
+            var Configuration = new ConfigurationBuilder()
+                .AddUserSecrets<CosmosDbServiceTest>()
+                .Build();
+
+            FCosmosDb = Configuration.GetSection("CosmosDb").Get<CosmosDb>();
+
+        }
+
+        [Fact]
+        public async Task StartCosmosClient_Test() 
+        {
+
+            // Arrange
+            var LDatabaseName = FCosmosDb.DatabaseName;
+            var LContainerName = "TestContainer";
+            var LAccount = FCosmosDb.Account;
+            var LKey = FCosmosDb.Key;
+
+            var LClient = new CosmosClient(LAccount, LKey, new CosmosClientOptions()
+            {
+                SerializerOptions = new CosmosSerializationOptions()
+                {
+                    PropertyNamingPolicy = CosmosPropertyNamingPolicy.CamelCase
+                }
+            });
+
+            var LCosmosDbService = new CosmosDbService(LClient, LDatabaseName, LContainerName);
+
+            // Act
+            var LDatabase = await LClient.CreateDatabaseIfNotExistsAsync(LDatabaseName);
+            var LResult = await LDatabase.Database.CreateContainerIfNotExistsAsync(LContainerName, "/id");
+
+            // Assert
+            LCosmosDbService.Should().NotBeNull();
+            LDatabase.StatusCode.Should().Be(HttpStatusCode.OK);
+            LResult.StatusCode.Should().Be(HttpStatusCode.OK);
+
+        }
+
+    }
+
+}
---
+++ BackEnd.IntegrationTests/SendGridServiceTest.cs
@@ -0,0 +1,53 @@
+﻿using Xunit;
+using FluentAssertions;
+using System;
+using System.Net;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Microsoft.Extensions.Configuration;
+using TokanPages.BackEnd.SendGrid;
+using TokanPages.BackEnd.Settings;
+
+namespace BackEnd.IntegrationTests
+{
+
+    public class SendGridServiceTest
+    {
+
+        private readonly SendGridKeys FSendGridKeys;
+
+        public SendGridServiceTest()
+        {
+
+            var Configuration = new ConfigurationBuilder()
+                .AddUserSecrets<SendGridServiceTest>()
+                .Build();
+
+            FSendGridKeys = Configuration.GetSection("SendGridKeys").Get<SendGridKeys>();
+
+        }
+
+        [Fact]
+        public async Task Send_Test()
+        {
+
+            // Arrange
+            var LSendGridService = new SendGridService(FSendGridKeys);
+
+            LSendGridService.From      = "contact@tomkandula.com";
+            LSendGridService.Tos       = new List<string> { "tom@tomkandula.com" };
+            LSendGridService.Subject   = "Integration Test";
+            LSendGridService.PlainText = string.Empty;
+            LSendGridService.HtmlBody  = $"<p><b>Run Test</b></p><p>ID: {Guid.NewGuid()}</p>";
+
+            // Act
+            var LResult = await LSendGridService.Send();
+
+            // Assert
+            LResult.StatusCode.Should().Be(HttpStatusCode.Accepted);
+
+        }
+
+    }
+
+}
