40b55a51d8dae44b353fd2f19af92ea1c7f04878
Tomasz Kandula <tomasz.kandula@gmail.com>
2020-11-21 18:10:51 +0100

    feat: add new models for cosmosdb containers

---
+++ TokanPages/BackEnd/Database/Model/Subscribers.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text.Json.Serialization;
+
+namespace TokanPages.BackEnd.Database.Model
+{
+
+    public class Subscribers
+    {
+
+        [JsonPropertyName("id")]
+        public string Id { get; set; }
+
+        [JsonPropertyName("emailAddress")]
+        public string EmailAddress { get; set; }
+
+        [JsonPropertyName("status")]
+        public string Status { get; set; }
+
+        [JsonPropertyName("newsletterCount")]
+        public int NewsletterCount { get; set; }
+
+        [JsonPropertyName("registered")]
+        public DateTime Registered { get; set; }
+
+        [JsonPropertyName("lastUpdated")]
+        public DateTime LastUpdated { get; set; }
+
+    }
+
+}
