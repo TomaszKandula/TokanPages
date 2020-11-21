4964e03561614515e6cc2fab97c39461a1b7bab8
Tomasz 'Tom' Kandula <tomasz.kandula@gmail.com>
2020-11-21 19:45:54 +0100

    refactor: remove 'virtual' keyword where not needed (#115)

--- TokanPages/BackEnd/AppLogger/AppLogger.cs
+++ TokanPages/BackEnd/AppLogger/AppLogger.cs
@@ -6,27 +6,27 @@ namespace TokanPages.BackEnd.AppLogger
     public class AppLogger : IAppLogger
     {
 
-        public virtual void LogDebug(string AMessage)
+        public void LogDebug(string AMessage)
         {
             Log.Debug(AMessage);
         }
 
-        public virtual void LogInfo(string AMessage)
+        public void LogInfo(string AMessage)
         {
             Log.Information(AMessage);
         }
 
-        public virtual void LogWarn(string AMessage)
+        public void LogWarn(string AMessage)
         {
             Log.Warning(AMessage);
         }
 
-        public virtual void LogError(string AMessage)
+        public void LogError(string AMessage)
         {
             Log.Error(AMessage);
         }
 
-        public virtual void LogFatality(string AMessage)
+        public void LogFatality(string AMessage)
         {
             Log.Fatal(AMessage);
         }
