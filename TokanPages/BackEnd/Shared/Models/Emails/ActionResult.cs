819f6251964035827a80c96d5b22e18ce227ebc2
Tomasz Kandula <tomasz.kandula@gmail.com>
2020-11-22 19:53:47 +0100

    feat: add smtp-client service

---
+++ TokanPages/BackEnd/Shared/Models/Emails/ActionResult.cs
@@ -0,0 +1,11 @@
+﻿namespace TokanPages.BackEnd.Shared.Models.Emails
+{
+
+    public class ActionResult
+    {
+        public bool IsSucceeded { get; set; }
+        public string ErrorCode { get; set; }
+        public string ErrorDesc { get; set; } = "n/a";
+    }
+
+}
---
+++ TokanPages/BackEnd/SmtpClient/ISmtpClientService.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using System.Collections.Generic;
+using TokanPages.BackEnd.Shared.Models.Emails;
+
+namespace TokanPages.BackEnd.SmtpClient
+{
+
+    public interface ISmtpClientService
+    {
+        string From { get; set; }
+        List<string> Tos { get; set; }
+        string Subject { get; set; }
+        string PlainText { get; set; }
+        string HtmlBody { get; set; }
+        Task<ActionResult> Send();
+    }
+
+}
