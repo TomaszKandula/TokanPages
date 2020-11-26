3661a10ff1ddec3b296cb8ada547c9acd3ccd64a
Tomasz 'Tom' Kandula <tomasz.kandula@gmail.com>
2020-11-26 16:46:04 +0100

    refactor: move validation methods to shared module (#139)

--- TokanPages/FrontEnd/src/Shared/validate.ts
+++ TokanPages/FrontEnd/src/Shared/validate.ts
@@ -19,7 +19,92 @@ function ValidateEmail(email: string): any
 
 }
 
+interface IValidateInputs
+{
+    FirstName: string;
+    LastName:  string;
+    Email:     string;
+    Subject:   string;
+    Message:   string;
+    Terms:     boolean;
+}
+
+function ValidateContactForm(props: IValidateInputs): any
+{
+
+    let Constraints = 
+    {
+        FirstName:
+        {
+            presence: true,
+            length: 
+            {
+                minimum: 2,
+                message: "must be at least 2 characters"
+            }
+        },
+        LastName:
+        {
+            presence: true,
+            length: 
+            {
+                minimum: 2,
+                message: "must be at least 2 characters"
+            }
+        },
+        Email: 
+        {
+            email: 
+            {
+                message: "does not look like a valid email"
+            }
+        },
+        Subject:
+        {
+            presence: true,
+            length: 
+            {
+                minimum: 6,
+                message: "must be at least 6 characters"
+            }
+        },
+        Message:
+        {
+            presence: true,
+            length: 
+            {
+                minimum: 20,
+                message: "must be at least 20 characters"
+            }
+        },
+        Terms:
+        {
+            presence: true,
+            inclusion:
+            {
+                within: [true],
+                message: "^You must accept terms of use and privacy policy"
+            }
+        }
+    };
+
+    let Results = Validate(
+    {
+        FirstName: props.FirstName,
+        LastName:  props.LastName,
+        Email:     props.Email,
+        Subject:   props.Subject,
+        Message:   props.Message,
+        Terms:     props.Terms
+    }, 
+    Constraints);
+
+    return Results;
+
+}
+
 export 
 {
-    ValidateEmail
+    ValidateEmail, 
+    ValidateContactForm
 }
