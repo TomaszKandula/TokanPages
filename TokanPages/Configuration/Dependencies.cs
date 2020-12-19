41b6d3dec0ca1638480276a153aed6e53c240a9b
Tomasz Kandula <tomasz.kandula@gmail.com>
2020-12-19 17:20:08 +0100

    refactor: move dependencies registration to separate folder

---
+++ TokanPages/Configuration/Dependencies.cs
@@ -0,0 +1,84 @@
+﻿using System.Reflection;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TokanPages.AppLogger;
+using TokanPages.Backend.Storage;
+using TokanPages.Backend.Database;
+using TokanPages.Backend.SmtpClient;
+using TokanPages.Backend.Shared.Settings;
+using TokanPages.Backend.Storage.Settings;
+using TokanPages.Backend.SmtpClient.Settings;
+using TokanPages.Backend.Core.TemplateHelper;
+using TokanPages.Backend.Cqrs.Handlers.Commands.Mailer;
+using TokanPages.Backend.Cqrs.Handlers.Queries.Articles;
+using TokanPages.Backend.Cqrs.Handlers.Commands.Articles;
+using TokanPages.Backend.Cqrs.Handlers.Queries.Subscribers;
+using TokanPages.Backend.Cqrs.Handlers.Commands.Subscribers;
+using MediatR;
+
+namespace TokanPages.Configuration
+{
+
+    public static class Dependencies
+    {
+
+        public static void Register(IServiceCollection AServices, IConfiguration AConfiguration) 
+        {
+            SetupAppSettings(AServices, AConfiguration);
+            SetupLogger(AServices);
+            SetupDatabase(AServices, AConfiguration);
+            SetupServices(AServices);
+            SetupMediatR(AServices);
+        }
+
+        private static void SetupAppSettings(IServiceCollection AServices, IConfiguration AConfiguration) 
+        {
+            AServices.AddSingleton(AConfiguration.GetSection("AzureStorage").Get<AzureStorageSettings>());
+            AServices.AddSingleton(AConfiguration.GetSection("SmtpServer").Get<SmtpServerSettings>());
+            AServices.AddSingleton(AConfiguration.GetSection("AppUrls").Get<AppUrls>());
+        }
+
+        private static void SetupLogger(IServiceCollection AServices) 
+        {
+            AServices.AddSingleton<IAppLogger, AppLogger.AppLogger>();
+        }
+
+        private static void SetupDatabase(IServiceCollection AServices, IConfiguration AConfiguration) 
+        {
+            AServices.AddDbContext<DatabaseContext>(AOptions =>
+            {
+                AOptions.UseSqlServer(AConfiguration.GetConnectionString("DbConnect"),
+                AAddOptions => AAddOptions.EnableRetryOnFailure());
+            });
+        }
+
+        private static void SetupServices(IServiceCollection AServices) 
+        {
+            AServices.AddScoped<ISmtpClientService, SmtpClientService>();
+            AServices.AddScoped<IAzureStorageService, AzureStorageService>();
+            AServices.AddScoped<ITemplateHelper, TemplateHelper>();
+        }
+
+        private static void SetupMediatR(IServiceCollection AServices) 
+        {
+            AServices.AddMediatR(Assembly.GetExecutingAssembly());
+            AServices.AddTransient<IRequestHandler<VerifyEmailAddressCommand, VerifyEmailAddressCommandResult>, VerifyEmailAddressCommandHandler>();
+            AServices.AddTransient<IRequestHandler<SendMessageCommand, Unit>, SendMessageCommandHandler>();
+            AServices.AddTransient<IRequestHandler<SendNewsletterCommand, Unit>, SendNewsletterCommandHandler>();
+            AServices.AddTransient<IRequestHandler<GetAllArticlesQuery, IEnumerable<Backend.Domain.Entities.Articles>>, GetAllArticlesQueryHandler>();
+            AServices.AddTransient<IRequestHandler<GetArticleQuery, Backend.Domain.Entities.Articles>, GetArticleQueryHandler>();
+            AServices.AddTransient<IRequestHandler<AddArticleCommand, Unit>, AddArticleCommandHandler>();
+            AServices.AddTransient<IRequestHandler<UpdateArticleCommand, Unit>, UpdateArticleCommandHandler>();
+            AServices.AddTransient<IRequestHandler<RemoveArticleCommand, Unit>, RemoveArticleCommandHandler>();
+            AServices.AddTransient<IRequestHandler<GetAllSubscribersQuery, IEnumerable<Backend.Domain.Entities.Subscribers>>, GetAllSubscribersQueryHandler>();
+            AServices.AddTransient<IRequestHandler<GetSubscriberQuery, Backend.Domain.Entities.Subscribers>, GetSubscriberQueryHandler>();
+            AServices.AddTransient<IRequestHandler<AddSubscriberCommand, Unit>, AddSubscriberCommandHandler>();
+            AServices.AddTransient<IRequestHandler<UpdateSubscriberCommand, Unit>, UpdateSubscriberCommandHandler>();
+            AServices.AddTransient<IRequestHandler<RemoveSubscriberCommand, Unit>, RemoveSubscriberCommandHandler>();
+        }
+
+    }
+
+}
--- TokanPages/Startup.cs
+++ TokanPages/Startup.cs
@@ -1,30 +1,15 @@
-using System.Reflection;
-using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.OpenApi.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.ResponseCompression;
-using TokanPages.AppLogger;
 using TokanPages.Middleware;
+using TokanPages.Configuration;
 using TokanPages.CustomHandlers;
-using TokanPages.Backend.Storage;
-using TokanPages.Backend.Database;
-using TokanPages.Backend.SmtpClient;
 using TokanPages.Backend.Shared.Settings;
-using TokanPages.Backend.Storage.Settings;
-using TokanPages.Backend.SmtpClient.Settings;
-using TokanPages.Backend.Core.TemplateHelper;
-using TokanPages.Backend.Cqrs.Handlers.Commands.Mailer;
-using TokanPages.Backend.Cqrs.Handlers.Queries.Articles;
-using TokanPages.Backend.Cqrs.Handlers.Commands.Articles;
-using TokanPages.Backend.Cqrs.Handlers.Queries.Subscribers;
-using TokanPages.Backend.Cqrs.Handlers.Commands.Subscribers;
-using MediatR;
 
 namespace TokanPages
 {
@@ -43,60 +28,33 @@ namespace TokanPages
         {
 
             AServices.AddMvc(AOption => AOption.CacheProfiles
-            .Add("Standard", new CacheProfile()
-            {
-                Duration = 10,
-                Location = ResponseCacheLocation.Any,
-                NoStore = false
-            }));
-
-            AServices.AddControllers();
-
-            AServices.AddSpaStaticFiles(configuration =>
-            {
-                configuration.RootPath = "ClientApp/build";
-            });
+                .Add("Standard", new CacheProfile() 
+                { 
+                    Duration = 10, 
+                    Location = ResponseCacheLocation.Any, 
+                    NoStore = false 
+                }));
 
             AServices.AddMvc(AOption => AOption.EnableEndpointRouting = false)
                 .SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
 
-            AServices.AddSingleton(Configuration.GetSection("AzureStorage").Get<AzureStorageSettings>());
-            AServices.AddSingleton(Configuration.GetSection("SmtpServer").Get<SmtpServerSettings>());
-            AServices.AddSingleton(Configuration.GetSection("AppUrls").Get<AppUrls>());
+            Dependencies.Register(AServices, Configuration);
 
-            AServices.AddSingleton<IAppLogger, AppLogger.AppLogger>();
-            AServices.AddDbContext<DatabaseContext>(AOptions =>
-            {
-                AOptions.UseSqlServer(Configuration.GetConnectionString("DbConnect"),
-                AAddOptions => AAddOptions.EnableRetryOnFailure());
+            AServices.AddControllers();
+            
+            AServices.AddSpaStaticFiles(configuration => 
+            { 
+                configuration.RootPath = "ClientApp/build"; 
             });
-            AServices.AddScoped<ISmtpClientService, SmtpClientService>();          
-            AServices.AddScoped<IAzureStorageService, AzureStorageService>();
-            AServices.AddScoped<ITemplateHelper, TemplateHelper>();
-
-            AServices.AddMediatR(Assembly.GetExecutingAssembly());
-            AServices.AddTransient<IRequestHandler<VerifyEmailAddressCommand, VerifyEmailAddressCommandResult>, VerifyEmailAddressCommandHandler>();
-            AServices.AddTransient<IRequestHandler<SendMessageCommand, Unit>, SendMessageCommandHandler>();
-            AServices.AddTransient<IRequestHandler<SendNewsletterCommand, Unit>, SendNewsletterCommandHandler>();
-            AServices.AddTransient<IRequestHandler<GetAllArticlesQuery, IEnumerable<Backend.Domain.Entities.Articles>>, GetAllArticlesQueryHandler>();
-            AServices.AddTransient<IRequestHandler<GetArticleQuery, Backend.Domain.Entities.Articles>, GetArticleQueryHandler>();
-            AServices.AddTransient<IRequestHandler<AddArticleCommand, Unit>, AddArticleCommandHandler>();
-            AServices.AddTransient<IRequestHandler<UpdateArticleCommand, Unit>, UpdateArticleCommandHandler>();
-            AServices.AddTransient<IRequestHandler<RemoveArticleCommand, Unit>, RemoveArticleCommandHandler>();
-            AServices.AddTransient<IRequestHandler<GetAllSubscribersQuery, IEnumerable<Backend.Domain.Entities.Subscribers>>, GetAllSubscribersQueryHandler>();
-            AServices.AddTransient<IRequestHandler<GetSubscriberQuery, Backend.Domain.Entities.Subscribers>, GetSubscriberQueryHandler>();
-            AServices.AddTransient<IRequestHandler<AddSubscriberCommand, Unit>, AddSubscriberCommandHandler>();
-            AServices.AddTransient<IRequestHandler<UpdateSubscriberCommand, Unit>, UpdateSubscriberCommandHandler>();
-            AServices.AddTransient<IRequestHandler<RemoveSubscriberCommand, Unit>, RemoveSubscriberCommandHandler>();
-
-            AServices.AddResponseCompression(AOptions =>
-            {
-                AOptions.Providers.Add<GzipCompressionProvider>();
+            
+            AServices.AddResponseCompression(AOptions => 
+            { 
+                AOptions.Providers.Add<GzipCompressionProvider>(); 
             });
-
-            AServices.AddSwaggerGen(AOption =>
-            {
-                AOption.SwaggerDoc("v1", new OpenApiInfo { Title = "TokanPagesApi", Version = "v1" });
+            
+            AServices.AddSwaggerGen(AOption => 
+            { 
+                AOption.SwaggerDoc("v1", new OpenApiInfo { Title = "TokanPagesApi", Version = "v1" }); 
             });
 
         }
