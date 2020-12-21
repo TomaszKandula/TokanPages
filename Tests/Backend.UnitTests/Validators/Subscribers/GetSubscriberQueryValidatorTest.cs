7412e8f4c07fab6a7548fab6ec8cd6a0e8ed3bf2
Tomasz Kandula <tomasz.kandula@gmail.com>
2020-12-21 22:47:16 +0100

    test: add subscribers validator tests

---
+++ Tests/Backend.UnitTests/Validators/Subscribers/GetSubscriberQueryValidatorTest.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Xunit;
+using FluentAssertions;
+using TokanPages.Backend.Shared.Resources;
+using TokanPages.Backend.Cqrs.Handlers.Queries.Subscribers;
+
+namespace Backend.UnitTests.Validators.Subscribers
+{
+
+    public class GetSubscriberQueryValidatorTest
+    {
+
+        [Fact]
+        public void GetSubscriber_WhenIdIsCorrect_ShouldFinishSuccessfull() 
+        {
+
+            // Arrange
+            var LGetSubscriberQuery = new GetSubscriberQuery 
+            { 
+                Id = Guid.NewGuid()
+            };
+
+            // Act
+            var LValidator = new GetSubscriberQueryValidator();
+            var LResult = LValidator.Validate(LGetSubscriberQuery);
+
+            // Assert
+            LResult.Errors.Should().BeEmpty();
+
+        }
+
+        [Fact]
+        public void GetSubscriber_WhenIdIsEmpty_ShouldThrowError()
+        {
+
+            // Arrange
+            var LGetSubscriberQuery = new GetSubscriberQuery
+            {
+                Id = Guid.Empty
+            };
+
+            // Act
+            var LValidator = new GetSubscriberQueryValidator();
+            var LResult = LValidator.Validate(LGetSubscriberQuery);
+
+            // Assert
+            LResult.Errors.Count.Should().Be(1);
+            LResult.Errors[0].ErrorCode.Should().Be(nameof(ValidationCodes.REQUIRED));
+
+        }
+
+    }
+
+}
---
+++ Tests/Backend.UnitTests/Validators/Subscribers/UpdateSubscriberCommandValidatorTest.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using Xunit;
+using FluentAssertions;
+using TokanPages.Backend.Shared.Resources;
+using TokanPages.Backend.Cqrs.Handlers.Commands.Subscribers;
+
+namespace Backend.UnitTests.Validators.Subscribers
+{
+
+    public class UpdateSubscriberCommandValidatorTest
+    {
+
+        [Fact]
+        public void UpdateSubscriber_WhenAllFieldsAreCorrect_ShouldFinishSuccessfull() 
+        {
+
+            // Arrange
+            var LUpdateSubscriberCommand = new UpdateSubscriberCommand 
+            { 
+                Id = Guid.NewGuid(),
+                Email = "tokan@dfds.com",
+                IsActivated = true,
+                Count = 0
+            };
+
+            // Act
+            var LValidator = new UpdateSubscriberCommandValidator();
+            var LResult = LValidator.Validate(LUpdateSubscriberCommand);
+
+            // Assert
+            LResult.Errors.Should().BeEmpty();
+
+        }
+
+        [Fact]
+        public void UpdateSubscriber_WhenIdIsEmpty_ShouldThrowError()
+        {
+
+            // Arrange
+            var LUpdateSubscriberCommand = new UpdateSubscriberCommand
+            {
+                Id = Guid.Empty,
+                Email = "tokan@dfds.com",
+                IsActivated = true,
+                Count = 0
+            };
+
+            // Act
+            var LValidator = new UpdateSubscriberCommandValidator();
+            var LResult = LValidator.Validate(LUpdateSubscriberCommand);
+
+            // Assert
+            LResult.Errors.Count.Should().Be(1);
+            LResult.Errors[0].ErrorCode.Should().Be(nameof(ValidationCodes.REQUIRED));
+
+        }
+
+        [Fact]
+        public void UpdateSubscriber_WhenEmailIsEmpty_ShouldThrowError()
+        {
+
+            // Arrange
+            var LUpdateSubscriberCommand = new UpdateSubscriberCommand
+            {
+                Id = Guid.NewGuid(),
+                Email = string.Empty,
+                IsActivated = true,
+                Count = 0
+            };
+
+            // Act
+            var LValidator = new UpdateSubscriberCommandValidator();
+            var LResult = LValidator.Validate(LUpdateSubscriberCommand);
+
+            // Assert
+            LResult.Errors.Count.Should().Be(1);
+            LResult.Errors[0].ErrorCode.Should().Be(nameof(ValidationCodes.REQUIRED));
+
+        }
+
+        [Fact]
+        public void UpdateSubscriber_WhenEmailTooLong_ShouldThrowError()
+        {
+
+            // Arrange
+            var LUpdateSubscriberCommand = new UpdateSubscriberCommand
+            {
+                Id = Guid.NewGuid(),
+                Email = new string('T', 256),
+                IsActivated = true,
+                Count = 0
+            };
+
+            // Act
+            var LValidator = new UpdateSubscriberCommandValidator();
+            var LResult = LValidator.Validate(LUpdateSubscriberCommand);
+
+            // Assert
+            LResult.Errors.Count.Should().Be(1);
+            LResult.Errors[0].ErrorCode.Should().Be(nameof(ValidationCodes.EMAIL_TOO_LONG));
+
+        }
+
+        [Fact]
+        public void UpdateSubscriber_WhenCountIsessThanZero_ShouldThrowError()
+        {
+
+            // Arrange
+            var LUpdateSubscriberCommand = new UpdateSubscriberCommand
+            {
+                Id = Guid.NewGuid(),
+                Email = "tokan@dfds.com",
+                IsActivated = true,
+                Count = -1
+            };
+
+            // Act
+            var LValidator = new UpdateSubscriberCommandValidator();
+            var LResult = LValidator.Validate(LUpdateSubscriberCommand);
+
+            // Assert
+            LResult.Errors.Count.Should().Be(1);
+            LResult.Errors[0].ErrorCode.Should().Be(nameof(ValidationCodes.LESS_THAN_ZERO));
+
+        }
+
+    }
+
+}
