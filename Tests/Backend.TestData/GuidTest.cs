cdeaf70181deebbc9e2219487a736afe1cb9ba0a
Tomasz Kandula <tomasz.kandula@gmail.com>
2020-12-27 16:32:13 +0100

    feat: add check if string holds valid GUID

---
+++ Tests/Backend.TestData/GuidTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Backend.TestData
+{
+
+    public static class GuidTest
+    {
+
+        public static bool Check(string AValue) 
+        {
+            return Guid.TryParse(AValue, out var _);
+        }
+
+    }
+
+}
