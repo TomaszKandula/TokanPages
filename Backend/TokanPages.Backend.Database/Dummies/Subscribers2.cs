157fb33f2abc7467338e9c29bbc20f6c9f7f8586
Tomasz Kandula <tomasz.kandula@gmail.com>
2020-12-29 18:51:14 +0100

    refactor: remove nested subscriber classes to separate folder

---
+++ Backend/TokanPages.Backend.Database/Dummies/Subscribers2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TokanPages.Backend.Database.Dummies
+{
+
+    public class Subscribers2
+    {
+        public static Guid Id = Guid.Parse("ec8dd29c-464c-4e7a-897c-ce0ace2619ec");
+        public static string Email = "tokan@dfds.com";
+        public static DateTime Registered = DateTime.Parse("2020-01-25 05:09:19");
+        public static DateTime? LastUpdated = null;
+        public static bool IsActivated = false;
+        public static int Count = 0;
+    }
+
+}
