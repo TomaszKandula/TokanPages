daadc255131a01b56c31adc2f6428be2cb1a4edd
Tomasz Kandula <tomasz.kandula@gmail.com>
2020-12-29 18:45:32 +0100

    refactor: remove nested user classes to separate folder

---
+++ Backend/TokanPages.Backend.Database/Dummies/User1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TokanPages.Backend.Database.Dummies
+{
+
+    public class User1
+    {
+        public static Guid Id = Guid.Parse("08be222f-dfcd-42db-8509-fd78ef09b912");
+        public static DateTime Registered = DateTime.Parse("2020-01-10 12:15:15");
+        public static string EmailAddress = "ester.exposito@gmail.com";
+        public static string UserAlias = "ester";
+        public static string FirstName = "Ester";
+        public static string LastName = "Exposito";
+        public static bool IsActivated = true;
+        public static DateTime? LastLogged = DateTime.Parse("2020-01-10 15:00:33");
+        public static DateTime? LastUpdated = null;
+    }
+
+}
