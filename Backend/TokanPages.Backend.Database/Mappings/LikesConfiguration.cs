fa32b0c33c8614b657044afee4c762d00fe5f4d2
Tomasz 'Tom' Kandula <tomasz.kandula@gmail.com>
2021-01-28 17:16:35 +0100

    feat: add new database table likes (#335)
    * feat: add new database table, update migrations
    * feat: removing connection string

---
+++ Backend/TokanPages.Backend.Database/Mappings/LikesConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TokanPages.Backend.Domain.Entities;
+
+namespace TokanPages.Backend.Database.Mappings
+{
+    public class LikesConfiguration : IEntityTypeConfiguration<Likes>
+    {
+        public void Configure(EntityTypeBuilder<Likes> AModelBuilder)
+        {
+            AModelBuilder.Property(e => e.Id).ValueGeneratedNever();
+            AModelBuilder
+                .HasOne(e => e.Article)
+                .WithMany(e => e.Likes)
+                .HasForeignKey(e => e.ArticleId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_Likes_Articles");
+            AModelBuilder
+                .HasOne(e => e.User)
+                .WithMany(e => e.Likes)
+                .HasForeignKey(e => e.UserId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_Likes_Users");
+        }
+    }
+}
