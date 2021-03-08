bcbb43e4713ba3f7adfa3a2fa1c971c796db7591
Tomasz 'Tom' Kandula <tomasz.kandula@orange.com>
2021-03-08 22:26:16 +0100

    feat: add functions to get read time, add tests

--- TokanPages/ClientApp/src/Shared/__tests__/helpers.test.ts
+++ TokanPages/ClientApp/src/Shared/__tests__/helpers.test.ts
@@ -1,4 +1,5 @@
 import * as helpers from "../helpers";
+import { ITextObject } from "Shared/ContentRender/Model/textModel";
 
 describe("Verify helper methods.", () => 
 {
@@ -56,5 +57,42 @@ describe("Verify helper methods.", () =>
         const expectation: string = "01/10/2020";
 
         expect(helpers.FormatDateTime(sourceDateTime, false)).toBe(expectation);
+    });
+
+    test("Should convert TextObject to raw Text.", () => 
+    {
+        const jsonObject: string = `
+        {
+            "items": 
+            [
+                { 
+                    "id": "2d844a70-36d1-4cac-a5a2-89fb01b1c52c",
+                    "type": "html",
+                    "value": "<h1>This is test object</h1><p>We will use it for testing.</p>",
+                    "prop": "",
+                    "text": "" 
+                }
+            ]
+        }`
+
+        const textObject: ITextObject = JSON.parse(jsonObject) as ITextObject;
+        const expectation: string = "This is test object  We will use it for testing.";
+
+        expect(helpers.TextObjectToRawText(textObject)).toBe(expectation);
+    });
+
+    test("Should return proper number of words in text.", () => 
+    {
+        const rawText: string = "This is test object  We will use it for testing.";
+        const expectation: number = 10;
+        expect(helpers.CountWords(rawText)).toBe(expectation);        
+    });
+
+    test("Should return read time for given number of words", () => 
+    {
+        const wordsNumber = 700;
+        const wordsPerMinute = 130;
+        const expectation: string = "5.38";
+        expect(helpers.GetReadTime(wordsNumber, wordsPerMinute)).toBe(expectation);
     });
 });
