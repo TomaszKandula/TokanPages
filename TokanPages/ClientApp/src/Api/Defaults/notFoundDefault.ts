417143d93ea0323d499eb371f331f5e6144ba520
Tomasz 'Tom' Kandula <tomasz.kandula@orange.com>
2021-03-19 16:49:27 +0100

    refactor: remove default wording

--- TokanPages/ClientApp/src/Api/Defaults/notFoundDefault.ts
+++ TokanPages/ClientApp/src/Api/Defaults/notFoundDefault.ts
@@ -4,9 +4,9 @@ export const notFoundDefault: INotFound =
 {
     content:
     {
-        code: "404",
-        header: "Page not found",
-        description: "The requested page could not be located. Checkout for any URL misspelling.",
-        button: "Return to the homepage"
+        code: "",
+        header: "",
+        description: "",
+        button: ""
     }
 } 
