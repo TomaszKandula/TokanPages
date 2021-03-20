0103bfd6e88e8d93567839591d6f921a77aaedda
Tomasz 'Tom' Kandula <tomasz.kandula@orange.com>
2021-03-20 16:42:17 +0100

    test: update snapshots, change path to lib for JEST, update component tests

--- TokanPages/ClientApp/src/Components/Account/__tests__/signinForm.test.tsx
+++ TokanPages/ClientApp/src/Components/Account/__tests__/signinForm.test.tsx
@@ -6,9 +6,9 @@ import { signinFormDefault } from "../../../Api/Defaults";
 
 describe("Test account component: SigninForm.", () => 
 {
-    it("Renders correctly '<SigninForm />' ", () => 
+    it("Renders correctly '<SigninForm />' when content is loaded.", () => 
     {
-        const tree = shallow(<SigninForm content={signinFormDefault.content} />);
+        const tree = shallow(<SigninForm signinForm={signinFormDefault} isLoading={false} />);
         expect(tree).toMatchSnapshot();
     });
 });
--- TokanPages/ClientApp/src/Components/Account/__tests__/signupForm.test.tsx
+++ TokanPages/ClientApp/src/Components/Account/__tests__/signupForm.test.tsx
@@ -6,9 +6,9 @@ import { signupFormDefault } from "../../../Api/Defaults";
 
 describe("Test account component: SignupForm.", () => 
 {
-    it("Renders correctly '<SignupForm />' ", () => 
+    it("Renders correctly '<SignupForm />' when content is loaded.", () => 
     {
-        const tree = shallow(<SignupForm content={signupFormDefault.content} />);
+        const tree = shallow(<SignupForm signupForm={signupFormDefault} isLoading={false} />);
         expect(tree).toMatchSnapshot();
     });
 });
