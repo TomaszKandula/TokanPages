1dc78f2915d62072cb0c6012b5e2b62c8648da0c
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2021-03-30 16:06:07 +0200

    feat: use diff. components for title, subtitle and header (#405)
    * feat: add new db tables (for photo album feature)
    * feat: add different styles for content type
    * feat: add header class
    * feat: use separate components for titles and headers

--- TokanPages/ClientApp/src/Shared/ContentRender/Hooks/styleRenderText.tsx
+++ TokanPages/ClientApp/src/Shared/ContentRender/Hooks/styleRenderText.tsx
@@ -3,11 +3,30 @@ import { CustomColours } from "Theme/customColours";
 
 const useStyles = makeStyles(() => (
 {
-    typography:
+    common:
     {
         fontSize: 19,
         textAlign: "left",
-        color: CustomColours.typography.darkGray1,
+        color: CustomColours.typography.darkGray1
+    },
+    title:
+    {
+        fontSize: 28,
+        fontWeight: "bold",
+        lineHeight: 0.5
+    },
+    subTitle:
+    {
+        lineHeight: 0.5
+    },
+    header:
+    {
+        fontSize: 25,
+        fontWeight: "bold",
+        lineHeight: 1.0
+    },
+    paragraph:
+    {
         lineHeight: 2.2
     }
 }));    
