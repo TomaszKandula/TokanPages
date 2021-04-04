6685f13ee4547457880692ad0e08f260f08c8c9e
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2021-04-04 13:04:02 +0200

    feat: add 'IRaiseError' to app. state

--- TokanPages/ClientApp/src/Redux/applicationState.ts
+++ TokanPages/ClientApp/src/Redux/applicationState.ts
@@ -5,9 +5,11 @@ import { IAddSubscriber } from "./States/addSubscriberState";
 import { IUpdateSubscriber } from "./States/updateSubscriberState";
 import { IRemoveSubscriber } from "./States/removeSubscriberState";
 import { IUpdateArticle } from "./States/updateArticleState";
+import { IRaiseError } from "./Errors/raiseErrorState";
 
 export interface IApplicationState 
 {
+    raiseError: IRaiseError;
     selectArticle: IArticle;
     listArticles: IArticles;
     updateArticle: IUpdateArticle,
