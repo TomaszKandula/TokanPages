d25fc93cafea1789d8f59ae9a9e21e5a7f54e51f
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2021-04-05 11:04:06 +0200

    refactor: relocate scroll component

--- TokanPages/ClientApp/src/Components/Layout/navigation.tsx
+++ TokanPages/ClientApp/src/Components/Layout/navigation.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import { Link } from "react-router-dom";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
-import HideOnScroll from "../../Shared/Scroll/hideOnScroll";
+import HideOnScroll from "../../Shared/Components/Scroll/hideOnScroll";
 import { ICONS_PATH } from "../../Shared/constants";
 import { renderImage } from "../../Shared/Components/renderImage";
 import { INavigationContentDto } from "../../Api/Models/";
