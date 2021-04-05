a9cf14001a73d30aaec01879d3a0d88c8bd950b7
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2021-04-05 21:56:14 +0200

    refactor: use action/reducer for static content (#426)
    * feat: add new action/reducer to store
    * refactor: use actions to get static content
    * refactor: update final implementation

--- TokanPages/ClientApp/src/Components/Content/staticContent.tsx
+++ TokanPages/ClientApp/src/Components/Content/staticContent.tsx
@@ -1,39 +1,67 @@
 import * as React from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import Container from "@material-ui/core/Container";
 import Box from "@material-ui/core/Box";
 import { Divider, IconButton } from "@material-ui/core";
 import { ArrowBack } from "@material-ui/icons";
-import axios from "axios";
 import Validate from "validate.js";
-import useStyles from "./styleStaticContent";
 import CenteredCircularLoader from "../../Shared/Components/ProgressBar/centeredCircularLoader";
 import { RenderContent } from "../../Shared/Components/ContentRender/renderContent";
 import { ITextObject } from "../../Shared/Components/ContentRender/Models/textModel";
+import useStyles from "./styleStaticContent";
+import { IApplicationState } from "../../Redux/applicationState";
+import { 
+    ActionCreators,
+    TRequestContent, 
+    REQUEST_POLICY, 
+    REQUEST_STORY, 
+    REQUEST_TERMS 
+} from "../../Redux/Actions/getStaticContentAction";
 
-export default function StaticContent(props: { dataUrl: string }) 
+export default function StaticContent(props: { content: TRequestContent }) 
 {
     const classes = useStyles();
     const [ data, setData ] = React.useState<ITextObject>({ items: [] });
 
+    const dispatch = useDispatch();
+    const getStaticContentState = useSelector((state: IApplicationState) => state.getStaticContent);
+
     const fetchData = React.useCallback(async () => 
     {
-        const response = await axios.get<ITextObject>(
-        props.dataUrl, 
+        switch(props.content)
         {
-            method: "GET", 
-            responseType: "json"
-        });
+            case REQUEST_STORY: 
+                if (Validate.isEmpty(getStaticContentState.story.items) && !getStaticContentState.story.isLoading)
+                {
+                    dispatch(ActionCreators.getStoryContent());
+                    return;
+                }
+                setData(getStaticContentState.story);
+            break;
 
-        setData(response.data);
+            case REQUEST_TERMS: 
+                if (Validate.isEmpty(getStaticContentState.terms.items) && !getStaticContentState.terms.isLoading)
+                {
+                    dispatch(ActionCreators.getTermsContent());
+                    return;
+                }
+                setData(getStaticContentState.terms);
+            break;
+    
+            case REQUEST_POLICY: 
+                if (Validate.isEmpty(getStaticContentState.policy.items) && !getStaticContentState.policy.isLoading)
+                {
+                    dispatch(ActionCreators.getPolicyContent());
+                    return;
+                }
+                setData(getStaticContentState.policy);
+            break;
+        }
     }, 
-    [ props.dataUrl ]);
+    [ dispatch, getStaticContentState, props.content ]);
 
-    React.useEffect(() => 
-    { 
-        fetchData() 
-    }, 
-    [ data.items.length, fetchData ]);
+    React.useEffect(() => { fetchData() }, [ data.items.length, fetchData ]);
 
     return (
         <section>
---
+++ TokanPages/ClientApp/src/Redux/Actions/getStaticContentAction.ts
@@ -0,0 +1,78 @@
+import { AppThunkAction } from "../applicationState";
+import { ITextObject } from "../../Shared/Components/ContentRender/Models/textModel";
+import { GetErrorMessage } from "../../Shared/helpers";
+import { UnexpectedStatusCode } from "../../Shared/textWrappers";
+import { POLICY_URL, STORY_URL, TERMS_URL } from "../../Shared/constants";
+import { RAISE_ERROR, TErrorActions } from "./raiseErrorAction";
+import { GetData } from "../../Api/request";
+
+export const REQUEST_STORY = "REQUEST_STORY";
+export const REQUEST_TERMS = "REQUEST_TERMS";
+export const REQUEST_POLICY = "REQUEST_POLICY";
+
+export const RECEIVE_STORY = "RECEIVE_STORY";
+export const RECEIVE_TERMS = "RECEIVE_TERMS";
+export const RECEIVE_POLICY = "RECEIVE_POLICY";
+
+export interface IRequestStory { type: typeof REQUEST_STORY }
+export interface IRequestTerms { type: typeof REQUEST_TERMS }
+export interface IRequestPolicy { type: typeof REQUEST_POLICY }
+
+export interface IReceiveStory { type: typeof RECEIVE_STORY, payload: ITextObject }
+export interface IReceiveTerms { type: typeof RECEIVE_TERMS, payload: ITextObject }
+export interface IReceivePolicy { type: typeof RECEIVE_POLICY, payload: ITextObject }
+
+export type TKnownActions = 
+    IRequestStory | 
+    IRequestTerms | 
+    IRequestPolicy |
+    IReceiveStory | 
+    IReceiveTerms | 
+    IReceivePolicy | 
+    TErrorActions
+;
+
+export type TReceiveContent = 
+    typeof RECEIVE_STORY | 
+    typeof RECEIVE_TERMS | 
+    typeof RECEIVE_POLICY
+;
+
+export type TRequestContent = 
+    typeof REQUEST_STORY | 
+    typeof REQUEST_TERMS | 
+    typeof REQUEST_POLICY
+;
+
+const DispatchCall = async (dispatch: any, url: string, request: TRequestContent, receive: TReceiveContent) =>
+{
+    dispatch({ type: request });
+
+    let result = await GetData(url);
+
+    if (result.error !== null)
+    {
+        dispatch({ type: RAISE_ERROR, errorObject: GetErrorMessage(result.error) });
+        return;
+    }
+
+    return result.status === 200
+        ? dispatch({ type: receive, payload: result.data })
+        : dispatch({ type: RAISE_ERROR, errorObject: UnexpectedStatusCode(result.status as number) });   
+}
+
+export const ActionCreators = 
+{
+    getStoryContent: ():  AppThunkAction<TKnownActions> => async (dispatch) => 
+    {
+        DispatchCall(dispatch, STORY_URL, REQUEST_STORY, RECEIVE_STORY);
+    },
+    getTermsContent: ():  AppThunkAction<TKnownActions> => async (dispatch) => 
+    {
+        DispatchCall(dispatch, TERMS_URL, REQUEST_TERMS, RECEIVE_TERMS);
+    },
+    getPolicyContent: ():  AppThunkAction<TKnownActions> => async (dispatch) => 
+    {
+        DispatchCall(dispatch, POLICY_URL, REQUEST_POLICY, RECEIVE_POLICY);
+    }
+}
