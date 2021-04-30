594970928d5083fdec50b44751fb00f4385cb72e
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2021-05-01 00:16:36 +0200

    refactor: update code style, use IEnumerable

--- Backend/TokanPages.Backend.SmtpClient/SmtpClientObject.cs
+++ Backend/TokanPages.Backend.SmtpClient/SmtpClientObject.cs
@@ -7,14 +7,23 @@ namespace TokanPages.Backend.SmtpClient
     public abstract class SmtpClientObject
     {
         public abstract string From { get; set; }
+
         public abstract List<string> Tos { get; set; }
+        
         public abstract List<string> Ccs { get; set; }
+        
         public abstract List<string> Bccs { get; set; }
+        
         public abstract string Subject { get; set; }
+        
         public abstract string PlainText { get; set; }
+        
         public abstract string HtmlBody { get; set; }
+        
         public abstract Task<SendActionResult> Send();
-        public abstract List<CheckActionResult> IsAddressCorrect(List<string> AEmailAddress);
+        
+        public abstract List<CheckActionResult> IsAddressCorrect(IEnumerable<string> AEmailAddress);
+        
         public abstract Task<bool> IsDomainCorrect(string AEmailAddress);
     }
 }
