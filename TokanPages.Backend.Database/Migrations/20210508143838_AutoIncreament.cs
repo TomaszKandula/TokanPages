0ccb4b76abc78b218d5a138bc508f12b8a095e11
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2021-05-08 16:54:11 +0200

    feat: add new migrations

---
+++ TokanPages.Backend.Database/Migrations/20210508143838_AutoIncreament.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TokanPages.Backend.Database.Migrations
+{
+    public partial class AutoIncreament : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
