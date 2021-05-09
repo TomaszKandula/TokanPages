df95102dac3215ad18732ab3c9ab45b5b05ffa91
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2021-05-10 00:33:31 +0200

    refactor: make property init only

--- TokanPages.Backend/TokanPages.Backend.Core/Entities/Entity`TKey.cs
+++ TokanPages.Backend/TokanPages.Backend.Core/Entities/Entity`TKey.cs
@@ -5,6 +5,6 @@ namespace TokanPages.Backend.Core.Entities
     public abstract class Entity<TKey>
     {
         [Key]
-        public TKey Id { get; set; }
+        public TKey Id { get; init; }
     }
 }
