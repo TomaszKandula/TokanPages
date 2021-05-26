bf7611eb5bcdf860a4aa800707349591e9fb8c77
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2021-05-26 09:58:32 +0200

    test: update tests, use combinedDefaults

--- TokanPages/ClientApp/src/Components/Account/__tests__/signinForm.test.tsx
+++ TokanPages/ClientApp/src/Components/Account/__tests__/signinForm.test.tsx
@@ -2,13 +2,13 @@ import "../../../setupTests";
 import React from "react";
 import { shallow } from "enzyme";
 import SigninForm from "../signinForm";
-import { signinFormContentDefault } from "../../../Api/Defaults";
+import { combinedDefaults } from "../../../Redux/combinedDefaults";
 
 describe("Test account component: SigninForm.", () => 
 {
     it("Renders correctly '<SigninForm />' when content is loaded.", () => 
     {
-        const tree = shallow(<SigninForm signinForm={signinFormContentDefault} isLoading={false} />);
+        const tree = shallow(<SigninForm signinForm={combinedDefaults.getSigninFormContent} isLoading={false} />);
         expect(tree).toMatchSnapshot();
     });
 });
--- TokanPages/ClientApp/src/Components/Account/__tests__/signupForm.test.tsx
+++ TokanPages/ClientApp/src/Components/Account/__tests__/signupForm.test.tsx
@@ -2,13 +2,13 @@ import "../../../setupTests";
 import React from "react";
 import { shallow } from "enzyme";
 import SignupForm from "../signupForm";
-import { signupFormContentDefault } from "../../../Api/Defaults";
+import { combinedDefaults } from "../../../Redux/combinedDefaults";
 
 describe("Test account component: SignupForm.", () => 
 {
     it("Renders correctly '<SignupForm />' when content is loaded.", () => 
     {
-        const tree = shallow(<SignupForm signupForm={signupFormContentDefault} isLoading={false} />);
+        const tree = shallow(<SignupForm signupForm={combinedDefaults.getSignupFormContent} isLoading={false} />);
         expect(tree).toMatchSnapshot();
     });
 });
