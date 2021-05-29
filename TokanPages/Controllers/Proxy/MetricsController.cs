a1e516c9785e41e5ff421d65859de7450307b985
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2021-05-29 20:12:29 +0200

    feat: add endpoint for SonarQube quality gate large badge

--- TokanPages/Controllers/Proxy/MetricsController.cs
+++ TokanPages/Controllers/Proxy/MetricsController.cs
@@ -20,6 +20,7 @@ namespace TokanPages.Controllers.Proxy
         
         /// <summary>
         /// Returns badge from SonarQube server for given project name and metric type.
+        /// All badges have the same style.
         /// </summary>
         /// <param name="AProject">SonarQube analysis project name</param>
         /// <param name="AMetric">SonarQube metric type</param>
@@ -30,32 +31,55 @@ namespace TokanPages.Controllers.Proxy
             try
             {
                 var LRequestUrl = $"{FSonarQube.Server}/api/project_badges/measure?project={AProject}&metric={AMetric}";
-
-                using var LHttpClient = new HttpClient();
-                using var LRequest = new HttpRequestMessage(new HttpMethod("GET"), LRequestUrl);
-                
-                var LBase64Authorization = Convert.ToBase64String(Encoding.ASCII.GetBytes($"{FSonarQube.Token}:"));
-                LRequest.Headers.TryAddWithoutValidation(AUTHORIZATION, $"Basic {LBase64Authorization}"); 
-                
-                var LResponse = await LHttpClient.SendAsync(LRequest);
-                var LContent = await LResponse.Content.ReadAsStringAsync();
-
-                return new ContentResult 
-                {
-                    StatusCode = 200,
-                    ContentType = Constants.ContentTypes.IMAGE_SVG,
-                    Content = LContent
-                };
+                var LContent = await GetContent(LRequestUrl);
+                return GetContentResult(200, Constants.ContentTypes.IMAGE_SVG, LContent);
             }
             catch (Exception LException)
             {
-                return new ContentResult 
-                {
-                    StatusCode = 500,
-                    ContentType = Constants.ContentTypes.TEXT_PLAIN,
-                    Content = LException.Message
-                };
+                return GetContentResult(500, Constants.ContentTypes.TEXT_PLAIN, LException.Message);
             }
+        }
+
+        /// <summary>
+        /// Returns large quality gate badge from SonarQube server for given project name.
+        /// </summary>
+        /// <param name="AProject">SonarQube analysis project name</param>
+        /// <returns>SonarQube badge</returns>
+        [HttpGet]
+        public async Task<ContentResult> GetQualityGate([FromQuery] string AProject)
+        {
+            try
+            {
+                var LRequestUrl = $"{FSonarQube.Server}/api/project_badges/quality_gate?project={AProject}";
+                var LContent = await GetContent(LRequestUrl);
+                return GetContentResult(200, Constants.ContentTypes.IMAGE_SVG, LContent);
+            }
+            catch (Exception LException)
+            {
+                return GetContentResult(500, Constants.ContentTypes.TEXT_PLAIN, LException.Message);
+            }
+        }
+        
+        private async Task<string> GetContent(string ARequestUrl)
+        {
+            using var LHttpClient = new HttpClient();
+            using var LRequest = new HttpRequestMessage(new HttpMethod("GET"), ARequestUrl);
+
+            var LBase64Authorization = Convert.ToBase64String(Encoding.ASCII.GetBytes($"{FSonarQube.Token}:"));
+            LRequest.Headers.TryAddWithoutValidation(AUTHORIZATION, $"Basic {LBase64Authorization}"); 
+                
+            var LResponse = await LHttpClient.SendAsync(LRequest);
+            return await LResponse.Content.ReadAsStringAsync();
+        }
+
+        private static ContentResult GetContentResult(int? AStatusCode, string AContentType, string AContent)
+        {
+            return new () 
+            {
+                StatusCode = AStatusCode,
+                ContentType = AContentType,
+                Content = AContent
+            };
         }
     }
 }
