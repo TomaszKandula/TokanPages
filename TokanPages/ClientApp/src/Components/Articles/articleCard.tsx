9a40fb1e04ce5f37c86e6ed28802468f6e8d0ed6
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2021-05-30 23:00:44 +0200

    refactor: split article card logic from its view

--- TokanPages/ClientApp/src/Components/Articles/articleCard.tsx
+++ TokanPages/ClientApp/src/Components/Articles/articleCard.tsx
@@ -1,15 +1,9 @@
 import * as React from "react";
 import { useDispatch } from "react-redux";
 import { useHistory } from "react-router-dom";
-import Card from "@material-ui/core/Card";
-import CardActions from "@material-ui/core/CardActions";
-import CardContent from "@material-ui/core/CardContent";
-import Button from "@material-ui/core/Button";
-import Typography from "@material-ui/core/Typography";
-import { Grid } from "@material-ui/core";
-import articleCardStyle from "./Styles/articleCardStyle";
 import { ActionCreators } from "../../Redux/Actions/selectArticleAction";
 import { ARTICLE_PATH, IMAGE_URL } from "../../Shared/constants";
+import ArticleCardView from "./articleCardView";
 
 export interface IArticleCard
 {
@@ -20,12 +14,7 @@ export interface IArticleCard
 
 export default function ArticleCard(props: IArticleCard)
 {
-    const classes = articleCardStyle();
-    const content = 
-    {
-        button: "Read"
-    };
-
+    const content = { button: "Read" };
     const articleUrl = ARTICLE_PATH.replace("{ID}", props.id);
     const imageUrl = IMAGE_URL.replace("{ID}", props.id);
 
@@ -38,28 +27,12 @@ export default function ArticleCard(props: IArticleCard)
         history.push(articleUrl);
     };
 
-    return(
-        <div data-aos="fade-up">
-            <Card className={classes.root} elevation={4}>
-                <Grid container spacing={2}>
-                    <Grid item xs={4}>
-                        <img className={classes.img} alt="" src={imageUrl} />
-                    </Grid>
-                    <Grid item xs={8}>
-                        <CardContent>
-                            <Typography gutterBottom variant="h5" component="h2" className={classes.title}>
-                                {props.title}
-                            </Typography>
-                            <Typography variant="body2" color="textSecondary" component="p" className={classes.description}>
-                                {props.description}
-                            </Typography>
-                        </CardContent>
-                        <CardActions>
-                            <Button onClick={onClickEvent} size="small" color="primary">{content.button}</Button>
-                        </CardActions>
-                    </Grid>
-                </Grid>
-            </Card>
-        </div>
-    );
+    return (<ArticleCardView bind=
+    {{
+        imageUrl: imageUrl,
+        title: props.title,
+        description: props.description,
+        onClickEvent: onClickEvent,
+        buttonText: content.button
+    }}/>);
 }
---
+++ TokanPages/ClientApp/src/Components/Articles/articleCardView.tsx
@@ -0,0 +1,51 @@
+import * as React from "react";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import { Grid } from "@material-ui/core";
+import articleCardStyle from "./Styles/articleCardStyle";
+
+interface IBinding
+{
+    bind: IProperties;
+}
+
+interface IProperties
+{
+    imageUrl: string;
+    title: string;
+    description: string;
+    onClickEvent: any;
+    buttonText: string;
+}
+
+export default function ArticleCardView(props: IBinding)
+{
+    const classes = articleCardStyle();
+    return(
+        <div data-aos="fade-up">
+            <Card className={classes.root} elevation={4}>
+                <Grid container spacing={2}>
+                    <Grid item xs={4}>
+                        <img className={classes.img} alt="" src={props.bind?.imageUrl} />
+                    </Grid>
+                    <Grid item xs={8}>
+                        <CardContent>
+                            <Typography gutterBottom variant="h5" component="h2" className={classes.title}>
+                                {props.bind?.title}
+                            </Typography>
+                            <Typography variant="body2" color="textSecondary" component="p" className={classes.description}>
+                                {props.bind?.description}
+                            </Typography>
+                        </CardContent>
+                        <CardActions>
+                            <Button onClick={props.bind?.onClickEvent} size="small" color="primary">{props.bind?.buttonText}</Button>
+                        </CardActions>
+                    </Grid>
+                </Grid>
+            </Card>
+        </div>
+    );
+}
