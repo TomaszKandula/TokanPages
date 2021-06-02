d917b728f37f34cea1884ed4d56252082b8a7d6a
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2021-06-02 23:32:32 +0200

    refactor: rename folders, update namespaces

---
+++ TokanPages.Backend.Tests/Validators/Subscribers/AddSubscriberCommandValidatorTest.cs
@@ -0,0 +1,46 @@
+﻿using Xunit;
+using FluentAssertions;
+using TokanPages.Backend.Core.Generators;
+using TokanPages.Backend.Shared.Resources;
+using TokanPages.Backend.Cqrs.Handlers.Commands.Subscribers;
+
+namespace TokanPages.Backend.Tests.Validators.Subscribers
+{
+    public class AddSubscriberCommandValidatorTest
+    {
+        [Fact]
+        public void GivenEmail_WhenAddSubscriber_ShouldFinishSuccessful() 
+        {
+            // Arrange
+            var LAddSubscriberCommand = new AddSubscriberCommand 
+            { 
+                Email = StringProvider.GetRandomEmail()
+            };
+
+            // Act
+            var LValidator = new AddSubscriberCommandValidator();
+            var LResult = LValidator.Validate(LAddSubscriberCommand);
+
+            // Assert
+            LResult.Errors.Should().BeEmpty();
+        }
+
+        [Fact]
+        public void GivenEmptyEmail_WhenAddSubscriber_ShouldFinishSuccessful()
+        {
+            // Arrange
+            var LAddSubscriberCommand = new AddSubscriberCommand
+            {
+                Email = string.Empty
+            };
+
+            // Act
+            var LValidator = new AddSubscriberCommandValidator();
+            var LResult = LValidator.Validate(LAddSubscriberCommand);
+
+            // Assert
+            LResult.Errors.Count.Should().Be(1);
+            LResult.Errors[0].ErrorCode.Should().Be(nameof(ValidationCodes.REQUIRED));
+        }
+    }
+}
