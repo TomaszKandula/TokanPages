9115af8b984f4866808ddb7bbf1970945faba75d
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2021-06-17 11:14:39 +0200

    refactor: remove entities configuration from code coverage report

--- TokanPages.Backend/TokanPages.Backend.Database/Mappings/ArticleLikesConfiguration.cs
+++ TokanPages.Backend/TokanPages.Backend.Database/Mappings/ArticleLikesConfiguration.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.Diagnostics.CodeAnalysis;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using TokanPages.Backend.Domain.Entities;
 
 namespace TokanPages.Backend.Database.Mappings
 {
+    [ExcludeFromCodeCoverage]
     public class ArticleLikesConfiguration : IEntityTypeConfiguration<ArticleLikes>
     {
         public void Configure(EntityTypeBuilder<ArticleLikes> ABuilder)
