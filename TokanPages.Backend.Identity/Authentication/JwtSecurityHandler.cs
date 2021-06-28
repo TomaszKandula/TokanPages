7283f39a305d4dc51f83651103ed284df7de9ee0
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2021-06-28 23:59:12 +0200

    refactor: add custom token validator

---
+++ TokanPages.Backend.Identity/Authentication/JwtSecurityHandler.cs
@@ -0,0 +1,34 @@
+using System.Security.Claims;
+using System.Diagnostics.CodeAnalysis;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.IdentityModel.Tokens;
+
+namespace TokanPages.Backend.Identity.Authentication
+{
+    [ExcludeFromCodeCoverage]
+    public class JwtSecurityHandler : ISecurityTokenValidator
+    {
+        private readonly JwtSecurityTokenHandler FTokenHandler;
+
+        public bool CanValidateToken => true;
+
+        public int MaximumTokenSizeInBytes { get; set; } = TokenValidationParameters.DefaultMaximumTokenSizeInBytes;
+
+        public JwtSecurityHandler()
+        {
+            FTokenHandler = new JwtSecurityTokenHandler();
+        }
+
+        public bool CanReadToken(string ASecurityToken)
+        {
+            return FTokenHandler.CanReadToken(ASecurityToken);
+        }
+
+        public ClaimsPrincipal ValidateToken(string ASecurityToken, TokenValidationParameters AValidationParameters,
+            out SecurityToken AValidatedToken)
+        {
+            var LPrincipal = FTokenHandler.ValidateToken(ASecurityToken, AValidationParameters, out AValidatedToken);
+            return LPrincipal;
+        }
+    }
+}
