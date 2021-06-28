014176e2ee1863699e1514d91b7f746271fb462b
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2021-06-28 12:00:25 +0200

    refactor: remove unused assemblies

--- TokanPages.WebApi/Startup.cs
+++ TokanPages.WebApi/Startup.cs
@@ -1,8 +1,6 @@
 using System;
-using System.IO;
 using System.Net;
 using System.Linq;
-using System.Reflection;
 using System.Net.Sockets;
 using System.Diagnostics.CodeAnalysis;
 using Microsoft.OpenApi.Models;
