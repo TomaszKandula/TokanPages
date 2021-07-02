b697773e83a8ca8b2b1bbd407f78ac6a701cc8cd
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2021-07-02 19:19:26 +0200

    test: add new test for BCrypt implementation

---
+++ TokanPages.Backend.Tests/Services/CipherTest.cs
@@ -0,0 +1,32 @@
+using Xunit;
+using FluentAssertions;
+using TokanPages.Backend.Cqrs.Services.Cipher;
+using TokanPages.Backend.Core.Services.DataProviderService;
+
+namespace TokanPages.Backend.Tests.Services
+{
+    public class CipherTest
+    {
+        private readonly DataProviderService FDataProviderService;
+        
+        public CipherTest() => FDataProviderService = new DataProviderService();
+
+        [Fact]
+        public void GivenPlainTextPassword_WhenInvokeGetHashedPassword_ShouldReturnHashedPassword()
+        {
+            // Arrange
+            const int CIPHER_LOG_ROUNDS = 12;
+            var LPlainTextPassword = FDataProviderService.GetRandomString();
+            var LCipher = new Cipher();
+
+            // Act
+            var LSalt = LCipher.GenerateSalt(CIPHER_LOG_ROUNDS);
+            var LHashed = LCipher.GetHashedPassword(LPlainTextPassword, LSalt);
+
+            // Assert
+            LHashed.Should().NotBeNullOrEmpty();
+            var LVerify = LCipher.VerifyPassword(LPlainTextPassword, LHashed);
+            LVerify.Should().BeTrue();
+        }
+    }
+}
