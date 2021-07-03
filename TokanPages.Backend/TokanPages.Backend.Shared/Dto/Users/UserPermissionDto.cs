2fff9fc6f00ecee88797fe0625586bee4895c3ce
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2021-07-03 20:00:08 +0200

    refactor: add new DTOs

---
+++ TokanPages.Backend/TokanPages.Backend.Shared/Dto/Users/UserPermissionDto.cs
@@ -0,0 +1,7 @@
+namespace TokanPages.Backend.Shared.Dto.Users
+{
+    public class UserPermissionDto
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ TokanPages.Backend/TokanPages.Backend.Shared/Dto/Users/UserRoleDto.cs
@@ -0,0 +1,9 @@
+namespace TokanPages.Backend.Shared.Dto.Users
+{
+    public class UserRoleDto
+    {
+        public string Name { get; set; }
+        
+        public string Description { get; set; }
+    }
+}
