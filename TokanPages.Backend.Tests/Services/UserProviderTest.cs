f847c4e422e1ddc3cbf7a87c826b6c07bfe5efb0
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2021-07-03 22:12:44 +0200

    test: add test for getUser method

--- TokanPages.Backend.Tests/Services/UserProviderTest.cs
+++ TokanPages.Backend.Tests/Services/UserProviderTest.cs
@@ -140,5 +140,92 @@ namespace TokanPages.Backend.Tests.Services
             var LResult = await Assert.ThrowsAsync<BusinessException>(LUserProvider.GetUserId);
             LResult.ErrorCode.Should().Be(nameof(ErrorCodes.ACCESS_DENIED));
         }
+
+        [Fact]
+        public async Task GivenValidUserIdWithinHttpContextClaims_WhenInvokeGetUser_ShouldReturnJsonObject()
+        {
+            // Arrange
+            var LLoggedUserId = Guid.Parse("2431eeba-866c-4e45-ad64-c409dd824df9");
+            var LUser = new Domain.Entities.Users
+            {
+                Id = LLoggedUserId,
+                EmailAddress = FDataProviderService.GetRandomEmail(),
+                UserAlias = FDataProviderService.GetRandomString(),
+                FirstName = FDataProviderService.GetRandomString(),
+                LastName = FDataProviderService.GetRandomString(),
+                IsActivated = true,
+                Registered = DateTime.Now,
+                LastUpdated = null,
+                LastLogged = null,
+                CryptedPassword = FDataProviderService.GetRandomString()
+            };
+
+            var LUsers = new List<Domain.Entities.Users> { LUser };
+            var LHttpContext = new Mock<IHttpContextAccessor>();
+            LHttpContext
+                .Setup(AHttpContext => AHttpContext.HttpContext.User.Claims)
+                .Returns(new List<Claim>
+                {
+                    new (Claims.UserId, LLoggedUserId.ToString())
+                });
+            
+            var LDatabaseContext = GetTestDatabaseContext();
+            await LDatabaseContext.Users.AddRangeAsync(LUsers);
+            await LDatabaseContext.SaveChangesAsync();
+            
+            // Act
+            var LUserProvider = new UserProvider(LHttpContext.Object, LDatabaseContext);
+            var LResult = await LUserProvider.GetUser();
+            
+            // Assert
+            LResult.UserId.Should().Be(LUser.Id);
+            LResult.AliasName.Should().Be(LUser.UserAlias);
+            LResult.AvatarName.Should().Be(LUser.AvatarName);
+            LResult.FirstName.Should().Be(LUser.FirstName);
+            LResult.LastName.Should().Be(LUser.LastName);
+            LResult.ShortBio.Should().Be(LUser.ShortBio);
+            LResult.Registered.Should().Be(LUser.Registered);
+        }
+        
+        [Fact]
+        public async Task GivenInvalidUserIdWithinHttpContextClaims_WhenInvokeGetUser_ShouldThrowError()
+        {
+            // Arrange
+            var LUser = new Domain.Entities.Users
+            {
+                Id = Guid.Parse("2431eeba-866c-4e45-ad64-c409dd824df9"),
+                EmailAddress = FDataProviderService.GetRandomEmail(),
+                UserAlias = FDataProviderService.GetRandomString(),
+                FirstName = FDataProviderService.GetRandomString(),
+                LastName = FDataProviderService.GetRandomString(),
+                IsActivated = true,
+                Registered = DateTime.Now,
+                LastUpdated = null,
+                LastLogged = null,
+                CryptedPassword = FDataProviderService.GetRandomString()
+            };
+
+            var LUsers = new List<Domain.Entities.Users> { LUser };
+
+            var LHttpContext = new Mock<IHttpContextAccessor>();
+            LHttpContext
+                .Setup(AHttpContext => AHttpContext.HttpContext.User.Claims)
+                .Returns(new List<Claim>
+                {
+                    new (Claims.UserId, Guid.NewGuid().ToString())
+                });
+            
+            var LDatabaseContext = GetTestDatabaseContext();
+            await LDatabaseContext.Users.AddRangeAsync(LUsers);
+            await LDatabaseContext.SaveChangesAsync();
+            
+            // Act
+            // Assert
+            var LUserProvider = new UserProvider(LHttpContext.Object, LDatabaseContext);
+            var LResult = await Assert.ThrowsAsync<BusinessException>(LUserProvider.GetUser);
+            LResult.ErrorCode.Should().Be(nameof(ErrorCodes.ACCESS_DENIED));
+        }
+        
+        
     }
 }
