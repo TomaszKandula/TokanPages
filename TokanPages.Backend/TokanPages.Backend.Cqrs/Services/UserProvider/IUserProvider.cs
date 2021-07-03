b142aa8a6d1a9f4616452f641cd0d93b9d2af103
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2021-07-03 20:00:32 +0200

    refactor: update user provider implementation

--- TokanPages.Backend/TokanPages.Backend.Cqrs/Services/UserProvider/IUserProvider.cs
+++ TokanPages.Backend/TokanPages.Backend.Cqrs/Services/UserProvider/IUserProvider.cs
@@ -1,5 +1,7 @@
 ﻿using System;
-using TokanPages.Backend.Domain.Entities;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using TokanPages.Backend.Shared.Dto.Users;
 
 namespace TokanPages.Backend.Cqrs.Services.UserProvider
 {
@@ -7,8 +9,16 @@ namespace TokanPages.Backend.Cqrs.Services.UserProvider
     {
         string GetRequestIpAddress();
 
-        Guid? GetUserId();
+        Task<Guid?> GetUserId();
         
-        Users GetUserData();
+        Task<GetUserDto> GetUser();
+
+        Task<List<UserRoleDto>> GetUserRoles();
+
+        Task<List<UserPermissionDto>> GetUserPermissions();
+
+        Task<bool> HasRoleAssigned(string AUserRoleName);
+
+        Task<bool> HasPermissionAssigned(string AUserPermissionName);
     }
 }
--- TokanPages.Backend/TokanPages.Backend.Cqrs/Services/UserProvider/UserProvider.cs
+++ TokanPages.Backend/TokanPages.Backend.Cqrs/Services/UserProvider/UserProvider.cs
@@ -1,7 +1,14 @@
 ﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Http;
-using TokanPages.Backend.Domain.Entities;
-using TokanPages.Backend.Database.Initializer.Data.Users;
+using Microsoft.EntityFrameworkCore;
+using TokanPages.Backend.Database;
+using TokanPages.Backend.Core.Exceptions;
+using TokanPages.Backend.Shared.Resources;
+using TokanPages.Backend.Shared.Dto.Users;
+using TokanPages.Backend.Identity.Authorization;
 
 namespace TokanPages.Backend.Cqrs.Services.UserProvider
 {
@@ -11,29 +18,171 @@ namespace TokanPages.Backend.Cqrs.Services.UserProvider
         
         private readonly IHttpContextAccessor FHttpContextAccessor;
 
-        public UserProvider(IHttpContextAccessor AHttpContextAccessor) 
-            => FHttpContextAccessor = AHttpContextAccessor;
+        private readonly DatabaseContext FDatabaseContext;
+
+        private List<UserPermissionDto> FUserPermissions;
+
+        private List<UserRoleDto> FUserRoles;
+        
+        private GetUserDto FUsers;
+        
+        public UserProvider(IHttpContextAccessor AHttpContextAccessor, DatabaseContext ADatabaseContext)
+        {
+            FHttpContextAccessor = AHttpContextAccessor;
+            FDatabaseContext = ADatabaseContext;
+        }
 
         public UserProvider() { }
 
         public virtual string GetRequestIpAddress() 
         {
-            var LRemoteIpAddress = FHttpContextAccessor.HttpContext?.Request.Headers["X-Forwarded-For"].ToString();
+            var LRemoteIpAddress = FHttpContextAccessor.HttpContext?
+                .Request.Headers["X-Forwarded-For"].ToString();
+            
             return string.IsNullOrEmpty(LRemoteIpAddress) 
                 ? LOCALHOST 
                 : LRemoteIpAddress.Split(':')[0];
         }
 
-        public virtual Guid? GetUserId() 
+        public virtual async Task<Guid?> GetUserId()
         {
-            // TODO: return logged user id
-            return User1.FId;
+            await EnsureUserData();
+            return FUsers.UserId;
         }
 
-        public virtual Users GetUserData() 
+        public virtual async Task<GetUserDto> GetUser()
         {
-            // TODO: return basic data
-            return new ();
+            await EnsureUserData();
+            return FUsers;
+        }
+
+        public virtual async Task<List<UserRoleDto>> GetUserRoles()
+        {
+            await EnsureUserRoles();
+            return FUserRoles;
+        }
+
+        public virtual async Task<List<UserPermissionDto>> GetUserPermissions()
+        {
+            await EnsureUserPermissions();
+            return FUserPermissions;
+        }
+
+        public virtual async Task<bool> HasRoleAssigned(string AUserRoleName)
+        {
+            if (string.IsNullOrEmpty(AUserRoleName))
+                throw ArgumentNullException;
+            
+            var LUserId = UserIdFromClaim();
+            var LGivenRoles = await FDatabaseContext.UserRoles
+                .AsNoTracking()
+                .Include(AUserRoles => AUserRoles.Role)
+                .Where(AUserRole => AUserRole.UserId == LUserId && AUserRole.Role.Name == AUserRoleName)
+                .ToListAsync();
+
+            return LGivenRoles.Any();
+        }
+
+        public virtual async Task<bool> HasPermissionAssigned(string AUserPermissionName)
+        {
+            if (string.IsNullOrEmpty(AUserPermissionName))
+                throw ArgumentNullException;
+            
+            var LUserId = UserIdFromClaim();
+            var LGivenPermissions = await FDatabaseContext.UserPermissions
+                .AsNoTracking()
+                .Include(AUserPermissions => AUserPermissions.Permission)
+                .Where(AUserPermissions => AUserPermissions.UserId == LUserId && AUserPermissions.Permission.Name == AUserPermissionName)
+                .ToListAsync();
+
+            return LGivenPermissions.Any();
+        }
+        
+        private static BusinessException AccessDeniedException 
+            => new (nameof(ErrorCodes.ACCESS_DENIED), ErrorCodes.ACCESS_DENIED);
+
+        private static BusinessException ArgumentNullException
+            => new (nameof(ErrorCodes.ARGUMENT_NULL_EXCEPTION), ErrorCodes.ARGUMENT_NULL_EXCEPTION);
+        
+        private Guid UserIdFromClaim()
+        {
+            var LUserClaims = FHttpContextAccessor.HttpContext?.User.Claims ?? throw AccessDeniedException;
+            var LUserId = LUserClaims.First(AClaim => AClaim.Type.Contains(Claims.UserId)).Value;
+            return Guid.Parse(LUserId);
+        }
+
+        private async Task EnsureUserRoles()
+        {
+            if (FUserRoles != null)
+                return;
+            
+            var LUserRoles = await FDatabaseContext.UserRoles
+                .AsNoTracking()
+                .Include(AUserRoles => AUserRoles.Role)
+                .Where(AUserRoles => AUserRoles.UserId == UserIdFromClaim())
+                .ToListAsync();
+
+            if (!LUserRoles.Any())
+                throw AccessDeniedException;
+
+            FUserRoles = new List<UserRoleDto>();
+            foreach (var LItem in LUserRoles)
+            {
+                FUserRoles.Add(new UserRoleDto
+                {
+                    Name = LItem.Role.Name,
+                    Description = LItem.Role.Description
+                });
+            }
+        }
+
+        private async Task EnsureUserPermissions()
+        {
+            if (FUserPermissions != null)
+                return;
+            
+            var LUserPermissions = await FDatabaseContext.UserPermissions
+                .AsNoTracking()
+                .Include(AUserPermissions => AUserPermissions.Permission)
+                .Where(AUserPermissions => AUserPermissions.UserId == UserIdFromClaim())
+                .ToListAsync();
+
+            if (!LUserPermissions.Any())
+                throw AccessDeniedException;
+
+            FUserPermissions = new List<UserPermissionDto>();
+            foreach (var LItem in LUserPermissions)
+            {
+                FUserPermissions.Add(new UserPermissionDto
+                {
+                    Name = LItem.Permission.Name
+                });
+            }
+        }
+
+        private async Task EnsureUserData()
+        {
+            if (FUsers != null) 
+                return;
+            
+            var LUsers = await FDatabaseContext.Users
+                .AsNoTracking()
+                .Where(AUsers => AUsers.Id == UserIdFromClaim())
+                .ToListAsync();
+
+            if (!LUsers.Any())
+                throw AccessDeniedException;
+
+            FUsers = new GetUserDto
+            {
+                UserId = LUsers.First().Id,
+                AliasName = LUsers.First().UserAlias,
+                AvatarName = LUsers.First().AvatarName,
+                FirstName = LUsers.First().FirstName,
+                LastName = LUsers.First().LastName,
+                ShortBio = LUsers.First().ShortBio,
+                Registered = LUsers.First().Registered
+            };
         }
     }
 }
