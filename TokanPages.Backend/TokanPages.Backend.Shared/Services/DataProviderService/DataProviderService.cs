384ff12087bc1691a7b1690233f0ac3ab0d946b7
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2021-07-03 14:42:15 +0200

    refactor: add comments for methods

--- TokanPages.Backend/TokanPages.Backend.Shared/Services/DataProviderService/DataProviderService.cs
+++ TokanPages.Backend/TokanPages.Backend.Shared/Services/DataProviderService/DataProviderService.cs
@@ -14,6 +14,17 @@ namespace TokanPages.Backend.Shared.Services.DataProviderService
 
         public DataProviderService() => FRandom = new Random();
 
+        /// <summary>
+        /// Returns randomized Date within given range.
+        /// </summary>
+        /// <remarks>
+        /// It uses System.Random function, therefore it should not be used
+        /// for security-critical applications or for protecting sensitive data.
+        /// </remarks>>
+        /// <param name="AMin">Minimum value of expected date. Value can be null (if so, default day and month is: 1 JAN).</param>
+        /// <param name="AMax">Maximum value of expected date. Value can be null (if so, default day and month is: 31 DEC).</param>
+        /// <param name="ADefaultYear">If not given, it uses 2020 year as default value.</param>
+        /// <returns>New randomized date.</returns>
         public override DateTime GetRandomDateTime(DateTime? AMin = null, DateTime? AMax = null, int ADefaultYear = 2020)
         {
             AMin ??= new DateTime(ADefaultYear, 1, 1); 
@@ -23,21 +34,79 @@ namespace TokanPages.Backend.Shared.Services.DataProviderService
 
             return AMin.Value.AddDays(FRandom.Next(0, LDayRange));
         }
-
+        
+        /// <summary>
+        /// Returns randomized enumeration.
+        /// </summary>
+        /// <remarks>
+        /// It uses System.Random function, therefore it should not be used
+        /// for security-critical applications or for protecting sensitive data.
+        /// </remarks>>
+        /// <typeparam name="T">Given type.</typeparam>
+        /// <returns>New randomized enumeration.</returns>
         public override T GetRandomEnum<T>()
         {
             var LValues = Enum.GetValues(typeof(T)); 
             return (T)LValues.GetValue(FRandom.Next(LValues.Length));
         }
 
+        /// <summary>
+        /// Returns randomized integer number within given range.
+        /// </summary>
+        /// <remarks>
+        /// It uses System.Random function, therefore it should not be used
+        /// for security-critical applications or for protecting sensitive data.
+        /// </remarks>>
+        /// <param name="AMin">A boundary value, lowest possible.</param>
+        /// <param name="AMax">A boundary value, highest possible.</param>
+        /// <returns>New randomized integer number.</returns>
         public override int GetRandomInteger(int AMin = 0, int AMax = 12) => FRandom.Next(AMin, AMax + 1);
 
+        /// <summary>
+        /// Returns randomized decimal number within given range.
+        /// </summary>
+        /// <remarks>
+        /// It uses System.Random function, therefore it should not be used
+        /// for security-critical applications or for protecting sensitive data.
+        /// </remarks>>
+        /// <param name="AMin">A boundary value, lowest possible.</param>
+        /// <param name="AMax">A boundary value, highest possible.</param>
+        /// <returns>New randomized decimal number.</returns>
         public override decimal GetRandomDecimal(int AMin = 0, int AMax = 9999) => FRandom.Next(AMin, AMax);
 
+        /// <summary>
+        /// Returns randomized stream of bytes.
+        /// </summary>
+        /// <remarks>
+        /// It uses System.Random function, therefore it should not be used
+        /// for security-critical applications or for protecting sensitive data.
+        /// </remarks>>
+        /// <param name="ASizeInKb">Expected size in kBytes.</param>
+        /// <returns>New randomized stream of bytes.</returns>
         public override MemoryStream GetRandomStream(int ASizeInKb = 12) => new (GetRandomByteArray(ASizeInKb));
-        
+
+        /// <summary>
+        /// Returns randomized e-mail address.
+        /// </summary>
+        /// <remarks>
+        /// It uses System.Random function, therefore it should not be used
+        /// for security-critical applications or for protecting sensitive data.
+        /// </remarks>>
+        /// <param name="ALength">Expected length of the name, 12 characters by default.</param>
+        /// <param name="ADomain">Domain, "gmail.com" by default.</param>
+        /// <returns>New randomized e-mail address.</returns>
         public override string GetRandomEmail(int ALength = 12, string ADomain = "gmail.com") => $"{GetRandomString(ALength)}@{ADomain}";
 
+        /// <summary>
+        /// Returns randomized string with given length and prefix (optional).
+        /// </summary>
+        /// <remarks>
+        /// It uses System.Random function, therefore it should not be used
+        /// for security-critical applications or for protecting sensitive data.
+        /// </remarks>>
+        /// <param name="ALength">Expected length, 12 characters by default.</param>
+        /// <param name="APrefix">Optional prefix.</param>
+        /// <returns>New randomized string.</returns>
         public override string GetRandomString(int ALength = 12, string APrefix = "")
         {
             if (ALength == 0) 
@@ -55,6 +124,15 @@ namespace TokanPages.Backend.Shared.Services.DataProviderService
             return LString;
         }
 
+        /// <summary>
+        /// Returns a new security token with given claims and expiration date and time.
+        /// </summary>
+        /// <param name="AExpires">Value of the 'expiration' claim. This value should be in UTC.</param>
+        /// <param name="AClaimsIdentity">Claims that will be used when creating a security token.</param>
+        /// <param name="AWebSecret">String used to generate token key.</param>
+        /// <param name="AIssuer">Issuer of a security token.</param>
+        /// <param name="ATargetAudience">Target audience.</param>
+        /// <returns>New JSON Web Token.</returns>
         public override string GenerateJwt(DateTime AExpires, ClaimsIdentity AClaimsIdentity, string AWebSecret, string AIssuer, string ATargetAudience)
         {
             var LTokenHandler = new JwtSecurityTokenHandler();
