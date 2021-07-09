af4c4da0f830b68497c53e9f0e034a92fbadb08f
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2021-07-10 00:03:17 +0200

    refactor: add new DTOs

---
+++ TokanPages.Backend/TokanPages.Backend.Shared/Dto/Articles/UpdateArticleCountDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace TokanPages.Backend.Shared.Dto.Articles
+{
+    [ExcludeFromCodeCoverage]
+    public class UpdateArticleCountDto
+    {
+        public Guid Id { get; set; }
+    }
+}
---
+++ TokanPages.Backend/TokanPages.Backend.Shared/Dto/Articles/UpdateArticleVisibilityDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace TokanPages.Backend.Shared.Dto.Articles
+{
+    [ExcludeFromCodeCoverage]
+    public class UpdateArticleVisibilityDto
+    {
+        public Guid Id { get; set; }
+        
+        public bool IsPublished { get; set; }
+    }
+}
