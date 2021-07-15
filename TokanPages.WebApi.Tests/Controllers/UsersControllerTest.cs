74097dbab613c550fb46f9c208dc2e282cd1e1a9
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2021-07-16 00:42:26 +0200

    test: add new endpoint test (for authentication)

--- TokanPages.WebApi.Tests/Controllers/UsersControllerTest.cs
+++ TokanPages.WebApi.Tests/Controllers/UsersControllerTest.cs
@@ -7,9 +7,11 @@ namespace TokanPages.WebApi.Tests.Controllers
     using System.Threading.Tasks;
     using System.Net.Http.Headers;
     using System.Collections.Generic;
+    using Backend.Core.Extensions;
     using Backend.Shared.Dto.Users;
     using Backend.Shared.Resources;
     using Backend.Cqrs.Handlers.Queries.Users;
+    using Backend.Cqrs.Handlers.Commands.Users;
     using Backend.Database.Initializer.Data.Users;
     using FluentAssertions;
     using Newtonsoft.Json;
@@ -22,6 +24,71 @@ namespace TokanPages.WebApi.Tests.Controllers
         private readonly CustomWebApplicationFactory<TestStartup> FWebAppFactory;
 
         public UsersControllerTest(CustomWebApplicationFactory<TestStartup> AWebAppFactory) => FWebAppFactory = AWebAppFactory;
+
+        [Fact]
+        public async Task GivenValidCredentials_WhenAuthenticateUser_ShouldSucceed()
+        {
+            // Arrange
+            var LRequest = $"{API_BASE_URL}/AuthenticateUser/";
+            var LNewRequest = new HttpRequestMessage(HttpMethod.Post, LRequest);
+
+            var LPayLoad = new AuthenticateUserDto
+            {
+                EmailAddress = User1.EMAIL_ADDRESS,
+                Password = "user1password"
+            };
+
+            // Act
+            var LHttpClient = FWebAppFactory.CreateClient();
+            LNewRequest.Content = new StringContent(JsonConvert.SerializeObject(LPayLoad), System.Text.Encoding.Default, "application/json");
+
+            // Assert
+            var LResponse = await LHttpClient.SendAsync(LNewRequest);
+
+            // Assert
+            LResponse.EnsureSuccessStatusCode();
+
+            var LContent = await LResponse.Content.ReadAsStringAsync();
+            LContent.Should().NotBeNullOrEmpty();
+            
+            var LDeserialized = JsonConvert.DeserializeObject<AuthenticateUserCommandResult>(LContent);
+            LDeserialized.UserId.ToString().IsGuid().Should().BeTrue();
+            LDeserialized.FirstName.Should().Be(User1.FIRST_NAME);
+            LDeserialized.LastName.Should().Be(User1.LAST_NAME);
+            LDeserialized.AliasName.Should().Be(User1.USER_ALIAS);
+            LDeserialized.ShortBio.Should().Be(User1.SHORT_BIO);
+            LDeserialized.AvatarName.Should().Be(User1.AVATAR_NAME);
+            LDeserialized.Registered.Should().Be(User1.FRegistered);
+            LDeserialized.Jwt.Should().NotBeEmpty();
+        }
+
+        [Fact]
+        public async Task GivenInvalidCredentials_WhenAuthenticateUser_ShouldThrowError()
+        {
+            // Arrange
+            var LRequest = $"{API_BASE_URL}/AuthenticateUser/";
+            var LNewRequest = new HttpRequestMessage(HttpMethod.Post, LRequest);
+
+            var LPayLoad = new AuthenticateUserDto
+            {
+                EmailAddress = User1.EMAIL_ADDRESS,
+                Password = DataUtilityService.GetRandomString()
+            };
+
+            // Act
+            var LHttpClient = FWebAppFactory.CreateClient();
+            LNewRequest.Content = new StringContent(JsonConvert.SerializeObject(LPayLoad), System.Text.Encoding.Default, "application/json");
+
+            // Assert
+            var LResponse = await LHttpClient.SendAsync(LNewRequest);
+
+            // Assert
+            LResponse.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+
+            var LContent = await LResponse.Content.ReadAsStringAsync();
+            LContent.Should().NotBeNullOrEmpty();
+            LContent.Should().Contain(ErrorCodes.INVALID_CREDENTIALS);
+        }
         
         [Fact]
         public async Task GivenAllFieldsAreProvided_WhenAddUser_ShouldReturnNewGuid() 
