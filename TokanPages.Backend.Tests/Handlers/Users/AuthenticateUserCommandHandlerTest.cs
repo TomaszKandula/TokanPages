5371e90947236fd13e8f0af9cc480779ef803b39
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2021-07-17 15:00:20 +0200

    test: update to the changed implementation

--- TokanPages.Backend.Tests/Handlers/Users/AuthenticateUserCommandHandlerTest.cs
+++ TokanPages.Backend.Tests/Handlers/Users/AuthenticateUserCommandHandlerTest.cs
@@ -3,9 +3,7 @@ namespace TokanPages.Backend.Tests.Handlers.Users
     using System;
     using System.Linq;
     using System.Threading;
-    using System.Security.Claims;
     using System.Threading.Tasks;
-    using System.Collections.Generic;
     using Microsoft.EntityFrameworkCore;
     using Shared.Models;
     using Domain.Entities;
@@ -29,7 +27,8 @@ namespace TokanPages.Backend.Tests.Handlers.Users
             var LEmailAddress = DataUtilityService.GetRandomEmail();
             var LPlainTextPassword = DataUtilityService.GetRandomString(10);
             var LCryptedPassword = DataUtilityService.GetRandomString(60);
-            var LGeneratedJwt = DataUtilityService.GetRandomString(255);
+            var LGeneratedUserToken = DataUtilityService.GetRandomString(255);
+            var LIpAddress = DataUtilityService.GetRandomIpAddress().ToString();
             
             var LUser = new Users
             {
@@ -45,42 +44,8 @@ namespace TokanPages.Backend.Tests.Handlers.Users
                 CryptedPassword = LCryptedPassword
             };
 
-            var LRoles = new List<Roles>
-            {
-                new ()
-                {
-                    Id = Guid.NewGuid(),
-                    Name = nameof(Identity.Authorization.Roles.EverydayUser),
-                    Description = "User"
-                },
-                new ()
-                {
-                    Id = Guid.NewGuid(),
-                    Name = nameof(Identity.Authorization.Roles.ArticlePublisher),
-                    Description = "User can publish articles"
-                }
-            };
-
-            var LUserRoles = new List<UserRoles>
-            {
-                new ()
-                {
-                    Id = Guid.NewGuid(),
-                    UserId = LUser.Id,
-                    RoleId = LRoles[0].Id
-                },
-                new ()
-                {
-                    Id = Guid.NewGuid(),
-                    UserId = LUser.Id,
-                    RoleId = LRoles[1].Id
-                }
-            };
-
             var LDatabaseContext = GetTestDatabaseContext();
             await LDatabaseContext.Users.AddAsync(LUser);
-            await LDatabaseContext.Roles.AddRangeAsync(LRoles);
-            await LDatabaseContext.UserRoles.AddRangeAsync(LUserRoles);
             await LDatabaseContext.SaveChangesAsync();
 
             var LAuthenticateUserCommand = new AuthenticateUserCommand
@@ -95,16 +60,6 @@ namespace TokanPages.Backend.Tests.Handlers.Users
                     .VerifyPassword(It.IsAny<string>(), It.IsAny<string>()))
                 .Returns(true);
             
-            var LMockedJwtUtilityService = new Mock<IJwtUtilityService>();
-            LMockedJwtUtilityService
-                .Setup(AUtilityService => AUtilityService.GenerateJwt(
-                    It.IsAny<DateTime>(),
-                    It.IsAny<ClaimsIdentity>(),
-                    It.IsAny<string>(),
-                    It.IsAny<string>(),
-                    It.IsAny<string>()))
-                .Returns(LGeneratedJwt);
-
             var LRefreshTokenExpires = DateTimeService.Now.AddDays(10);
             var LRefreshTokenCreated = DateTimeService.Now;
             var LGeneratedRefreshToken = new RefreshToken
@@ -112,8 +67,10 @@ namespace TokanPages.Backend.Tests.Handlers.Users
                 Token = DataUtilityService.GetRandomString(),
                 Expires = LRefreshTokenExpires,
                 Created = LRefreshTokenCreated,
-                CreatedByIp = DataUtilityService.GetRandomIpAddress().ToString()
+                CreatedByIp = LIpAddress
             };
+
+            var LMockedJwtUtilityService = new Mock<IJwtUtilityService>();
             LMockedJwtUtilityService
                 .Setup(AUtilityService => AUtilityService
                     .GenerateRefreshToken(It.IsAny<string>(), It.IsAny<int>()))
@@ -126,7 +83,40 @@ namespace TokanPages.Backend.Tests.Handlers.Users
                 .Returns(LRandomDateTime);
             
             var LMockedUserServiceProvider = new Mock<IUserServiceProvider>();
-            var LMockedIdentityServer = new Mock<IdentityServer>();
+            LMockedUserServiceProvider
+                .Setup(AUserService => AUserService.GenerateUserToken(
+                    It.IsAny<Users>(), 
+                    It.IsAny<DateTime>(), 
+                    CancellationToken.None))
+                .ReturnsAsync(LGeneratedUserToken);
+            
+            LMockedUserServiceProvider
+                .Setup(AUserService => AUserService.GetRequestIpAddress())
+                .Returns(LIpAddress);
+
+            LMockedUserServiceProvider
+                .Setup(AUserService => AUserService
+                    .SetRefreshTokenCookie(
+                        It.IsAny<string>(), 
+                        It.IsAny<int>(), 
+                        It.IsAny<bool>(), 
+                        It.IsAny<string>()));
+
+            LMockedUserServiceProvider
+                .Setup(AUserService => AUserService
+                    .DeleteOutdatedRefreshTokens(
+                        It.IsAny<Guid>(),
+                        It.IsAny<CancellationToken>()));
+            
+            var LIdentityServer = new IdentityServer
+            {
+                Issuer = DataUtilityService.GetRandomString(),
+                Audience = DataUtilityService.GetRandomString(),
+                WebSecret = DataUtilityService.GetRandomString(),
+                RequireHttps = false,
+                WebTokenExpiresIn = 90,
+                RefreshTokenExpiresIn = 120
+            };
             
             // Act
             var LAuthenticateUserCommandHandler = new AuthenticateUserCommandHandler(
@@ -135,7 +125,7 @@ namespace TokanPages.Backend.Tests.Handlers.Users
                 LMockedJwtUtilityService.Object, 
                 LMockedDateTimeService.Object, 
                 LMockedUserServiceProvider.Object, 
-                LMockedIdentityServer.Object);
+                LIdentityServer);
             
             var LResult = await LAuthenticateUserCommandHandler.Handle(LAuthenticateUserCommand, CancellationToken.None);
 
@@ -147,183 +137,14 @@ namespace TokanPages.Backend.Tests.Handlers.Users
             LResult.LastName.Should().Be(LUser.LastName);
             LResult.ShortBio.Should().Be(LUser.ShortBio);
             LResult.Registered.Should().Be(LUser.Registered);
-            LResult.Jwt.Should().NotBeNullOrEmpty();
-            LResult.Jwt.Length.Should().BeGreaterThan(0);
+            LResult.UserToken.Should().NotBeNullOrEmpty();
+            LResult.UserToken.Length.Should().BeGreaterThan(0);
             
             var LUserRefreshTokens = await LDatabaseContext.UserRefreshTokens
                 .Where(AUserRefreshToken => AUserRefreshToken.UserId == LUser.Id)
                 .ToListAsync();
 
             LUserRefreshTokens.Should().NotHaveCount(0);
-
-            var LUserRefreshToken = LUserRefreshTokens.First();
-            LUserRefreshToken.UserId.Should().Be(LUser.Id);
-            LUserRefreshToken.Token.Should().NotBeEmpty();
-            LUserRefreshToken.Expires.Should().Be(LRefreshTokenExpires);
-            LUserRefreshToken.Created.Should().Be(LRefreshTokenCreated);
-            LUserRefreshToken.CreatedByIp.Should().NotBeEmpty();
-            LUserRefreshToken.Revoked.Should().BeNull();
-            LUserRefreshToken.RevokedByIp.Should().BeNull();
-            LUserRefreshToken.ReplacedByToken.Should().BeNull();
-            LUserRefreshToken.ReasonRevoked.Should().BeNull();
-        }
-
-        [Fact]
-        public async Task GivenOldRefreshTokens_WhenAuthenticateUser_ShouldSucceed()
-        {
-            // Arrange
-            var LEmailAddress = DataUtilityService.GetRandomEmail();
-            var LPlainTextPassword = DataUtilityService.GetRandomString(10);
-            var LCryptedPassword = DataUtilityService.GetRandomString(60);
-            var LGeneratedJwt = DataUtilityService.GetRandomString(255);
-            
-            var LUser = new Users
-            {
-                Id = Guid.NewGuid(),
-                EmailAddress = LEmailAddress,
-                UserAlias = DataUtilityService.GetRandomString(),
-                FirstName = DataUtilityService.GetRandomString(),
-                LastName = DataUtilityService.GetRandomString(),
-                IsActivated = true,
-                Registered = DateTimeService.Now,
-                LastUpdated = null,
-                LastLogged = null,
-                CryptedPassword = LCryptedPassword
-            };
-
-            var LRoles = new List<Roles>
-            {
-                new ()
-                {
-                    Id = Guid.NewGuid(),
-                    Name = nameof(Identity.Authorization.Roles.EverydayUser),
-                    Description = "User"
-                },
-                new ()
-                {
-                    Id = Guid.NewGuid(),
-                    Name = nameof(Identity.Authorization.Roles.ArticlePublisher),
-                    Description = "User can publish articles"
-                }
-            };
-
-            var LUserRoles = new List<UserRoles>
-            {
-                new ()
-                {
-                    Id = Guid.NewGuid(),
-                    UserId = LUser.Id,
-                    RoleId = LRoles[0].Id
-                },
-                new ()
-                {
-                    Id = Guid.NewGuid(),
-                    UserId = LUser.Id,
-                    RoleId = LRoles[1].Id
-                }
-            };
-
-            var LExistingRefreshTokens = new List<UserRefreshTokens>
-            {
-                new ()
-                {
-                    Id = Guid.NewGuid(),
-                    UserId = LUser.Id,
-                    Token = DataUtilityService.GetRandomString(),
-                    Expires = DateTimeService.Now.AddDays(-6),
-                    Created = DateTimeService.Now.AddDays(-7),
-                    CreatedByIp = DataUtilityService.GetRandomIpAddress().ToString()
-                },
-                new ()
-                {
-                    Id = Guid.NewGuid(),
-                    UserId = LUser.Id,
-                    Token = DataUtilityService.GetRandomString(),
-                    Expires = DateTimeService.Now.AddDays(-6),
-                    Created = DateTimeService.Now.AddDays(-5),
-                    CreatedByIp = DataUtilityService.GetRandomIpAddress().ToString()
-                },
-            };
-            
-            var LDatabaseContext = GetTestDatabaseContext();
-            await LDatabaseContext.Users.AddAsync(LUser);
-            await LDatabaseContext.Roles.AddRangeAsync(LRoles);
-            await LDatabaseContext.UserRoles.AddRangeAsync(LUserRoles);
-            await LDatabaseContext.UserRefreshTokens.AddRangeAsync(LExistingRefreshTokens);
-            await LDatabaseContext.SaveChangesAsync();
-
-            var LAuthenticateUserCommand = new AuthenticateUserCommand
-            {
-                EmailAddress = LEmailAddress,
-                Password = LPlainTextPassword
-            };
-
-            var LMockedCipheringService = new Mock<ICipheringService>();
-            LMockedCipheringService
-                .Setup(AService => AService
-                    .VerifyPassword(It.IsAny<string>(), It.IsAny<string>()))
-                .Returns(true);
-            
-            var LMockedJwtUtilityService = new Mock<IJwtUtilityService>();
-            LMockedJwtUtilityService
-                .Setup(AUtilityService => AUtilityService.GenerateJwt(
-                    It.IsAny<DateTime>(),
-                    It.IsAny<ClaimsIdentity>(),
-                    It.IsAny<string>(),
-                    It.IsAny<string>(),
-                    It.IsAny<string>()))
-                .Returns(LGeneratedJwt);
-
-            var LRefreshTokenExpires = DateTimeService.Now.AddDays(10);
-            var LRefreshTokenCreated = DateTimeService.Now;
-            var LGeneratedRefreshToken = new RefreshToken
-            {
-                Token = DataUtilityService.GetRandomString(),
-                Expires = LRefreshTokenExpires,
-                Created = LRefreshTokenCreated,
-                CreatedByIp = DataUtilityService.GetRandomIpAddress().ToString()
-            };
-            LMockedJwtUtilityService
-                .Setup(AUtilityService => AUtilityService
-                    .GenerateRefreshToken(It.IsAny<string>(), It.IsAny<int>()))
-                .Returns(LGeneratedRefreshToken);
-            
-            var LMockedDateTimeService = new Mock<IDateTimeService>();
-            var LRandomDateTime = DateTimeService.Now.AddMinutes(10);
-            LMockedDateTimeService
-                .SetupGet(ADateTimeService => ADateTimeService.Now)
-                .Returns(LRandomDateTime);
-            
-            var LMockedUserServiceProvider = new Mock<IUserServiceProvider>();
-            var LMockedIdentityServer = new Mock<IdentityServer>();
-            
-            // Act
-            var LAuthenticateUserCommandHandler = new AuthenticateUserCommandHandler(
-                LDatabaseContext, 
-                LMockedCipheringService.Object, 
-                LMockedJwtUtilityService.Object, 
-                LMockedDateTimeService.Object, 
-                LMockedUserServiceProvider.Object, 
-                LMockedIdentityServer.Object);
-            
-            var LResult = await LAuthenticateUserCommandHandler.Handle(LAuthenticateUserCommand, CancellationToken.None);
-
-            // Assert
-            LResult.UserId.Should().Be(LUser.Id);
-            LResult.AliasName.Should().Be(LUser.UserAlias);
-            LResult.AvatarName.Should().Be(LUser.AvatarName);
-            LResult.FirstName.Should().Be(LUser.FirstName);
-            LResult.LastName.Should().Be(LUser.LastName);
-            LResult.ShortBio.Should().Be(LUser.ShortBio);
-            LResult.Registered.Should().Be(LUser.Registered);
-            LResult.Jwt.Should().NotBeNullOrEmpty();
-            LResult.Jwt.Length.Should().BeGreaterThan(0);
-            
-            var LUserRefreshTokens = await LDatabaseContext.UserRefreshTokens
-                .Where(AUserRefreshToken => AUserRefreshToken.UserId == LUser.Id)
-                .ToListAsync();
-
-            LUserRefreshTokens.Should().HaveCount(1);
 
             var LUserRefreshToken = LUserRefreshTokens.First();
             LUserRefreshToken.UserId.Should().Be(LUser.Id);
@@ -359,42 +180,8 @@ namespace TokanPages.Backend.Tests.Handlers.Users
                 CryptedPassword = LCryptedPassword
             };
 
-            var LRoles = new List<Roles>
-            {
-                new()
-                {
-                    Id = Guid.NewGuid(),
-                    Name = nameof(Identity.Authorization.Roles.EverydayUser),
-                    Description = "User"
-                },
-                new()
-                {
-                    Id = Guid.NewGuid(),
-                    Name = nameof(Identity.Authorization.Roles.ArticlePublisher),
-                    Description = "User can publish articles"
-                }
-            };
-
-            var LUserRoles = new List<UserRoles>
-            {
-                new()
-                {
-                    Id = Guid.NewGuid(),
-                    UserId = LUser.Id,
-                    RoleId = LRoles[0].Id
-                },
-                new()
-                {
-                    Id = Guid.NewGuid(),
-                    UserId = LUser.Id,
-                    RoleId = LRoles[1].Id
-                }
-            };
-
             var LDatabaseContext = GetTestDatabaseContext();
             await LDatabaseContext.Users.AddAsync(LUser);
-            await LDatabaseContext.Roles.AddRangeAsync(LRoles);
-            await LDatabaseContext.UserRoles.AddRangeAsync(LUserRoles);
             await LDatabaseContext.SaveChangesAsync();
 
             var LAuthenticateUserCommand = new AuthenticateUserCommand
@@ -445,42 +232,8 @@ namespace TokanPages.Backend.Tests.Handlers.Users
                 CryptedPassword = LCryptedPassword
             };
 
-            var LRoles = new List<Roles>
-            {
-                new()
-                {
-                    Id = Guid.NewGuid(),
-                    Name = nameof(Identity.Authorization.Roles.EverydayUser),
-                    Description = "User"
-                },
-                new()
-                {
-                    Id = Guid.NewGuid(),
-                    Name = nameof(Identity.Authorization.Roles.ArticlePublisher),
-                    Description = "User can publish articles"
-                }
-            };
-
-            var LUserRoles = new List<UserRoles>
-            {
-                new()
-                {
-                    Id = Guid.NewGuid(),
-                    UserId = LUser.Id,
-                    RoleId = LRoles[0].Id
-                },
-                new()
-                {
-                    Id = Guid.NewGuid(),
-                    UserId = LUser.Id,
-                    RoleId = LRoles[1].Id
-                }
-            };
-
             var LDatabaseContext = GetTestDatabaseContext();
             await LDatabaseContext.Users.AddAsync(LUser);
-            await LDatabaseContext.Roles.AddRangeAsync(LRoles);
-            await LDatabaseContext.UserRoles.AddRangeAsync(LUserRoles);
             await LDatabaseContext.SaveChangesAsync();
 
             var LAuthenticateUserCommand = new AuthenticateUserCommand
