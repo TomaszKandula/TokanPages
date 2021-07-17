be355bb2219be5636a33ffce9acc7b1827aa1c1b
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2021-07-17 14:58:22 +0200

    refactor: move common methods from authenticate and re-authenticate handlers

--- TokanPages.Backend/TokanPages.Backend.Cqrs/Services/UserServiceProvider/IUserServiceProvider.cs
+++ TokanPages.Backend/TokanPages.Backend.Cqrs/Services/UserServiceProvider/IUserServiceProvider.cs
@@ -1,15 +1,20 @@
 ﻿namespace TokanPages.Backend.Cqrs.Services.UserServiceProvider
 {
     using System;
+    using System.Threading;
     using System.Threading.Tasks;
     using System.Collections.Generic;
+    using Shared;
+    using Domain.Entities;
     using Shared.Dto.Users;
 
     public interface IUserServiceProvider
     {
         string GetRequestIpAddress();
 
-        DateTimeOffset? SetRefreshTokenCookie(string ARefreshToken, int AExpiresIn, bool AIsHttpOnly = true);
+        string GetRefreshTokenCookie(string ACookieName);
+        
+        void SetRefreshTokenCookie(string ARefreshToken, int AExpiresIn, bool AIsHttpOnly = true, string ACookieName = Constants.CookieNames.REFRESH_TOKEN);
 
         Task<Guid?> GetUserId();
         
@@ -22,5 +27,21 @@
         Task<bool?> HasRoleAssigned(string AUserRoleName);
 
         Task<bool?> HasPermissionAssigned(string AUserPermissionName);
+
+        Task<string> GenerateUserToken(Users AUsers, DateTime ATokenExpires, CancellationToken ACancellationToken);
+
+        Task DeleteOutdatedRefreshTokens(Guid AUserId, CancellationToken ACancellationToken);
+
+        UserRefreshTokens ReplaceRefreshToken(Guid AUserId, UserRefreshTokens ASavedUserRefreshTokens, string ARequesterIpAddress);
+
+        void RevokeDescendantRefreshTokens(IEnumerable<UserRefreshTokens> AUserRefreshTokens, UserRefreshTokens ASavedUserRefreshTokens, string ARequesterIpAddress, string AReason);
+
+        void RevokeRefreshToken(UserRefreshTokens AUserRefreshTokens, string ARequesterIpAddress, string AReason = null, string AReplacedByToken = null);
+
+        bool IsRefreshTokenExpired(UserRefreshTokens AUserRefreshTokens);
+
+        bool IsRefreshTokenRevoked(UserRefreshTokens AUserRefreshTokens);
+
+        bool IsRefreshTokenActive(UserRefreshTokens AUserRefreshTokens);
     }
 }
