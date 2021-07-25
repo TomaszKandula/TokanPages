72332a28ce6fb3139b78211d782d903859c4bd0d
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2021-07-26 00:55:26 +0200

    refactor: extend DTO model

--- TokanPages.ClientApp/src/Api/Models/Components/navigationContentDto.ts
+++ TokanPages.ClientApp/src/Api/Models/Components/navigationContentDto.ts
@@ -3,5 +3,26 @@ export interface INavigationContentDto
     content: 
     {
         logo: string;
+        menu:
+        {
+            image: string,
+            items: IItem[]
+        }
     };
 }
+
+interface IFields
+{
+    id: string;
+    name: string,
+    link: string,
+    icon: string,
+    enabled: boolean
+}
+
+interface IItem extends IFields
+{
+    subitems: ISubitem[]
+}
+
+interface ISubitem extends IFields { }
