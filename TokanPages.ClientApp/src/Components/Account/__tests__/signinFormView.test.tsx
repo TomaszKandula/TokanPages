7b47ec4985f00fc10d8558584c245b01a5b0e681
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2021-08-13 20:45:21 +0200

    test: update unit test and snapshot for signin component

--- TokanPages.ClientApp/src/Components/Account/__tests__/signinFormView.test.tsx
+++ TokanPages.ClientApp/src/Components/Account/__tests__/signinFormView.test.tsx
@@ -1,14 +1,23 @@
 import "../../../setupTests";
 import React from "react";
+import { useDispatch } from "react-redux";
+import { Dispatch } from "redux";
 import { shallow } from "enzyme";
 import SigninForm from "../signinForm";
 import { combinedDefaults } from "../../../Redux/combinedDefaults";
 
+const jestFunction = jest.fn();
+const mockedDispatch = useDispatch as jest.Mock<Dispatch>;
+mockedDispatch.mockImplementation(() => jestFunction);
+
 describe("Test account component: SigninForm.", () => 
 {
+    const wrapper = shallow(<div><SigninForm content={combinedDefaults.getSigninFormContent.content} isLoading={false}/></div>);
+    beforeEach(() => wrapper.find("SigninForm").dive());
+    
     it("Renders correctly '<SigninForm />' when content is loaded.", () => 
     {
-        const tree = shallow(<SigninForm content={combinedDefaults.getSigninFormContent.content} isLoading={false} />);
-        expect(tree).toMatchSnapshot();
+        expect(mockedDispatch).toBeCalledTimes(1);
+        expect(wrapper).toMatchSnapshot();
     });
 });
