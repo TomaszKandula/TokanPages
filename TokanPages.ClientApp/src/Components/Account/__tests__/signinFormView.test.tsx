52554af1b3d1e83f07fbee0261747f7d9bd9b95b
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2021-08-14 18:05:24 +0200

    test: add new mocks, update snapshot

--- TokanPages.ClientApp/src/Components/Account/__tests__/signinFormView.test.tsx
+++ TokanPages.ClientApp/src/Components/Account/__tests__/signinFormView.test.tsx
@@ -1,23 +1,44 @@
 import "../../../setupTests";
 import React from "react";
-import { useDispatch } from "react-redux";
-import { Dispatch } from "redux";
+import * as Redux from "react-redux";
+import * as Router from "react-router";
 import { shallow } from "enzyme";
 import SigninForm from "../signinForm";
 import { combinedDefaults } from "../../../Redux/combinedDefaults";
 
-const jestFunction = jest.fn();
-const mockedDispatch = useDispatch as jest.Mock<Dispatch>;
-mockedDispatch.mockImplementation(() => jestFunction);
+jest.mock("react-router", () => 
+({
+    ...jest.requireActual("react-router") as typeof Router,
+    useHistory: () => (jest.fn())
+}));
 
 describe("Test account component: SigninForm.", () => 
 {
-    const wrapper = shallow(<div><SigninForm content={combinedDefaults.getSigninFormContent.content} isLoading={false}/></div>);
-    beforeEach(() => wrapper.find("SigninForm").dive());
+    const testContent = 
+    {
+        caption: "Sign in",
+        button: "Sign in",
+        link1: "Don't have an account?",
+        link2: "Forgot password?"
+    };
+
+    const useDispatchMock = jest.spyOn(Redux, "useDispatch");
+    const useSelectorMock = jest.spyOn(Redux, "useSelector");
+    const wrapper = shallow(<div><SigninForm content={testContent} isLoading={false}/></div>);
+
+    beforeEach(() => 
+    {
+        useSelectorMock.mockClear();
+        useDispatchMock.mockClear();
+        wrapper.find("SigninForm").dive();
+    });
     
     it("Renders correctly '<SigninForm />' when content is loaded.", () => 
     {
-        expect(mockedDispatch).toBeCalledTimes(1);
+        useDispatchMock.mockReturnValue(jest.fn());
+        useSelectorMock.mockReturnValue(combinedDefaults);
+    
+        expect(useDispatchMock).toBeCalledTimes(1);
         expect(wrapper).toMatchSnapshot();
     });
 });
