fc534068e62d097c4161041eccee1a8cd0841cfe
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2021-08-16 14:26:49 +0200

    refactor: add sing-out logic and view

---
+++ TokanPages.ClientApp/src/Components/Account/userSignout.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+const UserSignout = () => 
+{
+
+
+    return (<></>);
+    
+}
+
+export default UserSignout;
---
+++ TokanPages.ClientApp/src/Components/Account/userSignoutView.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+const UserSignoutView = () => 
+{
+
+
+    return (<></>);
+    
+}
+
+export default UserSignoutView;
