57056ce6d0f7435fd57413ea5d1feaee44891bd4
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2021-08-20 12:13:32 +0200

    test: update test for changed view

--- TokanPages.ClientApp/src/Components/Account/__tests__/userSignupView.test.tsx
+++ TokanPages.ClientApp/src/Components/Account/__tests__/userSignupView.test.tsx
@@ -1,14 +1,14 @@
 import "../../../setupTests";
 import React from "react";
 import { shallow } from "enzyme";
-import UserSignupView from "../userSignupView";
+import UserSignup from "../userSignup";
 import { combinedDefaults } from "../../../Redux/combinedDefaults";
 
 describe("Test account component: SignupForm.", () => 
 {
-    it("Renders correctly '<UserSignupView />' when content is loaded.", () => 
+    it("Renders correctly '<UserSignup />' when content is loaded.", () => 
     {
-        const tree = shallow(<UserSignupView content={combinedDefaults.getUserSignupContent.content} isLoading={false} />);
+        const tree = shallow(<UserSignup content={combinedDefaults.getUserSignupContent.content} isLoading={false} />);
         expect(tree).toMatchSnapshot();
     });
 });
