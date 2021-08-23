756b66ac5f123af5e081808e1e86f46cd26777c1
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2021-08-23 20:59:28 +0200

    refactor: use arrow function

--- TokanPages.WebApi.Tests/TestBase.cs
+++ TokanPages.WebApi.Tests/TestBase.cs
@@ -19,31 +19,25 @@ namespace TokanPages.WebApi.Tests
             JwtUtilityService = new JwtUtilityService();
         }
 
-        protected ClaimsIdentity GetValidClaimsIdentity()
+        protected ClaimsIdentity GetValidClaimsIdentity() => new (new[]
         {
-            return new (new[]
-            {
-                new Claim(ClaimTypes.Name, DataUtilityService.GetRandomString()),
-                new Claim(ClaimTypes.Role, nameof(Roles.EverydayUser)),
-                new Claim(ClaimTypes.Role, nameof(Roles.GodOfAsgard)),
-                new Claim(ClaimTypes.NameIdentifier, User1.FId.ToString()),
-                new Claim(ClaimTypes.GivenName, User1.FIRST_NAME),
-                new Claim(ClaimTypes.Surname, User1.LAST_NAME),
-                new Claim(ClaimTypes.Email, User1.EMAIL_ADDRESS)
-            });
-        }
-        
-        protected ClaimsIdentity GetInvalidClaimsIdentity()
+            new Claim(ClaimTypes.Name, DataUtilityService.GetRandomString()),
+            new Claim(ClaimTypes.Role, nameof(Roles.EverydayUser)),
+            new Claim(ClaimTypes.Role, nameof(Roles.GodOfAsgard)),
+            new Claim(ClaimTypes.NameIdentifier, User1.FId.ToString()),
+            new Claim(ClaimTypes.GivenName, User1.FIRST_NAME),
+            new Claim(ClaimTypes.Surname, User1.LAST_NAME),
+            new Claim(ClaimTypes.Email, User1.EMAIL_ADDRESS)
+        });
+
+        protected ClaimsIdentity GetInvalidClaimsIdentity() => new (new[]
         {
-            return new (new[]
-            {
-                new Claim(ClaimTypes.Name, DataUtilityService.GetRandomString()),
-                new Claim(ClaimTypes.Role, DataUtilityService.GetRandomString()),
-                new Claim(ClaimTypes.NameIdentifier, Guid.NewGuid().ToString()),
-                new Claim(ClaimTypes.GivenName, DataUtilityService.GetRandomString()),
-                new Claim(ClaimTypes.Surname, DataUtilityService.GetRandomString()),
-                new Claim(ClaimTypes.Email, DataUtilityService.GetRandomString())
-            });
-        }
+            new Claim(ClaimTypes.Name, DataUtilityService.GetRandomString()),
+            new Claim(ClaimTypes.Role, DataUtilityService.GetRandomString()),
+            new Claim(ClaimTypes.NameIdentifier, Guid.NewGuid().ToString()),
+            new Claim(ClaimTypes.GivenName, DataUtilityService.GetRandomString()),
+            new Claim(ClaimTypes.Surname, DataUtilityService.GetRandomString()),
+            new Claim(ClaimTypes.Email, DataUtilityService.GetRandomString())
+        });
     }
 }
