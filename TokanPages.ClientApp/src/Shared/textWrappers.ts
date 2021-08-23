cdbc2d75a9041a7e916a430bd9bf35f7280c909f
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2021-08-23 19:55:34 +0200

    refactor: simplify code

--- TokanPages.ClientApp/src/Shared/textWrappers.ts
+++ TokanPages.ClientApp/src/Shared/textWrappers.ts
@@ -1,77 +1,20 @@
 import { ConvertPropsToFields, HtmlRenderLines } from "./helpers";
-import { 
-    MESSAGE_OUT_ERROR, 
-    MESSAGE_OUT_SUCCESS, 
-    MESSAGE_OUT_WARNING, 
-    NEWSLETTER_ERROR, 
-    NEWSLETTER_SUCCESS,
-    NEWSLETTER_WARNING,
-    SUBSCRIBER_DEL_ERROR,
-    UNEXPECTED_STATUS,
-    UPDATE_ARTICLE_ERROR,
-    UPDATE_ARTICLE_SUCCESS,
-    UPDATE_ARTICLE_WARNING
-} from "../Shared/constants";
+import { UNEXPECTED_STATUS } from "../Shared/constants";
 
-// EMAIL MESSAGES
-
-const MessageOutSuccess = (): string =>
+const ProduceSuccessText = (template: string = ""): string =>
 {
-    return MESSAGE_OUT_SUCCESS;
+    return template;
 }
 
-const MessageOutWarning = (object: any): string =>
+const ProduceWarningText = (object: any, template: string): string =>
 {
-    return MESSAGE_OUT_WARNING.replace("{LIST}", HtmlRenderLines(ConvertPropsToFields(object), "li"));
+    return template.replace("{LIST}", HtmlRenderLines(ConvertPropsToFields(object), "li"));
 }
 
-const MessageOutError = (error: string): string =>
+const ProduceErrorText = (error: string, template: string): string =>
 {
-    return MESSAGE_OUT_ERROR.replace("{ERROR}", error);
+    return template.replace("{ERROR}", error);
 }
-
-// NEWSLETTERS
-
-const NewsletterSuccess = (): string =>
-{
-    return NEWSLETTER_SUCCESS;
-}
-
-const NewsletterWarning = (object: any): string =>
-{
-    return NEWSLETTER_WARNING.replace("{LIST}", HtmlRenderLines(ConvertPropsToFields(object), "li"));
-}
-
-const NewsletterError = (error: string): string =>
-{
-    return NEWSLETTER_ERROR.replace("{ERROR}", error);
-}
-
-// SUBSCRIBERS
-
-const SubscriberOnDeleteError = (error: string): string =>
-{
-    return SUBSCRIBER_DEL_ERROR.replace("{ERROR}", error);
-}
-
-// ARTICLES
-
-const UpdateArticleSuccess = (): string =>
-{
-    return UPDATE_ARTICLE_SUCCESS;
-}
-
-const UpdateArticleWarning = (object: any): string =>
-{
-    return UPDATE_ARTICLE_WARNING.replace("{LIST}", HtmlRenderLines(ConvertPropsToFields(object), "li"));
-}
-
-const UpdateArticleError = (error: string): string =>
-{
-    return UPDATE_ARTICLE_ERROR.replace("{ERROR}", error);
-}
-
-// OTHER
 
 const UnexpectedStatusCode = (statusCode: number): string =>
 {
@@ -80,15 +23,8 @@ const UnexpectedStatusCode = (statusCode: number): string =>
 
 export 
 {
-    MessageOutSuccess,
-    MessageOutWarning,
-    MessageOutError,
-    NewsletterSuccess,
-    NewsletterWarning,
-    NewsletterError,
-    SubscriberOnDeleteError,
-    UpdateArticleSuccess,
-    UpdateArticleWarning,
-    UpdateArticleError,
+    ProduceSuccessText,
+    ProduceWarningText,
+    ProduceErrorText,
     UnexpectedStatusCode
 }
