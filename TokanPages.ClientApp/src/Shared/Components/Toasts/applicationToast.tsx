afa49b9aeb1adb8fd5dfd097b3bf02d78fca3ca3
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2021-08-29 12:10:21 +0200

    refactor: improve application toast

--- TokanPages.ClientApp/src/Shared/Components/Toasts/applicationToast.tsx
+++ TokanPages.ClientApp/src/Shared/Components/Toasts/applicationToast.tsx
@@ -4,27 +4,25 @@ import { Slide, SlideProps } from "@material-ui/core";
 import { IApplicationState } from "../../../Redux/applicationState";
 import { ActionCreators } from "../../../Redux/Actions/raiseErrorAction";
 import { RECEIVED_ERROR_MESSAGE } from "../../constants";
-import { DialogType } from "../../../Shared/enums";
+import { DialogType } from "../../enums";
 import ApplicationToastView from "./applicationToastView";
+import Validate from "validate.js";
 
-interface IDefaultToastState 
+interface IToastState 
 {
     isOpen: boolean;
     errorMessage: string;
 }
 
-const DefaultToastState: IDefaultToastState = 
+const ToastState: IToastState = 
 {
     isOpen: false, 
     errorMessage: ""    
 }
 
-const TransitionLeft = (props: Omit<SlideProps, "direction">) =>
-{
-    return <Slide {...props} direction="left" />;
-}
+const TransitionLeft = (props: Omit<SlideProps, "direction">) => <Slide {...props} direction="left" />;
 
-const ApplicationErrorToast = (): JSX.Element => 
+const ApplicationToast = (): JSX.Element => 
 {
     const vertical = "top";
     const horizontal = "right";
@@ -32,15 +30,15 @@ const ApplicationErrorToast = (): JSX.Element =>
     const autoHideDuration: number = 15000;
 
     const dispatch = useDispatch();
-    const [toastState, setToastState] = React.useState(DefaultToastState);   
+    const [toastState, setToastState] = React.useState(ToastState);   
     const raiseErrorState = useSelector((state: IApplicationState) => state.raiseError);
     
     const clearError = React.useCallback(() => 
     { 
-        if (!toastState.isOpen && toastState.errorMessage !== "")
+        if (!toastState.isOpen && Validate.isEmpty(toastState.errorMessage))
         {
             dispatch(ActionCreators.clearError());
-            setToastState(DefaultToastState);
+            setToastState(ToastState);
         }
 
     }, [ dispatch, toastState ]);
@@ -48,7 +46,6 @@ const ApplicationErrorToast = (): JSX.Element =>
     const raiseError = React.useCallback(() => 
     {
         if (raiseErrorState?.dialogType !== DialogType.toast) return;
-        
         if (raiseErrorState?.defaultErrorMessage === RECEIVED_ERROR_MESSAGE)
         {
             setToastState(
@@ -83,4 +80,4 @@ const ApplicationErrorToast = (): JSX.Element =>
     }}/>);
 }
 
-export default ApplicationErrorToast;
+export default ApplicationToast;
