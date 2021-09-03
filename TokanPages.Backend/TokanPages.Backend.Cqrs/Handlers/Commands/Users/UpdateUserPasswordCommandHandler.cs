3495d5e903bd51537d2ea0a1bcedfb3cf08dc980
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2021-09-03 11:14:52 +0200

    refactor: use ResetId to allow not logged user to setup new password after reset has been done

--- TokanPages.Backend/TokanPages.Backend.Cqrs/Handlers/Commands/Users/UpdateUserPasswordCommandHandler.cs
+++ TokanPages.Backend/TokanPages.Backend.Cqrs/Handlers/Commands/Users/UpdateUserPasswordCommandHandler.cs
@@ -4,7 +4,9 @@ namespace TokanPages.Backend.Cqrs.Handlers.Commands.Users
     using System.Threading;
     using System.Threading.Tasks;
     using Microsoft.EntityFrameworkCore;
+    using Services.UserServiceProvider;
     using Services.CipheringService;
+    using Identity.Authorization;
     using Shared.Resources;
     using Core.Exceptions;
     using Core.Logger;
@@ -16,13 +18,16 @@ namespace TokanPages.Backend.Cqrs.Handlers.Commands.Users
     {
         private readonly DatabaseContext FDatabaseContext;
 
+        private readonly IUserServiceProvider FUserServiceProvider;
+
         private readonly ICipheringService FCipheringService;
         
         private readonly ILogger FLogger;
         
-        public UpdateUserPasswordCommandHandler(DatabaseContext ADatabaseContext, ICipheringService ACipheringService, ILogger ALogger)
+        public UpdateUserPasswordCommandHandler(DatabaseContext ADatabaseContext, IUserServiceProvider AUserServiceProvider, ICipheringService ACipheringService, ILogger ALogger)
         {
             FDatabaseContext = ADatabaseContext;
+            FUserServiceProvider = AUserServiceProvider;
             FCipheringService = ACipheringService;
             FLogger = ALogger;
         }
@@ -35,12 +40,26 @@ namespace TokanPages.Backend.Cqrs.Handlers.Commands.Users
 
             if (!LUsers.Any())
                 throw new BusinessException(nameof(ErrorCodes.USER_DOES_NOT_EXISTS), ErrorCodes.USER_DOES_NOT_EXISTS);
-            
+
+            if (ARequest.ResetId == null)
+            {
+                var LHasRoleEverydayUser = await FUserServiceProvider.HasRoleAssigned(Roles.EverydayUser.ToString()) ?? false;
+                var LHasRoleGodOfAsgard = await FUserServiceProvider.HasRoleAssigned(Roles.GodOfAsgard.ToString()) ?? false;
+                
+                if (!LHasRoleEverydayUser || !LHasRoleGodOfAsgard)
+                    throw new BusinessException(nameof(ErrorCodes.ACCESS_DENIED), ErrorCodes.ACCESS_DENIED);
+            }
+
             var LCurrentUser = LUsers.First();
+
+            if (LCurrentUser.ResetId == ARequest.ResetId)
+                throw new BusinessException(nameof(ErrorCodes.INVALID_RESET_ID), ErrorCodes.INVALID_RESET_ID);
+
             LCurrentUser.ResetId = null;
             LCurrentUser.CryptedPassword = FCipheringService.GetHashedPassword(ARequest.NewPassword, FCipheringService.GenerateSalt(Constants.CIPHER_LOG_ROUNDS));
             await FDatabaseContext.SaveChangesAsync(ACancellationToken);
 
+            FLogger.LogInformation($"User password has been reset successfully (UserId: {ARequest.Id}).");
             return await Task.FromResult(Unit.Value);
         }
     }
