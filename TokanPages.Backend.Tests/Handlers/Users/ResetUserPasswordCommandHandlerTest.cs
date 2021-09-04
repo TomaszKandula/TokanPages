110fa2f5e07ecc45dfbb5428402aa1e59f301016
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2021-09-04 13:29:12 +0200

    test: add new handler test for reset password

---
+++ TokanPages.Backend.Tests/Handlers/Users/ResetUserPasswordCommandHandlerTest.cs
@@ -0,0 +1,104 @@
+namespace TokanPages.Backend.Tests.Handlers.Users
+{
+    using Moq;
+    using Xunit;
+    using FluentAssertions;
+    using System;
+    using System.Net;
+    using System.Net.Http;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using TokanPages.Backend.Cqrs.Handlers.Commands.Users;
+    using Shared.Services.TemplateService;
+    using Storage.Models;
+    using Shared.Models;
+    using Moq.Protected;
+    using Core.Logger;
+    using SmtpClient;
+
+    public class ResetUserPasswordCommandHandlerTest : TestBase
+    {
+        [Fact]
+        public async Task GivenValidEmailAddress_WhenResetUserPassword_ShouldFinishSuccessful()
+        {
+            // Arrange
+            var LUser = new TokanPages.Backend.Domain.Entities.Users
+            {
+                EmailAddress = DataUtilityService.GetRandomEmail(),
+                UserAlias = DataUtilityService.GetRandomString(),
+                FirstName = DataUtilityService.GetRandomString(),
+                LastName = DataUtilityService.GetRandomString(),
+                IsActivated = true,
+                Registered = DateTimeService.Now,
+                LastUpdated = null,
+                LastLogged = null,
+                CryptedPassword = DataUtilityService.GetRandomString(),
+                ResetId = null
+            };
+
+            var LDatabaseContext = GetTestDatabaseContext();
+            await LDatabaseContext.Users.AddAsync(LUser);
+            await LDatabaseContext.SaveChangesAsync();
+
+            var LResetUserPasswordCommand = new ResetUserPasswordCommand
+            {
+                EmailAddress = LUser.EmailAddress
+            };
+
+            var LMockedLogger = new Mock<ILogger>();
+            var LMockedSmtpClientService = new Mock<ISmtpClientService>();
+            var LMockedTemplateService = new Mock<ITemplateService>();
+            var LMockedAzureStorage = new Mock<AzureStorage>();
+            var LMockedApplicationPaths = new Mock<ApplicationPaths>();
+
+            var LMockedHttpMessageHandler = new Mock<HttpMessageHandler>();
+
+            var LSendActionResult = new ActionResult { IsSucceeded = true };
+            LMockedSmtpClientService
+                .Setup(ASmtpClient => ASmtpClient.Send(CancellationToken.None))
+                .Returns(Task.FromResult(LSendActionResult));
+
+            LMockedHttpMessageHandler
+                .Protected()
+                .Setup<Task<HttpResponseMessage>>(
+                    "SendAsync", 
+                    ItExpr.IsAny<HttpRequestMessage>(), 
+                    ItExpr.IsAny<CancellationToken>())
+                .ReturnsAsync(new HttpResponseMessage
+                {
+                    StatusCode = HttpStatusCode.OK
+                });
+
+            var LHttpClient = new HttpClient(LMockedHttpMessageHandler.Object)
+            {
+                BaseAddress = new Uri("http://localhost:5000/")
+            };
+
+            // Act
+            var LUpdateUserCommandHandler = new ResetUserPasswordCommandHandler(
+                LDatabaseContext, 
+                LMockedLogger.Object,
+                LHttpClient,
+                LMockedSmtpClientService.Object,
+                LMockedTemplateService.Object,
+                LMockedAzureStorage.Object,
+                LMockedApplicationPaths.Object
+            );
+            await LUpdateUserCommandHandler.Handle(LResetUserPasswordCommand, CancellationToken.None);
+
+            // Assert
+            var LUserEntity = await LDatabaseContext.Users.FindAsync(LUser.Id);
+
+            LUserEntity.Should().NotBeNull();
+            LUserEntity.EmailAddress.Should().Be(LUser.EmailAddress);
+            LUserEntity.UserAlias.Should().Be(LUser.UserAlias);
+            LUserEntity.FirstName.Should().Be(LUser.FirstName);
+            LUserEntity.LastName.Should().Be(LUser.LastName);
+            LUserEntity.IsActivated.Should().BeTrue();
+            LUserEntity.LastUpdated.Should().BeNull();
+            LUserEntity.LastLogged.Should().BeNull();
+            LUserEntity.CryptedPassword.Should().BeEmpty();
+            LUserEntity.ResetId.Should().NotBeNull();
+        }
+    }
+}
