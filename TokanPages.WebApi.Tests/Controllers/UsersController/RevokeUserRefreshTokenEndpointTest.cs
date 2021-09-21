0b5bb9ed93ff478b3ec5a015846555c7d8646165
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2021-09-21 14:09:26 +0200

    test: add new test for revoke token endpoint

---
+++ TokanPages.WebApi.Tests/Controllers/UsersController/RevokeUserRefreshTokenEndpointTest.cs
@@ -0,0 +1,63 @@
+namespace TokanPages.WebApi.Tests.Controllers.UsersController
+{
+    using Xunit;
+    using System.Net;
+    using System.Net.Http;
+    using System.Threading.Tasks;
+    using System.Net.Http.Headers;
+    using Newtonsoft.Json;
+    using Backend.Shared.Dto.Users;
+
+    public partial class UsersControllerTest
+    {
+        [Fact]
+        public async Task GivenAnyRefreshToken_WhenRevokeUserRefreshTokenAsNotAdmin_ShouldReturnUnauthorized()
+        {
+            // Arrange
+            var LRequest = $"{API_BASE_URL}/RevokeUserRefreshToken/";
+            var LNewRequest = new HttpRequestMessage(HttpMethod.Post, LRequest);
+
+            var LPayLoad = new RevokeUserRefreshTokenDto
+            {
+                RefreshToken = DataUtilityService.GetRandomString(100)
+            };
+
+            var LHttpClient = FWebAppFactory.CreateClient();
+            LNewRequest.Content = new StringContent(JsonConvert.SerializeObject(LPayLoad), System.Text.Encoding.Default, "application/json");
+            
+            // Act
+            var LResponse = await LHttpClient.SendAsync(LNewRequest);
+
+            // Assert
+            await EnsureStatusCode(LResponse, HttpStatusCode.Unauthorized);
+        }
+        
+        [Fact]
+        public async Task GivenUnknownRefreshToken_WhenRevokeUserRefreshTokenAsAdmin_ShouldReturnBadRequest()
+        {
+            // Arrange
+            var LRequest = $"{API_BASE_URL}/RevokeUserRefreshToken/";
+            var LNewRequest = new HttpRequestMessage(HttpMethod.Post, LRequest);
+
+            var LPayLoad = new RevokeUserRefreshTokenDto
+            {
+                RefreshToken = DataUtilityService.GetRandomString(100)
+            };
+
+            var LHttpClient = FWebAppFactory.CreateClient();
+            LNewRequest.Content = new StringContent(JsonConvert.SerializeObject(LPayLoad), System.Text.Encoding.Default, "application/json");
+
+            var LTokenExpires = DateTimeService.Now.AddDays(30);
+            var LJwt = JwtUtilityService.GenerateJwt(
+                LTokenExpires, GetValidClaimsIdentity(), FWebAppFactory.WebSecret, FWebAppFactory.Issuer, FWebAppFactory.Audience);
+            
+            LNewRequest.Headers.Authorization = new AuthenticationHeaderValue("Bearer", LJwt);
+            
+            // Act
+            var LResponse = await LHttpClient.SendAsync(LNewRequest);
+
+            // Assert
+            await EnsureStatusCode(LResponse, HttpStatusCode.BadRequest);
+        }
+    }
+}
