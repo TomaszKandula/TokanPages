56ba933eb2038aedb2eca3f00c6ad5fa96174e0b
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2021-10-02 19:02:07 +0200

    feat: add article text to 'get article' response

--- TokanPages.Backend/TokanPages.Backend.Cqrs/Handlers/Queries/Articles/GetArticleQueryHandler.cs
+++ TokanPages.Backend/TokanPages.Backend.Cqrs/Handlers/Queries/Articles/GetArticleQueryHandler.cs
@@ -1,15 +1,21 @@
 namespace TokanPages.Backend.Cqrs.Handlers.Queries.Articles
 {
+    using System.Net;
     using System.Linq;
+    using System.Net.Http;
     using System.Threading;
     using System.Threading.Tasks;
+    using System.Collections.Generic;
     using Microsoft.EntityFrameworkCore;
     using Database;
+    using Storage.Models;
     using Core.Exceptions;
     using Core.Extensions;
     using Shared.Resources;
     using Shared.Dto.Users;
+    using Shared.Dto.Content.Common;
     using Services.UserServiceProvider;
+    using Core.Utilities.JsonSerializer;
 
     public class GetArticleQueryHandler : TemplateHandler<GetArticleQuery, GetArticleQueryResult>
     {
@@ -17,16 +23,30 @@ namespace TokanPages.Backend.Cqrs.Handlers.Queries.Articles
         
         private readonly IUserServiceProvider FUserServiceProvider;
 
-        public GetArticleQueryHandler(DatabaseContext ADatabaseContext, IUserServiceProvider AUserServiceProvider)
+        private readonly IJsonSerializer FJsonSerializer; 
+        
+        private readonly AzureStorage FAzureStorage;
+        
+        private readonly HttpClient FHttpClient;
+        
+        public GetArticleQueryHandler(DatabaseContext ADatabaseContext, IUserServiceProvider AUserServiceProvider, 
+            IJsonSerializer AJsonSerializer, AzureStorage AAzureStorage, HttpClient AHttpClient)
         {
             FDatabaseContext = ADatabaseContext;
             FUserServiceProvider = AUserServiceProvider;
+            FJsonSerializer = AJsonSerializer;
+            FAzureStorage = AAzureStorage;
+            FHttpClient = AHttpClient;
         }
 
         public override async Task<GetArticleQueryResult> Handle(GetArticleQuery ARequest, CancellationToken ACancellationToken)
         {
             var LUserId = await FUserServiceProvider.GetUserId();
             var LIsAnonymousUser = LUserId == null;
+
+            var LTextRequestUrl = $"{FAzureStorage.BaseUrl}/content/articles/{ARequest.Id}/text.json";
+            var LTextAsString = await GetJsonData(LTextRequestUrl, ACancellationToken);
+            var LTextAsObject = FJsonSerializer.Deserialize<List<Section>>(LTextAsString);
 
             var LGetArticleLikes = await FDatabaseContext.ArticleLikes
                 .Where(ALikes => ALikes.ArticleId == ARequest.Id)
@@ -36,10 +56,7 @@ namespace TokanPages.Backend.Cqrs.Handlers.Queries.Articles
                 .Select(ALikes => ALikes.LikeCount)
                 .ToListAsync(ACancellationToken);
 
-            var LArticleLikes = !LGetArticleLikes.Any() 
-                ? 0 
-                : LGetArticleLikes.FirstOrDefault();
-
+            var LArticleLikes = !LGetArticleLikes.Any() ? 0 : LGetArticleLikes.FirstOrDefault();
             var LCurrentArticle = await FDatabaseContext.Articles
                 .AsNoTracking()
                 .Include(ALikes => ALikes.ArticleLikes)
@@ -67,7 +84,8 @@ namespace TokanPages.Backend.Cqrs.Handlers.Queries.Articles
                         LastName = AFields.User.LastName,
                         ShortBio = AFields.User.ShortBio,
                         Registered = AFields.User.Registered
-                    }
+                    },
+                    Text = LTextAsObject
                 })
                 .ToListAsync(ACancellationToken);
 
@@ -75,6 +93,16 @@ namespace TokanPages.Backend.Cqrs.Handlers.Queries.Articles
                 throw new BusinessException(nameof(ErrorCodes.ARTICLE_DOES_NOT_EXISTS), ErrorCodes.ARTICLE_DOES_NOT_EXISTS);
 
             return LCurrentArticle.First();
+        }
+
+        private async Task<string> GetJsonData(string AUrl, CancellationToken ACancellationToken)
+        {
+            var LResponseContent = await FHttpClient.GetAsync(AUrl, ACancellationToken);
+
+            if (LResponseContent.StatusCode != HttpStatusCode.OK)
+                throw new BusinessException(nameof(ErrorCodes.ERROR_UNEXPECTED), ErrorCodes.ERROR_UNEXPECTED);
+                    
+            return await LResponseContent.Content.ReadAsStringAsync(ACancellationToken);
         }
     }
 }
