7ea54eb4d19f18782984698cc4feede937375a71
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2021-10-02 19:00:52 +0200

    refactor: remove double request to API and storage separately, make only one call to API

--- TokanPages.ClientApp/src/Redux/Actions/Articles/selectArticleAction.ts
+++ TokanPages.ClientApp/src/Redux/Actions/Articles/selectArticleAction.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { AppThunkAction } from "../../applicationState";
 import { IArticleItem } from "../../../Shared/Components/ContentRender/Models/articleItemModel";
-import { API_QUERY_GET_ARTICLE, ARTICLE_URL, NULL_RESPONSE_ERROR } from "../../../Shared/constants";
+import { API_QUERY_GET_ARTICLE, NULL_RESPONSE_ERROR } from "../../../Shared/constants";
 import { TErrorActions } from "./../raiseErrorAction";
 import { UnexpectedStatusCode } from "../../../Shared/textWrappers";
 import { RaiseError } from "../../../Shared/helpers";
@@ -25,50 +25,24 @@ export const ActionCreators =
     {
         dispatch({ type: REQUEST_ARTICLE });
 
-        const artcileDetailsUrl = API_QUERY_GET_ARTICLE.replace("{id}", id);
-        const articleTextUrl = ARTICLE_URL.replace("{ID}", id);
-        const requestDetails = axios(artcileDetailsUrl, EnrichConfiguration({ method: "GET", responseType: "json" }));
-        const requestText = axios(articleTextUrl, EnrichConfiguration({ method: "GET", responseType: "json" }));
-
-        axios.all([requestDetails, requestText]).then(axios.spread((...responses) => 
-        {
-            const detailsResponse = responses[0];
-            const textResponse = responses[1];
-
-            if (detailsResponse.status === 200 && textResponse.status === 200)
-            {
-                if (detailsResponse.data === null)
-                    return RaiseError(dispatch, NULL_RESPONSE_ERROR);
-
-                if (textResponse.data === null)
-                    return RaiseError(dispatch, NULL_RESPONSE_ERROR);
-                
-                const combineData: IArticleItem = 
-                {
-                    id: detailsResponse.data.id,
-                    title: detailsResponse.data.title,
-                    description: detailsResponse.data.description,
-                    isPublished: detailsResponse.data.isPublished,
-                    likeCount: detailsResponse.data.likeCount,
-                    userLikes: detailsResponse.data.userLikes,
-                    readCount: detailsResponse.data.readCount,
-                    createdAt: detailsResponse.data.createdAt,
-                    updatedAt: detailsResponse.data.updatedAt,
-                    author: detailsResponse.data.author,
-                    text: textResponse.data.items
-                };
-
-                return dispatch({ type: RECEIVE_ARTICLE, payload: combineData });
-            }
-            else
-            {
-                if (detailsResponse.status !== 200) 
-                    return RaiseError(dispatch, UnexpectedStatusCode(detailsResponse.status));
-
-                if (textResponse.status !== 200) 
-                    return RaiseError(dispatch, UnexpectedStatusCode(textResponse.status));
-            }
+        const url = API_QUERY_GET_ARTICLE.replace("{id}", id);
+        axios(url, EnrichConfiguration(
+        { 
+            method: "GET", 
+            responseType: "json" 
         }))
+        .then(response => 
+        {
+            if (response.status === 200)
+            {
+                return response.data === null 
+                    ? RaiseError(dispatch, NULL_RESPONSE_ERROR) 
+                    : dispatch({ type: RECEIVE_ARTICLE, payload: response.data });
+            }
+
+            RaiseError(dispatch, UnexpectedStatusCode(response.status));
+
+        })
         .catch(error => 
         {
             RaiseError(dispatch, error);
