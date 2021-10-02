84d4529408a74d5943411a9eacf75389bad2963d
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2021-10-03 01:30:24 +0200

    refactor: use custom http client and method moved to serializer

--- TokanPages.Backend/TokanPages.Backend.Cqrs/Handlers/Queries/Content/GetContentQueryHandler.cs
+++ TokanPages.Backend/TokanPages.Backend.Cqrs/Handlers/Queries/Content/GetContentQueryHandler.cs
@@ -2,30 +2,29 @@ namespace TokanPages.Backend.Cqrs.Handlers.Queries.Content
 {
     using System.Net;
     using System.Linq;
-    using System.Net.Http;
     using System.Threading;
     using System.Threading.Tasks;
-    using System.Collections.Generic;
     using Newtonsoft.Json.Linq;
     using Storage.Models;
     using Core.Exceptions;
     using Shared.Resources;
     using Shared.Dto.Content;
     using Core.Utilities.JsonSerializer;
+    using Core.Utilities.CustomHttpClient;
 
     public class GetContentQueryHandler : TemplateHandler<GetContentQuery, GetContentQueryResult>
     {
         private const string DEFAULT_LANGUAGE = "en";
         
-        private readonly HttpClient FHttpClient;
+        private readonly ICustomHttpClient FCustomHttpClient;
 
         private readonly IJsonSerializer FJsonSerializer; 
         
         private readonly AzureStorage FAzureStorage;
 
-        public GetContentQueryHandler(HttpClient AHttpClient, IJsonSerializer AJsonSerializer, AzureStorage AAzureStorage)
+        public GetContentQueryHandler(ICustomHttpClient ACustomHttpClient, IJsonSerializer AJsonSerializer, AzureStorage AAzureStorage)
         {
-            FHttpClient = AHttpClient;
+            FCustomHttpClient = ACustomHttpClient;
             FJsonSerializer = AJsonSerializer;
             FAzureStorage = AAzureStorage;
         }
@@ -49,56 +48,48 @@ namespace TokanPages.Backend.Cqrs.Handlers.Queries.Content
             };
         }
 
-        private static dynamic GetObjectByLanguage(JToken AToken, string AComponentName, string ASelectedLanguage)
+        private dynamic GetObjectByLanguage(JToken AToken, string AComponentName, string ASelectedLanguage)
         {
             return AComponentName switch
             {
                 // Components
-                "activateAccount" => MapObjects<ActivateAccountDto>(AToken).SingleOrDefault(AItem => AItem.Language == ASelectedLanguage),
-                "articleFeatures" => MapObjects<ArticleFeaturesDto>(AToken).SingleOrDefault(AItem => AItem.Language == ASelectedLanguage),
-                "contactForm" => MapObjects<ContactFormDto>(AToken).SingleOrDefault(AItem => AItem.Language == ASelectedLanguage),
-                "cookiesPrompt" => MapObjects<CookiesPromptDto>(AToken).SingleOrDefault(AItem => AItem.Language == ASelectedLanguage),
-                "featured" => MapObjects<FeaturedDto>(AToken).SingleOrDefault(AItem => AItem.Language == ASelectedLanguage),
-                "features" => MapObjects<FeaturesDto>(AToken).SingleOrDefault(AItem => AItem.Language == ASelectedLanguage),
-                "footer" => MapObjects<FooterDto>(AToken).SingleOrDefault(AItem => AItem.Language == ASelectedLanguage),
-                "header" => MapObjects<HeaderDto>(AToken).SingleOrDefault(AItem => AItem.Language == ASelectedLanguage),
-                "navigation" => MapObjects<NavigationDto>(AToken).SingleOrDefault(AItem => AItem.Language == ASelectedLanguage),
-                "newsletter" => MapObjects<NewsletterDto>(AToken).SingleOrDefault(AItem => AItem.Language == ASelectedLanguage),
-                "notFound" => MapObjects<NotFoundDto>(AToken).SingleOrDefault(AItem => AItem.Language == ASelectedLanguage),
-                "resetPassword" => MapObjects<ResetPasswordDto>(AToken).SingleOrDefault(AItem => AItem.Language == ASelectedLanguage),
-                "testimonials" => MapObjects<TestimonialsDto>(AToken).SingleOrDefault(AItem => AItem.Language == ASelectedLanguage),
-                "unsubscribe" => MapObjects<UnsubscribeDto>(AToken).SingleOrDefault(AItem => AItem.Language == ASelectedLanguage),
-                "updatePassword" => MapObjects<UpdatePasswordDto>(AToken).SingleOrDefault(AItem => AItem.Language == ASelectedLanguage),
-                "updateSubscriber" => MapObjects<UpdateSubscriberDto>(AToken).SingleOrDefault(AItem => AItem.Language == ASelectedLanguage),
-                "userSignin" => MapObjects<UserSigninDto>(AToken).SingleOrDefault(AItem => AItem.Language == ASelectedLanguage),
-                "userSignout" => MapObjects<UserSignoutDto>(AToken).SingleOrDefault(AItem => AItem.Language == ASelectedLanguage),
-                "userSignup" => MapObjects<UserSignupDto>(AToken).SingleOrDefault(AItem => AItem.Language == ASelectedLanguage),
-                "wrongPagePrompt" => MapObjects<WrongPagePromptDto>(AToken).SingleOrDefault(AItem => AItem.Language == ASelectedLanguage),
+                "activateAccount" => FJsonSerializer.MapObjects<ActivateAccountDto>(AToken).SingleOrDefault(AItem => AItem.Language == ASelectedLanguage),
+                "articleFeatures" => FJsonSerializer.MapObjects<ArticleFeaturesDto>(AToken).SingleOrDefault(AItem => AItem.Language == ASelectedLanguage),
+                "contactForm" => FJsonSerializer.MapObjects<ContactFormDto>(AToken).SingleOrDefault(AItem => AItem.Language == ASelectedLanguage),
+                "cookiesPrompt" => FJsonSerializer.MapObjects<CookiesPromptDto>(AToken).SingleOrDefault(AItem => AItem.Language == ASelectedLanguage),
+                "featured" => FJsonSerializer.MapObjects<FeaturedDto>(AToken).SingleOrDefault(AItem => AItem.Language == ASelectedLanguage),
+                "features" => FJsonSerializer.MapObjects<FeaturesDto>(AToken).SingleOrDefault(AItem => AItem.Language == ASelectedLanguage),
+                "footer" => FJsonSerializer.MapObjects<FooterDto>(AToken).SingleOrDefault(AItem => AItem.Language == ASelectedLanguage),
+                "header" => FJsonSerializer.MapObjects<HeaderDto>(AToken).SingleOrDefault(AItem => AItem.Language == ASelectedLanguage),
+                "navigation" => FJsonSerializer.MapObjects<NavigationDto>(AToken).SingleOrDefault(AItem => AItem.Language == ASelectedLanguage),
+                "newsletter" => FJsonSerializer.MapObjects<NewsletterDto>(AToken).SingleOrDefault(AItem => AItem.Language == ASelectedLanguage),
+                "notFound" => FJsonSerializer.MapObjects<NotFoundDto>(AToken).SingleOrDefault(AItem => AItem.Language == ASelectedLanguage),
+                "resetPassword" => FJsonSerializer.MapObjects<ResetPasswordDto>(AToken).SingleOrDefault(AItem => AItem.Language == ASelectedLanguage),
+                "testimonials" => FJsonSerializer.MapObjects<TestimonialsDto>(AToken).SingleOrDefault(AItem => AItem.Language == ASelectedLanguage),
+                "unsubscribe" => FJsonSerializer.MapObjects<UnsubscribeDto>(AToken).SingleOrDefault(AItem => AItem.Language == ASelectedLanguage),
+                "updatePassword" => FJsonSerializer.MapObjects<UpdatePasswordDto>(AToken).SingleOrDefault(AItem => AItem.Language == ASelectedLanguage),
+                "updateSubscriber" => FJsonSerializer.MapObjects<UpdateSubscriberDto>(AToken).SingleOrDefault(AItem => AItem.Language == ASelectedLanguage),
+                "userSignin" => FJsonSerializer.MapObjects<UserSigninDto>(AToken).SingleOrDefault(AItem => AItem.Language == ASelectedLanguage),
+                "userSignout" => FJsonSerializer.MapObjects<UserSignoutDto>(AToken).SingleOrDefault(AItem => AItem.Language == ASelectedLanguage),
+                "userSignup" => FJsonSerializer.MapObjects<UserSignupDto>(AToken).SingleOrDefault(AItem => AItem.Language == ASelectedLanguage),
+                "wrongPagePrompt" => FJsonSerializer.MapObjects<WrongPagePromptDto>(AToken).SingleOrDefault(AItem => AItem.Language == ASelectedLanguage),
                 // Documents
-                "terms" => MapObjects<DocumentDto>(AToken).SingleOrDefault(AItem => AItem.Language == ASelectedLanguage),
-                "policy" => MapObjects<DocumentDto>(AToken).SingleOrDefault(AItem => AItem.Language == ASelectedLanguage),
-                "myStory" => MapObjects<DocumentDto>(AToken).SingleOrDefault(AItem => AItem.Language == ASelectedLanguage),
+                "terms" => FJsonSerializer.MapObjects<DocumentDto>(AToken).SingleOrDefault(AItem => AItem.Language == ASelectedLanguage),
+                "policy" => FJsonSerializer.MapObjects<DocumentDto>(AToken).SingleOrDefault(AItem => AItem.Language == ASelectedLanguage),
+                "myStory" => FJsonSerializer.MapObjects<DocumentDto>(AToken).SingleOrDefault(AItem => AItem.Language == ASelectedLanguage),
                 _ => null
             };
         }
 
         private async Task<string> GetJsonData(string AUrl, CancellationToken ACancellationToken)
         {
-            var LResponseContent = await FHttpClient.GetAsync(AUrl, ACancellationToken);
+            var LConfiguration = new Configuration { Url = AUrl, Method = "GET" };
+            var (LContent, LStatusCode) = await FCustomHttpClient.Execute(LConfiguration, ACancellationToken);
 
-            if (LResponseContent.StatusCode != HttpStatusCode.OK)
+            if (LStatusCode != HttpStatusCode.OK)
                 throw new BusinessException(nameof(ErrorCodes.ERROR_UNEXPECTED), ErrorCodes.ERROR_UNEXPECTED);
-                    
-            return await LResponseContent.Content.ReadAsStringAsync(ACancellationToken);
-        }
 
-        private static IEnumerable<T> MapObjects<T>(JToken AComponent) where T : new()
-        {
-            return AComponent switch
-            {
-                JArray => AComponent.ToObject<IEnumerable<T>>(),
-                _ => new List<T>()
-            };
+            return LContent;
         }
     }
 }
