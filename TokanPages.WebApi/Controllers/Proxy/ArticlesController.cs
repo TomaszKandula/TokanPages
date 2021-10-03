600c4b8c7a578249d6ccba5f51c40f0b9e586f98
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2021-10-03 13:02:40 +0200

    feat: add new proxy controller for getting article main image

---
+++ TokanPages.WebApi/Controllers/Proxy/ArticlesController.cs
@@ -0,0 +1,43 @@
+namespace TokanPages.WebApi.Controllers.Proxy
+{
+    using System;
+    using System.Net;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Mvc;
+    using Backend.Shared;
+    using Backend.Shared.Models;
+    using Backend.Storage.Models;
+    using Backend.Shared.Attributes;
+    using Backend.Core.Utilities.CustomHttpClient;
+    using Backend.Core.Utilities.CustomHttpClient.Models;
+
+    public class ArticlesController : ProxyBaseController
+    {
+        public ArticlesController(ICustomHttpClient ACustomHttpClient, SonarQube ASonarQube, AzureStorage AAzureStorage) 
+            : base(ACustomHttpClient, ASonarQube, AAzureStorage) { }
+
+        [HttpGet("Images")]
+        [ETagFilter(200)]
+        public async Task<ContentResult> GetArticleImage([FromQuery] string AId)
+        {
+            try
+            {
+                var LRequestUrl = $"{FAzureStorage.BaseUrl}/content/articles/{AId}/image.jpeg";
+                var LConfiguration = new Configuration { Url = LRequestUrl, Method = "GET"};
+                var LResults = await FCustomHttpClient.Execute(LConfiguration);
+
+                return FCustomHttpClient.GetContentResult(
+                    (int)LResults.StatusCode, 
+                    LResults.Content, 
+                    LResults.ContentType?.MediaType);
+            }
+            catch (Exception LException)
+            {
+                return FCustomHttpClient.GetContentResult(
+                    (int)HttpStatusCode.InternalServerError, 
+                    LException.Message, 
+                    Constants.ContentTypes.TEXT_PLAIN);
+            }
+        }
+    }
+}
