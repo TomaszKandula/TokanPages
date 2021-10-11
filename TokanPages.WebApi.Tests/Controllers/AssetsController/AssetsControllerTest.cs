be714299352531908b8ed5741216652b5144924c
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2021-10-11 20:17:23 +0200

    test: add assets endpoint tests

---
+++ TokanPages.WebApi.Tests/Controllers/AssetsController/AssetsControllerTest.cs
@@ -0,0 +1,13 @@
+namespace TokanPages.WebApi.Tests.Controllers.AssetsController
+{
+    using Xunit;
+
+    public partial class AssetsControllerTest : TestBase, IClassFixture<CustomWebApplicationFactory<TestStartup>>
+    {
+        private const string API_BASE_URL = "/api/v1/assets";
+        
+        private readonly CustomWebApplicationFactory<TestStartup> FWebAppFactory;
+
+        public AssetsControllerTest(CustomWebApplicationFactory<TestStartup> AWebAppFactory) => FWebAppFactory = AWebAppFactory;
+    }
+}
---
+++ TokanPages.WebApi.Tests/Controllers/AssetsController/AssetsEndpointTest.cs
@@ -0,0 +1,44 @@
+namespace TokanPages.WebApi.Tests.Controllers.AssetsController
+{
+    using Xunit;
+    using FluentAssertions;
+    using System.Net;
+    using System.Threading.Tasks;
+
+    public partial class AssetsControllerTest
+    {
+        [Theory]
+        [InlineData("images/icons/__github.png")]
+        [InlineData("images/icons/__linkedin.png")]
+        public async Task GivenValidBlobName_WhenRequestingAsset_ShouldSucceed(string ABlobName)
+        {
+            // Arrange
+            var LRequest = $"{API_BASE_URL}/?ABlobName={ABlobName}";
+
+            // Act
+            var LHttpClient = FWebAppFactory.CreateClient();
+            var LResponse = await LHttpClient.GetAsync(LRequest);
+            await EnsureStatusCode(LResponse, HttpStatusCode.OK);
+
+            // Assert
+            var LContent = await LResponse.Content.ReadAsStringAsync();
+            LContent.Should().NotBeNullOrEmpty();
+        }
+
+        [Fact]
+        public async Task GivenInvalidBlobName_WhenRequestingAsset_ShouldReturnNotFound()
+        {
+            // Arrange
+            var LRequest = $"{API_BASE_URL}/?ABlobName={DataUtilityService.GetRandomString()}";
+
+            // Act
+            var LHttpClient = FWebAppFactory.CreateClient();
+            var LResponse = await LHttpClient.GetAsync(LRequest);
+            await EnsureStatusCode(LResponse, HttpStatusCode.NotFound);
+
+            // Assert
+            var LContent = await LResponse.Content.ReadAsStringAsync();
+            LContent.Should().NotBeNullOrEmpty();
+        }
+    }
+}
