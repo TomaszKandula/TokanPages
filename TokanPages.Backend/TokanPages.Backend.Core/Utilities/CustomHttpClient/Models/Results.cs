02bd121c26a712afcaeac64c3fb0ce0b254031af
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2021-10-11 16:50:34 +0200

    refactor: remove whitespace

--- TokanPages.Backend/TokanPages.Backend.Core/Utilities/CustomHttpClient/Models/Results.cs
+++ TokanPages.Backend/TokanPages.Backend.Core/Utilities/CustomHttpClient/Models/Results.cs
@@ -10,7 +10,7 @@ namespace TokanPages.Backend.Core.Utilities.CustomHttpClient.Models
         public HttpStatusCode StatusCode { get; set; }
 
         public MediaTypeHeaderValue? ContentType { get; set; }
-        
+
         public byte[]? Content { get; set; }
     }
 }
