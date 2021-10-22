00332bb7fae79babe477181a726ae6c9a49041e4
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2021-10-22 21:09:29 +0200

    fix: add missing casting

--- TokanPages.Backend/TokanPages.Backend.Cqrs/Services/UserServiceProvider/UserServiceProvider.cs
+++ TokanPages.Backend/TokanPages.Backend.Cqrs/Services/UserServiceProvider/UserServiceProvider.cs
@@ -77,7 +77,7 @@
             if (expiresIn == 0)
                 throw ArgumentZeroException;
 
-            var expires = _dateTimeService.Now.AddMinutes(Math.Abs(expiresIn));
+            var expires = (DateTimeOffset)_dateTimeService.Now.AddMinutes(Math.Abs(expiresIn));
             var cookieOptions = new CookieOptions
             {
                 HttpOnly = isHttpOnly,
