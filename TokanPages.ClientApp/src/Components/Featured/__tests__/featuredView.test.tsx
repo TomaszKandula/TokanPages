18136afed7716d32639acc17f31db20d4da4659d
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2021-10-30 18:32:09 +0200

    test: update urls and snapshots

--- TokanPages.ClientApp/src/Components/Featured/__tests__/featuredView.test.tsx
+++ TokanPages.ClientApp/src/Components/Featured/__tests__/featuredView.test.tsx
@@ -14,15 +14,15 @@ describe("Test component: featuredView.", () =>
             title1: "Memory Management",
             subtitle1: "The basics you should know, when writing an application...",
             link1: "https://.../net-memory-management-64389b01f642",
-            image1: "https://.../images/section_featured/article0.jpg",
+            image1: "article0.jpg",
             title2: "Stored Procedures",
             subtitle2: "I explain why I do not need them that much...",
             link2: "https://.../i-said-goodbye-to-stored-procedures-539d56350486",
-            image2: "https://.../images/section_featured/article1.jpg",
+            image2: "article1.jpg",
             title3: "SQL Injection",
             subtitle3: "This article will explore the issue in greater detail...",
             link3: "https://.../sql-injection-1bde8bb76ebc",
-            image3: "https://.../images/section_featured/article2.jpg"
+            image3: "article2.jpg"
         }}/>);
         expect(tree).toMatchSnapshot();
     });
