42124e92e7a75e39e458ddb85ae2a8293eec1568
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2021-10-31 10:50:53 +0100

    refactor: move entity key definition to domain service

--- TokanPages.Backend/TokanPages.Backend.Domain/Entities/Subscribers.cs
+++ TokanPages.Backend/TokanPages.Backend.Domain/Entities/Subscribers.cs
@@ -3,7 +3,6 @@
     using System;
     using System.Diagnostics.CodeAnalysis;
     using System.ComponentModel.DataAnnotations;
-    using Core.Entities;
 
     [ExcludeFromCodeCoverage]
     public class Subscribers : Entity<Guid>
