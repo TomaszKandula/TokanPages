6634c3fe60085b2c81634bb56add82c7e479b5f1
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2021-11-18 18:13:57 +0100

    refactor: rename project, update namespaces and assemblies

---
+++ TokanPages.UnitTests/Handlers/Subscribers/AddSubscriberCommandHandlerTest.cs
@@ -0,0 +1,89 @@
+﻿namespace TokanPages.UnitTests.Handlers.Subscribers
+{
+    using Moq;
+    using Xunit;
+    using FluentAssertions;
+    using System;
+    using System.Linq;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Backend.Core.Exceptions;
+    using Backend.Core.Utilities.LoggerService;
+    using Backend.Core.Utilities.DateTimeService;
+    using Backend.Cqrs.Handlers.Commands.Subscribers;
+
+    public class AddSubscriberCommandHandlerTest : TestBase
+    {
+        [Fact]
+        public async Task GivenProvidedEmail_WhenAddSubscriber_ShouldAddEntity() 
+        {
+            // Arrange
+            var addSubscriberCommand = new AddSubscriberCommand 
+            { 
+                Email = DataUtilityService.GetRandomEmail()
+            };
+
+            var databaseContext = GetTestDatabaseContext();
+            var mockedLogger = new Mock<ILoggerService>();
+            var mockedDateTime = new Mock<IDateTimeService>();
+
+            const string testDateTime = "2020-01-01";
+            mockedDateTime
+                .Setup(dateTime => dateTime.Now)
+                .Returns(DateTime.Parse(testDateTime));
+            
+            var addSubscriberCommandHandler = new AddSubscriberCommandHandler(
+                databaseContext, 
+                mockedLogger.Object,
+                mockedDateTime.Object);
+
+            // Act
+            await addSubscriberCommandHandler.Handle(addSubscriberCommand, CancellationToken.None);
+
+            // Assert
+            var subscribersEntity = databaseContext.Subscribers.ToList();
+
+            subscribersEntity.Should().HaveCount(1);
+            subscribersEntity[0].Email.Should().Be(addSubscriberCommand.Email);
+            subscribersEntity[0].Count.Should().Be(0);
+            subscribersEntity[0].IsActivated.Should().BeTrue();
+            subscribersEntity[0].Registered.Should().HaveDay(DateTime.Parse(testDateTime).Day);
+            subscribersEntity[0].Registered.Should().HaveMonth(DateTime.Parse(testDateTime).Month);
+            subscribersEntity[0].Registered.Should().HaveYear(DateTime.Parse(testDateTime).Year);
+            subscribersEntity[0].LastUpdated.Should().BeNull();
+        }
+
+        [Fact]
+        public async Task GivenExistingEmail_WhenAddSubscriber_ShouldThrowError()
+        {
+            // Arrange
+            var testEmail = DataUtilityService.GetRandomEmail();
+            var subscribers = new TokanPages.Backend.Domain.Entities.Subscribers 
+            { 
+                Email = testEmail,
+                IsActivated = true,
+                Count = 0,
+                Registered = DateTime.Now,
+                LastUpdated = null
+            };
+
+            var databaseContext = GetTestDatabaseContext();
+            await databaseContext.Subscribers.AddAsync(subscribers);
+            await databaseContext.SaveChangesAsync();
+
+            var mockedLogger = new Mock<ILoggerService>();
+            var mockedDateTime = new Mock<IDateTimeService>();
+
+            var addSubscriberCommand = new AddSubscriberCommand { Email = testEmail };
+            var addSubscriberCommandHandler = new AddSubscriberCommandHandler(
+                databaseContext, 
+                mockedLogger.Object,
+                mockedDateTime.Object);
+
+            // Act
+            // Assert
+            await Assert.ThrowsAsync<BusinessException>(() 
+                => addSubscriberCommandHandler.Handle(addSubscriberCommand, CancellationToken.None));
+        }
+    }
+}
---
+++ TokanPages.UnitTests/Validators/Articles/UpdateArticleVisibilityCommandValidatorTest.cs
@@ -0,0 +1,48 @@
+namespace TokanPages.UnitTests.Validators.Articles
+{
+    using Xunit;
+    using FluentAssertions;
+    using System;
+    using Backend.Shared.Resources;
+    using Backend.Cqrs.Handlers.Commands.Articles;
+
+    public class UpdateArticleVisibilityCommandValidatorTest
+    {
+        [Fact]
+        public void GivenValidGuidAndPublishFlag_WhenUpdateArticleVisibility_ShouldReturnSuccess()
+        {
+            // Arrange
+            var updateArticleVisibilityCommand = new UpdateArticleVisibilityCommand
+            {
+                Id = Guid.NewGuid(),
+                IsPublished = true
+            };
+
+            // Act
+            var updateArticleVisibilityCommandValidator = new UpdateArticleVisibilityCommandValidator();
+            var result = updateArticleVisibilityCommandValidator.Validate(updateArticleVisibilityCommand);
+
+            // Assert
+            result.Errors.Should().BeEmpty();
+        }
+        
+        [Fact]
+        public void GivenEmptyGuidAndPublishFlag_WhenUpdateArticleVisibility_ShouldReturnSuccess()
+        {
+            // Arrange
+            var updateArticleVisibilityCommand = new UpdateArticleVisibilityCommand
+            {
+                Id = Guid.Empty,
+                IsPublished = true
+            };
+
+            // Act
+            var updateArticleVisibilityCommandValidator = new UpdateArticleVisibilityCommandValidator();
+            var result = updateArticleVisibilityCommandValidator.Validate(updateArticleVisibilityCommand);
+
+            // Assert
+            result.Errors.Count.Should().Be(1);
+            result.Errors[0].ErrorCode.Should().Be(nameof(ValidationCodes.REQUIRED));
+        }
+    }
+}
