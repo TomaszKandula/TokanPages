9468afc0746a6928daa88f622113c0eed471825c
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2021-11-29 17:55:46 +0100

    refactor: add new exceptions

---
+++ TokanPages.Backend/TokanPages.Backend.Core/Exceptions/AuthorizationException.cs
@@ -0,0 +1,19 @@
+namespace TokanPages.Backend.Core.Exceptions
+{
+    using System;
+    using System.Runtime.Serialization;
+    using System.Diagnostics.CodeAnalysis;
+
+    [ExcludeFromCodeCoverage]
+    [Serializable]
+    public class AuthorizationException : Exception
+    {
+        public string ErrorCode { get; }
+
+        protected AuthorizationException(SerializationInfo serializationInfo, 
+            StreamingContext streamingContext) : base(serializationInfo, streamingContext) { }
+
+        public AuthorizationException(string errorCode, string errorMessage = "") : base(errorMessage)
+            => ErrorCode = errorCode;
+    }
+}
