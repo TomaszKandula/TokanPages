fc64c8cf42ef2a281ab31d0fb245f7a806ecda2f
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2022-01-07 23:29:32 +0100

    feat: improve Azure Blob Storage implementation, add new method

--- TokanPages.Services/TokanPages.Services.AzureStorageService/IAzureBlobStorage.cs
+++ TokanPages.Services/TokanPages.Services.AzureStorageService/IAzureBlobStorage.cs
@@ -1,22 +1,26 @@
+#nullable enable
 namespace TokanPages.Services.AzureStorageService;
 
 using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 using Models;
 using Backend.Shared;
 
 public interface IAzureBlobStorage
 {
-    Task<StorageByteContent> ReadAllBytes(string sourceFilePath ,CancellationToken cancellationToken);
+    Task<StorageByteContent?> ReadAllBytes(string sourceFilePath, CancellationToken cancellationToken = default);
 
-    Task<StorageStreamContent> OpenRead(string sourceFilePath, CancellationToken cancellationToken);
+    Task<StorageStreamContent?> OpenRead(string sourceFilePath, CancellationToken cancellationToken = default);
         
-    Task UploadFile(Stream sourceStream, string destinationPath, CancellationToken cancellationToken, string contentType = Constants.ContentTypes.Stream);
-        
-    Task<string> GetFileContentType(string sourceFilePath, CancellationToken cancellationToken);
-        
-    Task<bool> DeleteFile(string sourceFilePath, CancellationToken cancellationToken);
+    Task<List<string>> GetBlobListing(string? filterByPath = default, int pageSize = 10, string? continuationToken = default, CancellationToken cancellationToken = default);
 
-    Task UploadContent(string content, string destinationPath, CancellationToken cancellationToken);
+    Task UploadFile(Stream sourceStream, string destinationPath, CancellationToken cancellationToken = default, string contentType = Constants.ContentTypes.Stream);
+        
+    Task<string> GetFileContentType(string sourceFilePath, CancellationToken cancellationToken = default);
+        
+    Task<bool> DeleteFile(string sourceFilePath, CancellationToken cancellationToken = default);
+
+    Task UploadContent(string content, string destinationPath, CancellationToken cancellationToken = default);
 }
