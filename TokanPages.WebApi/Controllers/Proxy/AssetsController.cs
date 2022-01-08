48e71211b2abf56631f450ad361e52fc3596efa2
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2022-01-08 22:44:23 +0100

    feat: add new endpoint to get assets list

--- TokanPages.WebApi/Controllers/Proxy/AssetsController.cs
+++ TokanPages.WebApi/Controllers/Proxy/AssetsController.cs
@@ -3,19 +3,29 @@ namespace TokanPages.WebApi.Controllers.Proxy;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
+using Attributes;
 using Services.Caching.Assets;
+using Backend.Domain.Enums;
+using Backend.Cqrs.Handlers.Queries.Assets;
+using MediatR;
 
 [ApiVersion("1.0")]
-public class AssetsController : ProxyBaseController
+public class AssetsController : ApiBaseController
 {
     private readonly IAssetsCache _assetsCache;
 
-    public AssetsController(IAssetsCache assetsCache) => _assetsCache = assetsCache;
+    public AssetsController(IMediator mediator,  IAssetsCache assetsCache) : base(mediator) => _assetsCache = assetsCache;
 
     [HttpGet]
     [ProducesResponseType(typeof(IActionResult), StatusCodes.Status200OK)]
     public async Task<IActionResult> GetAsset([FromQuery] string blobName, bool noCache = false)
         => await _assetsCache.GetAsset(blobName, noCache);
+
+    [HttpGet]
+    [AuthorizeUser(Roles.GodOfAsgard)]
+    [ProducesResponseType(typeof(GetAssetsListQueryResult), StatusCodes.Status200OK)]
+    public async Task<GetAssetsListQueryResult> GetAssetsList() 
+        => await Mediator.Send(new GetAssetsListQuery());
 
     [HttpGet("Article")]
     [ProducesResponseType(typeof(IActionResult), StatusCodes.Status200OK)]
