3bfc94c06547fc598016eba061f7d44e4d69fc1b
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2022-01-13 15:48:05 +0100

    feat: add HttpRequests database table

--- TokanPages.Backend/TokanPages.Backend.Database/DatabaseContext.cs
+++ TokanPages.Backend/TokanPages.Backend.Database/DatabaseContext.cs
@@ -11,27 +11,27 @@ public class DatabaseContext : DbContext
     public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options) { }
 
     public virtual DbSet<Articles> Articles { get; set; }
-        
+
     public virtual DbSet<Subscribers> Subscribers { get; set; }
-        
+
     public virtual DbSet<Users> Users { get; set; }
-        
+
     public virtual DbSet<ArticleLikes> ArticleLikes { get; set; }
 
     public virtual DbSet<ArticleCounts> ArticleCounts { get; set; }
 
     public virtual DbSet<Albums> Albums { get; set; }
-        
+
     public virtual DbSet<Photos> Photos { get; set; }
-        
+
     public virtual DbSet<PhotoGears> PhotoGears { get; set; }
-       
+
     public virtual DbSet<PhotoCategories> PhotoCategories { get; set; }
 
     public virtual DbSet<Roles> Roles { get; set; }
-        
+
     public virtual DbSet<Permissions> Permissions { get; set; }
-        
+
     public virtual DbSet<DefaultPermissions> DefaultPermissions { get; set; }
 
     public virtual DbSet<UserPermissions> UserPermissions { get; set; }
@@ -41,7 +41,9 @@ public class DatabaseContext : DbContext
     public virtual DbSet<UserTokens> UserTokens { get; set; }
 
     public virtual DbSet<UserRefreshTokens> UserRefreshTokens { get; set; }
-        
+
+    public virtual DbSet<HttpRequests> HttpRequests { get; set; }
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
