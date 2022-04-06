b487a675204cb508f20f8d331bc3a133347d5f3b
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2022-04-07 00:41:33 +0200

    feat: improve logic and layout of component view for unsubscribe action

--- TokanPages.ClientApp/src/Components/Unsubscribe/unsubscribeView.tsx
+++ TokanPages.ClientApp/src/Components/Unsubscribe/unsubscribeView.tsx
@@ -6,6 +6,7 @@ import Container from "@material-ui/core/Container";
 import Typography from "@material-ui/core/Typography";
 import { Card, CardContent } from "@material-ui/core";
 import Skeleton from "@material-ui/lab/Skeleton";
+import { IContent } from "../../Api/Models/Components/unsubscribeContentDto";
 import unsubscribeStyle from "./unsubscribeStyle";
 
 interface IBinding 
@@ -16,19 +17,30 @@ interface IBinding
 interface IProperties
 {
     isLoading: boolean;
-    caption: string;
-    text1: string;
-    text2: string;
-    text3: string;
+    contentPre: IContent;
+    contentPost: IContent;
     buttonHandler: any;
     buttonState: boolean;
     progress: boolean;
-    buttonText: string;
+    isRemoved: boolean;
 }
 
 const UnsubscribeView = (props: IBinding): JSX.Element =>
 {
     const classes = unsubscribeStyle();
+
+    const content: IContent = props.bind?.isRemoved ? props.bind?.contentPost : props.bind?.contentPre;
+
+    const ActiveButton = (): JSX.Element => 
+    {
+        return(
+            <Button onClick={props.bind?.buttonHandler} type="submit" fullWidth variant="contained" disabled={props.bind?.progress || !props.bind?.buttonState}>
+                {props.bind?.progress &&  <CircularProgress size={20} />}
+                {!props.bind?.progress && content.button}
+            </Button>
+        );
+    }
+
     return (
         <section className={classes.section}>
             <Container maxWidth="sm">
@@ -38,28 +50,25 @@ const UnsubscribeView = (props: IBinding): JSX.Element =>
                         <Box textAlign="center" mb={3}>
                             <Box mt={2} mb={2}>
                                 <Typography variant="h4" component="h4" gutterBottom={true}>
-                                    {props.bind?.isLoading ? <Skeleton variant="text" /> : props.bind?.caption}
+                                    {props.bind?.isLoading ? <Skeleton variant="text" /> : content.caption}
                                 </Typography>
                             </Box>
                             <Box mt={5} mb={2}>
                                 <Typography variant="h6" component="h6" color="textSecondary">
-                                    {props.bind?.isLoading ? <Skeleton variant="text" /> : props.bind?.text1}
+                                    {props.bind?.isLoading ? <Skeleton variant="text" /> : content.text1}
                                 </Typography>
                             </Box>
                             <Box mt={5} mb={2}>
-                                <Typography variant="body1" color="textSecondary">
-                                    {props.bind?.isLoading ? <Skeleton variant="text" /> : props.bind?.text2}
+                                <Typography variant="h6" color="textSecondary">
+                                    {props.bind?.isLoading ? <Skeleton variant="text" /> : content.text2}
                                 </Typography>
                             </Box>
                             <Box mt={5} mb={7}>
-                                <Typography variant="body1" color="textSecondary">
-                                    {props.bind?.isLoading ? <Skeleton variant="text" /> : props.bind?.text3}
+                                <Typography variant="h6" color="textSecondary">
+                                    {props.bind?.isLoading ? <Skeleton variant="text" /> : content.text3}
                                 </Typography>
                             </Box>
-                            <Button onClick={props.bind?.buttonHandler} type="submit" fullWidth variant="contained" color="primary" disabled={!props.bind?.buttonState}>
-                                {props.bind?.progress &&  <CircularProgress size={20} />}
-                                {!props.bind?.progress && props.bind?.buttonText}
-                            </Button>
+                            {props.bind?.isLoading ? <Skeleton variant="rect" /> : <ActiveButton />}
                         </Box>
                         </CardContent>
                     </Card>
