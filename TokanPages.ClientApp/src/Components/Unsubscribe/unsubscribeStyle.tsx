7e02a1b284dbf646d49c59e760865ab21e2c3587
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2022-04-11 17:12:13 +0200

    feat: update component styles

--- TokanPages.ClientApp/src/Components/Unsubscribe/unsubscribeStyle.tsx
+++ TokanPages.ClientApp/src/Components/Unsubscribe/unsubscribeStyle.tsx
@@ -7,12 +7,46 @@ const unsubscribeStyle = makeStyles(() => (
     {
         backgroundColor: CustomColours.colours.white
     },
+    caption:
+    {
+        fontSize: "2.0rem",
+        fontWeight: 400,
+        color: CustomColours.colours.black
+    },
+    text1:
+    {
+        fontSize: "1.2rem",
+        fontWeight: 500,
+        color: CustomColours.colours.gray1
+    },
+    text2:
+    {
+        fontSize: "1.2rem",
+        fontWeight: 400,
+        color: CustomColours.colours.gray1        
+    },
+    text3:
+    {
+        fontSize: "1.2rem",
+        fontWeight: 400,
+        color: CustomColours.colours.gray1        
+    },
     card:
     {
         marginTop: 10,
         marginLeft: 15,
         marginRight: 15,
         marginBottom: 10
+    },
+    button:
+    {
+        "&:hover": 
+        {
+            color: CustomColours.colours.white,
+            background: CustomColours.colours.darkViolet1,
+        },
+        color: CustomColours.colours.white,
+        background: CustomColours.colours.violet,
     }
 }));
 
