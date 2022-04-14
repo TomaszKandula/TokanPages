f909a3d8addfa89c515d560a1f7c5b14aa04c328
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2022-04-15 01:05:52 +0200

    feat: implement final layout and style for clients logos

--- TokanPages.ClientApp/src/Components/Clients/clientsStyle.tsx
+++ TokanPages.ClientApp/src/Components/Clients/clientsStyle.tsx
@@ -1,11 +1,32 @@
 import { makeStyles } from "@material-ui/core/styles";
 import { CustomColours } from "../../Theme/customColours";
 
-const ClientsStyle = makeStyles(() => (
+const ClientsStyle = makeStyles((theme) => (
 {
+    divider:
+    {
+        marginTop: "42px",
+        height: "2px",
+        backgroundColor: CustomColours.colours.lightGray1
+    },
     section:
     {
-        backgroundColor: CustomColours.colours.lightGray3
+        backgroundColor: CustomColours.colours.white,
+        marginTop: "42px",
+        marginBottom: "42px"
+    },
+    logo:
+    {
+        height: "48px",
+        paddingLeft: theme.spacing(4),
+        paddingRight: theme.spacing(4),
+        marginBottom: theme.spacing(4),
+        [theme.breakpoints.down("md")]: 
+        {
+            paddingLeft: theme.spacing(2),
+            paddingRight: theme.spacing(2)
+        },
+        color: CustomColours.colours.gray1   
     }
 }
 ));
--- TokanPages.ClientApp/src/Components/Clients/clientsView.tsx
+++ TokanPages.ClientApp/src/Components/Clients/clientsView.tsx
@@ -1,16 +1,41 @@
+import { Box, Container } from "@material-ui/core";
+import Skeleton from "@material-ui/lab/Skeleton";
 import * as React from "react";
 import { IGetClientsContent } from "../../Redux/States/Content/getClientsContentState";
+import { ICONS_PATH } from "../../Shared/constants";
 import ClientsStyle from "./clientsStyle";
 
 const ClientsView = (props: IGetClientsContent): JSX.Element => 
 {
     const classes = ClientsStyle();
 
-    console.log(props);
+    const getImagePath = (value: string): string => 
+    {
+        return `${ICONS_PATH}${value}`;
+    }
+
+    const RenderImages = (): JSX.Element => 
+    {
+        return(
+            <Box pt={4} display="flex" flexWrap="wrap" justifyContent="center">
+                <div data-aos="zoom-in">
+                    {props.content?.images.map((item, index) => (
+                        <img key={index} src={getImagePath(item)} alt="" className={classes.logo} />
+                    ))}
+                </div>
+            </Box>
+        );
+    }
 
     return(
-        <div className={classes.section}>
-        </div>
+        <>
+        <div className={classes.divider}></div>
+        <section className={classes.section}>
+            <Container maxWidth="lg">
+                {props.isLoading ? <Skeleton variant="rect" height="48px" /> : <RenderImages />}
+            </Container>
+        </section>
+        </>
     );
 }
 
