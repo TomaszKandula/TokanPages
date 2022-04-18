f169daff78cab783c2b82196a6fe4bb730d2ab76
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2022-04-19 01:05:40 +0200

    feat: apply imports from shared utilities and error services

--- TokanPages.ClientApp/src/Shared/Services/updateUserDataService.ts
+++ TokanPages.ClientApp/src/Shared/Services/updateUserDataService.ts
@@ -1,6 +1,6 @@
 import { useDispatch, useSelector } from "react-redux";
 import { USER_DATA } from "../../Shared/constants";
-import { GetDataFromStorage } from "../../Shared/helpers";
+import { GetDataFromStorage } from "./StorageServices";
 import { ActionCreators } from "../../Redux/Actions/Users/updateUserDataAction";
 import { IApplicationState } from "../../Redux/applicationState";
 import { IAuthenticateUserResultDto } from "../../Api/Models";
@@ -10,7 +10,7 @@ export const UpdateUserData = (): void =>
 {
     const dispatch = useDispatch();    
     const selector = useSelector((state: IApplicationState) => state.updateUserData);
-    const data = GetDataFromStorage(USER_DATA) as IAuthenticateUserResultDto;
+    const data = GetDataFromStorage({ key: USER_DATA }) as IAuthenticateUserResultDto;
 
     if (Object.entries(data).length !== 0 && Validate.isEmpty(selector?.userData?.userId))
     {
