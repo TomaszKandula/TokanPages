a262a2408956e80b2fc4aae559ede16527383212
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2022-04-19 10:45:57 +0200

    fix: resolve implementation error

--- TokanPages.ClientApp/src/Shared/Services/Renderers/RenderHtmlLine/renderHtmlLine.ts
+++ TokanPages.ClientApp/src/Shared/Services/Renderers/RenderHtmlLine/renderHtmlLine.ts
@@ -3,5 +3,5 @@ import { IRenderHtmlLine } from "./interface";
 
 export const RenderHtmlLine = (props: IRenderHtmlLine): string => 
 {
-    return Validate.isDefined(Text) ? `<${props.tag}>${props.text}</${props.tag}>` : " ";
+    return Validate.isDefined(props.text) ? `<${props.tag}>${props.text}</${props.tag}>` : " ";
 }
