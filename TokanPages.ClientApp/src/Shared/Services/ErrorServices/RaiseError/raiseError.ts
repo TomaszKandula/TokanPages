32e94ee7561f88e048713a47da2fbe8d562d6998
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2022-04-19 12:30:14 +0200

    fix: resolve undefined error

--- TokanPages.ClientApp/src/Shared/Services/ErrorServices/RaiseError/raiseError.ts
+++ TokanPages.ClientApp/src/Shared/Services/ErrorServices/RaiseError/raiseError.ts
@@ -1,10 +1,22 @@
+import { UNEXPECTED_ERROR } from "../../../../Shared/constants";
 import { RAISE_ERROR } from "../../../../Redux/Actions/raiseErrorAction";
 import { GetErrorMessage } from "../GetErrorMessage/getErrorMessage";
 import { IRaiseError } from "./interface";
 
 export const RaiseError = (props: IRaiseError): string =>
 {
-    let error = typeof(props.errorObject) !== "string" ? GetErrorMessage(props.errorObject) : props.errorObject;
+    let error: string = UNEXPECTED_ERROR;
+    const errorObjectType = typeof(props.errorObject);
+
+    if (errorObjectType === "object")
+    {
+        error = GetErrorMessage({ errorObject: props.errorObject });
+    }
+    else if (errorObjectType === "string")
+    {
+        error = props.errorObject;
+    }
+
     props.dispatch({ type: RAISE_ERROR, errorObject: error });
     return "";
 }
