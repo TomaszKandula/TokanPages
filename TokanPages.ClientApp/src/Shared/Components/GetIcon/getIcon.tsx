d7313005a762e9edb921fcaef808cdff983cc221
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2022-04-21 01:25:19 +0200

    feat: allow to display non-material icons like GitHub (not in Google material library)

--- TokanPages.ClientApp/src/Shared/Components/GetIcon/getIcon.tsx
+++ TokanPages.ClientApp/src/Shared/Components/GetIcon/getIcon.tsx
@@ -1,17 +1,37 @@
 import * as React from "react";
-import Icon from '@material-ui/core/Icon';
+import Icon from "@material-ui/core/Icon";
+import GitHub from "@material-ui/icons/GitHub";
+import LinkedIn from "@material-ui/icons/LinkedIn";
 
 interface IProperty
 {
     iconName: string;
 }
 
+const GetNonMaterialIcon = (name: string): JSX.Element | undefined => 
+{
+    switch(name)
+    {
+        case "github": return <GitHub />;
+        case "linkedin": return <LinkedIn />;
+        default: return undefined;
+    }
+}
+
 export const GetIcon = (props: IProperty): JSX.Element =>
 {
-    return(
-        <Icon>
-            {props.iconName !== "" 
-            ? props.iconName.toLowerCase() 
-            : "star"}
-        </Icon>);
+    let iconName = props.iconName !== "" ? props.iconName.toLowerCase() : "X"
+    
+    const DisplayIcon = (args: { name: string }): JSX.Element => 
+    {
+        const nonMaterialIcon = GetNonMaterialIcon(args.name);
+        if (nonMaterialIcon !== undefined)
+        {
+            return(nonMaterialIcon);
+        }
+
+        return(<Icon>{args.name}</Icon>);
+    }
+ 
+    return(<DisplayIcon name={iconName} />);
 }
