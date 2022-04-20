3464a85873f688b2f898c50d672dda5193db4d19
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2022-04-20 18:15:32 +0200

    refactor: simplify code

--- TokanPages.ClientApp/src/Shared/Components/GetIcon/getIcon.tsx
+++ TokanPages.ClientApp/src/Shared/Components/GetIcon/getIcon.tsx
@@ -1,30 +1,5 @@
 import * as React from "react";
-import { 
-    Apple, 
-    Assignment, 
-    Build, 
-    ContactMail, 
-    DirectionsBike, 
-    Gavel, 
-    Home, 
-    MusicNote, 
-    Person, 
-    PersonAdd, 
-    PhotoCamera,
-    PhotoAlbum, 
-    Policy, 
-    Star, 
-    Subject,
-    SportsSoccer, 
-    ViewList, 
-    VpnKey,
-    Lock,
-    Code,
-    GitHub,
-    MenuBook,
-    Edit,
-    VideoLibrary,
-} from "@material-ui/icons";
+import Icon from '@material-ui/core/Icon';
 
 interface IProperty
 {
@@ -33,32 +8,10 @@ interface IProperty
 
 export const GetIcon = (props: IProperty): JSX.Element =>
 {
-    switch(props.iconName)
-    {
-        case "Person": return <Person />;
-        case "PersonAdd": return <PersonAdd />;
-        case "Home": return <Home />;
-        case "ViewList": return <ViewList />;
-        case "Subject": return <Subject />;
-        case "Build": return <Build />;
-        case "Assignment": return <Assignment />;
-        case "Star": return <Star />;
-        case "PhotoCamera": return <PhotoCamera />;
-        case "PhotoAlbum": return <PhotoAlbum />;
-        case "SportsSoccer": return <SportsSoccer />;
-        case "MusicNote": return <MusicNote />;
-        case "DirectionsBike": return <DirectionsBike />;
-        case "ContactMail": return <ContactMail />;
-        case "Gavel": return <Gavel />;
-        case "Policy": return <Policy />;
-        case "VpnKey": return <VpnKey />;
-        case "Lock": return <Lock />; 
-        case "Code": return <Code />; 
-        case "GitHub": return <GitHub />; 
-        case "MenuBook": return <MenuBook />; 
-        case "Edit": return <Edit />;
-        case "VideoLibrary": return <VideoLibrary />;
-
-        default: return <Apple />;
-    }
+    return(
+        <Icon>
+            {props.iconName !== "" 
+            ? props.iconName.toLowerCase() 
+            : "star"}
+        </Icon>);
 }
