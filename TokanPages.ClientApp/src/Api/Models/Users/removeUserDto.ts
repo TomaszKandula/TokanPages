a564ed3ee9951a2c1ea7d7bbb828c3ac83d982e4
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2022-05-14 21:20:47 +0200

    feat: update DTO models, allow null value

--- TokanPages.ClientApp/src/Api/Models/Users/removeUserDto.ts
+++ TokanPages.ClientApp/src/Api/Models/Users/removeUserDto.ts
@@ -1,4 +1,4 @@
 export interface IRemoveUserDto
 {
-    id: string;
+    id?: string;
 }
--- TokanPages.ClientApp/src/Api/Models/Users/updateUserPasswordDto.ts
+++ TokanPages.ClientApp/src/Api/Models/Users/updateUserPasswordDto.ts
@@ -1,6 +1,6 @@
 export interface IUpdateUserPasswordDto
 {
-    id: string;
+    id?: string;
     resetId?: string;
     oldPassword?: string;
     newPassword: string;
