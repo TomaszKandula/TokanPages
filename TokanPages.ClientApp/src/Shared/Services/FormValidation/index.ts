ecd800b08d38a3f824926deffc281c62f5aa0b0f
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2022-05-14 14:41:20 +0200

    feat: add new validator for the update password form

--- TokanPages.ClientApp/src/Shared/Services/FormValidation/index.ts
+++ TokanPages.ClientApp/src/Shared/Services/FormValidation/index.ts
@@ -5,6 +5,7 @@ export type { IValidateSigninForm } from "./ValidateSigninForm/interface";
 export type { IValidateSignupForm } from "./ValidateSignupForm/interface";
 export type { IValidateUpdateForm } from "./ValidateUpdateForm/interface";
 export type { IValidateAccountForm } from "./ValidateAccountForm/interface";
+export type { IValidatePasswordForm } from "./ValidatePasswordForm/interface";
 
 export { ValidateContactForm } from "./ValidateContactForm/validateContactForm";
 export { ValidateEmailForm } from "./ValidateEmailForm/validateEmailForm";
@@ -13,3 +14,4 @@ export { ValidateSigninForm } from "./ValidateSigninForm/validateSigninForm";
 export { ValidateSignupForm } from "./ValidateSignupForm/validateSignupForm";
 export { ValidateUpdateForm } from "./ValidateUpdateForm/validateUpdateForm";
 export { ValidateAccountForm } from "./ValidateAccountForm/validateAccountForm";
+export { ValidatePasswordForm } from "./ValidatePasswordForm/validatePasswordForm";
