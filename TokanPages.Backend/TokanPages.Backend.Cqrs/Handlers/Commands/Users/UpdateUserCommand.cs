5903914da7187d5aac3b617c9d822119e17d4319
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2022-05-14 20:44:03 +0200

    feat: make user ID optional in payload

--- TokanPages.Backend/TokanPages.Backend.Cqrs/Handlers/Commands/Users/UpdateUserCommand.cs
+++ TokanPages.Backend/TokanPages.Backend.Cqrs/Handlers/Commands/Users/UpdateUserCommand.cs
@@ -6,7 +6,7 @@ using MediatR;
 
 public class UpdateUserCommand : IRequest<Unit>
 {
-    public Guid Id { get; set; }
+    public Guid? Id { get; set; }
 
     public bool IsActivated { get; set; }
 
