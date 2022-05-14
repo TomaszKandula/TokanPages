efc0245ffd6694b751ceca79f38d1cf516fb9d00
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2022-05-14 20:36:46 +0200

    feat: check given refresh token against user data

--- TokanPages.Backend/TokanPages.Backend.Cqrs/Handlers/Commands/Users/RevokeUserRefreshTokenCommandHandler.cs
+++ TokanPages.Backend/TokanPages.Backend.Cqrs/Handlers/Commands/Users/RevokeUserRefreshTokenCommandHandler.cs
@@ -10,7 +10,7 @@ using Shared.Resources;
 using Services.UserService;
 using Core.Utilities.LoggerService;
 using MediatR;
-    
+
 public class RevokeUserRefreshTokenCommandHandler : Cqrs.RequestHandler<RevokeUserRefreshTokenCommand, Unit>
 {
     private readonly IUserService _userService;
@@ -23,16 +23,18 @@ public class RevokeUserRefreshTokenCommandHandler : Cqrs.RequestHandler<RevokeUs
 
     public override async Task<Unit> Handle(RevokeUserRefreshTokenCommand request, CancellationToken cancellationToken)
     {
+        var user = await _userService.GetUser();
         var refreshTokens = await DatabaseContext.UserRefreshTokens
+            .Where(tokens => tokens.UserId == user.UserId)
             .Where(tokens => tokens.Token == request.RefreshToken)
             .SingleOrDefaultAsync(cancellationToken);
 
         if (refreshTokens == null)
-            throw new BusinessException(nameof(ErrorCodes.INVALID_REFRESH_TOKEN), ErrorCodes.INVALID_REFRESH_TOKEN);
+            throw new AuthorizationException(nameof(ErrorCodes.INVALID_REFRESH_TOKEN), ErrorCodes.INVALID_REFRESH_TOKEN);
 
         var requestIpAddress = _userService.GetRequestIpAddress();
-        const string reason = "Revoked by Admin";
-            
+        var reason = $"Revoked by {user.AliasName} (ID: {user.UserId})";
+
         await _userService.RevokeRefreshToken(refreshTokens, requestIpAddress, reason, null, true, cancellationToken);            
         return Unit.Value;
     }
