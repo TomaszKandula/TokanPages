4f800148978ae6338b533f226643b096ee110562
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2022-05-15 12:16:04 +0200

    feat: add redux for removal action

---
+++ TokanPages.ClientApp/src/Redux/Reducers/Users/removeAccountReducer.ts
@@ -0,0 +1,38 @@
+import { Action, Reducer } from "redux";
+import { combinedDefaults } from "../../combinedDefaults";
+import { IRemoveAccount } from "../../../Redux/States/Users/removeAccountState";
+import { OperationStatus } from "../../../Shared/enums";
+import { 
+    TKnownActions,
+    REMOVE_ACCOUNT,
+    REMOVE_ACCOUNT_CLEAR,
+    REMOVE_ACCOUNT_RESPONSE
+} from "../../Actions/Users/removeAccountAction";
+
+export const RemoveAccountReducer: Reducer<IRemoveAccount> = (state: IRemoveAccount | undefined, incomingAction: Action): IRemoveAccount => 
+{
+    if (state === undefined) return combinedDefaults.removeAccount;
+
+    const action = incomingAction as TKnownActions;
+    switch (action.type) 
+    {
+        case REMOVE_ACCOUNT_CLEAR:
+            return {
+                operationStatus: OperationStatus.notStarted,
+                attachedErrorObject: { }
+            };
+        case REMOVE_ACCOUNT:
+            return { 
+                operationStatus: OperationStatus.inProgress,
+                attachedErrorObject: state.attachedErrorObject
+            };
+
+        case REMOVE_ACCOUNT_RESPONSE:
+            return { 
+                operationStatus: OperationStatus.hasFinished,
+                attachedErrorObject: { }
+            };
+
+        default: return state;
+    }
+};
