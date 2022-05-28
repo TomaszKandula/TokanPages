82ad9cc0a19d0a858e26ff22f28690a65b98bfcb
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2022-05-28 16:27:34 +0200

    feat: add domain contracts

---
+++ TokanPages.Backend/TokanPages.Backend.Domain/Contracts/IAuditable.cs
@@ -0,0 +1,14 @@
+﻿namespace TokanPages.Backend.Domain.Contracts;
+
+using System;
+
+public interface IAuditable
+{
+	Guid CreatedBy { get; set; }
+
+	DateTime CreatedAt { get; set; }
+
+	Guid? ModifiedBy { get; set; }
+
+	DateTime? ModifiedAt { get; set; }
+}
