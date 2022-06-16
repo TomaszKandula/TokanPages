745a82e3c684f343847668f6ad9f4cfa481b3831
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2022-06-16 12:41:56 +0200

    refactor: allow nullable in the project

--- TokanPages.Services/TokanPages.Services.AzureStorageService/Models/StorageStreamContent.cs
+++ TokanPages.Services/TokanPages.Services.AzureStorageService/Models/StorageStreamContent.cs
@@ -6,7 +6,7 @@ using System.Diagnostics.CodeAnalysis;
 [ExcludeFromCodeCoverage]
 public class StorageStreamContent
 {
-    public Stream Content { get; set; }
+    public Stream? Content { get; set; }
 
-    public string ContentType { get; set; }
+    public string? ContentType { get; set; }
 }
