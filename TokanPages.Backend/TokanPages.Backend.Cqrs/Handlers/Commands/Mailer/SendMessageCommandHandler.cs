6feb136a71a8d2e53258803abf583f0438c10a74
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2022-06-18 15:59:44 +0200

    refactor: use updated application settings

--- TokanPages.Backend/TokanPages.Backend.Cqrs/Handlers/Commands/Mailer/SendMessageCommandHandler.cs
+++ TokanPages.Backend/TokanPages.Backend.Cqrs/Handlers/Commands/Mailer/SendMessageCommandHandler.cs
@@ -5,7 +5,6 @@ using System.Threading;
 using System.Globalization;
 using System.Threading.Tasks;
 using System.Collections.Generic;
-using Shared;
 using Database;
 using Shared.Services;
 using Core.Extensions;
@@ -50,14 +49,18 @@ public class SendMessageCommandHandler : Cqrs.RequestHandler<SendMessageCommand,
             { "{DATE_TIME}", dateTime }
         };
 
-        var templateUrl = $"{_applicationSettings.AzureStorage.BaseUrl}{Constants.Emails.Templates.ContactForm}";
+        var baseUrl = _applicationSettings.AzureStorage.BaseUrl;
+        var contactForm = _applicationSettings.ApplicationPaths.Templates.ContactForm;
+
+        var templateUrl = $"{baseUrl}{contactForm}";
         var template = await _emailSenderService.GetEmailTemplate(templateUrl, cancellationToken);
         LoggerService.LogInformation($"Getting email template from URL: {templateUrl}.");
 
+        var contact = _applicationSettings.EmailSender.Addresses.Contact;
         var payload = new SenderPayloadDto
         {
-            From = Constants.Emails.Addresses.Contact,
-            To = new List<string> { Constants.Emails.Addresses.Contact },
+            From = contact,
+            To = new List<string> { contact },
             Subject = $"New user message from {request.FirstName}",
             Body = template.MakeBody(templateValues)
         };
