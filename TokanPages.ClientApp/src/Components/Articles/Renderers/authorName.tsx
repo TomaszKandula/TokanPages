93024f8db2eaad7ac510ebc9e25cfdca9b7734cf
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2022-07-09 17:08:17 +0200

    refactor: simplify code

--- TokanPages.ClientApp/src/Components/Articles/Renderers/authorName.tsx
+++ TokanPages.ClientApp/src/Components/Articles/Renderers/authorName.tsx
@@ -1,9 +1,5 @@
 export const AuthorName = (firstName: string, lastName: string, aliasName: string) => 
 {
-    const fullNameWithAlias = firstName + " '" 
-        + aliasName + "' " 
-        + lastName;
-    return firstName && lastName 
-        ? fullNameWithAlias
-        : aliasName;
+    const fullNameWithAlias = `${firstName} '${aliasName}' ${lastName}`;
+    return firstName && lastName ? fullNameWithAlias : aliasName;
 };
