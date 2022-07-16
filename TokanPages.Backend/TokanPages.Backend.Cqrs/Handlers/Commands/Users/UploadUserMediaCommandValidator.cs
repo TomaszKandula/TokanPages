a16f3332b5c254c19c23ecda7144051b147f4fdc
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2022-07-16 21:11:10 +0200

    feat: add new properties to be validated

--- TokanPages.Backend/TokanPages.Backend.Cqrs/Handlers/Commands/Users/UploadUserMediaCommandValidator.cs
+++ TokanPages.Backend/TokanPages.Backend.Cqrs/Handlers/Commands/Users/UploadUserMediaCommandValidator.cs
@@ -1,6 +1,7 @@
 namespace TokanPages.Backend.Cqrs.Handlers.Commands.Users;
 
 using System;
+using Domain.Enums;
 using Shared.Services;
 using Shared.Resources;
 using FluentValidation;
@@ -18,6 +19,28 @@ public class UploadUserMediaCommandValidator : AbstractValidator<UploadUserMedia
                 .WithErrorCode(nameof(ValidationCodes.INVALID_GUID_VALUE))
                 .WithMessage(ValidationCodes.INVALID_GUID_VALUE);
         });
+
+        RuleFor(command => command.MediaTarget)
+            .IsInEnum()
+            .NotEqual(UserMedia.NotSpecified)
+            .WithErrorCode(nameof(ValidationCodes.NOT_SPECIFIED_MEDIA_TARGET))
+            .WithMessage(ValidationCodes.NOT_SPECIFIED_MEDIA_TARGET);
+
+        RuleFor(command => command.MediaName)
+            .NotEmpty()
+            .WithErrorCode(nameof(ValidationCodes.REQUIRED))
+            .WithMessage(ValidationCodes.REQUIRED)
+            .MaximumLength(100)
+            .WithErrorCode(nameof(ValidationCodes.TOO_LONG_MEDIA_NAME))
+            .WithMessage(ValidationCodes.TOO_LONG_MEDIA_NAME);
+
+        RuleFor(command => command.MediaType)
+            .NotEmpty()
+            .WithErrorCode(nameof(ValidationCodes.REQUIRED))
+            .WithMessage(ValidationCodes.REQUIRED)
+            .MaximumLength(100)
+            .WithErrorCode(nameof(ValidationCodes.TOO_LONG_MEDIA_TYPE))
+            .WithMessage(ValidationCodes.TOO_LONG_MEDIA_TYPE);
 
         RuleFor(command => command.Data)
             .NotEmpty()
