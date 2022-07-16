7fbeba421a546c58bc52089c78d897420c5d325b
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2022-07-16 12:12:31 +0200

    feat: convert content type to media type

--- TokanPages.Backend/TokanPages.Backend.Cqrs/Mappers/AssetsMapper.cs
+++ TokanPages.Backend/TokanPages.Backend.Cqrs/Mappers/AssetsMapper.cs
@@ -1,8 +1,9 @@
 namespace TokanPages.Backend.Cqrs.Mappers;
 
 using System.Diagnostics.CodeAnalysis;
-using Core.Extensions;
 using Dto.Assets;
+using Shared.Constants;
+using Core.Extensions;
 using Handlers.Commands.Assets;
 
 [ExcludeFromCodeCoverage]
@@ -11,7 +12,19 @@ public static class AssetsMapper
     public static AddSingleAssetCommand MapToAddSingleAssetCommand(AddSingleAssetDto model) => new()
     {
         MediaName = model.Data.FileName,
-        MediaType = model.Data.ContentType,
+        MediaType = model.Data.ContentType.ToMediaType(),
         Data = model.Data.GetByteArray()
     };
+
+    private static string ToMediaType(this string contentType)
+    {
+        return contentType switch
+        {
+            ContentTypes.ImageJpeg => "images",
+            ContentTypes.ImagePng => "images",
+            ContentTypes.ImageSvg => "images",
+            ContentTypes.VideoMpeg => "videos",
+            _ => "uncategorized"
+        };
+    }
 }
