58ae5ec98e19ea1801a58944c2a0f68b1ab0cc07
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2022-08-06 19:12:48 +0200

    docs: remove comments

--- TokanPages.ClientApp/src/index.tsx
+++ TokanPages.ClientApp/src/index.tsx
@@ -9,12 +9,8 @@ import CssBaseline from "@material-ui/core/CssBaseline";
 import configureStore from "./Redux/configureStore";
 import App from "./app";
 
-// Create browser history to use in the Redux store
 const baseUrl = document.getElementsByTagName("base")[0].getAttribute("href") as string;
 const history = createBrowserHistory({ basename: baseUrl });
-
-// Get the application-wide store instance, 
-// prepopulating with state from the server where available.
 const store = configureStore(history);
 
 ReactDOM.render(
