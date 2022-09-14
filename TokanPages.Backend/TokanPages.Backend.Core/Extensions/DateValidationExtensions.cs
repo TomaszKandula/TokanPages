80202c85490f77bd60b2aefc838709b1f164a529
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2022-09-14 10:32:11 +0200

    refactor: relocate files

--- TokanPages.Backend/TokanPages.Backend.Core/Extensions/DateValidationExtensions.cs
+++ TokanPages.Backend/TokanPages.Backend.Core/Extensions/DateValidationExtensions.cs
@@ -11,9 +11,9 @@ public static class DateValidationExtensions
     public static IRuleBuilderOptions<T, DateTime> IsSameOrLaterThanDate<T>(this IRuleBuilder<T, DateTime> ruleBuilder, DateTime beforeDate)
         => ruleBuilder.Must(date => date >= beforeDate);
 
-    public static IRuleBuilderOptions<T, DateRangeValidator> IsValidDateRange<T>(this IRuleBuilder<T, DateRangeValidator> ruleBuilder)
+    public static IRuleBuilderOptions<T, DateRangeModel> IsValidDateRange<T>(this IRuleBuilder<T, DateRangeModel> ruleBuilder)
         =>  ruleBuilder.Must(date => date.StartDate <= date.EndDate);
         
-    public static IRuleBuilderOptions<T, DateRangeValidator> AreDatesSame<T>(this IRuleBuilder<T, DateRangeValidator> ruleBuilder)
+    public static IRuleBuilderOptions<T, DateRangeModel> AreDatesSame<T>(this IRuleBuilder<T, DateRangeModel> ruleBuilder)
         =>  ruleBuilder.Must(date => date.StartDate.Date != date.EndDate.Date);
 }
--- TokanPages.Backend/TokanPages.Backend.Core/Extensions/Models/DateRangeModel.cs
+++ TokanPages.Backend/TokanPages.Backend.Core/Extensions/Models/DateRangeModel.cs
@@ -4,12 +4,12 @@ using System.Diagnostics.CodeAnalysis;
 namespace TokanPages.Backend.Core.Models;
 
 [ExcludeFromCodeCoverage]
-public class DateRangeValidator
+public class DateRangeModel
 {
     public DateTime StartDate { get; }
     public DateTime EndDate { get; }
 
-    public DateRangeValidator(DateTime startDate, DateTime endDate)
+    public DateRangeModel(DateTime startDate, DateTime endDate)
     {
         StartDate = startDate;
         EndDate = endDate;
