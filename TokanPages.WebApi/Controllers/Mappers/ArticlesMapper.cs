3c3bfa4997d3ea377d763d5fc3a8679c03f81f01
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2022-09-14 17:03:13 +0200

    docs: add XML comments

--- TokanPages.WebApi/Controllers/Mappers/ArticlesMapper.cs
+++ TokanPages.WebApi/Controllers/Mappers/ArticlesMapper.cs
@@ -4,9 +4,17 @@ using TokanPages.WebApi.Dto.Articles;
 
 namespace TokanPages.WebApi.Controllers.Mappers;
 
+/// <summary>
+/// Articles mapper
+/// </summary>
 [ExcludeFromCodeCoverage]
 public static class ArticlesMapper
 {
+    /// <summary>
+    /// Maps request DTO to given command 
+    /// </summary>
+    /// <param name="model">AddArticleDto</param>
+    /// <returns>AddArticleCommand</returns>
     public static AddArticleCommand MapToAddArticleCommand(AddArticleDto model) => new()
     {
         Title = model.Title,
@@ -14,7 +22,12 @@ public static class ArticlesMapper
         TextToUpload = model.TextToUpload,
         ImageToUpload = model.ImageToUpload
     };
-        
+
+    /// <summary>
+    /// Maps request DTO to given command 
+    /// </summary>
+    /// <param name="model">UpdateArticleContentDto</param>
+    /// <returns>UpdateArticleContentCommand</returns>
     public static UpdateArticleContentCommand MapToUpdateArticleCommand(UpdateArticleContentDto model) => new()
     {
         Id = model.Id,
@@ -24,23 +37,43 @@ public static class ArticlesMapper
         ImageToUpload = model.ImageToUpload
     };
 
+    /// <summary>
+    /// Maps request DTO to given command 
+    /// </summary>
+    /// <param name="model">UpdateArticleCountDto</param>
+    /// <returns>UpdateArticleCountCommand</returns>
     public static UpdateArticleCountCommand MapToUpdateArticleCommand(UpdateArticleCountDto model) => new()
     {
         Id = model.Id
     };
-        
+
+    /// <summary>
+    /// Maps request DTO to given command 
+    /// </summary>
+    /// <param name="model">UpdateArticleLikesDto</param>
+    /// <returns>UpdateArticleLikesCommand</returns>
     public static UpdateArticleLikesCommand MapToUpdateArticleCommand(UpdateArticleLikesDto model) => new() 
     {
         Id = model.Id,
         AddToLikes = model.AddToLikes 
     };
 
+    /// <summary>
+    /// Maps request DTO to given command 
+    /// </summary>
+    /// <param name="model">UpdateArticleVisibilityDto</param>
+    /// <returns>UpdateArticleVisibilityCommand</returns>
     public static UpdateArticleVisibilityCommand MapToUpdateArticleCommand(UpdateArticleVisibilityDto model) => new()
     {
         Id = model.Id, 
         IsPublished = model.IsPublished
     };
-        
+
+    /// <summary>
+    /// Maps request DTO to given command 
+    /// </summary>
+    /// <param name="model">RemoveArticleDto</param>
+    /// <returns>RemoveArticleCommand</returns>
     public static RemoveArticleCommand MapToRemoveArticleCommand(RemoveArticleDto model) => new() 
     { 
         Id = model.Id
--- TokanPages.WebApi/Controllers/Mappers/SubscribersMapper.cs
+++ TokanPages.WebApi/Controllers/Mappers/SubscribersMapper.cs
@@ -4,14 +4,27 @@ using TokanPages.WebApi.Dto.Subscribers;
 
 namespace TokanPages.WebApi.Controllers.Mappers;
 
+/// <summary>
+/// Subscribers mapper
+/// </summary>
 [ExcludeFromCodeCoverage]
 public static class SubscribersMapper
 {
+    /// <summary>
+    /// Maps request DTO to given command
+    /// </summary>
+    /// <param name="model">AddSubscriberDto</param>
+    /// <returns>AddSubscriberCommand</returns>
     public static AddSubscriberCommand MapToAddSubscriberCommand(AddSubscriberDto model) => new()
     {
         Email = model.Email
     };
 
+    /// <summary>
+    /// Maps request DTO to given command
+    /// </summary>
+    /// <param name="model">UpdateSubscriberDto</param>
+    /// <returns>UpdateSubscriberCommand</returns>
     public static UpdateSubscriberCommand MapToUpdateSubscriberCommand(UpdateSubscriberDto model) => new() 
     { 
         Id = model.Id,
@@ -20,6 +33,11 @@ public static class SubscribersMapper
         Count = model.Count
     };
 
+    /// <summary>
+    /// Maps request DTO to given command
+    /// </summary>
+    /// <param name="model">RemoveSubscriberDto</param>
+    /// <returns>RemoveSubscriberCommand</returns>
     public static RemoveSubscriberCommand MapToRemoveSubscriberCommand(RemoveSubscriberDto model) => new() 
     { 
         Id = model.Id
