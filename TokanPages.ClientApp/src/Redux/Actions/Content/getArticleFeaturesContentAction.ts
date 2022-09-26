0d4eef2449e1ce242cc41948c52d78542360a9af
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2022-09-26 23:37:48 +0200

    feat: upgrade actions for user language capability

--- TokanPages.ClientApp/src/Redux/Actions/Content/getArticleFeaturesContentAction.ts
+++ TokanPages.ClientApp/src/Redux/Actions/Content/getArticleFeaturesContentAction.ts
@@ -7,7 +7,6 @@ import { GET_ARTICLE_FEAT_CONTENT, NULL_RESPONSE_ERROR } from "../../../Shared/c
 import { TErrorActions } from "../raiseErrorAction";
 import { IArticleFeaturesContentDto } from "../../../Api/Models";
 import { EnrichConfiguration } from "../../../Api/Request";
-import { GetUserLanguageFromStore } from "../../../Shared/Services/languageService";
 
 export const REQUEST_ARTICE_FEATURES = "REQUEST_ARTICE_FEATURES";
 export const RECEIVE_ARTICE_FEATURES = "RECEIVE_ARTICE_FEATURES";
@@ -26,8 +25,8 @@ export const ActionCreators =
 
         dispatch({ type: REQUEST_ARTICE_FEATURES });
 
-        const language = GetUserLanguageFromStore();
-        const queryParam = language === "" ? "" : `&language=${language}`;
+        const id = getState().userLanguage.id;
+        const queryParam = id === "" ? "" : `&language=${id}`;
 
         axios(EnrichConfiguration(
         {
--- TokanPages.ClientApp/src/Redux/Actions/Content/getResetPasswordContentAction.ts
+++ TokanPages.ClientApp/src/Redux/Actions/Content/getResetPasswordContentAction.ts
@@ -7,7 +7,6 @@ import { GET_RESET_PASSWORD_CONTENT, NULL_RESPONSE_ERROR } from "../../../Shared
 import { TErrorActions } from "./../raiseErrorAction";
 import { IResetPasswordContentDto } from "../../../Api/Models";
 import { EnrichConfiguration } from "../../../Api/Request";
-import { GetUserLanguageFromStore } from "../../../Shared/Services/languageService";
 
 export const REQUEST_RESET_PASSWORD_CONTENT = "REQUEST_RESET_PASSWORD_CONTENT";
 export const RECEIVE_RESET_PASSWORD_CONTENT = "RECEIVE_RESET_PASSWORD_CONTENT";
@@ -26,8 +25,8 @@ export const ActionCreators =
 
         dispatch({ type: REQUEST_RESET_PASSWORD_CONTENT });
 
-        const language = GetUserLanguageFromStore();
-        const queryParam = language === "" ? "" : `&language=${language}`;
+        const id = getState().userLanguage.id;
+        const queryParam = id === "" ? "" : `&language=${id}`;
 
         axios(EnrichConfiguration(
         {
