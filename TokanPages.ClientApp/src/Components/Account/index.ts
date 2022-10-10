12feb35e63249126bf010a7a1f62df15b4fcd437
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2022-10-10 17:21:06 +0200

    refactor: replace one large component with many smaller components

--- TokanPages.ClientApp/src/Components/Account/index.ts
+++ TokanPages.ClientApp/src/Components/Account/index.ts
@@ -1,11 +1,9 @@
 export { ActivateAccount } from "./ActivateAccount/activateAccount";
 export { ResetPassword } from "./ResetPassword/resetPassword";
 export { UpdatePassword } from "./UpdatePassword/updatePassword";
-export { UserAccount } from "./UserAccount/userAccount";
-export { UserSignin } from "./UserSignin/userSignin";
-export { UserSignout } from "./UserSignout/userSignout";
-export { UserSignup } from "./UserSignup/userSignup";
-
 export { UserInfo } from "./UserAccount/UserInfo/userInfo";
 export { UserPassword } from "./UserAccount/UserPassword/userPassword";
 export { UserRemoval } from "./UserAccount/UserRemoval/userRemoval";
+export { UserSignin } from "./UserSignin/userSignin";
+export { UserSignout } from "./UserSignout/userSignout";
+export { UserSignup } from "./UserSignup/userSignup";
--- TokanPages.ClientApp/src/Pages/AccountPage/index.tsx
+++ TokanPages.ClientApp/src/Pages/AccountPage/index.tsx
@@ -2,7 +2,9 @@ import * as React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { IApplicationState } from "../../Store/Configuration";
 import { Navigation, Footer } from "../../Components/Layout";
-import { UserAccount } from "../../Components/Account";
+import { UserInfo } from "../../Components/Account";
+import { UserPassword } from "../../Components/Account";
+import { UserRemoval } from "../../Components/Account";
 
 import { 
     ContentNavigationAction, 
@@ -30,7 +32,9 @@ export const AccountPage = (): JSX.Element =>
     return(
         <>
             <Navigation content={navigation?.content} isLoading={navigation?.isLoading} />
-            <UserAccount content={account?.content} isLoading={navigation?.isLoading} />
+            <UserInfo content={account?.content} isLoading={navigation?.isLoading}  />
+            <UserPassword content={account?.content} isLoading={navigation?.isLoading} />
+            <UserRemoval content={account?.content} isLoading={navigation?.isLoading} />
             <Footer content={footer?.content} isLoading={footer?.isLoading} />
         </>
     );
