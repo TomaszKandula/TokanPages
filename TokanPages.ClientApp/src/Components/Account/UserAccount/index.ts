e83b990f83be389a3b056e89b37b7549e185b9ee
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2022-10-10 17:23:43 +0200

    refactor: add and use index for account components

---
+++ TokanPages.ClientApp/src/Components/Account/UserAccount/index.ts
@@ -0,0 +1,3 @@
+export { UserInfo } from "./UserInfo/userInfo";
+export { UserPassword } from "./UserPassword/userPassword";
+export { UserRemoval } from "./UserRemoval/userRemoval";
--- TokanPages.ClientApp/src/Components/Account/index.ts
+++ TokanPages.ClientApp/src/Components/Account/index.ts
@@ -1,9 +1,10 @@
 export { ActivateAccount } from "./ActivateAccount/activateAccount";
 export { ResetPassword } from "./ResetPassword/resetPassword";
 export { UpdatePassword } from "./UpdatePassword/updatePassword";
-export { UserInfo } from "./UserAccount/UserInfo/userInfo";
-export { UserPassword } from "./UserAccount/UserPassword/userPassword";
-export { UserRemoval } from "./UserAccount/UserRemoval/userRemoval";
 export { UserSignin } from "./UserSignin/userSignin";
 export { UserSignout } from "./UserSignout/userSignout";
 export { UserSignup } from "./UserSignup/userSignup";
+
+export { UserInfo } from "./UserAccount";
+export { UserPassword } from "./UserAccount";
+export { UserRemoval } from "./UserAccount";
