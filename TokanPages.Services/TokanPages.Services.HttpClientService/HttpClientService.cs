0337a2cdb62ec40ed65d3bcaa3ddf2363acf10f1
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2022-12-16 23:40:08 +0100

    feat: allow redirect

--- TokanPages.Services/TokanPages.Services.HttpClientService/HttpClientService.cs
+++ TokanPages.Services/TokanPages.Services.HttpClientService/HttpClientService.cs
@@ -1,3 +1,4 @@
+using System.Net;
 using System.Text;
 using Microsoft.AspNetCore.WebUtilities;
 using Newtonsoft.Json;
@@ -36,7 +37,7 @@ public class HttpClientService : IHttpClientService
         var contentType = response.Content.Headers.ContentType;
         var content = await response.Content.ReadAsByteArrayAsync(cancellationToken);
 
-        if (response.IsSuccessStatusCode)
+        if (response.IsSuccessStatusCode || response.StatusCode == HttpStatusCode.Redirect)
             return new ExecutionResult
             {
                 StatusCode = response.StatusCode,
@@ -57,8 +58,16 @@ public class HttpClientService : IHttpClientService
         var content = await response.Content.ReadAsByteArrayAsync(cancellationToken);
         var stringContent = Encoding.ASCII.GetString(content);
 
-        if (response.IsSuccessStatusCode) 
-            return JsonConvert.DeserializeObject<T>(stringContent, GetSettings())!;
+        try
+        {
+            if (response.IsSuccessStatusCode || response.StatusCode == HttpStatusCode.Redirect)
+                return JsonConvert.DeserializeObject<T>(stringContent, GetSettings())!;
+        }
+        catch (Exception exception)
+        {
+            _loggerService.LogError($"{ErrorCodes.CANNOT_PARSE}. Exception: {exception}. Full response: {stringContent}.");
+            throw new BusinessException(nameof(ErrorCodes.CANNOT_PARSE), ErrorCodes.CANNOT_PARSE);
+        }
 
         _loggerService.LogError($"{ErrorCodes.HTTP_REQUEST_FAILED}. Full response: {stringContent}.");
         throw new BusinessException(nameof(ErrorCodes.HTTP_REQUEST_FAILED), ErrorCodes.HTTP_REQUEST_FAILED);
