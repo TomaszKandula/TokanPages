4c8ca89861d19d00ba87c6318d5c0c4f1e8f68d4
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2022-12-25 20:19:21 +0100

    refactor: simplify and add usage screen

---
+++ TokanPages.Persistence/TokanPages.Persistence.MigrationRunner/Helpers/InputArguments.cs
@@ -0,0 +1,19 @@
+namespace TokanPages.Persistence.MigrationRunner.Helpers;
+
+public static class InputArguments
+{
+    public static IList<string>? Normalize(IReadOnlyCollection<string> arguments)
+    {
+        if (arguments.Count <= 0) 
+            return null;
+
+        var result = new List<string>();
+        foreach (var argument in arguments)
+        {
+            var item = argument.ToLower();
+            result.Add(item);
+        }
+
+        return result;
+    }
+}
--- TokanPages.Persistence/TokanPages.Persistence.MigrationRunner/Program.cs
+++ TokanPages.Persistence/TokanPages.Persistence.MigrationRunner/Program.cs
@@ -1,16 +1,61 @@
-﻿using TokanPages.Persistence.Database;
+﻿using TokanPages.Backend.Shared.Services;
+using TokanPages.Persistence.Database;
+using TokanPages.Persistence.MigrationRunner.Helpers;
 
 namespace TokanPages.Persistence.MigrationRunner;
 
 internal static class Program
 {
+    private const string Option1 = "  --migrate         Applies any pending migrations for the context to the database.";
+    private const string Option2 = "                    It will create the database if it does not already exist.";
+    private const string Option3 = "  --seed            Seeds the test data to the existing database.";
+    private const string Option4 = "  --migrate-seed    It will execute migration and seed the test data afterwards.";
+    
     private static void Main(string[] args)
     {
-        var databaseMigrator = new DatabaseMigrator();
-        var dataSeeder = new DataSeeder();
+        var arguments = InputArguments.Normalize(args);
+        if (arguments is null || arguments.Count > 1)
+        {
+            ConsolePrints.PrintOnInfo("");
+            ConsolePrints.PrintOnInfo("Migration Runner");
+            ConsolePrints.PrintOnInfo("----------------");
+            ConsolePrints.PrintOnInfo("");
+            ConsolePrints.PrintOnInfo("It accepts only one argument. Available options:");
+            ConsolePrints.PrintOnInfo("");
+            ConsolePrints.PrintOnInfo(Option1);
+            ConsolePrints.PrintOnInfo(Option2);
+            ConsolePrints.PrintOnInfo(Option3);
+            ConsolePrints.PrintOnInfo(Option4);
+            ConsolePrints.PrintOnInfo("");
+            return;
+        }
 
-        var connection = databaseMigrator.GetConnectionString<DatabaseContext>();
-        databaseMigrator.ValidateConnectionString(connection);
-        databaseMigrator.RunAndMigrate<DatabaseContext>(connection, nameof(DatabaseContext));
+        var connection = DatabaseConnection.GetConnectionString<DatabaseContext>();
+        DatabaseConnection.ValidateConnectionString(connection);
+
+        var migrator = new DatabaseMigrator();
+        var seeder = new DataSeeder();
+
+        foreach (var option in arguments)
+        {
+            switch (option)
+            {
+                case "--migrate":
+                    migrator.RunAndMigrate<DatabaseContext>(connection, nameof(DatabaseContext));
+                    break;
+
+                case "--seed":
+                    seeder.Seed<DatabaseContext>(connection, nameof(DatabaseContext));
+                    break;
+
+                case "--migrate-seed":
+                    migrator.RunAndMigrate<DatabaseContext>(connection, nameof(DatabaseContext));
+                    seeder.Seed<DatabaseContext>(connection, nameof(DatabaseContext));
+                    break;
+
+                default:
+                    throw new Exception("Unsupported option.");
+            }
+        }
     }
 }
