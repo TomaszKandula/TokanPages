8a747a37b1b682689a3581d907a4e498291636fc
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2023-01-06 00:24:36 +0100

    refactor: simplify code, inherit from business exception

--- TokanPages.Backend/TokanPages.Backend.Core/Exceptions/AccessException.cs
+++ TokanPages.Backend/TokanPages.Backend.Core/Exceptions/AccessException.cs
@@ -5,13 +5,11 @@ namespace TokanPages.Backend.Core.Exceptions;
 
 [ExcludeFromCodeCoverage]
 [Serializable]
-public class AccessException : Exception
+public class AccessException : BusinessException
 {
-    public string ErrorCode { get; } = "";
+    protected AccessException(SerializationInfo serializationInfo, StreamingContext streamingContext) 
+        : base(serializationInfo, streamingContext) { }
 
-    protected AccessException(SerializationInfo serializationInfo, 
-        StreamingContext streamingContext) : base(serializationInfo, streamingContext) { }
-
-    public AccessException(string errorCode, string errorMessage = "") : base(errorMessage)
-        => ErrorCode = errorCode;
+    public AccessException(string errorCode, string errorMessage = "") 
+        : base(errorCode, errorMessage) { }
 }
