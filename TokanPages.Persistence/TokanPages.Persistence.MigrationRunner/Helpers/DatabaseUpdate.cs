35fdb3398321480d2c49260f5aec9412433c55e4
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2023-01-06 15:18:04 +0100

    refactor: exclude from code coverage

--- TokanPages.Persistence/TokanPages.Persistence.MigrationRunner/Helpers/DatabaseUpdate.cs
+++ TokanPages.Persistence/TokanPages.Persistence.MigrationRunner/Helpers/DatabaseUpdate.cs
@@ -1,5 +1,8 @@
+using System.Diagnostics.CodeAnalysis;
+
 namespace TokanPages.Persistence.MigrationRunner.Helpers;
 
+[ExcludeFromCodeCoverage]
 public static class DatabaseUpdate
 {
     private const string Directory = "Resources";
