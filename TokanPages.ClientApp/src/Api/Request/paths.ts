7bd676e66c923ca4d820e5daa0ad8f790bf69e16
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2023-01-14 19:20:52 +0100

    feat: register new endpoint

--- TokanPages.ClientApp/src/Api/Request/paths.ts
+++ TokanPages.ClientApp/src/Api/Request/paths.ts
@@ -27,6 +27,7 @@ export const AUTHENTICATE = `${API_USERS_URI}/authenticateUser`;
 export const REAUTHENTICATE = `${API_USERS_URI}/reAuthenticateUser`;
 export const RESET_USER_PASSWORD = `${API_USERS_URI}/resetUserPassword`;
 export const UPDATE_USER_PASSWORD = `${API_USERS_URI}/updateUserPassword`;
+export const UPLOAD_USER_MEDIA = `${API_USERS_URI}/uploadUserMedia`;
 
 export const GET_SUBSCRIBERS = `${API_SUBSCRIBERS_URI}/getAllSubscribers`;
 export const GET_SUBSCRIBER = `${API_SUBSCRIBERS_URI}/{id}/getSubscriber`;
