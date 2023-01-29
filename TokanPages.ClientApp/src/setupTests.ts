b3e59294ca5c948cdb9394dbdf499e837503da50
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2023-01-29 01:03:58 +0100

    test: use enzyme adapter for react 17

--- TokanPages.ClientApp/src/setupTests.ts
+++ TokanPages.ClientApp/src/setupTests.ts
@@ -4,7 +4,7 @@
 // learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom";
 import Enzyme from "enzyme";
-import Adapter from "enzyme-adapter-react-16";
+import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
 import enableHooks from "jest-react-hooks-shallow";
 
 enableHooks(jest);
