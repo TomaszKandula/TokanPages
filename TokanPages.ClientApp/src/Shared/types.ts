af574afa4048bab8e15edf7d2a72547b278853a8
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2023-01-31 22:17:15 +0100

    fix: remove redundant 'undefined'

--- TokanPages.ClientApp/src/Shared/types.ts
+++ TokanPages.ClientApp/src/Shared/types.ts
@@ -2,4 +2,4 @@ export type ReactSyntheticEvent = React.SyntheticEvent;
 export type ReactMouseEvent = React.MouseEvent<HTMLElement, MouseEvent>;
 export type ReactKeyboardEvent = React.KeyboardEvent<HTMLInputElement>;
 export type ReactChangeEvent = React.ChangeEvent<HTMLInputElement>;
-export type LanguageChangeEvent = React.ChangeEvent<{ name?: string | undefined; value: unknown }>;
+export type LanguageChangeEvent = React.ChangeEvent<{ name?: string; value: unknown }>;
