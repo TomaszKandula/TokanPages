2e427c034cc13cc3b5eeb4c98a410baf65b9ab68
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2023-01-31 18:24:13 +0100

    refactor: add custom event types

---
+++ TokanPages.ClientApp/src/Shared/types.ts
@@ -0,0 +1,5 @@
+export type ReactSyntheticEvent = React.SyntheticEvent;
+export type ReactMouseEvent = React.MouseEvent<HTMLElement, MouseEvent>;
+export type ReactKeyboardEvent = React.KeyboardEvent<HTMLInputElement>;
+export type ReactChangeEvent = React.ChangeEvent<HTMLInputElement>;
+export type LanguageChangeEvent = React.ChangeEvent<{ name?: string | undefined; value: unknown }>;
