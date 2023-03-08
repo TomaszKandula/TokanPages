868b57d309de28ab5106779947d7af52be8615f4
Tomasz Tom Kandula <tom@tomkandula.com>
2023-03-08 23:44:32 +0100

    fix: add return object

---
+++ TokanPages.Backend/TokanPages.Backend.Application/Users/Commands/UpdateUserCommandResult.cs
@@ -0,0 +1,20 @@
+namespace TokanPages.Backend.Application.Users.Commands;
+
+public class UpdateUserCommandResult
+{
+    public bool? IsActivated { get; set; }
+
+    public string? UserAlias { get; set; }
+
+    public string? FirstName { get; set; }
+
+    public string? LastName { get; set; }
+
+    public string? EmailAddress { get; set; }
+
+    public string? UserAboutText { get; set; }
+
+    public string? UserImageName { get; set; }
+
+    public string? UserVideoName { get; set; } 
+}
