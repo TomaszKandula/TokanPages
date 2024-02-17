3dcde1d76a9d9fab153f420fe954a06e442712a8
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2024-02-17 17:15:07 +0100

    feat: remove unused code

--- TokanPages.WebApi/Controllers/Api/AssetsController.cs
+++ TokanPages.WebApi/Controllers/Api/AssetsController.cs
@@ -26,28 +26,6 @@ public class AssetsController : ApiBaseController
     public AssetsController(IMediator mediator) : base(mediator) { }
 
     /// <summary>
-    /// Returns list of uploaded assets (files).
-    /// </summary>
-    /// <returns>Object.</returns>
-    [HttpGet]
-    [AuthorizeUser(Roles.GodOfAsgard)]
-    [ProducesResponseType(typeof(GetAssetsListQueryResult), StatusCodes.Status200OK)]
-    public async Task<GetAssetsListQueryResult> GetAssetsList() 
-        => await Mediator.Send(new GetAssetsListQuery());
-
-    /// <summary>
-    /// Returns storage asset file.
-    /// </summary>
-    /// <param name="blobName">Full asset name.</param>
-    /// <returns>File.</returns>
-    [HttpGet]
-    [ETagFilter]
-    [ResponseCache(Location = ResponseCacheLocation.Any, NoStore = false, Duration = 86400, VaryByQueryKeys = new [] { "blobName" })]
-    [ProducesResponseType(typeof(IActionResult), StatusCodes.Status200OK)]
-    public async Task<IActionResult> GetAsset([FromQuery] string blobName = "")
-        => await Mediator.Send(new GetSingleAssetQuery { BlobName = blobName });
-
-    /// <summary>
     /// Returns article asset (file associated with an article).
     /// </summary>
     /// <param name="id">Article ID.</param>
