027a63deb00e1c0cd215383192cc294f0781cdcc
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2024-02-27 22:21:05 +0100

    feat: remove swagger from hosted services

--- TokanPages.WebApi/TokanPages.HostedServices/Startup.cs
+++ TokanPages.WebApi/TokanPages.HostedServices/Startup.cs
@@ -1,13 +1,8 @@
 using System.Diagnostics.CodeAnalysis;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Http;
 using TokanPages.Backend.Core.Exceptions;
 using TokanPages.Backend.Configuration;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.ResponseCompression;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using Newtonsoft.Json.Converters;
 using Newtonsoft.Json.Serialization;
 using Serilog;
@@ -20,12 +15,6 @@ namespace TokanPages.HostedServices;
 [ExcludeFromCodeCoverage]
 public class Startup
 {
-    private const string ApiName = "Hosted Services API";
-
-    private const string DocVersion = "v1";
-
-    private static readonly string[] XmlDocs = { "TokanPages.HostedServices.xml" };
-
     private readonly IConfiguration _configuration;
 
     private readonly IHostEnvironment _environment;
@@ -64,7 +53,6 @@ public class Startup
 
         services.AddResponseCompression(options => options.Providers.Add<GzipCompressionProvider>());
         services.RegisterDependencies(_configuration);
-        services.SetupSwaggerOptions(_environment, ApiName, DocVersion, XmlDocs);
         services.SetupDockerInternalNetwork();
     }
 
@@ -84,11 +72,9 @@ public class Startup
         {
             endpoints.MapControllers();
             endpoints.MapGet("/", context 
-                => context.Response.WriteAsync(ApiName));
+                => context.Response.WriteAsync("Hosted Services API"));
             endpoints.MapGet("/hc/ready", context 
                 => context.Response.WriteAsync("{\"status\": \"live\"}"));
         });
-
-        builder.SetupSwaggerUi(_configuration, _environment, ApiName, DocVersion);
     }
 }
