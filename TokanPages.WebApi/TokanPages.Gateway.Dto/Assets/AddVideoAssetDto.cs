a1b7938205b05d85d54d6c8dd1977a5de78ad062
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2024-03-06 20:07:48 +0100

    feat: update models

---
+++ TokanPages.WebApi/TokanPages.Gateway.Dto/Assets/AddVideoAssetDto.cs
@@ -0,0 +1,24 @@
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.AspNetCore.Http;
+using TokanPages.Backend.Domain.Enums;
+
+namespace TokanPages.Gateway.Dto.Assets;
+
+/// <summary>
+/// Use it when you want to add video asset.
+/// </summary>
+[ExcludeFromCodeCoverage]
+public class AddVideoAssetDto
+{
+    /// <summary>
+    /// Mandatory.
+    /// </summary>
+    public ProcessingTarget Target { get; set; }
+
+    /// <summary>
+    /// Mandatory.
+    /// </summary>
+    [DataType(DataType.Upload)]
+    public IFormFile? BinaryData { get; set; }
+}
