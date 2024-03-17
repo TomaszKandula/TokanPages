8b284458c90e36c1243f244731bab1f15ffb2a6e
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2024-03-17 18:47:38 +0100

    feat: configure docker compose for many envs

---
+++ docker-compose-up-prod.sh
@@ -0,0 +1,25 @@
+# PREPARE DOCKER COMPOSE FILE
+cp docker-compose-prod.yml docker-compose-run.yml
+
+# SET ENVIRONMENT VARIABLES
+API_VERSION="1.0"
+APP_VERSION=${GIT_APP_VERSION} # THIS IS SET BY SEMANTIC RELEASE
+APP_BUILD_TEST="false"
+APP_DATE_TIME=$(date +"%Y-%m-%d at %T")
+SONAR_TOKEN=""
+SONAR_KEY=""
+SONAR_HOST=""
+
+# APPLY ENVIRONMENT VARIABLES
+sed -i "" \
+-e "s/\${API_VERSION}/${API_VERSION}/" \
+-e "s/\${APP_VERSION}/${APP_VERSION}/" \
+-e "s/\${APP_BUILD_TEST}/${APP_BUILD_TEST}/" \
+-e "s/\${APP_DATE_TIME}/${APP_DATE_TIME}/" \
+-e "s/\${SONAR_TOKEN}/${SONAR_TOKEN}/" \
+-e "s/\${SONAR_KEY}/${SONAR_KEY}/" \
+-e "s/\${SONAR_HOST}/${SONAR_HOST}/" \
+docker-compose-run.yml 
+
+# RUN DOCKER COMPOSE
+docker compose -f docker-compose-run.yml up
