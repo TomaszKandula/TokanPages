bf3032cac7713bdd50ce0e7d4427998d7af1a814
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2024-03-21 19:23:12 +0100

    feat: align for debian and macos

--- docker-compose-up-prod.sh
+++ docker-compose-up-prod.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # PREPARE DOCKER COMPOSE FILE
 cp docker-compose-prod.yml docker-compose-run.yml
 
@@ -10,16 +11,29 @@ SONAR_TOKEN=""
 SONAR_KEY=""
 SONAR_HOST=""
 
-# APPLY ENVIRONMENT VARIABLES
-sed -i "" \
--e "s/\${API_VERSION}/${API_VERSION}/" \
--e "s/\${APP_VERSION}/${APP_VERSION}/" \
--e "s/\${APP_BUILD_TEST}/${APP_BUILD_TEST}/" \
--e "s/\${APP_DATE_TIME}/${APP_DATE_TIME}/" \
--e "s/\${SONAR_TOKEN}/${SONAR_TOKEN}/" \
--e "s/\${SONAR_KEY}/${SONAR_KEY}/" \
--e "s/\${SONAR_HOST}/${SONAR_HOST}/" \
-docker-compose-run.yml 
+# APPLY ENVIRONMENT VARIABLES (CHECK DEBIAN OR MACOS)
+# MACOS REQUIRES EMPTY STRING FOR 'I' PARAMETER
+if [ -f "/etc/debian_version" ]; then
+  sed -i \
+  -e "s/\${API_VERSION}/${API_VERSION}/" \
+  -e "s/\${APP_VERSION}/${APP_VERSION}/" \
+  -e "s/\${APP_BUILD_TEST}/${APP_BUILD_TEST}/" \
+  -e "s/\${APP_DATE_TIME}/${APP_DATE_TIME}/" \
+  -e "s/\${SONAR_TOKEN}/${SONAR_TOKEN}/" \
+  -e "s/\${SONAR_KEY}/${SONAR_KEY}/" \
+  -e "s/\${SONAR_HOST}/${SONAR_HOST}/" \
+  docker-compose-run.yml 
+else
+  sed -i "" \
+  -e "s/\${API_VERSION}/${API_VERSION}/" \
+  -e "s/\${APP_VERSION}/${APP_VERSION}/" \
+  -e "s/\${APP_BUILD_TEST}/${APP_BUILD_TEST}/" \
+  -e "s/\${APP_DATE_TIME}/${APP_DATE_TIME}/" \
+  -e "s/\${SONAR_TOKEN}/${SONAR_TOKEN}/" \
+  -e "s/\${SONAR_KEY}/${SONAR_KEY}/" \
+  -e "s/\${SONAR_HOST}/${SONAR_HOST}/" \
+  docker-compose-run.yml 
+fi
 
 # RUN DOCKER COMPOSE
 docker compose -f docker-compose-run.yml up
