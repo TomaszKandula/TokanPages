0db753d1e633fe39c816bf8fe48627c6bb8fec94
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2024-04-07 15:46:59 +0200

    feat: add new exception

---
+++ TokanPages.Backend/TokanPages.Backend.Core/Exceptions/InvoiceProcessingException.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Runtime.Serialization;
+
+namespace TokanPages.Backend.Core.Exceptions;
+
+[ExcludeFromCodeCoverage]
+[Serializable]
+public class InvoiceProcessingException : Exception
+{
+    public string ErrorCode { get; } = "";
+
+    protected InvoiceProcessingException(SerializationInfo serializationInfo, 
+        StreamingContext streamingContext) : base(serializationInfo, streamingContext) { }
+
+    public InvoiceProcessingException(string errorCode, string errorMessage = "") : base(errorMessage)
+        => ErrorCode = errorCode;
+}
