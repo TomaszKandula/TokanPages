1e17f3e58cb88691fff31ca59159b2a53ef6b31c
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2024-04-11 16:33:49 +0200

    refactor: update model

--- TokanPages.Backend/TokanPages.Backend.Application/Users/Commands/AddUserCommand.cs
+++ TokanPages.Backend/TokanPages.Backend.Application/Users/Commands/AddUserCommand.cs
@@ -4,11 +4,11 @@ namespace TokanPages.Backend.Application.Users.Commands;
 
 public class AddUserCommand : IRequest<Guid>
 {
-    public string? FirstName { get; set; }
+    public string FirstName { get; set; } = "";
 
-    public string? LastName { get; set; }
+    public string LastName { get; set; } = "";
 
-    public string? EmailAddress { get; set; }
+    public string EmailAddress { get; set; } = "";
 
-    public string? Password { get; set; }
+    public string Password { get; set; } = "";
 }
