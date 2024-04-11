46ea3df8892d3cadac323b2e3e4e7860932c4d0e
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2024-04-11 16:34:13 +0200

    feat: use service bus to send email via worker

--- TokanPages.Backend/TokanPages.Backend.Application/Mailer/Commands/SendNewsletterCommandHandler.cs
+++ TokanPages.Backend/TokanPages.Backend.Application/Mailer/Commands/SendNewsletterCommandHandler.cs
@@ -2,6 +2,7 @@
 using Microsoft.Extensions.Configuration;
 using TokanPages.Backend.Core.Extensions;
 using TokanPages.Backend.Core.Utilities.LoggerService;
+using TokanPages.Backend.Domain.Entities;
 using TokanPages.Persistence.Database;
 using TokanPages.Services.EmailSenderService.Abstractions;
 using TokanPages.Services.EmailSenderService.Models;
@@ -14,6 +15,8 @@ public class SendNewsletterCommandHandler : RequestHandler<SendNewsletterCommand
 
     private readonly IConfiguration _configuration;
 
+    private static string CurrentEnv => Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Testing";
+
     public SendNewsletterCommandHandler(DatabaseContext databaseContext, ILoggerService loggerService, 
         IEmailSenderService emailSenderService, IConfiguration configuration) : base(databaseContext, loggerService)
     {
@@ -23,16 +26,27 @@ public class SendNewsletterCommandHandler : RequestHandler<SendNewsletterCommand
 
     public override async Task<Unit> Handle(SendNewsletterCommand request, CancellationToken cancellationToken)
     {
+        var isProduction = CurrentEnv == "Production";
         var deploymentOrigin = _configuration.GetValue<string>("Paths_DeploymentOrigin");
+        var developmentOrigin = _configuration.GetValue<string>("Paths_DevelopmentOrigin");
+
+        var origin = isProduction ? deploymentOrigin : developmentOrigin;
         var updateSubscriberPath = _configuration.GetValue<string>("Paths_UpdateSubscriber");
         var unsubscribePath = _configuration.GetValue<string>("Paths_Unsubscribe");
 
-        var updateSubscriberBaseLink = $"{deploymentOrigin}{updateSubscriberPath}";
-        var unsubscribeBaseLink = $"{deploymentOrigin}{unsubscribePath}";
+        var updateSubscriberBaseLink = $"{origin}{updateSubscriberPath}";
+        var unsubscribeBaseLink = $"{origin}{unsubscribePath}";
 
         LoggerService.LogInformation($"Update subscriber base URL: {updateSubscriberBaseLink}.");
         LoggerService.LogInformation($"Unsubscribe base URL: {unsubscribeBaseLink}.");
-            
+
+        if (request.SubscriberInfo is null)
+        {
+            var message = $"Object: {request.SubscriberInfo} is null. Cannot process newsletter to the subscribers.";
+            LoggerService.LogWarning(message);
+            return Unit.Value;
+        }
+
         foreach (var subscriber in request.SubscriberInfo)
         {
             var updateSubscriberLink = $"{updateSubscriberBaseLink}{subscriber.Id}";
@@ -51,16 +65,25 @@ public class SendNewsletterCommandHandler : RequestHandler<SendNewsletterCommand
             var template = await _emailSenderService.GetEmailTemplate(templateUrl, cancellationToken);
             LoggerService.LogInformation($"Getting newsletter template from URL: {templateUrl}.");
 
-            var contact = _configuration.GetValue<string>("Email_Address_Contact");
-            var payload = new SenderPayloadDto
+            var messageId = Guid.NewGuid();
+            var serviceBusMessage = new ServiceBusMessage
             {
-                From = contact,
+                Id = messageId,
+                IsConsumed = false
+            };
+
+            var payload = new SendMessageConfiguration
+            {
+                MessageId = messageId,
+                From = _configuration.GetValue<string>("Email_Address_Contact"),
                 To = new List<string> { subscriber.Email },
                 Subject = request.Subject,
                 Body = template.MakeBody(newValues)
             };
 
-            await _emailSenderService.SendEmail(payload, cancellationToken);
+            await DatabaseContext.ServiceBusMessages.AddAsync(serviceBusMessage, cancellationToken);
+            await DatabaseContext.SaveChangesAsync(cancellationToken);
+            await _emailSenderService.SendToServiceBus(payload, cancellationToken);
         }
 
         return Unit.Value;
