6310e3754e104fb41e6bd97791cc25a98a51415a
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2024-04-13 20:22:38 +0200

    refactor: relocate HC response writer

--- TokanPages.WebApi/TokanPages.Notifications/Startup.cs
+++ TokanPages.WebApi/TokanPages.Notifications/Startup.cs
@@ -1,8 +1,6 @@
 using System.Diagnostics.CodeAnalysis;
-using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.ResponseCompression;
-using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
 using Newtonsoft.Json.Serialization;
@@ -108,23 +106,6 @@ public class Startup
             endpoints.MapGet("/", context 
                 => context.Response.WriteAsync("Notifications API"));
         });
-        builder.UseHealthChecks("/hc", new HealthCheckOptions
-        {
-            ResponseWriter = async (context, report) =>
-            {
-                var result = new
-                {
-                    status = report.Status.ToString(),
-                    errors = report.Entries.Select(pair 
-                        => new
-                        {
-                            key = pair.Key, 
-                            value = Enum.GetName(typeof(HealthStatus), pair.Value.Status)
-                        })
-                };
-                context.Response.ContentType = "application/json";
-                await context.Response.WriteAsync(JsonConvert.SerializeObject(result));
-            }
-        });
+        builder.UseHealthChecks("/hc", HealthCheckSupport.WriteResponse());
     }
 }
