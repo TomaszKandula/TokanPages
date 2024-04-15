71e0aa49d606613c781074b7c4f11e460c182fc9
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2024-04-15 08:50:08 +0200

    fix: reduce cognitive complexity

--- TokanPages.ClientApp/src/Shared/Services/FormValidation/ValidateUpdateForm/validateUpdateForm.ts
+++ TokanPages.ClientApp/src/Shared/Services/FormValidation/ValidateUpdateForm/validateUpdateForm.ts
@@ -37,33 +37,10 @@ export const ValidateUpdateForm = (props: UpdateFormInput): any => {
         constraints
     );
 
-    if (!HaveSpecialCharacter(props.newPassword)) {
-        const data = HasProperty(result, "newPassword")
-            ? [...result.newPassword, PASSWORD_MISSING_CHAR]
-            : [PASSWORD_MISSING_CHAR];
-        result = { ...result, password: data };
-    }
-
-    if (!ContainNumber(props.newPassword)) {
-        const data = HasProperty(result, "newPassword")
-            ? [...result.newPassword, PASSWORD_MISSING_NUMBER]
-            : [PASSWORD_MISSING_NUMBER];
-        result = { ...result, password: data };
-    }
-
-    if (!HaveLargeLetter(props.newPassword)) {
-        const data = HasProperty(result, "newPassword")
-            ? [...result.newPassword, PASSWORD_MISSING_LARGE_LETTER]
-            : [PASSWORD_MISSING_LARGE_LETTER];
-        result = { ...result, password: data };
-    }
-
-    if (!HaveSmallLetter(props.newPassword)) {
-        const data = HasProperty(result, "newPassword")
-            ? [...result.newPassword, PASSWORD_MISSING_SMALL_LETTER]
-            : [PASSWORD_MISSING_SMALL_LETTER];
-        result = { ...result, password: data };
-    }
+    haveSpecialCharacter(props.newPassword, result);
+    containNumber(props.newPassword, result);
+    haveLargeLetter(props.newPassword, result);
+    haveSmallLetter(props.newPassword, result);
 
     if (props.newPassword !== props.verifyPassword) {
         const errorText = "Password do not match";
@@ -89,3 +66,39 @@ export const ValidateUpdateForm = (props: UpdateFormInput): any => {
 
     return result;
 };
+
+const haveSpecialCharacter = (newPassword: string, result: any): void => {
+    if (!HaveSpecialCharacter(newPassword)) {
+        const data = HasProperty(result, "newPassword")
+            ? [...result.newPassword, "New password " + PASSWORD_MISSING_CHAR]
+            : [PASSWORD_MISSING_CHAR];
+        result = { ...result, newPassword: data };
+    }
+}
+
+const containNumber = (newPassword: string, result: any): void => {
+    if (!ContainNumber(newPassword)) {
+        const data = HasProperty(result, "newPassword")
+            ? [...result.newPassword, "New password " + PASSWORD_MISSING_NUMBER]
+            : [PASSWORD_MISSING_NUMBER];
+        result = { ...result, newPassword: data };
+    }
+}
+
+const haveLargeLetter = (newPassword: string, result: any): void => {
+    if (!HaveLargeLetter(newPassword)) {
+        const data = HasProperty(result, "newPassword")
+            ? [...result.newPassword, "New password " + PASSWORD_MISSING_LARGE_LETTER]
+            : [PASSWORD_MISSING_LARGE_LETTER];
+        result = { ...result, newPassword: data };
+    }
+}
+
+const haveSmallLetter = (newPassword: string, result: any): void => {
+    if (!HaveSmallLetter(newPassword)) {
+        const data = HasProperty(result, "newPassword")
+            ? [...result.newPassword, "New password " + PASSWORD_MISSING_SMALL_LETTER]
+            : [PASSWORD_MISSING_SMALL_LETTER];
+        result = { ...result, newPassword: data };
+    }
+}
