e90f4363f3178962f4cb83dcb2648d8ffd89127d
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2024-04-24 10:51:40 +0200

    refactor: simplify code

--- TokanPages.WebApi/TokanPages.Gateway/Startup.cs
+++ TokanPages.WebApi/TokanPages.Gateway/Startup.cs
@@ -93,23 +93,6 @@ public class Startup
             endpoints.MapGet("/", context 
                 => context.Response.WriteAsync("Gateway API"));
         });
-        builder.UseHealthChecks("/hc", new HealthCheckOptions
-        {
-            ResponseWriter = async (context, report) =>
-            {
-                var result = new
-                {
-                    status = report.Status.ToString(),
-                    errors = report.Entries.Select(pair 
-                        => new
-                        {
-                            key = pair.Key, 
-                            value = Enum.GetName(typeof(HealthStatus), pair.Value.Status)
-                        })
-                };
-                context.Response.ContentType = "application/json";
-                await context.Response.WriteAsync(JsonConvert.SerializeObject(result));
-            }
-        });
+        builder.UseHealthChecks("/hc", HealthCheckSupport.WriteResponse());
     }
 }
