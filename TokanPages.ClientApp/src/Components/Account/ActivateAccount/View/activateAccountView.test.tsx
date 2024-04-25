38ab27716e238715a6ac04131658384f2a9bd307
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2024-04-25 23:20:00 +0200

    test: update tests

--- TokanPages.ClientApp/src/Components/Account/ActivateAccount/View/activateAccountView.test.tsx
+++ TokanPages.ClientApp/src/Components/Account/ActivateAccount/View/activateAccountView.test.tsx
@@ -1,23 +1,20 @@
 import "../../../../setupTests";
 import React from "react";
-import { shallow } from "enzyme";
+import { render } from "enzyme";
 import { ActivateAccountView } from "./activateAccountView";
 
 describe("test account group component: activateAccountView", () => {
     it("should render correctly '<ActivateAccountView />' when content is loaded.", () => {
-        const tree = shallow(
+        const html = render(
             <ActivateAccountView
                 isLoading={false}
                 caption={"Account Activation"}
                 text1={"Your account has been successfully activated!"}
                 text2={"You can now sign in."}
-                buttonHandler={jest.fn()}
-                buttonDisabled={false}
                 progress={false}
-                buttonText={"Go to main"}
             />
         );
 
-        expect(tree).toMatchSnapshot();
+        expect(html).toMatchSnapshot();
     });
 });
--- TokanPages.ClientApp/src/Components/Account/ActivateAccount/activateAccount.test.tsx
+++ TokanPages.ClientApp/src/Components/Account/ActivateAccount/activateAccount.test.tsx
@@ -1,15 +1,9 @@
 import "../../../setupTests";
 import React from "react";
 import * as Redux from "react-redux";
-import * as Router from "react-router";
 import * as Dom from "react-router-dom";
 import { render } from "enzyme";
 import { ActivateAccount } from "./activateAccount";
-
-jest.mock("react-router", () => ({
-    ...(jest.requireActual("react-router") as typeof Router),
-    useHistory: () => jest.fn(),
-}));
 
 jest.mock("react-router-dom", () => ({
     ...(jest.requireActual("react-router-dom") as typeof Dom),
@@ -27,27 +21,40 @@ describe("Test account group component: activateAccount", () => {
     const testId = "dba4043c-7428-4f72-ba13-fe782c7a88fa";
     const testContent = {
         language: "eng",
-        onProcessing: {
+        onVerifying: {
+            type: "Verifying",
+            caption: "Email Verification",
+            text1: "Processing your account..., please wait.",
+            text2: "",
+            button: ""
+          },
+          onProcessing: {
             type: "Processing",
             caption: "Account Activation",
             text1: "Processing your account..., please wait.",
             text2: "",
-            button: "",
-        },
-        onSuccess: {
+            button: ""
+          },
+          onSuccess: {
             type: "Success",
-            caption: "Account Activation",
-            text1: "Your account has been successfully activated!",
-            text2: "You can now sign in.",
+            caption: "Account Processing",
             button: "Go to main",
-        },
-        onError: {
+            noBusinessLock: {
+              text1: "Your account has been successfully processed!",
+              text2: "You can now use all our services."
+            },
+            businessLock: {
+              text1: "Your account has been <b>partially activated</b>.",
+              text2: "Please wait for our decision. You will hear from us soon."
+            }
+          },
+          onError: {
             type: "Error",
-            caption: "Account Activation",
-            text1: "Could not activate your account.",
+            caption: "Processing Error",
+            text1: "Could not process your account. Unexpected error occurred.",
             text2: "Please contact IT support.",
-            button: "Retry",
-        },
+            button: "Retry"
+        }
     };
 
     const useDispatchMock = jest.spyOn(Redux, "useDispatch");
@@ -61,8 +68,8 @@ describe("Test account group component: activateAccount", () => {
     });
 
     it("should render correctly '<ActivateAccount />' when content is loaded.", () => {
-        const html = render(<ActivateAccount id={testId} />);
-        expect(useDispatchMock).toBeCalledTimes(1);
+        const html = render(<ActivateAccount id={testId} type="" />);
+        expect(useDispatchMock).toBeCalledTimes(2);
         expect(html).toMatchSnapshot();
     });
 });
