4605c2990a16c913c4755eb061647268b35820c0
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2024-04-27 10:30:51 +0200

    refactor: remove unused code

--- TokanPages.ClientApp/src/Shared/constants.ts
+++ TokanPages.ClientApp/src/Shared/constants.ts
@@ -1,8 +1,8 @@
 import { IsRunningOnIOS } from "./Services/IsRunningOnIOS";
 
-export const DELAY_3_SECONDS = 3000;
-export const DELAY_30_SECONDS = 30000;
-export const SET_INTERVAL_DELAY = IsRunningOnIOS() ? DELAY_3_SECONDS : DELAY_30_SECONDS;
+export const DELAY_3_SECONDS: number = 3000;
+export const DELAY_30_SECONDS: number = 30000;
+export const SET_INTERVAL_DELAY: number = IsRunningOnIOS() ? DELAY_3_SECONDS : DELAY_30_SECONDS;
 
 export const LIKES_LIMIT_FOR_ANONYM: number = 25;
 export const LIKES_LIMIT_FOR_USER: number = 50;
@@ -17,7 +17,6 @@ export const USER_DATA: string = "USER_DATA";
 
 
 
-// --------------------------- TODO: move out below code from constants --------------------------- //
 export const PASSWORD_MISSING_CHAR: string = "must contain at least one of the following characters: !, @, #, $, %, ^, &, *";
 export const PASSWORD_MISSING_NUMBER: string = "must contain at least one number";
 export const PASSWORD_MISSING_LARGE_LETTER: string = "must contain at least one large letter";
@@ -27,7 +26,3 @@ export const UNEXPECTED_STATUS: string = "Received unexpected status code: {STAT
 export const UNEXPECTED_ERROR: string = "Unexpected error occured";
 export const VALIDATION_ERRORS: string = "Validation errors have been found";
 export const NULL_RESPONSE_ERROR: string = "The response data has returned null. Possible parsing issue due to invalid JSON/XML format";
-
-export const LIKES_HINT_FOR_ANONYM: string = "You may give up to {LEFT_LIKES} thumbs up!";
-export const LIKES_HINT_FOR_USER: string = "You may give up to {LEFT_LIKES} thumbs up!";
-export const MAX_LIKES_REACHED: string = "You've reached max thumbs up :)";
