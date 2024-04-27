253146c1f7e818fb9575736b5117d2db2af24c30
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2024-04-27 11:36:43 +0200

    feat: add custom hook

---
+++ TokanPages.ClientApp/src/Shared/Hooks/UseInterval/index.ts
@@ -0,0 +1,20 @@
+import * as React from "react";
+
+const useIsomorphicLayoutEffect = typeof window !== "undefined" ? React.useLayoutEffect : React.useEffect;
+
+export const useInterval = (callback: () => void, delay: number | null) => {
+    const savedCallback = React.useRef(callback);
+
+    useIsomorphicLayoutEffect(() => {
+        savedCallback.current = callback;
+    }, [callback]);
+
+    React.useEffect(() => {
+        if (!delay && delay !== 0) {
+            return;
+        }
+
+        const id = setInterval(() => savedCallback.current(), delay);
+        return () => clearInterval(id);
+    }, [delay]);
+};
