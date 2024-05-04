b0501f11c46f707b2a5f9edd1cf20e8de5ba6a43
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2024-05-04 18:01:36 +0200

    test: update test and snapshot

--- TokanPages.ClientApp/src/Components/Articles/ArticleDetail/View/articleDetailView.test.tsx
+++ TokanPages.ClientApp/src/Components/Articles/ArticleDetail/View/articleDetailView.test.tsx
@@ -1,10 +1,27 @@
 import "../../../../setupTests";
 import React from "react";
 import { shallow } from "enzyme";
+import { ArticleContentDto } from "../../../../Api/Models";
 import { ArticleDetailView } from "./articleDetailView";
 
 describe("test articles group component: ArticleDetailView", () => {
     it("should render correctly '<ArticleDetailView />' when content is loaded.", () => {
+        const testContent: ArticleContentDto = {
+            content: {
+                language: "eng",
+                textReadCount: "Read count:",
+                textFirstName: "First name:",
+                textSurname: "Last name:",
+                textRegistered: "Registered at:",
+                textLanguage: "Article language:",
+                textReadTime: "Read time:",
+                textPublished: "Published at:",
+                textUpdated: "Updated at:",
+                textWritten: "Written by",
+                textAbout: "About the author:"
+            }
+        };
+
         const tree = shallow(
             <ArticleDetailView
                 backButtonHandler={jest.fn()}
@@ -19,7 +36,7 @@ describe("test articles group component: ArticleDetailView", () => {
                 authorFirstName="Ester"
                 authorLastName="Exposito"
                 authorRegistered=""
-                articleReadTime="4 min."
+                articleReadTime="4"
                 articleCreatedAt="2020-01-10T12:15:15"
                 articleUpdatedAt="2020-01-10T12:15:15"
                 articleContent={<div>articleContent</div>}
@@ -29,6 +46,7 @@ describe("test articles group component: ArticleDetailView", () => {
                 renderAuthorName="Ester Exposito"
                 authorShortBio="Happy developer"
                 flagImage="eng.png"
+                content={testContent}
             />
         );
 
