ba324bea36ea9ab1792d1f4b26efb580c8b899ee
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2024-05-08 18:59:18 +0200

    feat: relocate showcase page

--- TokanPages.ClientApp/src/Pages/DocumentsPage/index.tsx
+++ TokanPages.ClientApp/src/Pages/DocumentsPage/index.tsx
@@ -3,3 +3,4 @@ export { ElectronicsPage } from "./ElectronicsPage";
 export { FootballPage } from "./FootballPage";
 export { GuitarPage } from "./GuitarPage";
 export { PhotographyPage } from "./PhotographyPage";
+export { ShowcasePage } from "./ShowcasePage";
