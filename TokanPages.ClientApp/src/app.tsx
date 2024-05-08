02314fe71f93e99a9dbbc75ef5bd9035bd412044
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2024-05-08 18:56:40 +0200

    feat: relocate routes for clarity

--- TokanPages.ClientApp/src/app.tsx
+++ TokanPages.ClientApp/src/app.tsx
@@ -1,14 +1,15 @@
 import * as React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import Fab from "@material-ui/core/Fab";
 import KeyboardArrowUpIcon from "@material-ui/icons/KeyboardArrowUp";
 import { GetContentManifestDto } from "./Api/Models";
 import { UpdateUserData } from "./Shared/Services/initializeService";
 import { UpdateUserLanguage } from "./Shared/Services/languageService";
 import { AppStyle } from "./app.style";
+import { WrongPage } from "Pages";
+import { Routes } from "routes";
 import AOS from "aos";
 import "aos/dist/aos.css";
-
 import {
     ClearPageStart,
     ScrollToTop,
@@ -17,31 +18,6 @@ import {
     ApplicationUserInfo,
     ApplicationSession,
 } from "./Shared/Components";
-
-import {
-    MainPage,
-    StoryPage,
-    ArticlesPage,
-    TermsPage,
-    PolicyPage,
-    ContactPage,
-    ShowcasePage,
-    SigninPage,
-    SignupPage,
-    SignoutPage,
-    AccountPage,
-    PasswordResetPage,
-    PasswordUpdatePage,
-    NewsletterRemovePage,
-    NewsletterUpdatePage,
-    ActivationPage,
-    WrongPage,
-    BicyclePage,
-    ElectronicsPage,
-    FootballPage,
-    GuitarPage,
-    PhotographyPage,
-} from "./Pages";
 
 interface Properties {
     manifest: GetContentManifestDto;
@@ -59,82 +35,12 @@ const App = (props: Properties): JSX.Element => {
         return () => clearInterval(intervalId);
     });
 
-    const redirect500px = React.useCallback(() => {
-        window.location.href = "https://500px.com/p/tomaszkandula?view=galleries";
-        return null;
-    }, []);
-
     return (
         <ApplicationSession>
             <Router>
                 <ClearPageStart>
                     <Switch>
-                        <Route exact path="/">
-                            <MainPage />
-                        </Route>
-                        <Route exact path="/mystory">
-                            <StoryPage />
-                        </Route>
-                        <Route exact path="/articles">
-                            <ArticlesPage />
-                        </Route>
-                        <Route exact path="/showcase">
-                            <ShowcasePage />
-                        </Route>
-
-                        <Route exact path="/bicycle">
-                            <BicyclePage />
-                        </Route>
-                        <Route exact path="/electronics">
-                            <ElectronicsPage />
-                        </Route>
-                        <Route exact path="/football">
-                            <FootballPage />
-                        </Route>
-                        <Route exact path="/guitar">
-                            <GuitarPage />
-                        </Route>
-                        <Route exact path="/photography">
-                            <PhotographyPage />
-                        </Route>
-
-                        <Route exact path="/terms">
-                            <TermsPage />
-                        </Route>
-                        <Route exact path="/policy">
-                            <PolicyPage />
-                        </Route>
-                        <Route exact path="/contact">
-                            <ContactPage />
-                        </Route>
-                        <Route exact path="/signin">
-                            <SigninPage />
-                        </Route>
-                        <Route exact path="/signup">
-                            <SignupPage />
-                        </Route>
-                        <Route exact path="/signout">
-                            <SignoutPage />
-                        </Route>
-                        <Route exact path="/account">
-                            <AccountPage />
-                        </Route>
-                        <Route exact path="/accountactivation">
-                            <ActivationPage />
-                        </Route>
-                        <Route exact path="/updatepassword">
-                            <PasswordUpdatePage />
-                        </Route>
-                        <Route exact path="/resetpassword">
-                            <PasswordResetPage />
-                        </Route>
-                        <Route exact path="/update-newsletter">
-                            <NewsletterUpdatePage />
-                        </Route>
-                        <Route exact path="/remove-newsletter">
-                            <NewsletterRemovePage />
-                        </Route>
-                        <Route exact path="/albums" component={redirect500px} />
+                        <Routes />
                         <Route component={WrongPage} />
                     </Switch>
                 </ClearPageStart>
