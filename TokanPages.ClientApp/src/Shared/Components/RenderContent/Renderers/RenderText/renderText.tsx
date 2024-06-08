17ec6489b65250f180d3a566e6b157f47ec3d069
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2024-06-08 16:14:56 +0200

    feat: add LINK render sub component

--- TokanPages.ClientApp/src/Shared/Components/RenderContent/Renderers/RenderText/renderText.tsx
+++ TokanPages.ClientApp/src/Shared/Components/RenderContent/Renderers/RenderText/renderText.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { useLocation } from "react-router-dom";
 import { Box, Typography } from "@material-ui/core";
 import { TextItem } from "../../Models/TextModel";
 import { RenderTextStyle } from "./renderTextStyle";
@@ -6,7 +7,36 @@ import { ReactHtmlParser } from "../../../../../Shared/Services/Renderers";
 import "../../../../../Theme/Css/customDropCap.css";
 
 interface DataProps { 
-    data: string 
+    data?: string 
+    text?: string;
+}
+
+const NO_CONTENT = "EMPTY_CONTENT_PROVIDED";
+
+const RenderLink = (props: DataProps): JSX.Element => {
+    const location = useLocation();
+    const classes = RenderTextStyle();
+    const data = props.data ?? "";
+
+    const onClickHandler = React.useCallback(() => {
+        const element = document.querySelector(data);
+        location.hash = data;
+
+        if (element) {
+            element.scrollIntoView({ behavior: "smooth" });
+        }
+    }, [ data ]);
+
+    return (
+        <Typography 
+            variant="body1" 
+            component="span" 
+            className={`${classes.common} ${classes.paragraph}`} 
+            onClick={onClickHandler}
+        >
+            <ReactHtmlParser html={props.text ?? NO_CONTENT} />
+        </Typography>
+    );
 }
 
 const RenderTitle = (props: DataProps): JSX.Element => {
@@ -14,7 +44,7 @@ const RenderTitle = (props: DataProps): JSX.Element => {
     return (
         <Box mt={7} mb={0}>
             <Typography variant="body1" component="span" className={`${classes.common} ${classes.title}`}>
-                <ReactHtmlParser html={props.data} />
+                <ReactHtmlParser html={props.data ?? NO_CONTENT} />
             </Typography>
         </Box>
     );
@@ -25,7 +55,7 @@ const RenderSubtitle = (props: DataProps): JSX.Element => {
     return (
         <Box mt={1} mb={5}>
             <Typography variant="body1" component="span" className={`${classes.common} ${classes.subTitle}`}>
-                <ReactHtmlParser html={props.data} />
+                <ReactHtmlParser html={props.data ?? NO_CONTENT} />
             </Typography>
         </Box>
     );
@@ -36,7 +66,7 @@ const RenderHeader = (props: DataProps): JSX.Element => {
     return (
         <Box mt={7} mb={2}>
             <Typography variant="body1" component="span" className={`${classes.common} ${classes.header}`}>
-                <ReactHtmlParser html={props.data} />
+                <ReactHtmlParser html={props.data ?? NO_CONTENT} />
             </Typography>
         </Box>
     );
@@ -46,17 +76,17 @@ const RenderParagraph = (props: DataProps): JSX.Element => {
     const classes = RenderTextStyle();
     return (
         <Typography variant="body1" component="span" className={`${classes.common} ${classes.paragraph}`}>
-            <ReactHtmlParser html={props.data} />
+            <ReactHtmlParser html={props.data ?? NO_CONTENT} />
         </Typography>
     );
 };
 
 const RenderParagraphWithDropCap = (props: DataProps): JSX.Element => {
     const classes = RenderTextStyle();
-    const replaced = props.data.replace("<p>", "<p class='custom-drop-cap'>");
+    const replaced = props.data?.replace("<p>", "<p class='custom-drop-cap'>");
     return (
         <Typography variant="body1" component="span" className={`${classes.common} ${classes.paragraph}`}>
-            <ReactHtmlParser html={replaced} />
+            <ReactHtmlParser html={replaced ?? NO_CONTENT} />
         </Typography>
     );
 };
@@ -65,6 +95,8 @@ export const RenderText = (props: TextItem): JSX.Element => {
     const data: string = props.value as string;
 
     switch (props.prop) {
+        case "link":
+            return <RenderLink data={data} text={props.text} />;
         case "title":
             return <RenderTitle data={data} />;
         case "subtitle":
