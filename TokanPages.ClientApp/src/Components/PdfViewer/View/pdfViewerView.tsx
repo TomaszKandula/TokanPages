e3ac117607dda43be8efa69b9cab97e27b69d016
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2024-06-24 12:13:21 +0200

    feat: replace with custom implementation (pdfjsLib)

--- TokanPages.ClientApp/src/Components/PdfViewer/View/pdfViewerView.tsx
+++ TokanPages.ClientApp/src/Components/PdfViewer/View/pdfViewerView.tsx
@@ -1,25 +1,17 @@
 import * as React from "react";
-import { Document, Page, pdfjs } from "react-pdf";
-
-const pdfWorker = "pdfjs-dist/build/pdf.worker.min.mjs";
 
 interface PdfViewerViewProps {
     pdfFile: string;
 }
 
-export const PdfViewerView = (props: PdfViewerViewProps): JSX.Element => {
-    pdfjs.GlobalWorkerOptions.workerSrc = new URL(pdfWorker, import.meta.url).toString();
-
-    const [numPages, setNumPages] = React.useState<number>();
-    const [pageNumber, _] = React.useState<number>(1);    
-
-    const onDocumentLoadSuccess = React.useCallback(() => {
-        setNumPages(numPages);
-    }, []);
+export const PdfViewerView = (_: PdfViewerViewProps): JSX.Element => {
+    //@ts-expect-error
+    let { pdfjsLib } = globalThis;
+    pdfjsLib.GlobalWorkerOptions.workerSrc = "//mozilla.github.io/pdf.js/build/pdf.worker.mjs";
 
     return (
-        <Document file={props.pdfFile} onLoadSuccess={onDocumentLoadSuccess} >
-            <Page pageNumber={pageNumber} />
-        </Document>
+        <div>
+            <canvas></canvas>
+        </div>
     );
 }
