5914b13b5b4f37f09510d6d5affce35f723b5925
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2024-07-02 14:12:28 +0200

    feat: render 'itemspan', hide terms and policy from navbar

--- TokanPages.ClientApp/src/Shared/Components/ListRender/renderNavbarMenu.tsx
+++ TokanPages.ClientApp/src/Shared/Components/ListRender/renderNavbarMenu.tsx
@@ -20,9 +20,32 @@ export const RenderNavbarMenu = (props: Properties): JSX.Element => {
         const isAnonymous = props.isAnonymous && (item.link === "/account" || item.link === "/signout");
         const isNotAnonymous = !props.isAnonymous && (item.link === "/signin" || item.link === "/signup");
         const isRoot = item.link === "/";
+        const isTerms = item.link === "/terms";
+        const isPolicy = item.link === "/policy";
 
         switch (item.type) {
             case "item": {
+                if (isAnonymous) return;
+                if (isNotAnonymous) return;
+                if (isRoot) return;
+                if (isTerms) return;
+                if (isPolicy) return;
+
+                renderBuffer.push(
+                    <RenderNavbarItem
+                        key={item.id}
+                        id={item.id}
+                        type={item.type}
+                        value={item.value}
+                        link={item.link}
+                        icon={item.icon}
+                        enabled={item.enabled}
+                    />
+                );
+                break;
+            }
+
+            case "itemspan": {
                 if (isAnonymous) return;
                 if (isNotAnonymous) return;
                 if (isRoot) return;
