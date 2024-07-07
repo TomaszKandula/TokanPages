7c7613578657a25584f277935e3d5606055789ef
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2024-07-07 12:05:00 +0200

    feat: extend style

--- TokanPages.ClientApp/src/Shared/Components/ListRender/Renderers/RenderNavbarItemSpan/renderNavbarItemSpanStyle.tsx
+++ TokanPages.ClientApp/src/Shared/Components/ListRender/Renderers/RenderNavbarItemSpan/renderNavbarItemSpanStyle.tsx
@@ -8,8 +8,11 @@ export const RenderNavbarItemSpanStyle = makeStyles(theme => ({
     button: {
         textTransform: "none",
         fontWeight: 400,
+        "&:hover": {
+            backgroundColor: Colours.colours.white,
+        },
     },
-    text: {
+    list_item_text: {
         [theme.breakpoints.down(1100)]: {
             fontSize: 14,
         },
@@ -20,5 +23,11 @@ export const RenderNavbarItemSpanStyle = makeStyles(theme => ({
         whiteSpace: "nowrap",
         overflow: "hidden",
         textOverflow: "ellipsis",
+        "&:hover": {
+            color: Colours.colours.darkViolet1,
+        },
+    },
+    list_item_text_selected: {
+        color: Colours.colours.darkViolet1,
     },
 }));
