4ffe3593c2b374c7a60ea2a07ebd603af9954ba5
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2024-07-07 11:09:03 +0200

    feat: extend style

--- TokanPages.ClientApp/src/Shared/Components/ListRender/Renderers/RenderNavbarItem/renderNavbarItemStyle.tsx
+++ TokanPages.ClientApp/src/Shared/Components/ListRender/Renderers/RenderNavbarItem/renderNavbarItemStyle.tsx
@@ -5,7 +5,12 @@ export const RenderNavbarItemStyle = makeStyles(theme => ({
     href: {
         color: Colours.colours.black,
     },
-    text: {
+    list_item: {
+        "&:hover": {
+            backgroundColor: Colours.colours.white,
+        },
+    },
+    list_item_text: {
         [theme.breakpoints.down(1100)]: {
             fontSize: 14,
         },
@@ -16,5 +21,23 @@ export const RenderNavbarItemStyle = makeStyles(theme => ({
         whiteSpace: "nowrap",
         overflow: "hidden",
         textOverflow: "ellipsis",
+        "&:hover": {
+            color: Colours.colours.darkViolet1,
+        },
+        "&::after": {
+	        content: "''",
+            display: "inline-block",
+            position: "absolute",
+            width: 30,
+            height: 1,
+            backgroundColor: Colours.colours.darkViolet1,
+            bottom: 7,
+            left: "50%",
+            transform: "translate(-50%, 0px) scaleX(0)",
+            transition: "transform 0.3s ease 0s",
+        },
+    },
+    list_item_text_selected: {
+        color: Colours.colours.darkViolet1,
     },
 }));
