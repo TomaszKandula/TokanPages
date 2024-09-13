9ce826aab3f8719f8492c7158469ea94179f7f87
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2024-09-13 20:30:51 +0200

    feat: update logic

--- TokanPages.ClientApp/src/Components/Business/businessForm.tsx
+++ TokanPages.ClientApp/src/Components/Business/businessForm.tsx
@@ -1,5 +1,12 @@
 import * as React from "react";
 import { BusinessFormView } from "./View/businessFormView";
+import { useDispatch, useSelector } from "react-redux";
+import { ApplicationState } from "../../Store/Configuration";
+import { ApplicationDialogAction, ApplicationMessageAction } from "../../Store/Actions";
+import { OperationStatus } from "../../Shared/enums";
+import { RECEIVED_ERROR_MESSAGE } from "../../Shared/constants";
+import { SuccessMessage } from "../../Shared/Services/Utilities";
+import { ReactChangeEvent, ReactKeyboardEvent } from "../../Shared/types";
 
 export interface BusinessFormProps {
     pt?: number;
@@ -10,56 +17,154 @@ export interface BusinessFormProps {
     background?: React.CSSProperties;
 }
 
-//TODO: add logic
+const internalSubjectText = "Incoming Business Inquiry";
+const internalMessageText = "Please check the internal payload for more details.";
+const formDefault = {
+    companyText: "",
+    firstNameText: "",
+    lastNameText: "",
+    emailText: "",
+    phoneText: "",
+    descriptionText: "",
+}
+
 export const BusinessForm = (props: BusinessFormProps): JSX.Element => {
+    const dispatch = useDispatch();
+
+    const content = useSelector((state: ApplicationState) => state.contentTemplates?.content);
+    const businessForm = useSelector((state: ApplicationState) => state.contentBusinessForm);
+    const email = useSelector((state: ApplicationState) => state.applicationEmail);
+    const error = useSelector((state: ApplicationState) => state.applicationError);
+
+    const hasNotStarted = email?.status === OperationStatus.notStarted;
+    const hasFinished = email?.status === OperationStatus.hasFinished;
+    const hasError = error?.errorMessage === RECEIVED_ERROR_MESSAGE;
+
+    const [form, setForm] = React.useState(formDefault);
+    const [hasProgress, setHasProgress] = React.useState(false);
+
+    const showSuccess = (text: string) =>
+        dispatch(ApplicationDialogAction.raise(SuccessMessage(content.forms.textBusinessForm, text)));
+    // const showWarning = (text: string) =>
+    //     dispatch(ApplicationDialogAction.raise(WarningMessage(content.forms.textBusinessForm, text)));
+
+    const clearForm = React.useCallback(() => {
+        if (!hasProgress) return;
+        setHasProgress(false);
+        dispatch(ApplicationMessageAction.clear());
+    }, [hasProgress]);
+
+    React.useEffect(() => {
+        if (hasError) {
+            clearForm();
+            return;
+        }
+
+        if (hasNotStarted && hasProgress) {
+            dispatch(
+                ApplicationMessageAction.send({
+                    firstName: form.firstNameText,
+                    lastName: form.lastNameText,
+                    userEmail: form.emailText,
+                    emailFrom: form.emailText,
+                    emailTos: [form.emailText],
+                    subject: internalSubjectText,
+                    message: internalMessageText,
+                    businessData: JSON.stringify(form)
+                })
+            );
+
+            return;
+        }
+
+        if (hasFinished) {
+            clearForm();
+            setForm(formDefault);
+            showSuccess(content.templates.messageOut.success);
+        }
+    }, [hasProgress, hasError, hasNotStarted, hasFinished, content]);
+
+    const keyHandler = React.useCallback(
+        (event: ReactKeyboardEvent) => {
+            if (event.code === "Enter") {
+                buttonHandler();
+            }
+        },
+        [
+            form.companyText, 
+            form.descriptionText, 
+            form.emailText, 
+            form.firstNameText, 
+            form.lastNameText, 
+            form.phoneText
+        ]
+    );
+
+    const formHandler = React.useCallback(
+        (event: ReactChangeEvent) => {
+            setForm({ ...form, [event.currentTarget.name]: event.currentTarget.value });
+        },
+        [form]
+    );
+
+    const buttonHandler = React.useCallback(() => {
+        // const result = ValidateContactForm({
+        //     firstName: form.firstName,
+        //     lastName: form.lastName,
+        //     email: form.email,
+        //     subject: form.subject,
+        //     message: form.message,
+        //     terms: form.terms,
+        // });
+
+        // if (!Validate.isDefined(result)) {
+        //     setHasProgress(true);
+        //     return;
+        // }
+
+        //showWarning(GetTextWarning({ object: result, template: content.templates.messageOut.warning }));
+
+        console.log(form);
+
+    }, [form, content]);
+
     return(
         <BusinessFormView
             isLoading={false}
-            caption="Business Inquiry"
-            text=""
+            caption={businessForm.content.caption}
             progress={false}
-            buttonText="Submit"
-            keyHandler={() => {}}
-            formHandler={() => {}}
-            buttonHandler={() => {}}
-            companyText=""
-            companyLabel="Company Name"
-            firstNameText=""
-            firstNameLabel="First Name"
-            lastNameText=""
-            lastNameLabel="Last Name"
-            emailText=""
-            emailLabel="Business Email"
-            phoneText=""
-            phoneLabel="Business Phone"
-            techHeader="Which technologies are you interested in?"
-            techItems={[
-                { value: ".NET/C#", key: 0 },
-                { value: "JavaScript/TypeScript", key: 1 },
-                { value: "React/ReactNative", key: 2 },
-                { value: "Material UI", key: 3 },
-                { value: "MS SQL/T-SQL", key: 4 },
-                { value: "Entity Framework Core", key: 5 },
-                { value: "VPS w/Docker images", key: 6 },
-                { value: "Microsoft Azure Cloud", key: 7 },
-            ]}
-            techLabel=""
+            buttonText={businessForm.content.buttonText}
+            keyHandler={keyHandler}
+            formHandler={formHandler}
+            buttonHandler={buttonHandler}
+            companyText={form.companyText}
+            companyLabel={businessForm.content.companyLabel}
+            firstNameText={form.firstNameText}
+            firstNameLabel={businessForm.content.firstNameLabel}
+            lastNameText={form.lastNameText}
+            lastNameLabel={businessForm.content.lastNameLabel}
+            emailText={form.emailText}
+            emailLabel={businessForm.content.emailLabel}
+            phoneText={form.phoneText}
+            phoneLabel={businessForm.content.phoneLabel}
+            techLabel={businessForm.content.techLabel}
+            techItems={businessForm.content.techItems}
             description={{
-                text: "",
-                label: "Project description",
-                multiline: true,
-                rows: 12,
-                required: true,
+                text: form.descriptionText,
+                label: businessForm.content.description.label,
+                multiline: businessForm.content.description.multiline,
+                rows: businessForm.content.description.rows,
+                required: businessForm.content.description.required,
             }}
             pricing={{
-                caption: "Which services are you interested in?",
-                programing: "Software development (web/mobile/backend).",
-                programmingPrice: "<b>30 USD/hour</b>",
-                hosting: "Hosting and maintaining the solution on a dedicated VPS.",
-                hostingPrice: "<b>60 USD/month</b>",
-                support: "Additional <i>ad hoc</i> support after roll-out.",
-                supportPrice: "<b>15 USD/hour</b>",
-                info: "Please note: the above values are indicative only without 23% of VAT, and do not constitute a commercial offer.",
+                caption: businessForm.content.pricing.caption,
+                programing: businessForm.content.pricing.programing,
+                programmingPrice: businessForm.content.pricing.programmingPrice,
+                hosting: businessForm.content.pricing.hosting,
+                hostingPrice: businessForm.content.pricing.hostingPrice,
+                support: businessForm.content.pricing.support,
+                supportPrice: businessForm.content.pricing.supportPrice,
+                info: businessForm.content.pricing.info,
             }}
             pt={props.pt}
             pb={props.pb}
