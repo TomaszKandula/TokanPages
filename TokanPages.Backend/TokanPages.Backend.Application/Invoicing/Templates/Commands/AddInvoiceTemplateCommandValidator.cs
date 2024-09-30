f8ca8cad86fa39a0371d5a9126b4a2903ab4c2ce
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2024-09-30 18:31:16 +0200

    fix: check length if not null

--- TokanPages.Backend/TokanPages.Backend.Application/Invoicing/Templates/Commands/AddInvoiceTemplateCommandValidator.cs
+++ TokanPages.Backend/TokanPages.Backend.Application/Invoicing/Templates/Commands/AddInvoiceTemplateCommandValidator.cs
@@ -14,10 +14,13 @@ public class AddInvoiceTemplateCommandValidator : AbstractValidator<AddInvoiceTe
             .WithErrorCode(nameof(ValidationCodes.REQUIRED))
             .WithMessage(ValidationCodes.REQUIRED);
 
-        RuleFor(command => command.Data)
-            .Must(bytes => bytes!.Length <= 4 * 1024 * 1024)
-            .WithErrorCode(nameof(ValidationCodes.INVALID_FILE_SIZE))
-            .WithMessage(ValidationCodes.INVALID_FILE_SIZE);
+        When(command => command.Data != null, () =>
+        {
+            RuleFor(command => command.Data)
+                .Must(bytes => bytes!.Length <= 4 * 1024 * 1024)
+                .WithErrorCode(nameof(ValidationCodes.INVALID_FILE_SIZE))
+                .WithMessage(ValidationCodes.INVALID_FILE_SIZE);
+        });
 
         RuleFor(command => command.Description)
             .NotEmpty()
