edb53f1410be19babf900c06d30cbe7bf5dd9e6d
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2024-09-30 18:31:30 +0200

    test: add new validator tests

--- TokanPages.Tests/TokanPages.Tests.UnitTests/Validators/Invoicing/Templates/AddInvoiceTemplateCommandValidatorTest.cs
+++ TokanPages.Tests/TokanPages.Tests.UnitTests/Validators/Invoicing/Templates/AddInvoiceTemplateCommandValidatorTest.cs
@@ -1,6 +1,81 @@
+using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using TokanPages.Backend.Application.Invoicing.Templates.Commands;
+using TokanPages.Backend.Shared.Resources;
+using Xunit;
+
 namespace TokanPages.Tests.UnitTests.Validators.Invoicing.Templates;
 
 public class AddInvoiceTemplateCommandValidatorTest : TestBase
 {
-    
+    [Fact]
+    public async Task GivenTemplateFile_WhenAddInvoiceTemplate_ShouldSucceed()
+    {
+        // Arrange
+        const int testFileSizeInKb = 1;
+        const string name = "Test";
+        const string fileName = "TestFile.html";
+
+        var stream = DataUtilityService.GetRandomStream(testFileSizeInKb);
+        var form = new FormFile(stream, 0, testFileSizeInKb, name, fileName);
+
+        var command = new AddInvoiceTemplateCommand
+        {
+            Data = form,
+            Description = DataUtilityService.GetRandomString()
+        };
+
+        // Act
+        var validator = new AddInvoiceTemplateCommandValidator();
+        var result = await validator.ValidateAsync(command);
+
+        // Assert
+        result.Errors.Should().BeEmpty();
+    }
+
+    [Fact]
+    public async Task GivenEmptyFile_WhenAddInvoiceTemplate_ShouldFail()
+    {
+        // Arrange
+        var command = new AddInvoiceTemplateCommand
+        {
+            Data = null,
+            Description = string.Empty
+        };
+
+        // Act
+        var validator = new AddInvoiceTemplateCommandValidator();
+        var result = await validator.ValidateAsync(command);
+
+        // Assert
+        result.Errors.Count.Should().Be(2);
+        result.Errors[0].ErrorCode.Should().Be(nameof(ValidationCodes.REQUIRED));
+        result.Errors[1].ErrorCode.Should().Be(nameof(ValidationCodes.REQUIRED));
+    }
+
+    [Fact]
+    public async Task GivenTemplateFileTooLarge_WhenAddInvoiceTemplate_ShouldFail()
+    {
+        // Arrange
+        const int testFileSizeInKb = 5 * 1024 * 1024;
+        const string name = "Test";
+        const string fileName = "TestFile.html";
+
+        var stream = DataUtilityService.GetRandomStream(testFileSizeInKb);
+        var form = new FormFile(stream, 0, testFileSizeInKb, name, fileName);
+
+        var command = new AddInvoiceTemplateCommand
+        {
+            Data = form,
+            Description = DataUtilityService.GetRandomString()
+        };
+
+        // Act
+        var validator = new AddInvoiceTemplateCommandValidator();
+        var result = await validator.ValidateAsync(command);
+
+        // Assert
+        result.Errors.Count.Should().Be(1);
+        result.Errors[0].ErrorCode.Should().Be(nameof(ValidationCodes.INVALID_FILE_SIZE));
+    }
 }
--- TokanPages.Tests/TokanPages.Tests.UnitTests/Validators/Invoicing/Templates/GetInvoiceTemplateQueryValidatorTest.cs
+++ TokanPages.Tests/TokanPages.Tests.UnitTests/Validators/Invoicing/Templates/GetInvoiceTemplateQueryValidatorTest.cs
@@ -1,6 +1,44 @@
+using FluentAssertions;
+using TokanPages.Backend.Application.Invoicing.Templates.Queries;
+using TokanPages.Backend.Shared.Resources;
+using Xunit;
+
 namespace TokanPages.Tests.UnitTests.Validators.Invoicing.Templates;
 
 public class GetInvoiceTemplateQueryValidatorTest : TestBase
 {
-    
+    [Fact]
+    public async Task GivenValidInput_WhenGetInvoiceTemplate_ShouldSucceed()
+    {
+        // Arrange
+        var query = new GetInvoiceTemplateQuery
+        {
+            Id = Guid.NewGuid()
+        };
+
+        // Act
+        var validator = new GetInvoiceTemplateQueryValidator();
+        var result = await validator.ValidateAsync(query);
+
+        // Assert
+        result.Errors.Should().BeEmpty();
+    }
+
+    [Fact]
+    public async Task GivenEmptyInput_WhenGetInvoiceTemplate_ShouldFail()
+    {
+        // Arrange
+        var query = new GetInvoiceTemplateQuery
+        {
+            Id = Guid.Empty
+        };
+
+        // Act
+        var validator = new GetInvoiceTemplateQueryValidator();
+        var result = await validator.ValidateAsync(query);
+
+        // Assert
+        result.Errors.Count.Should().Be(1);
+        result.Errors[0].ErrorCode.Should().Be(nameof(ValidationCodes.INVALID_GUID_VALUE));
+    }
 }
