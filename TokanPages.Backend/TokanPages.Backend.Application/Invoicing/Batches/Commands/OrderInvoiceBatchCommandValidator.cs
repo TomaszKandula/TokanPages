a7d1eca8824c9d46204903a67f8ec700197e60c6
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2024-09-30 18:13:19 +0200

    fix: add missing empty check

--- TokanPages.Backend/TokanPages.Backend.Application/Invoicing/Batches/Commands/OrderInvoiceBatchCommandValidator.cs
+++ TokanPages.Backend/TokanPages.Backend.Application/Invoicing/Batches/Commands/OrderInvoiceBatchCommandValidator.cs
@@ -12,16 +12,25 @@ public class OrderInvoiceBatchCommandValidator : AbstractValidator<OrderInvoiceB
         RuleFor(command => command.UserId)
             .NotEmpty()
             .WithErrorCode(nameof(ValidationCodes.REQUIRED))
-            .WithMessage(ValidationCodes.REQUIRED);
+            .WithMessage(ValidationCodes.REQUIRED)
+            .NotEqual(Guid.Empty)
+            .WithErrorCode(nameof(ValidationCodes.INVALID_GUID_VALUE))
+            .WithMessage(ValidationCodes.INVALID_GUID_VALUE);
 
         RuleFor(command => command.UserCompanyId)
             .NotEmpty()
             .WithErrorCode(nameof(ValidationCodes.REQUIRED))
-            .WithMessage(ValidationCodes.REQUIRED);
+            .WithMessage(ValidationCodes.REQUIRED)
+            .NotEqual(Guid.Empty)
+            .WithErrorCode(nameof(ValidationCodes.INVALID_GUID_VALUE))
+            .WithMessage(ValidationCodes.INVALID_GUID_VALUE);
 
         RuleFor(command => command.UserBankAccountId)
             .NotEmpty()
             .WithErrorCode(nameof(ValidationCodes.REQUIRED))
-            .WithMessage(ValidationCodes.REQUIRED);
+            .WithMessage(ValidationCodes.REQUIRED)
+            .NotEqual(Guid.Empty)
+            .WithErrorCode(nameof(ValidationCodes.INVALID_GUID_VALUE))
+            .WithMessage(ValidationCodes.INVALID_GUID_VALUE);
     }
 }
