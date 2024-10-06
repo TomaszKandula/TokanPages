26ab9a7c6a7d06630b0fb13374ea5be01f2ae5ce
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2024-10-06 12:44:48 +0200

    feat: extend contract

--- TokanPages.WebApi/TokanPages.HostedServices/Services/Abstractions/ICachingProcessingConfig.cs
+++ TokanPages.WebApi/TokanPages.HostedServices/Services/Abstractions/ICachingProcessingConfig.cs
@@ -1,4 +1,12 @@
+using TokanPages.HostedServices.Services.Models;
+
 namespace TokanPages.HostedServices.Services.Abstractions;
 
 /// <inheritdoc />
-public interface ICachingProcessingConfig : IBaseConfig { }
+public interface ICachingProcessingConfig : IBaseConfig
+{
+    /// <summary>
+    /// List of URLs of web pages to be cached.
+    /// </summary>
+    public List<RoutePath> RoutePaths { get; set; }
+}
