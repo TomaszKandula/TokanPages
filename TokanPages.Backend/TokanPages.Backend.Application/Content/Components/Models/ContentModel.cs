08ae4db6dd04699d8012078c30cfcd7b1c0b23bb
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2024-10-06 13:50:34 +0200

    feat: use separate content model

---
+++ TokanPages.Backend/TokanPages.Backend.Application/Content/Components/Models/ContentModel.cs
@@ -0,0 +1,8 @@
+namespace TokanPages.Backend.Application.Content.Components.Models;
+
+public class ContentModel
+{
+    public string Type { get; set; } = "";
+
+    public string Name { get; set; } = "";
+}
