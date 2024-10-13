3be50ea78a85273069a21c765f2c3340498556ca
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2024-10-13 17:43:16 +0200

    feat: add logger

--- TokanPages.Services/TokanPages.Services.SpaCachingService/CachingService.cs
+++ TokanPages.Services/TokanPages.Services.SpaCachingService/CachingService.cs
@@ -1,5 +1,8 @@
 using PuppeteerSharp;
 using PuppeteerSharp.BrowserData;
+using TokanPages.Backend.Core.Exceptions;
+using TokanPages.Backend.Core.Utilities.LoggerService;
+using TokanPages.Backend.Shared.Resources;
 
 namespace TokanPages.Services.SpaCachingService;
 
@@ -13,6 +16,10 @@ public class CachingService : ICachingService
     private const string CacheDirName = "CacheDir";
 
     private const string DocumentFontReady = "document.fonts.ready";
+
+    private const string ServiceName = $"[{nameof(CachingService)}]";
+
+    private readonly ILoggerService _loggerService;
 
     private static LaunchOptions _launchOptions = new()
     {
@@ -38,70 +45,121 @@ public class CachingService : ICachingService
 
     public string CacheDir { get; }
 
-    public CachingService()
+    public CachingService(ILoggerService loggerService)
     {
-        var baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
-        var pdfDir = Path.Combine(baseDirectory, PdfDirName);
-        var cacheDir = Path.Combine(baseDirectory, CacheDirName);
-
-        PdfDir = pdfDir;
-        CacheDir = cacheDir;
-
-        if (!Directory.Exists(PdfDir))
-            Directory.CreateDirectory(PdfDir);
-
-        if (!Directory.Exists(CacheDir))
-            Directory.CreateDirectory(CacheDir);
+        _loggerService = loggerService;
+        PdfDir = GetPdfDir();
+        CacheDir = GetCacheDir();
+        EnsureWorkingDirectories();
     }
 
     /// <inheritdoc />
     public async Task<string> GeneratePdf(string sourceUrl)
     {
         await GetBrowser();
-        await using var browser = await Puppeteer.LaunchAsync(_launchOptions);
-        await using var page = await browser.NewPageAsync();
 
-        await page.GoToAsync(sourceUrl, waitUntil: WaitUntilOptions);
-        await page.EvaluateExpressionHandleAsync(DocumentFontReady);
+        try
+        {
+            await using var browser = await Puppeteer.LaunchAsync(_launchOptions);
+            await using var page = await browser.NewPageAsync();
 
-        var outputPath = Path.Combine(PdfDir, $"{Guid.NewGuid()}.pdf");
-        var fileInfo = new FileInfo(outputPath);
-        if (fileInfo.Exists)
-            fileInfo.Delete();
+            await page.GoToAsync(sourceUrl, waitUntil: WaitUntilOptions);
+            await page.EvaluateExpressionHandleAsync(DocumentFontReady);
 
-        await page.PdfAsync(outputPath);
-        return outputPath;
+            var outputPath = Path.Combine(PdfDir, $"{Guid.NewGuid()}.pdf");
+            var fileInfo = new FileInfo(outputPath);
+            if (fileInfo.Exists)
+                fileInfo.Delete();
+
+            await page.PdfAsync(outputPath);
+
+            _loggerService.LogInformation($"{ServiceName}: PDF has been generated.");
+            return outputPath;
+        }
+        catch (Exception exception)
+        {
+            throw FatalError(exception);
+        }
     }
 
     /// <inheritdoc />
     public async Task<string> RenderStaticPage(string sourceUrl, string pageName)
     {
         await GetBrowser();
-        await using var browser = await Puppeteer.LaunchAsync(_launchOptions);
-        await using var page = await browser.NewPageAsync();
 
-        await page.GoToAsync(sourceUrl, waitUntil: WaitUntilOptions);
-        await page.EvaluateExpressionHandleAsync(DocumentFontReady);
-        var htmlContent = await page.GetContentAsync();
+        try
+        {
+            await using var browser = await Puppeteer.LaunchAsync(_launchOptions);
+            await using var page = await browser.NewPageAsync();
 
-        var outputPath = Path.Combine(CacheDir, $"{pageName}.html");
-        await File.WriteAllTextAsync(outputPath, htmlContent);
-        return outputPath;
+            await page.GoToAsync(sourceUrl, waitUntil: WaitUntilOptions);
+            await page.EvaluateExpressionHandleAsync(DocumentFontReady);
+            var htmlContent = await page.GetContentAsync();
+
+            var outputPath = Path.Combine(CacheDir, $"{pageName}.html");
+            await File.WriteAllTextAsync(outputPath, htmlContent);
+
+            _loggerService.LogInformation($"{ServiceName}: Page has been rendered. Content length '{htmlContent.Length}'.");
+            return outputPath;
+        }
+        catch (Exception exception)
+        {
+            throw FatalError(exception);
+        }
     }
 
-    private static async Task GetBrowser()
+    private static string GetPdfDir()
     {
-        var browserFetcher = new BrowserFetcher
+        var baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+        return Path.Combine(baseDirectory, PdfDirName);
+    }
+
+    private static string GetCacheDir()
+    {
+        var baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+        return Path.Combine(baseDirectory, CacheDirName);
+    }
+
+    private void EnsureWorkingDirectories()
+    {
+        if (!Directory.Exists(PdfDir))
         {
-            Browser = SupportedBrowser.Chrome
-        };
+            Directory.CreateDirectory(PdfDir);
+            _loggerService.LogInformation($"{ServiceName}: Directory '{PdfDir}' has been created.");
+        }
 
-        await browserFetcher.DownloadAsync();
-        var path = browserFetcher
-            .GetInstalledBrowsers()
-            .First(browser => browser.BuildId == Chrome.DefaultBuildId)
-            .GetExecutablePath();
+        if (!Directory.Exists(CacheDir))
+        {
+            Directory.CreateDirectory(CacheDir);
+            _loggerService.LogInformation($"[{ServiceName}: Directory '{CacheDir}' has been created.");
+        }
+    }
 
-        _launchOptions.ExecutablePath = path;
+    private async Task GetBrowser()
+    {
+        try
+        {
+            _loggerService.LogInformation($"[{ServiceName}: Getting Chrome browser (ver. {Chrome.DefaultBuildId})...");
+
+            var browserFetcher = new BrowserFetcher { Browser = SupportedBrowser.Chrome };
+            await browserFetcher.DownloadAsync();
+            var path = browserFetcher
+                .GetInstalledBrowsers()
+                .First(browser => browser.BuildId == Chrome.DefaultBuildId)
+                .GetExecutablePath();
+
+            _launchOptions.ExecutablePath = path;
+            _loggerService.LogInformation($"[{ServiceName}: Browser downloaded, path '{path}'.");
+        }
+        catch (Exception exception)
+        {
+            throw FatalError(exception);
+        }
+    }
+
+    private Exception FatalError(Exception exception)
+    {
+        _loggerService.LogError($"[{ServiceName}: Error message: {exception.Message}. Details: {exception.InnerException?.Message ?? "n/a"}.");
+        return new GeneralException(nameof(ErrorCodes.ERROR_UNEXPECTED), ErrorCodes.ERROR_UNEXPECTED);
     }
 }
