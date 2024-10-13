edc3ad0b9b70961598dce259842490cf437db02c
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2024-10-13 17:43:44 +0200

    feat: update and add new tests

--- TokanPages.Tests/TokanPages.Tests.UnitTests/Services/CachingServiceTest.cs
+++ TokanPages.Tests/TokanPages.Tests.UnitTests/Services/CachingServiceTest.cs
@@ -1,4 +1,8 @@
 using FluentAssertions;
+using Moq;
+using TokanPages.Backend.Core.Exceptions;
+using TokanPages.Backend.Core.Utilities.LoggerService;
+using TokanPages.Backend.Shared.Resources;
 using TokanPages.Services.SpaCachingService;
 using Xunit;
 
@@ -11,7 +15,9 @@ public class CachingServiceTest : TestBase
     {
         // Arrange
         const string url = "http://www.google.com";
-        var cachingService = new CachingService();
+
+        var mockedLogger = new Mock<ILoggerService>();
+        var cachingService = new CachingService(mockedLogger.Object);
 
         // Act
         var result = await cachingService.GeneratePdf(url);
@@ -28,7 +34,9 @@ public class CachingServiceTest : TestBase
         const string url = "http://www.google.com";
         const string pageName = "google";
         const string expectedFileName = $"{pageName}.html";
-        var cachingService = new CachingService();
+
+        var mockedLogger = new Mock<ILoggerService>();
+        var cachingService = new CachingService(mockedLogger.Object);
 
         // Act
         var result = await cachingService.RenderStaticPage(url, pageName);
@@ -36,5 +44,40 @@ public class CachingServiceTest : TestBase
         // Assert
         result.Should().NotBeEmpty();
         result.Should().Contain(expectedFileName);
+    }
+
+    [Fact]
+    public async Task GivenWrongUrl_WhenGeneratePdf_ShouldThrowError()
+    {
+        // Arrange
+        const string url = "wrong url. com";
+
+        var mockedLogger = new Mock<ILoggerService>();
+        var cachingService = new CachingService(mockedLogger.Object);
+
+        // Act
+        // Assert
+        var result = await Assert.ThrowsAsync<GeneralException>(() => cachingService.GeneratePdf(url));
+
+        result.Message.Should().Be(ErrorCodes.ERROR_UNEXPECTED);
+        result.ErrorCode.Should().Be(nameof(ErrorCodes.ERROR_UNEXPECTED));
+    }
+
+    [Fact]
+    public async Task GivenWrongUrl_WhenRenderStaticPage_ShouldThrowError()
+    {
+        // Arrange
+        const string url = "wrong url. com";
+        const string pageName = "test";
+
+        var mockedLogger = new Mock<ILoggerService>();
+        var cachingService = new CachingService(mockedLogger.Object);
+
+        // Act
+        // Assert
+        var result = await Assert.ThrowsAsync<GeneralException>(() => cachingService.RenderStaticPage(url, pageName));
+
+        result.Message.Should().Be(ErrorCodes.ERROR_UNEXPECTED);
+        result.ErrorCode.Should().Be(nameof(ErrorCodes.ERROR_UNEXPECTED));
     }
 }
