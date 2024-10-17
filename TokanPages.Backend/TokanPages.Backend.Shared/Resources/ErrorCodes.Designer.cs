cf5483737094313fea9feb40906e238f57a1f7eb
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2024-10-17 20:13:24 +0200

    feat: add new error codes

--- TokanPages.Backend/TokanPages.Backend.Shared/Resources/ErrorCodes.Designer.cs
+++ TokanPages.Backend/TokanPages.Backend.Shared/Resources/ErrorCodes.Designer.cs
@@ -386,5 +386,17 @@ namespace TokanPages.Backend.Shared.Resources {
                 return ResourceManager.GetString("INVALID_ARGUMENT", resourceCulture);
             }
         }
+        
+        public static string MISSING_CACHE_FOLDER {
+            get {
+                return ResourceManager.GetString("MISSING_CACHE_FOLDER", resourceCulture);
+            }
+        }
+        
+        public static string MISSING_CACHE_FILE {
+            get {
+                return ResourceManager.GetString("MISSING_CACHE_FILE", resourceCulture);
+            }
+        }
     }
 }
