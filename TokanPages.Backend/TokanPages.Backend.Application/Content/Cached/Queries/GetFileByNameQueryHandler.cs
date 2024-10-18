5f754f27b343b69b768465dbf0006609434fe0a9
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2024-10-18 16:29:05 +0200

    feat: add query handler

---
+++ TokanPages.Backend/TokanPages.Backend.Application/Content/Cached/Queries/GetFileByNameQueryHandler.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.StaticFiles;
+using TokanPages.Backend.Core.Exceptions;
+using TokanPages.Backend.Core.Utilities.LoggerService;
+using TokanPages.Backend.Shared.Resources;
+using TokanPages.Persistence.Database;
+
+namespace TokanPages.Backend.Application.Content.Cached.Queries;
+
+public class GetFileByNameQueryHandler : RequestHandler<GetFileByNameQuery, FileContentResult>
+{
+    public GetFileByNameQueryHandler(DatabaseContext databaseContext, ILoggerService loggerService) 
+        : base(databaseContext, loggerService) { }
+
+    public override async Task<FileContentResult> Handle(GetFileByNameQuery request, CancellationToken cancellationToken)
+    {
+        var pathToFolder = $"{AppDomain.CurrentDomain.BaseDirectory}cached";
+        if (!Directory.Exists(pathToFolder))
+            throw new GeneralException(nameof(ErrorCodes.MISSING_CACHE_FOLDER), ErrorCodes.MISSING_CACHE_FOLDER);
+
+        var name = string.IsNullOrWhiteSpace(request.FileName) ? "index.html" : request.FileName;
+        var fullFilePath = $"{pathToFolder}{Path.DirectorySeparatorChar}{name}";
+        if (!File.Exists(fullFilePath))
+            throw new GeneralException(nameof(ErrorCodes.MISSING_CACHE_FILE), ErrorCodes.MISSING_CACHE_FILE);
+
+        var contentType = GetMimeType(name);
+        var file = await File.ReadAllBytesAsync(fullFilePath, cancellationToken);
+        return new FileContentResult(file, contentType);
+    }
+
+    private static string GetMimeType(string fileName)
+    {
+        var provider = new FileExtensionContentTypeProvider();
+        if (!provider.TryGetContentType(fileName, out var contentType))
+            contentType = "application/octet-stream";
+
+        return contentType;            
+    }
+}
