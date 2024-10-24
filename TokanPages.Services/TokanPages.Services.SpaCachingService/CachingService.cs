b207d82c0876aa84555299af2d1a874d2b4f3a8a
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2024-10-24 17:43:34 +0200

    fix: correct timeout

--- TokanPages.Services/TokanPages.Services.SpaCachingService/CachingService.cs
+++ TokanPages.Services/TokanPages.Services.SpaCachingService/CachingService.cs
@@ -16,7 +16,7 @@ namespace TokanPages.Services.SpaCachingService;
 /// </summary>
 public class CachingService : ICachingService
 {
-    private const int FiveMinutesTimeout = 300;
+    private const int FiveMinutesTimeout = 3000;
 
     private const string DocumentFontReady = "document.fonts.ready";
 
