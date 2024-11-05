8ff210ea573a4640231dc63b2c22bde44a0a213e
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2024-11-05 22:08:55 +0100

    feat: attach current state if render for cache

--- TokanPages.ClientApp/src/Shared/Services/initializeService.ts
+++ TokanPages.ClientApp/src/Shared/Services/initializeService.ts
@@ -1,8 +1,9 @@
 import { useDispatch } from "react-redux";
-import { USER_DATA } from "../constants";
-import { GetDataFromStorage } from "./StorageServices";
+import { ApplicationState } from "../../Store/Configuration";
 import { UserDataStoreAction } from "../../Store/Actions";
 import { AuthenticateUserResultDto } from "../../Api/Models";
+import { GetDataFromStorage } from "./StorageServices";
+import { USER_DATA } from "../constants";
 import Validate from "validate.js";
 import base64 from "base-64";
 import utf8 from "utf8";
@@ -23,3 +24,18 @@ export const UpdateUserData = (): void => {
         dispatch(UserDataStoreAction.update(data));
     }
 };
+
+export const AttachState = (state: ApplicationState): void => {
+    const queryParam = new URLSearchParams(window.location.search);
+    const param = queryParam.get("mode");
+
+    if (param === "static") {
+        const serialized = JSON.stringify(state);
+        const meta = document.createElement("meta");
+
+        meta.name = "attached-state";
+        meta.content = serialized;
+
+        document.head.appendChild(meta);
+    }
+}
