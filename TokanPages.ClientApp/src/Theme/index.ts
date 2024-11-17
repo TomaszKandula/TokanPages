3b4d671158c66b0a6749049a815a4bae4969d50f
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2024-11-17 15:58:04 +0100

    feat: add custom colours

--- TokanPages.ClientApp/src/Theme/index.ts
+++ TokanPages.ClientApp/src/Theme/index.ts
@@ -14,3 +14,22 @@ export const AppTheme = createTheme({
         fontWeightMedium: 500,
     },
 });
+
+export const AppColours = {
+    white: "#FFFFFF",
+    ghostWhite: "#F8F8FF",
+    violet: "#6367EF",
+    lightViolet: "#EBECFB",
+    darkViolet1: "#4649C3",
+    darkViolet2: "#171E33",
+    lightGray1: "#EFEFEF",
+    lightGray2: "#FAFAFA",
+    lightGray3: "#FCFCFC",
+    gray1: "#9E9E9E",
+    gray2: "#757575",
+    gray3: "#616161",
+    gray4: "#212121",
+    black: "#000000",
+    red: "#f44336",
+    redDark: "#e53935",
+}
