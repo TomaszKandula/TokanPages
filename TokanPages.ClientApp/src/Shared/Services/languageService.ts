7e03ad3e8e1a62643f0b51653dfd8894c97c4037
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2024-11-24 12:31:35 +0100

    feat: add language mapper

--- TokanPages.ClientApp/src/Shared/Services/languageService.ts
+++ TokanPages.ClientApp/src/Shared/Services/languageService.ts
@@ -2,6 +2,25 @@ import { useDispatch } from "react-redux";
 import { ApplicationLanguageAction } from "../../Store/Actions";
 import { GetContentManifestDto, LanguageItemDto } from "../../Api/Models";
 
+export const MapLanguageId = (input: string): string => {
+    switch (input.toLowerCase()) {
+        case "eng":
+            return "en.png";
+        case "fra":
+            return "fr.png";
+        case "ger":
+            return "de.png";
+        case "pol":
+            return "pl.png";
+        case "esp":
+            return "es.png";
+        case "ukr":
+            return "uk.png";
+        default: 
+            return "en.png";
+    }
+}
+
 export const GetDefaultId = (items: LanguageItemDto[]): string | undefined => {
     for (let index in items) {
         if (items[index].isDefault) {
