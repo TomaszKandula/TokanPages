d36bd60928c13a7df10ebbab78aac44bbbd9a7e7
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2024-12-03 18:41:41 +0100

    feat: update HTML parser

--- TokanPages.ClientApp/src/Shared/Services/Renderers/ReactHtmlParser/reactHtmlParser.tsx
+++ TokanPages.ClientApp/src/Shared/Services/Renderers/ReactHtmlParser/reactHtmlParser.tsx
@@ -7,10 +7,11 @@ interface Properties {
 
 export const ReactHtmlParser = (props: Properties): React.ReactElement => {
     const safeHTML = DOMPurify.sanitize(props.html, {
-        ALLOWED_TAGS: ["p", "b", "ul", "li", "a", "u", "i", "div", "span"],
+        ALLOWED_TAGS: ["p", "b", "ul", "ol", "li", "a", "u", "i", "div", "span"],
         FORBID_TAGS: ["style"],
         ALLOW_ARIA_ATTR: false,
         ALLOW_DATA_ATTR: false,
+        ADD_ATTR: ["target"],
     });
 
     return <div dangerouslySetInnerHTML={{ __html: safeHTML }}></div>;
