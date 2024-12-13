b01d7d03187946ddf22e4a6db5c33032deb60424
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2024-12-13 17:54:34 +0100

    feat: add endpoint w/mapper

--- TokanPages.WebApi/TokanPages.Content/Controllers/Mappers/CachedMapper.cs
+++ TokanPages.WebApi/TokanPages.Content/Controllers/Mappers/CachedMapper.cs
@@ -1,5 +1,6 @@
 using System.Diagnostics.CodeAnalysis;
 using TokanPages.Backend.Application.Content.Cached.Commands;
+using TokanPages.Backend.Application.Content.Cached.Commands.Models;
 using TokanPages.Content.Dto.Cached;
 
 namespace TokanPages.Content.Controllers.Mappers;
@@ -18,5 +19,22 @@ public static class CachedMapper
     public static UploadFileToLocalStorageCommand MapToUploadFileToLocalStorageCommand(UploadFileToLocalStorageDto model) => new()
     {
         BinaryData = model.BinaryData
+    };
+
+    /// <summary>
+    /// Map request DTO to a given command.
+    /// </summary>
+    /// <param name="model">Assets object.</param>
+    /// <returns>Command object.</returns>
+    public static OrderSpaCachingCommand MapToOrderSpaCachingCommand(RequestProcessingDto model) => new()
+    {
+        GetUrl = model.GetUrl,
+        PostUrl = model.PostUrl,
+        Files = model.Files,
+        Paths = model.Paths?.Select(item => new RoutePath
+        {
+            Name = item.Name,
+            Url = item.Url
+        })
     };
 }
