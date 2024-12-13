f8a2760f5c9767b2a5eb316cae2ac875d0490526
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2024-12-13 17:08:40 +0100

    feat: order spa caching

---
+++ TokanPages.Backend/TokanPages.Backend.Application/Content/Cached/Commands/Models/RequestProcessing.cs
@@ -0,0 +1,16 @@
+namespace TokanPages.Backend.Application.Content.Cached.Commands.Models;
+
+public class RequestProcessing
+{
+    public Guid MessageId { get; set; }
+
+    public string? TargetEnv { get; set; }
+
+    public string GetUrl { get; set; } = "";
+
+    public string PostUrl { get; set; } = "";
+
+    public string[]? Files { get; set; }
+
+    public List<RoutePath> Paths { get; set; } = new();
+}
