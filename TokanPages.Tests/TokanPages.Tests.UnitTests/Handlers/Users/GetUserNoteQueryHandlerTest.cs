fe6b360c61e33c0fbd297c2b469ed8af3b19eabf
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2025-01-12 21:22:12 +0100

    test: add handler test

--- TokanPages.Tests/TokanPages.Tests.UnitTests/Handlers/Users/GetUserNoteQueryHandlerTest.cs
+++ TokanPages.Tests/TokanPages.Tests.UnitTests/Handlers/Users/GetUserNoteQueryHandlerTest.cs
@@ -65,4 +65,60 @@ public class GetUserNoteQueryHandlerTest : TestBase
         result.Should().NotBeNull();
         result.Note.Should().Be(plainNote);
     }
+
+    [Fact]
+    public async Task GivenWrongNoteId_WhenGetUserNote_ShouldReturnEmptyObject()
+    {
+        // Arrange
+        const string plainNote = "This is my test note...";
+        var compressedNote = plainNote.CompressToBase64();
+
+        var userId = Guid.NewGuid();
+        var user = new Backend.Domain.Entities.User.Users
+        {
+            Id = userId,
+            EmailAddress = DataUtilityService.GetRandomEmail(),
+            UserAlias = DataUtilityService.GetRandomString(),
+            IsActivated = true,
+            CryptedPassword = DataUtilityService.GetRandomString()
+        };
+
+        var note = new Backend.Domain.Entities.User.UserNote
+        {
+            Id = Guid.NewGuid(),
+            UserId = userId,
+            Note = compressedNote,
+            CreatedBy = userId,
+            CreatedAt = DataUtilityService.GetRandomDateTime()
+        };
+
+        var databaseContext = GetTestDatabaseContext();
+        await databaseContext.Users.AddAsync(user);
+        await databaseContext.UserNotes.AddAsync(note);
+        await databaseContext.SaveChangesAsync();
+
+        var mockedLogger = new Mock<ILoggerService>();
+        var mockedUserService = new Mock<IUserService>();
+
+        mockedUserService
+            .Setup(service => service.GetActiveUser(
+                It.IsAny<Guid?>(), 
+                It.IsAny<bool>(), 
+                It.IsAny<CancellationToken>()))
+            .ReturnsAsync(user);
+
+        var query = new GetUserNoteQuery
+        {
+            UserNoteId = Guid.NewGuid()
+        };
+
+        var handler = new GetUserNoteQueryHandler(databaseContext, mockedLogger.Object, mockedUserService.Object);
+
+        // Act
+        var result = await handler.Handle(query, CancellationToken.None);
+
+        // Assert
+        result.Should().NotBeNull();
+        result.Note.Should().BeEmpty();
+    }
 }
