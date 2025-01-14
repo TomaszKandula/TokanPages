b598eef7394c078e64771b436d1fc5ffc3b971b4
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2025-01-14 20:14:25 +0100

    feat: add DTOs

---
+++ TokanPages.WebApi/TokanPages.Users.Dto/Users/AddUserNoteDto.cs
@@ -0,0 +1,12 @@
+namespace TokanPages.Users.Dto.Users;
+
+/// <summary>
+/// Use it when you want to add new user note.
+/// </summary>
+public class AddUserNoteDto
+{
+    /// <summary>
+    /// User note.
+    /// </summary>
+    public string Note { get; set; } = "";
+}
