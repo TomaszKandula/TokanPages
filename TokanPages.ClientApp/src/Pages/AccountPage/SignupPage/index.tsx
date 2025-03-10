6188e44e222212b3722c9c38f37e309fc343852e
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2025-03-10 16:35:16 +0100

    feat: post snapshot state for signin/signup

--- TokanPages.ClientApp/src/Pages/AccountPage/SignupPage/index.tsx
+++ TokanPages.ClientApp/src/Pages/AccountPage/SignupPage/index.tsx
@@ -4,10 +4,13 @@ import { ApplicationState } from "../../../Store/Configuration";
 import { ContentPageDataAction } from "../../../Store/Actions";
 import { UserSignup } from "../../../Components/Account";
 import { Navigation } from "../../../Components/Layout";
+import { TryPostStateSnapshot } from "../../../Shared/Services/SpaCaching";
 
 export const SignupPage = (): React.ReactElement => {
     const dispatch = useDispatch();
-    const language = useSelector((state: ApplicationState) => state.applicationLanguage);
+    const state = useSelector((state: ApplicationState) => state);
+    const language = state.applicationLanguage;
+    const accountUserSignup = state?.contentPageData?.components?.accountUserSignup;
 
     React.useEffect(() => {
         dispatch(
@@ -17,6 +20,12 @@ export const SignupPage = (): React.ReactElement => {
             )
         );
     }, [language?.id]);
+
+    React.useEffect(() => {
+        if (accountUserSignup?.language !== "") {
+            TryPostStateSnapshot(state);
+        }
+    }, [state]);
 
     return (
         <>
