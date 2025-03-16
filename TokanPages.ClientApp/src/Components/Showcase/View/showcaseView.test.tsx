d490c552cf1958ffa67e0e1ce7d59dc602443580
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2025-03-16 12:46:49 +0100

    test: add component test w/snapshot

---
+++ TokanPages.ClientApp/src/Components/Showcase/View/showcaseView.test.tsx
@@ -0,0 +1,43 @@
+import "../../../setupTests";
+import React from "react";
+import * as Redux from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import { render } from "@testing-library/react";
+import { ContentPageData } from "../../../Store/Defaults";
+import { ShowcaseView } from "./showcaseView";
+
+jest.mock("react-redux", () => ({
+    ...jest.requireActual("react-redux"),
+    useSelector: jest.fn(),
+}));
+
+describe("test component: showcaseView", () => {
+    const testContent = {
+        language: "en",
+        caption: "Showcase",
+        heading: "Most notable projects",
+        text: "Let's look at some of...",
+        image: "image.webp",
+        action: {
+            text: "Go to showcase",
+            href: "/en/showcase"
+        }
+    };
+
+    const pageData = ContentPageData;
+    pageData.components.featureShowcase = testContent;
+
+    beforeEach(() => {
+        jest.spyOn(Redux, "useSelector").mockReturnValueOnce(pageData);
+    });
+
+    it("should render correctly '<ShowcaseView />' when content is loaded.", () => {
+        const html = render(
+            <BrowserRouter>
+                <ShowcaseView />
+            </BrowserRouter>
+        );
+
+        expect(html).toMatchSnapshot();
+    });
+});
