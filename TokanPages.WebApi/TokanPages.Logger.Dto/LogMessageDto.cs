4d5981c71dc080991ebd5280f41627887e1f2b89
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2025-03-20 22:41:54 +0100

    feat: extend dto model

--- TokanPages.WebApi/TokanPages.Logger.Dto/LogMessageDto.cs
+++ TokanPages.WebApi/TokanPages.Logger.Dto/LogMessageDto.cs
@@ -6,6 +6,21 @@ namespace TokanPages.Logger.Dto;
 public class LogMessageDto
 {
     /// <summary>
+    /// 
+    /// </summary>
+    public DateTime EventDateTime { get; set; }
+
+    /// <summary>
+    /// Type of event.
+    /// </summary>
+    public string EventType { get; set; } = "";
+
+    /// <summary>
+    /// Severity (debug, info, warning, error, fatal).
+    /// </summary>
+    public string Severity { get; set; } = "";
+
+    /// <summary>
     /// Message.
     /// </summary>
     public string Message { get; set; } = "";
@@ -13,10 +28,25 @@ public class LogMessageDto
     /// <summary>
     /// Error details, stack trace from the frontend.
     /// </summary>
-    public string Exception { get; set; } = "";
+    public string StackTrace { get; set; } = "";
 
     /// <summary>
-    /// Severity (debug, info, warning, error, fatal).
+    /// Current URL of the page, when the exception occured.
     /// </summary>
-    public string Severity { get; set; } = "";
+    public string PageUrl { get; set; } = "";
+
+    /// <summary>
+    /// Parsed browser name.
+    /// </summary>
+    public string BrowserName { get; set; } = "";
+
+    /// <summary>
+    /// Parsed browser version.
+    /// </summary>
+    public string BrowserVersion { get; set; } = "";
+
+    /// <summary>
+    /// User agent string for the current browser.
+    /// </summary>
+    public string UserAgent { get; set; } = "";
 }
