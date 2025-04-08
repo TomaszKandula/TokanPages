b768457126455438fb71bab4674a7a6effc98a20
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2025-04-08 22:49:02 +0200

    feat: add optional button

--- TokanPages.ClientApp/src/Shared/Components/CustomCard/index.tsx
+++ TokanPages.ClientApp/src/Shared/Components/CustomCard/index.tsx
@@ -1,15 +1,22 @@
 import * as React from "react";
-import { Card, CardContent } from "@material-ui/core";
+import { Link } from "react-router-dom";
 import { Skeleton } from "@material-ui/lab";
+import { Button, Card, CardContent } from "@material-ui/core";
 import { ViewProperties } from "../../../Shared/Abstractions";
 
 type Colour = "info" | "success" | "warning" | "error";
+
+interface OptionsProps {
+    buttonLink: string;
+    buttonLabel: string;
+}
 
 interface CustomCardProps extends ViewProperties {
     caption: string;
     text: string[];
     icon: React.ReactElement;
     colour: Colour;
+    options?: OptionsProps;
 }
 
 const RenderIcon = (props: CustomCardProps) => {
@@ -18,6 +25,22 @@ const RenderIcon = (props: CustomCardProps) => {
         className: `custom-card-icon vertical-centre alert-${props.colour}` 
     });
 }
+
+const RenderButton = (props: CustomCardProps): React.ReactElement => {
+    return (
+        props.isLoading ? (
+            <Skeleton variant="rect" width="100%" height="40px" />
+        ) : (
+            <div className="mt-48">
+                <Link to={props.options?.buttonLink ?? ""} className="link" rel="noopener nofollow">
+                    <Button fullWidth variant="contained" className="button" disabled={props.isLoading}>
+                        {props.options?.buttonLabel}
+                    </Button>
+                </Link>
+            </div>
+        )
+    );
+};
 
 export const CustomCard = (props: CustomCardProps): React.ReactElement => {
     return (
@@ -31,12 +54,15 @@ export const CustomCard = (props: CustomCardProps): React.ReactElement => {
                 <h2 className="custom-card-caption text-centre">
                     {props.isLoading ? <Skeleton variant="text" /> : props.caption}
                 </h2>
-                {props.text.map((value: string, index: number) => (
-                    <div className="custom-card-text text-centre" key={index}>
-                        {props.isLoading ? <Skeleton variant="text" /> : value}
-                    </div>
-                ))}
-                <div className="mb-32"></div>
+                <div className="mt-48">
+                    {props.text.map((value: string, index: number) => (
+                        <div className="custom-card-text text-centre" key={index}>
+                            {props.isLoading ? <Skeleton variant="text" /> : value}
+                        </div>
+                    ))}
+                </div>
+                {props.options ? <RenderButton {...props} /> : null}
+                <div className={props.options ? "mb-25" : "mb-48"}></div>
             </CardContent>
         </Card>
     );
