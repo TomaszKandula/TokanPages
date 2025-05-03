3b02581e48f9787c865ed3ae02d92e1aaec582a8
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2025-05-03 15:06:49 +0200

    refactor: remove internal state

--- TokanPages.ClientApp/src/Shared/Components/Toasts/applicationToast.tsx
+++ TokanPages.ClientApp/src/Shared/Components/Toasts/applicationToast.tsx
@@ -2,28 +2,16 @@ import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Slide, SlideProps } from "@material-ui/core";
 import { ReactSyntheticEvent } from "../../../Shared/types";
+import { DialogType } from "../../../Shared/enums";
 import { ApplicationState } from "../../../Store/Configuration";
 import { ApplicationErrorAction } from "../../../Store/Actions";
-import { RECEIVED_ERROR_MESSAGE } from "../../constants";
-import { DialogType } from "../../enums";
 import { ApplicationToastView } from "./View/applicationToastView";
 import Validate from "validate.js";
-
-interface Properties {
-    isOpen: boolean;
-    errorMessage: string;
-}
-
-const ToastState: Properties = {
-    isOpen: false,
-    errorMessage: "",
-};
 
 const TransitionLeft = (props: Omit<SlideProps, "direction">): React.ReactElement => {
     return <Slide {...props} direction="left" />;
 };
 
-// TODO: remove component state
 export const ApplicationToast = (): React.ReactElement => {
     const vertical = "top";
     const horizontal = "right";
@@ -31,48 +19,29 @@ export const ApplicationToast = (): React.ReactElement => {
     const autoHideDuration: number = 15000;
 
     const dispatch = useDispatch();
-    const [toastState, setToastState] = React.useState(ToastState);
     const error = useSelector((state: ApplicationState) => state.applicationError);
 
-    const clearError = React.useCallback(() => {
-        if (!toastState.isOpen && !Validate.isEmpty(toastState.errorMessage)) {
-            dispatch(ApplicationErrorAction.clear());
-            setToastState(ToastState);
-        }
-    }, [dispatch, toastState]);
-
-    const raiseError = React.useCallback(() => {
-        if (error?.dialogType !== DialogType.toast) return;
-        if (error?.errorMessage === RECEIVED_ERROR_MESSAGE) {
-            setToastState({
-                isOpen: true,
-                errorMessage: error?.errorDetails ?? "",
-            });
-        }
-    }, [error]);
-
-    React.useEffect(() => raiseError(), [raiseError]);
-    React.useEffect(() => clearError(), [clearError]);
+    const isEmpty = Validate.isEmpty(error?.errorDetails);
+    const isOpen = !isEmpty && error.dialogType === DialogType.toast;
 
     const closeEventHandler = React.useCallback(
         (event?: ReactSyntheticEvent, reason?: string) => {
             if (event === undefined) return;
             if (reason === "clickaway") return;
-            setToastState({ ...toastState, isOpen: false });
-        },
-        [toastState]
+            dispatch(ApplicationErrorAction.clear());
+        },[]
     );
 
     return (
         <ApplicationToastView
             anchorOrigin={{ vertical, horizontal }}
-            isOpen={toastState.isOpen}
+            isOpen={isOpen}
             autoHideDuration={autoHideDuration}
             closeEventHandler={closeEventHandler}
             TransitionComponent={TransitionLeft}
             componentKey={vertical + horizontal}
             toastSeverity={toastSeverity}
-            toastMessage={toastState.errorMessage}
+            toastMessage={error?.errorDetails ?? error?.errorMessage}
         />
     );
 };
