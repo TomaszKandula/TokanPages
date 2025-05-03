062b1b05361c564e9195429aaf01dbdf3e45d3df
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2025-05-03 15:07:09 +0200

    refactor: relocate to hook

--- TokanPages.ClientApp/src/Shared/Components/ErrorBoundary/errorBoundaryView.tsx
+++ TokanPages.ClientApp/src/Shared/Components/ErrorBoundary/errorBoundaryView.tsx
@@ -1,44 +1,19 @@
 import * as React from "react";
 import { useErrorBoundaryContent } from "../../../Shared/Hooks";
 
-interface ErrorBoundaryViewProps {
-    title?: string;
-    subtitle?: string;
-    text?: string;
-    linkHref?: string;
-    linkText?: string;
-    footer?: string;
-}
-
-const defaultContent: ErrorBoundaryViewProps = {
-    title: "Critical Error",
-    subtitle: "Something went wrong...",
-    text: "Contact the site's administrator or support for assistance.",
-    linkHref: "mailto:admin@tomkandula.com",
-    linkText: "IT support",
-    footer: "tomkandula.com",
-};
-
 export const ErrorBoundaryView = () => {
     const content = useErrorBoundaryContent();
-
-    const title = content?.title ?? defaultContent.title;
-    const subtitle = content?.subtitle ?? defaultContent.subtitle;
-    const text = content?.text ?? defaultContent.text;
-    const href = content?.linkHref ?? defaultContent.linkHref;
-    const link = content?.linkText ?? defaultContent.linkText;
-    const footer = content?.footer ?? defaultContent.footer;
 
     return (
         <div id="error">
             <div className="error">
-                <h1>{title}</h1>
-                <h2>{subtitle}</h2>
+                <h1>{content?.title}</h1>
+                <h2>{content?.subtitle}</h2>
                 <div className="error-text">
-                    <p>{text}</p>
-                    <a href={href}>{link}</a>
+                    <p>{content?.text}</p>
+                    <a href={content?.linkHref}>{content?.linkText}</a>
                     <hr />
-                    <p>{footer}</p>
+                    <p>{content?.footer}</p>
                 </div>
             </div>
         </div>
