a0f9e1bc0e6131e6292adfe2521140b83742ebc1
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2025-05-11 12:44:33 +0200

    refactor: relocate css, remove theme provider

--- TokanPages.ClientApp/src/index.tsx
+++ TokanPages.ClientApp/src/index.tsx
@@ -5,8 +5,6 @@ import { Provider } from "react-redux";
 import { ConnectedRouter } from "connected-react-router";
 import { createBrowserHistory } from "history";
 import { createHead, UnheadProvider } from "@unhead/react/client";
-import { ThemeProvider } from "@material-ui/core";
-import { AppTheme } from "./Theme";
 import { ConfigureStore } from "./Store/Configuration";
 import { ErrorBoundary } from "./Shared/Components";
 import { IsPreRendered, TryGetStateSnapshot } from "./Shared/Services/SpaCaching";
@@ -27,11 +25,9 @@ const ReactApp = (manifest: GetContentManifestDto | undefined): void => {
             <Provider store={store}>
                 <UnheadProvider head={head}>
                     <ConnectedRouter history={history}>
-                        <ThemeProvider theme={AppTheme}>
                             <ErrorBoundary>
                                 <App manifest={manifest} />
                             </ErrorBoundary>
-                        </ThemeProvider>
                     </ConnectedRouter>
                 </UnheadProvider>
             </Provider>
