7fc86b38196320cb1690b1428208fd9f5c620da5
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2025-05-15 23:20:19 +0200

    refactor: relocate component, remove unused code

--- TokanPages.ClientApp/src/Components/Layout/Navigation/View/navigationView.tsx
+++ TokanPages.ClientApp/src/Components/Layout/Navigation/View/navigationView.tsx
@@ -3,12 +3,14 @@ import { Link } from "react-router-dom";
 import Icon from "@mdi/react";
 import { mdiArrowLeft } from "@mdi/js";
 import { APP_BAR_HEIGHT } from "../../../../Shared/constants";
-import { AppBar, IconButton } from "../../../../Shared/Components";
+import { AppBar, IconButton, RenderImage } from "../../../../Shared/Components";
 import { Properties } from "../Abstractions";
-import { SideMenuView } from "../SideMenu/sideMenuView";
 import { RenderToolbarLargeScreen } from "../RenderToolbarLargeScreen";
 import { RenderToolbarSmallScreen } from "../RenderToolbarSmallScreen";
 import "./navigationView.css";
+import { Drawer } from "@material-ui/core";
+import { GET_ICONS_URL } from "Api";
+import { RenderSideMenu } from "../RenderMenu";
 
 export const NavigationView = (props: Properties): React.ReactElement => {
     const mainPath = `/${props.languageId}`;
@@ -35,13 +37,24 @@ export const NavigationView = (props: Properties): React.ReactElement => {
                         <nav className="bulma-navbar navigation-nav-small-screen">
                             <RenderToolbarSmallScreen {...props} height={APP_BAR_HEIGHT} />
                         </nav>
-                        <SideMenuView
-                            drawerState={props.drawerState}
-                            closeHandler={props.closeHandler}
-                            isAnonymous={props.isAnonymous}
-                            languageId={props.languageId}
-                            menu={props.menu}
-                        />
+                        <Drawer anchor="left" open={props.drawerState.open} onClose={props.closeHandler}>
+                            <nav className="sidemenu-drawer-container">
+                                <div className="sidemenu-drawer-hero">
+                                    <RenderImage
+                                        base={GET_ICONS_URL}
+                                        source={props?.menu?.image}
+                                        title="Logo"
+                                        alt="An application logo"
+                                        className="sidemenu-logo"
+                                    />
+                                </div>
+                                <RenderSideMenu
+                                    isAnonymous={props.isAnonymous}
+                                    languageId={props.languageId}
+                                    items={props.menu?.items}
+                                />
+                            </nav>
+                        </Drawer>
                     </>
                 </AppBar>
             )}
