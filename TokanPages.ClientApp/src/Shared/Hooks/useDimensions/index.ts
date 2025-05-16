135b66c314729fe84ea968d910b1eadde57bbb35
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2025-05-16 15:53:51 +0200

    feat: extend hook

--- TokanPages.ClientApp/src/Shared/Hooks/useDimensions/index.ts
+++ TokanPages.ClientApp/src/Shared/Hooks/useDimensions/index.ts
@@ -3,16 +3,45 @@ import * as React from "react";
 interface UseDimensionsResult {
     width: number;
     height: number;
+    isMobile: boolean;
+    isTablet: boolean;
+    isDesktop: boolean;
 }
 
 export const useDimensions = (): UseDimensionsResult => {
-    const [width, setWidth]   = React.useState(window.innerWidth);
+    const [width, setWidth] = React.useState(window.innerWidth);
     const [height, setHeight] = React.useState(window.innerHeight);
+    const [isMobile, setIsMobile] = React.useState(false);
+    const [isTablet, setIsTablet] = React.useState(false);
+    const [isDesktop, setIsDesktop] = React.useState(false);
 
     const updateDimensions = (): void => {
         setWidth(window.innerWidth);
         setHeight(window.innerHeight);
-    }
+    };
+
+    React.useEffect(() => {
+        /* MOBILE */
+        if (width <= 768) {
+            setIsMobile(true);
+            setIsTablet(false);
+            setIsDesktop(false);
+        }
+
+        /* TABLET */
+        if (width > 768 && width < 1024) {
+            setIsMobile(false);
+            setIsTablet(true);
+            setIsDesktop(false);
+        }
+
+        /* DESKTOP */
+        if (width >= 1024) {
+            setIsMobile(false);
+            setIsTablet(false);
+            setIsDesktop(true);
+        }
+    }, [width]);
 
     React.useEffect(() => {
         window.addEventListener("resize", updateDimensions);
@@ -22,5 +51,8 @@ export const useDimensions = (): UseDimensionsResult => {
     return {
         width,
         height,
-    }
-}
+        isMobile,
+        isTablet,
+        isDesktop,
+    };
+};
