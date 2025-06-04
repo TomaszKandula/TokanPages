99d3d7a5023099ae1d67b07947b87cc5e181b9c4
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2025-06-04 17:05:48 +0200

    feat: remove MUI skeleton

--- TokanPages.ClientApp/src/Shared/Components/CustomCard/index.tsx
+++ TokanPages.ClientApp/src/Shared/Components/CustomCard/index.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 import { Link } from "react-router-dom";
-import { Skeleton } from "@material-ui/lab";
 import { ViewProperties } from "../../../Shared/Abstractions";
 import "./customCard.css";
 
@@ -27,27 +26,21 @@ const RenderIcon = (props: CustomCardProps) => {
     });
 };
 
-const RenderLinkButton = (props: CustomCardProps): React.ReactElement => {
-    return props.isLoading ? (
-        <Skeleton variant="rect" width="100%" height="40px" />
-    ) : (
-        <div className="mt-48">
-            <Link to={props.linkButton?.buttonLink ?? ""} className="link" rel="noopener nofollow">
-                <button className="bulma-button" disabled={props.isLoading}>
-                    {props.linkButton?.buttonLabel}
-                </button>
-            </Link>
-        </div>
-    );
-};
+const RenderLinkButton = (props: CustomCardProps): React.ReactElement => (
+    <div className="mt-48">
+        <Link to={props.linkButton?.buttonLink ?? ""} className="link" rel="noopener nofollow">
+            <button className={`bulma-button ${props.isLoading ? "bulma-is-skeleton" : ""}`} disabled={props.isLoading}>
+                {props.linkButton?.buttonLabel}
+            </button>
+        </Link>
+    </div>
+);
 
-const RenderExternalButton = (props: CustomCardProps): React.ReactElement => {
-    return props.isLoading ? (
-        <Skeleton variant="rect" width="100%" height="40px" />
-    ) : (
-        <div className="mt-48">{props.externalButton}</div>
-    );
-};
+const RenderExternalButton = (props: CustomCardProps): React.ReactElement => (
+    <div className={`mt-48 ${props.isLoading ? "bulma-is-skeleton" : ""}`}>
+        {props.externalButton}
+    </div>
+);
 
 export const CustomCard = (props: CustomCardProps): React.ReactElement => {
     return (
@@ -58,13 +51,13 @@ export const CustomCard = (props: CustomCardProps): React.ReactElement => {
                         <RenderIcon {...props} />
                     </div>
                 </div>
-                <h2 className="bulma-title has-text-centered">
-                    {props.isLoading ? <Skeleton variant="text" /> : props.caption}
+                <h2 className={`bulma-title has-text-centered ${props.isLoading ? "bulma-is-skeleton" : ""}`}>
+                    {props.caption}
                 </h2>
                 <div className="my-5">
                     {props.text.map((value: string, index: number) => (
-                        <p className="is-size-6 has-text-centered line-height-20" key={index}>
-                            {props.isLoading ? <Skeleton variant="text" /> : value}
+                        <p className={`is-size-6 has-text-centered line-height-20 ${props.isLoading ? "bulma-is-skeleton" : ""}`} key={index}>
+                            {value}
                         </p>
                     ))}
                 </div>
