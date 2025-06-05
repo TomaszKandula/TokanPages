a5bde5efeeb419fb69a19db66d79c2d9099ff2bd
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2025-06-05 20:12:04 +0200

    feat: rebuild component

--- TokanPages.ClientApp/src/Components/Account/PasswordReset/View/resetPasswordView.tsx
+++ TokanPages.ClientApp/src/Components/Account/PasswordReset/View/resetPasswordView.tsx
@@ -1,15 +1,11 @@
 import * as React from "react";
-import Container from "@material-ui/core/Container";
-import Grid from "@material-ui/core/Grid";
-import Typography from "@material-ui/core/Typography";
-import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
 import { Card, CardContent, CircularProgress } from "@material-ui/core";
-import { AccountCircle } from "@material-ui/icons";
 import Skeleton from "@material-ui/lab/Skeleton";
 import { ViewProperties } from "../../../../Shared/Abstractions";
 import { ReactChangeEvent, ReactKeyboardEvent } from "../../../../Shared/types";
 import { PasswordResetProps } from "../passwordReset";
+import { Icon } from "Shared/Components";
 
 interface Properties extends ViewProperties, PasswordResetProps {
     progress: boolean;
@@ -24,65 +20,60 @@ interface Properties extends ViewProperties, PasswordResetProps {
 
 const ActiveButton = (props: Properties): React.ReactElement => {
     return (
-        <Button
-            fullWidth
+        <button
             type="submit"
-            variant="contained"
             onClick={props.buttonHandler}
-            className="button"
-            disabled={props.progress}
+            className="bulma-button bulma-is-light bulma-is-fullwidth"
+            disabled={props.progress || props.email.length === 0}
         >
             {!props.progress ? props.button : <CircularProgress size={20} />}
-        </Button>
+        </button>
     );
 };
 
 export const PasswordResetView = (props: Properties): React.ReactElement => {
     return (
         <section className={`section ${props.background ?? ""}`}>
-            <Container className="container">
+            <div className="bulma-container bulma-is-max-tablet">
                 <div className={!props.className ? "pt-96 pb-80" : props.className}>
+                    
                     <Card elevation={0} className="card">
                         <CardContent className="card-content">
-                            <div className="text-centre mb-25">
-                                <AccountCircle className="account" />
-                                <Typography className="caption">
+                            
+                            <div className="has-text-centered">
+                                <Icon name="Account" size={3} className="account" />
+                                <p className="caption">
                                     {props.isLoading ? <Skeleton variant="text" /> : props.caption}
-                                </Typography>
+                                </p>
                             </div>
+                            
                             <div>
-                                <Grid container spacing={2}>
-                                    <Grid item xs={12}>
-                                        {props.isLoading ? (
-                                            <Skeleton variant="rect" width="100%" height="45px" />
-                                        ) : (
-                                            <TextField
-                                                required
-                                                fullWidth
-                                                id="email"
-                                                name="email"
-                                                variant="outlined"
-                                                autoComplete="email"
-                                                onKeyUp={props.keyHandler}
-                                                onChange={props.formHandler}
-                                                value={props.email}
-                                                label={props.labelEmail}
-                                            />
-                                        )}
-                                    </Grid>
-                                </Grid>
-                                <div className="mt-15 mb-15">
-                                    {props.isLoading ? (
-                                        <Skeleton variant="rect" width="100%" height="40px" />
-                                    ) : (
-                                        <ActiveButton {...props} />
-                                    )}
-                                </div>
+
+
+                                <TextField
+                                    required
+                                    fullWidth
+                                    id="email"
+                                    name="email"
+                                    variant="outlined"
+                                    autoComplete="email"
+                                    onKeyUp={props.keyHandler}
+                                    onChange={props.formHandler}
+                                    value={props.email}
+                                    label={props.labelEmail}
+                                />
+
+                                <ActiveButton {...props} />
+
+
                             </div>
+
                         </CardContent>
                     </Card>
+
                 </div>
-            </Container>
+            </div>
+
         </section>
     );
 };
