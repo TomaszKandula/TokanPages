3fe97468ecf1b2081a307349357e8f8935edc0bb
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2025-06-08 00:49:37 +0200

    feat: extend icon button component

--- TokanPages.ClientApp/src/Shared/Components/IconButton/iconButton.tsx
+++ TokanPages.ClientApp/src/Shared/Components/IconButton/iconButton.tsx
@@ -5,13 +5,22 @@ import "./iconButton.css";
 interface IconButtonProps {
     size?: number;
     children: React.ReactElement | React.ReactElement[];
+    hasNoHoverEffect?: boolean;
     onClick?: (event: ReactMouseEvent) => void;
+    onMouseDown?: (event: ReactMouseEvent) => void;
 }
 
 export const IconButton = (props: IconButtonProps): React.ReactElement => {
     const size = props.size ?? 48;
+    const className = props.hasNoHoverEffect ? "icon-button-no-hover" : "icon-button";
+
     return (
-        <button className="icon-button" onClick={props.onClick} style={{ height: size, width: size }}>
+        <button 
+            className={className}
+            onClick={props.onClick}
+            onMouseDown={props.onMouseDown}
+            style={{ height: size, width: size }}
+        >
             {props.children}
         </button>
     );
