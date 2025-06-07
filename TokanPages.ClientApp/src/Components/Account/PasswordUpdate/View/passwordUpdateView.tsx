fb7aa631237c6c53f919d460e020a9068465f8d7
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2025-06-08 00:50:26 +0200

    feat: replace MUI w/Bulma

--- TokanPages.ClientApp/src/Components/Account/PasswordUpdate/View/passwordUpdateView.tsx
+++ TokanPages.ClientApp/src/Components/Account/PasswordUpdate/View/passwordUpdateView.tsx
@@ -1,14 +1,7 @@
 import * as React from "react";
-import Container from "@material-ui/core/Container";
-import Grid from "@material-ui/core/Grid";
-import Typography from "@material-ui/core/Typography";
-import Button from "@material-ui/core/Button";
-import { Card, CardContent, CircularProgress } from "@material-ui/core";
-import { AccountCircle } from "@material-ui/icons";
-import Skeleton from "@material-ui/lab/Skeleton";
 import { ViewProperties } from "../../../../Shared/Abstractions";
 import { ReactChangeEvent, ReactKeyboardEvent } from "../../../../Shared/types";
-import { TextFiedWithPassword } from "../../../../Shared/Components";
+import { Icon, ProgressBar, TextFiedWithPassword } from "../../../../Shared/Components";
 import { PasswordUpdateProps } from "../passwordUpdate";
 
 interface Properties extends ViewProperties, PasswordUpdateProps {
@@ -27,77 +20,59 @@ interface Properties extends ViewProperties, PasswordUpdateProps {
 
 const ActiveButton = (props: Properties): React.ReactElement => {
     return (
-        <Button
-            fullWidth
+        <button
             type="submit"
-            variant="contained"
             onClick={props.buttonHandler}
-            className="button"
+            className={`bulma-button ${props.isLoading ? "bulma-is-skeleton" : ""} bulma-is-light bulma-is-fullwidth`}
             disabled={props.progress || props.disableForm}
         >
-            {!props.progress ? props.button : <CircularProgress size={20} />}
-        </Button>
+            {!props.progress ? props.button : <ProgressBar size={20} />}
+        </button>
     );
 };
 
 export const PasswordUpdateView = (props: Properties): React.ReactElement => {
     return (
         <section className={`section ${props.background ?? ""}`}>
-            <Container className="container">
+            <div className="bulma-container bulma-is-max-tablet">
                 <div className={!props.className ? "pt-96 pb-80" : props.className}>
-                    <Card elevation={0} className="card">
-                        <CardContent className="card-content">
-                            <div className="text-centre mb-25">
-                                <AccountCircle className="account" />
-                                <Typography className="caption">
-                                    {props.isLoading ? <Skeleton variant="text" /> : props.caption}
-                                </Typography>
+                    <div className="bulma-card">
+                        <div className="bulma-card-content">
+                            <div className="has-text-centered my-3">
+                                <Icon name="Account" size={3} className="account" />
+                                <p className={`is-size-3 ${props.isLoading ? "bulma-is-skeleton" : "has-text-grey"}`}>
+                                    {props.caption}
+                                </p>
                             </div>
-                            <div>
-                                <Grid container spacing={2}>
-                                    <Grid item xs={12}>
-                                        {props.isLoading ? (
-                                            <Skeleton variant="rect" width="100%" height="45px" />
-                                        ) : (
-                                            <TextFiedWithPassword
-                                                uuid="newPassword"
-                                                fullWidth={true}
-                                                value={props.newPassword}
-                                                label={props.labelNewPassword}
-                                                onKeyUp={props.keyHandler}
-                                                onChange={props.formHandler}
-                                                isDisabled={props.disableForm || props.progress}
-                                            />
-                                        )}
-                                    </Grid>
-                                    <Grid item xs={12}>
-                                        {props.isLoading ? (
-                                            <Skeleton variant="rect" width="100%" height="45px" />
-                                        ) : (
-                                            <TextFiedWithPassword
-                                                uuid="verifyPassword"
-                                                fullWidth={true}
-                                                value={props.verifyPassword}
-                                                label={props.labelVerifyPassword}
-                                                onKeyUp={props.keyHandler}
-                                                onChange={props.formHandler}
-                                                isDisabled={props.disableForm || props.progress}
-                                            />
-                                        )}
-                                    </Grid>
-                                </Grid>
-                                <div className="mt-15 mb-15">
-                                    {props.isLoading ? (
-                                        <Skeleton variant="rect" width="100%" height="40px" />
-                                    ) : (
-                                        <ActiveButton {...props} />
-                                    )}
-                                </div>
+                            <div className="my-3">
+                                <TextFiedWithPassword
+                                    uuid="newPassword"
+                                    fullWidth={true}
+                                    value={props.newPassword}
+                                    label={props.labelNewPassword}
+                                    onKeyUp={props.keyHandler}
+                                    onChange={props.formHandler}
+                                    isDisabled={props.disableForm || props.progress}
+                                />
                             </div>
-                        </CardContent>
-                    </Card>
+                            <div className="my-3">
+                                <TextFiedWithPassword
+                                    uuid="verifyPassword"
+                                    fullWidth={true}
+                                    value={props.verifyPassword}
+                                    label={props.labelVerifyPassword}
+                                    onKeyUp={props.keyHandler}
+                                    onChange={props.formHandler}
+                                    isDisabled={props.disableForm || props.progress}
+                                />
+                            </div>
+                            <div className="my-5">
+                                <ActiveButton {...props} />
+                            </div>
+                        </div>
+                    </div>
                 </div>
-            </Container>
+            </div>
         </section>
     );
 };
