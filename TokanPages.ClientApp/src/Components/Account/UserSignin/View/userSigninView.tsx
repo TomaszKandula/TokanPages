b1c475cedb9235e4dce6915f336e37f6a25ea8aa
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2025-06-08 19:37:44 +0200

    feat: replace MUI w/Bulma

--- TokanPages.ClientApp/src/Components/Account/UserSignin/View/userSigninView.tsx
+++ TokanPages.ClientApp/src/Components/Account/UserSignin/View/userSigninView.tsx
@@ -1,16 +1,8 @@
 import * as React from "react";
-import Container from "@material-ui/core/Container";
-import Grid from "@material-ui/core/Grid";
-import Typography from "@material-ui/core/Typography";
-import Button from "@material-ui/core/Button";
-import TextField from "@material-ui/core/TextField";
-import { Card, CardContent, CircularProgress } from "@material-ui/core";
-import { AccountCircle } from "@material-ui/icons";
-import Skeleton from "@material-ui/lab/Skeleton";
 import { LinkDto } from "../../../../Api/Models";
 import { ViewProperties } from "../../../../Shared/Abstractions";
 import { ReactChangeEvent, ReactKeyboardEvent } from "../../../../Shared/types";
-import { RedirectTo, TextFiedWithPassword } from "../../../../Shared/Components";
+import { Icon, ProgressBar, RedirectTo, Skeleton, TextFiedWithPassword } from "../../../../Shared/Components";
 import { UserSigninProps } from "../userSignin";
 
 interface UserSigninViewProps extends ViewProperties, UserSigninProps {
@@ -30,95 +22,71 @@ interface UserSigninViewProps extends ViewProperties, UserSigninProps {
 
 const ActiveButton = (props: UserSigninViewProps): React.ReactElement => {
     return (
-        <Button
-            fullWidth
+        <button
             type="submit"
-            variant="contained"
             onClick={props.buttonHandler}
-            className="button"
+            className="bulma-button bulma-is-light bulma-is-fullwidth"
             disabled={props.progress}
         >
-            {!props.progress ? props.button : <CircularProgress size={20} />}
-        </Button>
+            {!props.progress ? props.button : <ProgressBar size={20} />}
+        </button>
     );
 };
 
 export const UserSigninView = (props: UserSigninViewProps): React.ReactElement => {
     return (
         <section className={`section ${props.background ?? ""}`}>
-            <Container className="container">
+            <div className="bulma-container bulma-is-max-tablet">
                 <div className={!props.className ? "pt-32 pb-80" : props.className}>
-                    <Card elevation={0} className="card">
-                        <CardContent className="card-content">
-                            <div className="text-centre mb-25">
-                                <AccountCircle className="account" />
-                                <Typography className="caption">
-                                    {props.isLoading ? <Skeleton variant="text" /> : props.caption}
-                                </Typography>
-                            </div>
-                            <Grid container spacing={2}>
-                                <Grid item xs={12}>
-                                    {props.isLoading ? (
-                                        <Skeleton variant="rect" width="100%" height="45px" />
-                                    ) : (
-                                        <TextField
-                                            required
-                                            fullWidth
-                                            id="email"
-                                            name="email"
-                                            variant="outlined"
-                                            autoComplete="email"
-                                            onKeyUp={props.keyHandler}
-                                            onChange={props.formHandler}
-                                            value={props.email}
-                                            label={props.labelEmail}
-                                            disabled={props.progress}
-                                        />
-                                    )}
-                                </Grid>
-                                <Grid item xs={12}>
-                                    {props.isLoading ? (
-                                        <Skeleton variant="rect" width="100%" height="45px" />
-                                    ) : (
-                                        <TextFiedWithPassword
-                                            uuid="password"
-                                            fullWidth={true}
-                                            value={props.password}
-                                            label={props.labelPassword}
-                                            onKeyUp={props.keyHandler}
-                                            onChange={props.formHandler}
-                                            isDisabled={props.progress}
-                                        />
-                                    )}
-                                </Grid>
-                            </Grid>
-                            <div className="mt-15 mb-15">
-                                {props.isLoading ? (
-                                    <Skeleton variant="rect" width="100%" height="40px" />
-                                ) : (
+                    <div className="bulma-card">
+                        <div className="bulma-card-content">
+                            <Skeleton isLoading={props.isLoading}>
+                                <div className="has-text-centered">
+                                    <Icon name="Account" size={3} className="account" />
+                                    <p className="is-size-3 has-text-grey">
+                                        {props.caption}
+                                    </p>
+                                </div>
+                                <div className="my-5">
+                                    <input
+                                        required
+                                        id="email"
+                                        name="email"
+                                        autoComplete="email"
+                                        onKeyUp={props.keyHandler}
+                                        onChange={props.formHandler}
+                                        value={props.email}
+                                        placeholder={props.labelEmail}
+                                        disabled={props.progress}
+                                        className="bulma-input bulma-is-link mb-4"
+                                    />
+                                    <TextFiedWithPassword
+                                        className="mb-4"
+                                        uuid="password"
+                                        fullWidth={true}
+                                        value={props.password}
+                                        label={props.labelPassword}
+                                        onKeyUp={props.keyHandler}
+                                        onChange={props.formHandler}
+                                        isDisabled={props.progress}
+                                    />
+                                </div>
+                                <div className="my-5">
                                     <ActiveButton {...props} />
-                                )}
-                            </div>
-                            <Grid container spacing={2} className="actions">
-                                <Grid item xs={12} sm={6}>
-                                    {props.isLoading ? (
-                                        <Skeleton variant="text" />
-                                    ) : (
+                                </div>
+                                <div className="is-flex is-flex-direction-row my-5">
+                                    <div className="is-justify-content-flex-start">
                                         <RedirectTo path={props.link1?.href} name={props.link1?.text} />
-                                    )}
-                                </Grid>
-                                <Grid item xs={12} sm={6} className="secondaryAction">
-                                    {props.isLoading ? (
-                                        <Skeleton variant="text" />
-                                    ) : (
+                                    </div>
+                                    <div className="is-justify-content-flex-end">
                                         <RedirectTo path={props.link2?.href} name={props.link2?.text} />
-                                    )}
-                                </Grid>
-                            </Grid>
-                        </CardContent>
-                    </Card>
+                                    </div>
+                                </div>
+                            </Skeleton>
+                        </div>
+                    </div>
                 </div>
-            </Container>
+            </div>
         </section>
     );
 };
