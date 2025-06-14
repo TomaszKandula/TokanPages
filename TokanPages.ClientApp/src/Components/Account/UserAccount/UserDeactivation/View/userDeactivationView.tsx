3422a7f140d55e673ec03de111ce879b2510c00b
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2025-06-14 18:35:14 +0200

    feat: replace MUI w/Bulma

--- TokanPages.ClientApp/src/Components/Account/UserAccount/UserDeactivation/View/userDeactivationView.tsx
+++ TokanPages.ClientApp/src/Components/Account/UserAccount/UserDeactivation/View/userDeactivationView.tsx
@@ -1,12 +1,7 @@
 import * as React from "react";
-import { Button, CircularProgress, Grid, Typography } from "@material-ui/core";
-import Card from "@material-ui/core/Card";
-import CardContent from "@material-ui/core/CardContent";
-import Container from "@material-ui/core/Container";
-import Skeleton from "@material-ui/lab/Skeleton";
 import { ViewProperties } from "../../../../../Shared/Abstractions";
 import { SectionAccountDeactivation } from "../../../../../Api/Models";
-import { CustomDivider, RenderParagraphs } from "../../../../../Shared/Components";
+import { CustomDivider, ProgressBar, RenderParagraphs } from "../../../../../Shared/Components";
 import { UserDeactivationProps } from "../userDeactivation";
 
 interface UserDeactivationViewProps extends ViewProperties, UserDeactivationProps {
@@ -17,58 +12,42 @@ interface UserDeactivationViewProps extends ViewProperties, UserDeactivationProp
 
 const DeactivationButton = (props: UserDeactivationViewProps): React.ReactElement => {
     return (
-        <Button
-            fullWidth
+        <button
             type="submit"
-            variant="contained"
             onClick={props.buttonHandler}
             disabled={props.progress}
-            className="button-delete"
+            className="bulma-button bulma-is-danger bulma-is-light"
         >
-            {!props.progress ? props.section?.deactivateButtonText : <CircularProgress size={20} />}
-        </Button>
+            {!props.progress ? props.section?.deactivateButtonText : <ProgressBar size={20} />}
+        </button>
     );
 };
 
 export const UserDeactivationView = (props: UserDeactivationViewProps): React.ReactElement => {
     return (
         <section className={`section ${props.background ?? ""}`}>
-            <Container className="container-wide">
+            <div className="bulma-container bulma-is-max-desktop">
                 <div className="pb-40">
-                    <Card elevation={0} className="card">
-                        <CardContent className="card-content">
-                            <Typography component="span" className="caption black">
-                                {props.isLoading ? <Skeleton variant="text" /> : props.section?.caption}
-                            </Typography>
+                    <div className="bulma-card">
+                        <div className="bulma-card-content">
+                            <p className="is-size-4 has-text-grey">
+                                {props.section?.caption}
+                            </p>
                             <CustomDivider mt={15} mb={8} />
                             <div className="pt-8 pb-8">
-                                <Grid container spacing={2}>
-                                    <Grid item>
-                                        {props.isLoading ? (
-                                            <Skeleton variant="text" width="200px" />
-                                        ) : (
-                                            <RenderParagraphs
-                                                text={props.section?.warningText}
-                                                className="label user-account-text-item"
-                                            />
-                                        )}
-                                    </Grid>
-                                </Grid>
+                                <RenderParagraphs
+                                    text={props.section?.warningText}
+                                    className="is-size-6 has-text-grey line-height-20"
+                                />
                                 <CustomDivider mt={15} mb={15} />
-                                <Grid className="button-container-update">
-                                    <div className="mt-15 mb-15">
-                                        {props.isLoading ? (
-                                            <Skeleton variant="rect" width="150px" height="40px" />
-                                        ) : (
-                                            <DeactivationButton {...props} />
-                                        )}
-                                    </div>
-                                </Grid>
+                                <div className="has-text-right">
+                                    <DeactivationButton {...props} />
+                                </div>
                             </div>
-                        </CardContent>
-                    </Card>
+                        </div>
+                    </div>
                 </div>
-            </Container>
+            </div>
         </section>
     );
 };
