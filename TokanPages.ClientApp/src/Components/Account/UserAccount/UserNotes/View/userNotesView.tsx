0f6410c4cb0f03009ad0a6810c380b18de0695da
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2025-06-19 15:43:31 +0200

    feat: replace MUI w/Bulma

--- TokanPages.ClientApp/src/Components/Account/UserAccount/UserNotes/View/userNotesView.tsx
+++ TokanPages.ClientApp/src/Components/Account/UserAccount/UserNotes/View/userNotesView.tsx
@@ -1,21 +1,9 @@
 import * as React from "react";
-import { Skeleton } from "@material-ui/lab";
 import { UserNoteResultDto } from "../../../../../Api/Models";
-import { CustomDivider } from "../../../../../Shared/Components";
+import { ProgressBar } from "../../../../../Shared/Components";
 import { ReactChangeEvent } from "../../../../../Shared/types";
 import Validate from "validate.js";
-import {
-    Backdrop,
-    Button,
-    Card,
-    CardContent,
-    CircularProgress,
-    Container,
-    ListItem,
-    ListItemText,
-    TextField,
-    Typography,
-} from "@material-ui/core";
+import { TextField } from "@material-ui/core";
 import "./userNotesView.css";
 
 interface UserNotesViewProps {
@@ -57,22 +45,26 @@ interface RenderRowProps {
     onClick: (index: number) => void | undefined;
 }
 
-interface RenderTextProps extends UserNotesViewProps {
-    value: string;
-}
-
 const RenderRow = (props: RenderRowProps): React.ReactElement => {
     const highlightClass = props.selection === props.id ? "user-notes-highlight-row" : "";
     return (
-        <ListItem button className={highlightClass} key={props.id}>
-            <ListItemText primary={props.note} onClick={() => props.onClick(props.index)} />
-        </ListItem>
+        <div className={highlightClass} key={props.id}>
+            <p className="is-size-6 has-text-grey py-4 px-4" onClick={() => props.onClick(props.index)}>
+                {props.note}
+            </p>
+        </div>
     );
 };
 
-const RenderText = (props: RenderTextProps): React.ReactElement => {
-    return props.isLoading ? <Skeleton variant="text" /> : <>{props.value}</>;
-};
+const RenderBackdrop = (props: UserNotesViewProps) => (
+    <>
+        {props.hasProgress 
+        ? <div className="backdrop">
+            <ProgressBar colour="#fff" size={50} thickness={4} />
+        </div>
+        : null}
+    </>
+);
 
 export const UserNotesView = (props: UserNotesViewProps): React.ReactElement => {
     const hasNotes = props.userNotes && props.userNotes.length > 0;
@@ -81,31 +73,26 @@ export const UserNotesView = (props: UserNotesViewProps): React.ReactElement => 
 
     return (
         <section>
-            <Backdrop className="backdrop" open={props.hasProgress}>
-                <CircularProgress color="inherit" />
-            </Backdrop>
-            <Container className="container-wide">
+            <RenderBackdrop {...props} />
+            <div className="bulma-container bulma-is-max-desktop">
                 <div className="pt-120 pb-40">
-                    <Card elevation={0} className="card">
-                        <CardContent className="card-content">
-                            <Typography component="span" className="caption black">
-                                <RenderText {...props} value={props.captionText} />
-                            </Typography>
+                    <div className="bulma-card">
+                        <div className="bulma-card-content">
+                            <p className="is-size-4 has-text-grey">
+                                {props.captionText}
+                            </p>
+                            <hr />
+                            <div className="py-4">
 
-                            <CustomDivider mt={15} mb={8} />
-
-                            <div className="pt-25 pb-8">
-                                <div className="mb-25">
-                                    <Typography component="span" className="label">
-                                        <RenderText {...props} value={props.descriptionText} />
-                                    </Typography>
-                                </div>
+                                <p className="is-size-6 has-text-grey pb-5">
+                                    {props.descriptionText}
+                                </p>
 
                                 <div className="user-notes-box">
                                     <div className="user-notes-container">
-                                        <Typography component="span" className="label">
-                                            <RenderText {...props} value={`${props.listLabel}:`} />
-                                        </Typography>
+                                        <p className="is-size-6 has-text-grey">
+                                            {`${props.listLabel}:`}
+                                        </p>
                                         <div
                                             className={`user-notes-fixed-list user-notes-border mt-10 ${props.isLoading ? "loading-indicator" : ""}`}
                                         >
@@ -124,9 +111,9 @@ export const UserNotesView = (props: UserNotesViewProps): React.ReactElement => 
                                     </div>
 
                                     <div className="user-notes-message-box">
-                                        <Typography component="span" className="label">
-                                            <RenderText {...props} value={`${props.noteLabel}${noteUid}`} />
-                                        </Typography>
+                                        <p className="is-size-6 has-text-grey">
+                                            {`${props.noteLabel}${noteUid}`}
+                                        </p>
                                         <div className="user-notes-text-box mt-10">
                                             <TextField
                                                 required
@@ -146,42 +133,37 @@ export const UserNotesView = (props: UserNotesViewProps): React.ReactElement => 
                                 </div>
 
                                 <div className="user-notes-button-box">
-                                    <Button
-                                        fullWidth
+                                    <button
                                         type="submit"
-                                        variant="contained"
-                                        className="button-delete button-margin-right"
+                                        className="bulma-button bulma-is-danger bulma-is-light mr-3"
                                         disabled={props.isLoading || !hasNotes || !props.selection}
                                         onClick={props.removeButtonHandler}
                                     >
                                         {props.removeButtonText}
-                                    </Button>
-                                    <Button
-                                        fullWidth
+                                    </button>
+                                    <button
                                         type="submit"
-                                        variant="contained"
-                                        className="button-update button-margin-right"
+                                        className="bulma-button bulma-is-light mr-3"
                                         disabled={props.isLoading}
                                         onClick={props.clearButtonHandler}
                                     >
                                         {props.clearButtonText}
-                                    </Button>
-                                    <Button
-                                        fullWidth
+                                    </button>
+                                    <button
                                         type="submit"
-                                        variant="contained"
-                                        className="button-update"
+                                        className="bulma-button bulma-is-light bulma-is-link"
                                         disabled={props.isLoading || isEmpty}
                                         onClick={props.saveButtonHandler}
                                     >
                                         {props.saveButtonText}
-                                    </Button>
+                                    </button>
                                 </div>
+
                             </div>
-                        </CardContent>
-                    </Card>
+                        </div>
+                    </div>
                 </div>
-            </Container>
+            </div>
         </section>
     );
 };
