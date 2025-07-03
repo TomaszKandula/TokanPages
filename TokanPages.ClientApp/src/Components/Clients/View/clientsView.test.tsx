34ad923aea17acc529eba39eee13b5abec9373c8
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2025-07-03 17:23:42 +0200

    test: update test

--- TokanPages.ClientApp/src/Components/Clients/View/clientsView.test.tsx
+++ TokanPages.ClientApp/src/Components/Clients/View/clientsView.test.tsx
@@ -8,7 +8,7 @@ describe("test component: clientsView", () => {
     it("should render correctly '<ClientsView />' when content is loaded.", () => {
         const html = render(
             <BrowserRouter>
-                <ClientsView background="class-colour-white" />
+                <ClientsView className="class-colour-white" />
             </BrowserRouter>
         );
 
