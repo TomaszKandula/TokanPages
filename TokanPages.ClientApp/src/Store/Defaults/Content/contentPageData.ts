33d235e283aa442ccb3e47cff7c8390a2d11b79d
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2025-07-05 14:22:37 +0200

    feat: remove multiline

--- TokanPages.ClientApp/src/Store/Defaults/Content/contentPageData.ts
+++ TokanPages.ClientApp/src/Store/Defaults/Content/contentPageData.ts
@@ -161,7 +161,6 @@ export const ContentPageData: ContentPageDataState = {
             techItems: [],
             description: {
                 label: "",
-                multiline: false,
                 rows: 0,
                 required: false,
             },
