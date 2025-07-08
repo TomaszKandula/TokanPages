b754cf7de735bd60f677ccf30b4d86341a1fb0a4
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2025-07-08 23:18:44 +0200

    feat: extend custom image component

--- TokanPages.ClientApp/src/Shared/Components/CustomImage/customImage.tsx
+++ TokanPages.ClientApp/src/Shared/Components/CustomImage/customImage.tsx
@@ -2,13 +2,14 @@ import * as React from "react";
 import validate from "validate.js";
 
 interface RenderImageProps {
-    base: string;
     source: string;
+    base?: string;
     className?: string;
     width?: number;
     height?: number;
     alt?: string;
     title?: string;
+    onClick?: () => void;
 }
 
 export const RenderImage = (props: RenderImageProps): React.ReactElement | null => {
@@ -17,15 +18,25 @@ export const RenderImage = (props: RenderImageProps): React.ReactElement | null 
         className = `${className} lazyloaded`;
     }
 
-    return validate.isEmpty(props.source) || validate.isEmpty(props.base) ? null : (
+    let src = props.source;
+    if (!validate.isEmpty(props.base) && !validate.isEmpty(props.source)) {
+        src = `${props.base}/${props.source}`;
+    }
+
+    if (validate.isEmpty(props.source)) {
+        return null;
+    }
+
+    return (
         <img
-            src={`${props.base}/${props.source}`}
+            src={src}
             loading="lazy"
             width={props.width}
             height={props.height}
             className={className}
             alt={props.alt}
             title={props.title}
+            onClick={props.onClick}
         />
     );
 };
