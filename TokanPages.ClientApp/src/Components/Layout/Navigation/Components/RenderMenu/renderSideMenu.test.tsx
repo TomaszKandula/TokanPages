4fa8d22312d890f17a460b8f3848fcd7ff484a51
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2025-07-13 10:33:37 +0200

    test: update tests w/snapshots

--- TokanPages.ClientApp/src/Components/Layout/Navigation/Components/RenderMenu/renderSideMenu.test.tsx
+++ TokanPages.ClientApp/src/Components/Layout/Navigation/Components/RenderMenu/renderSideMenu.test.tsx
@@ -1,4 +1,4 @@
-import "../../../setupTests";
+import "../../../../../setupTests";
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import { render } from "@testing-library/react";
