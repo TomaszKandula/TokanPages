afd4e4c422e19cda4188b751550d046fe48700f6
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2025-07-19 21:53:55 +0200

    feat: use skeleton component

--- TokanPages.ClientApp/src/Components/Testimonials/View/testimonialsView.tsx
+++ TokanPages.ClientApp/src/Components/Testimonials/View/testimonialsView.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import { GET_TESTIMONIALS_URL } from "../../../Api";
 import { ViewProperties } from "../../../Shared/Abstractions";
-import { Animated, CustomImage, Collapsible } from "../../../Shared/Components";
+import { Animated, CustomImage, Collapsible, Skeleton } from "../../../Shared/Components";
 import "./testimonialsView.css";
 
 interface TestimonialsViewProps extends ViewProperties {
@@ -28,12 +28,15 @@ export const TestimonialsView = (props: TestimonialsViewProps): React.ReactEleme
         <div className="bulma-container">
             <div className="py-6">
                 <Animated dataAos="fade-down">
+                    <Skeleton isLoading={props.isLoading} height={40}>
                     <p className="is-size-3	has-text-centered has-text-link">{props.caption?.toUpperCase()}</p>
+                    </Skeleton>
                 </Animated>
                 <div className={`bulma-columns ${props.isMobile ? "p-4" : "p-6"}`}>
                     <div className={`bulma-column ${props.isMobile ? "mt-6" : ""}`}>
                         <Animated dataAos="fade-up" dataAosDelay={350}>
                             <div className="bulma-card">
+                                <Skeleton isLoading={props.isLoading} className="testimonials-card-image">
                                 <CustomImage
                                     base={GET_TESTIMONIALS_URL}
                                     source={props.photo1}
@@ -41,16 +44,23 @@ export const TestimonialsView = (props: TestimonialsViewProps): React.ReactEleme
                                     title="Testimonials"
                                     alt={`Picture of ${props.name1}`}
                                 />
+                                </Skeleton>
                                 <div className="bulma-card-content mt-6">
+                                    <Skeleton isLoading={props.isLoading} mode="Text" height={24} className="mt-6 pt-4">
                                     <p className="is-size-4 has-text-centered has-text-weight-semibold mt-6 p-4">
                                         {props.name1}
                                     </p>
+                                    </Skeleton>
+                                    <Skeleton isLoading={props.isLoading} mode="Text" height={24}>
                                     <p className="is-size-6 has-text-centered has-text-link p-2">{props.occupation1}</p>
+                                    </Skeleton>
+                                    <Skeleton isLoading={props.isLoading} height={100}>
                                     <Collapsible minHeight={120}>
                                         <h4 className="is-size-6 has-text-centered has-text-grey p-2 line-height-18">
                                             {props.text1}
                                         </h4>
                                     </Collapsible>
+                                    </Skeleton>
                                 </div>
                             </div>
                         </Animated>
@@ -58,6 +68,7 @@ export const TestimonialsView = (props: TestimonialsViewProps): React.ReactEleme
                     <div className={`bulma-column ${props.isMobile ? "mt-6" : ""}`}>
                         <Animated dataAos="fade-up" dataAosDelay={150}>
                             <div className="bulma-card">
+                                <Skeleton isLoading={props.isLoading} className="testimonials-card-image">
                                 <CustomImage
                                     base={GET_TESTIMONIALS_URL}
                                     source={props.photo2}
@@ -65,16 +76,23 @@ export const TestimonialsView = (props: TestimonialsViewProps): React.ReactEleme
                                     title="Testimonials"
                                     alt={`Picture of ${props.name2}`}
                                 />
+                                </Skeleton>
                                 <div className="bulma-card-content mt-6">
+                                    <Skeleton isLoading={props.isLoading} mode="Text" height={24} className="mt-6 p-4">
                                     <p className="is-size-4 has-text-centered has-text-weight-semibold mt-6 p-4">
                                         {props.name2}
                                     </p>
+                                    </Skeleton>
+                                    <Skeleton isLoading={props.isLoading} mode="Text" height={24}>
                                     <p className="is-size-6 has-text-centered has-text-link p-2">{props.occupation2}</p>
+                                    </Skeleton>
+                                    <Skeleton isLoading={props.isLoading} height={100}>
                                     <Collapsible minHeight={120}>
                                         <h4 className="is-size-6 has-text-centered has-text-grey p-2 line-height-18">
                                             {props.text2}
                                         </h4>
                                     </Collapsible>
+                                    </Skeleton>
                                 </div>
                             </div>
                         </Animated>
@@ -82,23 +100,31 @@ export const TestimonialsView = (props: TestimonialsViewProps): React.ReactEleme
                     <div className={`bulma-column ${props.isMobile ? "mt-6" : ""}`}>
                         <Animated dataAos="fade-up" dataAosDelay={250}>
                             <div className="bulma-card">
+                                <Skeleton isLoading={props.isLoading} className="testimonials-card-image">
                                 <CustomImage
                                     base={GET_TESTIMONIALS_URL}
-                                    source={props.photo3}
+                                    source={props.photo1}
                                     className="testimonials-card-image"
                                     title="Testimonials"
                                     alt={`Picture of ${props.name3}`}
                                 />
+                                </Skeleton>
                                 <div className="bulma-card-content mt-6">
+                                    <Skeleton isLoading={props.isLoading} mode="Text" height={24} className="mt-6 p-4">
                                     <p className="is-size-4 has-text-centered has-text-weight-semibold mt-6 p-4">
                                         {props.name3}
                                     </p>
+                                    </Skeleton>
+                                    <Skeleton isLoading={props.isLoading} mode="Text" height={24}>
                                     <p className="is-size-6 has-text-centered has-text-link p-2">{props.occupation3}</p>
+                                    </Skeleton>
+                                    <Skeleton isLoading={props.isLoading} height={100}>
                                     <Collapsible minHeight={120}>
                                         <h4 className="is-size-6 has-text-centered has-text-grey p-2 line-height-18">
                                             {props.text3}
                                         </h4>
                                     </Collapsible>
+                                    </Skeleton>
                                 </div>
                             </div>
                         </Animated>
