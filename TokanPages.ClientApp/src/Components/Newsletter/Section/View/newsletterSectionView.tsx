5f5f9ed9139c55e01abb3b1f83914ac8207bd40d
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2025-07-19 22:13:05 +0200

    feat: use skeleton component

--- TokanPages.ClientApp/src/Components/Newsletter/Section/View/newsletterSectionView.tsx
+++ TokanPages.ClientApp/src/Components/Newsletter/Section/View/newsletterSectionView.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import { ViewProperties } from "../../../../Shared/Abstractions";
 import { ReactChangeEvent, ReactKeyboardEvent } from "../../../../Shared/types";
-import { Animated, ProgressBar, TextField } from "../../../../Shared/Components";
+import { Animated, ProgressBar, Skeleton, TextField } from "../../../../Shared/Components";
 
 interface NewsletterViewProps extends ViewProperties {
     isMobile: boolean;
@@ -30,8 +30,7 @@ const ActiveButton = (props: NewsletterViewProps): React.ReactElement => {
     );
 };
 
-export const NewsletterSectionView = (props: NewsletterViewProps): React.ReactElement => {
-    return (
+export const NewsletterSectionView = (props: NewsletterViewProps): React.ReactElement => (
         <section className={props.background}>
             <div className="bulma-container">
                 <div className="py-6">
@@ -39,14 +38,19 @@ export const NewsletterSectionView = (props: NewsletterViewProps): React.ReactEl
                         <div className="bulma-columns bulma-is-vcentered">
                             <div className="bulma-column">
                                 <Animated dataAos="fade-down" dataAosDelay={150}>
+                                    <Skeleton isLoading={props.isLoading} mode="Text" height={32}>
                                     <p className="is-size-3 has-text-grey-dark has-text-centered">{props.caption}</p>
+                                    </Skeleton>
                                 </Animated>
                                 <Animated dataAos="zoom-in" dataAosDelay={200}>
+                                    <Skeleton isLoading={props.isLoading} mode="Text" height={24}>
                                     <p className="is-size-5 has-text-grey has-text-centered">{props.text}</p>
+                                    </Skeleton>
                                 </Animated>
                             </div>
                             <div className="bulma-column">
                                 <Animated dataAos="zoom-in" dataAosDelay={300}>
+                                    <Skeleton isLoading={props.isLoading} mode="Rect" height={40}>
                                     <TextField
                                         required
                                         uuid="email"
@@ -56,11 +60,14 @@ export const NewsletterSectionView = (props: NewsletterViewProps): React.ReactEl
                                         value={props.email}
                                         placeholder={props.labelEmail}
                                     />
+                                    </Skeleton>
                                 </Animated>
                             </div>
                             <div className="bulma-column">
                                 <Animated dataAos="zoom-in" dataAosDelay={350}>
+                                    <Skeleton isLoading={props.isLoading} mode="Rect" height={40}>
                                     <ActiveButton {...props} />
+                                    </Skeleton>
                                 </Animated>
                             </div>
                         </div>
@@ -69,4 +76,3 @@ export const NewsletterSectionView = (props: NewsletterViewProps): React.ReactEl
             </div>
         </section>
     );
-};
