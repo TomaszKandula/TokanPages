1d44f2588e12de51befb5980647798144de6eb0f
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2025-07-19 21:11:57 +0200

    feat: allow to pass class name

--- TokanPages.ClientApp/src/Shared/Components/Skeleton/skeleton.tsx
+++ TokanPages.ClientApp/src/Shared/Components/Skeleton/skeleton.tsx
@@ -9,22 +9,24 @@ interface SkeletonProps {
     width?: number;
     height?: number;
     disableMarginY?: boolean;
+    className?: string;
     children: React.ReactNode | React.ReactElement | React.ReactElement[];
 }
 
 export const Skeleton = (props: SkeletonProps): React.ReactElement => {
     const marginClass = props.disableMarginY ? "" : "my-2";
     const baseClass = "base skeleton";
+    const className = props.className ?? "";
     const styleProps: React.CSSProperties = { 
         width: props.width, height: props.height 
     };
 
     if (props.isLoading) {
         switch (props.mode) {
-            case "Circle": return <div className={`${baseClass}-circle ${marginClass}`} style={styleProps}></div>;
-            case "Rect": return <div className={`${baseClass}-rect ${marginClass}`} style={styleProps}></div>;
-            case "Text": return <div className={`${baseClass}-text ${marginClass}`} style={styleProps}></div>;
-            default: return <div className={`${baseClass} ${marginClass}`} style={styleProps}></div>;
+            case "Circle": return <div className={`${baseClass}-circle ${marginClass} ${className}`} style={styleProps}></div>;
+            case "Rect": return <div className={`${baseClass}-rect ${marginClass} ${className}`} style={styleProps}></div>;
+            case "Text": return <div className={`${baseClass}-text ${marginClass} ${className}`} style={styleProps}></div>;
+            default: return <div className={`${baseClass} ${marginClass} ${className}`} style={styleProps}></div>;
         }
     }
 
