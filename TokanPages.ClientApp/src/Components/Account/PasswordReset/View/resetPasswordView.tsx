771a41c9beed0c2b929ab51f9a8dddbdd95ef3c0
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2025-07-19 18:18:47 +0200

    feat: use skeleton component

--- TokanPages.ClientApp/src/Components/Account/PasswordReset/View/resetPasswordView.tsx
+++ TokanPages.ClientApp/src/Components/Account/PasswordReset/View/resetPasswordView.tsx
@@ -35,12 +35,16 @@ export const PasswordResetView = (props: Properties): React.ReactElement => {
                 <div className={!props.className ? "py-6" : props.className}>
                     <div className="bulma-card">
                         <div className="bulma-card-content">
-                            <Skeleton isLoading={props.isLoading}>
                                 <div className="is-flex is-flex-direction-column is-align-items-center">
+                                    <Skeleton isLoading={props.isLoading} mode="Circle" width={72} height={72}>
                                     <Icon name="AccountCircle" size={3} className="has-text-link" />
+                                    </Skeleton>
+                                    <Skeleton isLoading={props.isLoading} mode="Text" height={24}>
                                     <p className="is-size-3 has-text-grey">{props.caption}</p>
+                                    </Skeleton>
                                 </div>
                                 <div className="my-5">
+                                    <Skeleton isLoading={props.isLoading} mode="Rect">
                                     <TextField
                                         required
                                         uuid="email"
@@ -50,11 +54,13 @@ export const PasswordResetView = (props: Properties): React.ReactElement => {
                                         value={props.email}
                                         placeholder={props.labelEmail}
                                     />
+                                    </Skeleton>
                                 </div>
                                 <div className="my-5">
+                                    <Skeleton isLoading={props.isLoading} mode="Rect">
                                     <ActiveButton {...props} />
+                                    </Skeleton>
                                 </div>
-                            </Skeleton>
                         </div>
                     </div>
                 </div>
