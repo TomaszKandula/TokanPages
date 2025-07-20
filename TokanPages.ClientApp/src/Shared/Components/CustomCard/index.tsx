dc1c21221609395c2763a079b8436b8e160a7a77
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2025-07-20 09:39:31 +0200

    refactor: apply prettier

--- TokanPages.ClientApp/src/Shared/Components/CustomCard/index.tsx
+++ TokanPages.ClientApp/src/Shared/Components/CustomCard/index.tsx
@@ -48,35 +48,35 @@ const RenderExternalButton = (props: CustomCardProps): React.ReactElement => (
     </div>
 );
 
-export const CustomCard = (props: CustomCardProps): React.ReactElement => { 
+export const CustomCard = (props: CustomCardProps): React.ReactElement => {
     const media = useDimensions();
 
     return (
-    <div className={`bulma-card ${media.isMobile ? "mx-4" : ""}`}>
-        <div className="bulma-card-content">
-            <div className="custom-card-icon-background is-flex is-justify-content-center is-align-items-center my-6">
-                <div className="custom-card-icon-holder is-flex is-justify-content-center is-align-items-center">
-                    <Skeleton isLoading={props.isLoading} mode="Circle" width={72} height={72}>
-                        <RenderIcon {...props} />
-                    </Skeleton>
+        <div className={`bulma-card ${media.isMobile ? "mx-4" : ""}`}>
+            <div className="bulma-card-content">
+                <div className="custom-card-icon-background is-flex is-justify-content-center is-align-items-center my-6">
+                    <div className="custom-card-icon-holder is-flex is-justify-content-center is-align-items-center">
+                        <Skeleton isLoading={props.isLoading} mode="Circle" width={72} height={72}>
+                            <RenderIcon {...props} />
+                        </Skeleton>
+                    </div>
                 </div>
+                <Skeleton isLoading={props.isLoading} mode="Text" height={40}>
+                    <h2 className="bulma-title has-text-centered">{props.caption}</h2>
+                </Skeleton>
+                <div className="my-5">
+                    {props.text.map((value: string, _index: number) => (
+                        <Skeleton isLoading={props.isLoading} mode="Text" height={14}>
+                            <p className="is-size-6 has-text-centered line-height-20" key={uuidv4()}>
+                                {value}
+                            </p>
+                        </Skeleton>
+                    ))}
+                </div>
+                {props.linkButton ? <RenderLinkButton {...props} /> : null}
+                {props.externalButton ? <RenderExternalButton {...props} /> : null}
+                <div className={props.linkButton || props.externalButton ? "mb-2" : "mb-4"}></div>
             </div>
-            <Skeleton isLoading={props.isLoading} mode="Text" height={40}>
-                <h2 className="bulma-title has-text-centered">{props.caption}</h2>
-            </Skeleton>
-            <div className="my-5">
-                {props.text.map((value: string, _index: number) => (
-                    <Skeleton isLoading={props.isLoading} mode="Text" height={14}>
-                        <p className="is-size-6 has-text-centered line-height-20" key={uuidv4()}>
-                            {value}
-                        </p>
-                    </Skeleton>
-                ))}
-            </div>
-            {props.linkButton ? <RenderLinkButton {...props} /> : null}
-            {props.externalButton ? <RenderExternalButton {...props} /> : null}
-            <div className={props.linkButton || props.externalButton ? "mb-2" : "mb-4"}></div>
         </div>
-    </div>
-);
+    );
 };
