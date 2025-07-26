4040a7533853c086a175b44b4d81a9a6e103a518
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2025-07-26 13:07:20 +0200

    feat: change notification colour

--- TokanPages.ClientApp/src/Shared/Components/Notification/notification.tsx
+++ TokanPages.ClientApp/src/Shared/Components/Notification/notification.tsx
@@ -3,7 +3,7 @@ import { NotificationProps } from "./types";
 import { Icon } from "../Icon";
 
 export const Notification = (props: NotificationProps): React.ReactElement => (
-    <div className="bulma-notification bulma-is-info bulma-is-light is-flex is-align-items-center">
+    <div className="bulma-notification is-flex is-align-items-center">
         <Icon name="Information" size={1} className="has-text-link" />
         <span className="is-size-6 ml-4">{props.text}</span>
     </div>
