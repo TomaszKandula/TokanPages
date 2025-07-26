74fcf0b4ca5a7004af8a3ba90f5834ef4583a6d9
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2025-07-26 09:37:53 +0200

    feat: remove unused prop

--- TokanPages.ClientApp/src/Components/Account/PasswordUpdate/View/passwordUpdateView.tsx
+++ TokanPages.ClientApp/src/Components/Account/PasswordUpdate/View/passwordUpdateView.tsx
@@ -31,9 +31,9 @@ const ActiveButton = (props: Properties): React.ReactElement => (
 );
 
 export const PasswordUpdateView = (props: Properties): React.ReactElement => (
-    <section className={props.background}>
+    <section className={props.className}>
         <div className="bulma-container bulma-is-max-tablet">
-            <div className={!props.className ? "py-6" : props.className}>
+            <div className="py-6">
                 <div className={`bulma-card ${props.isMobile ? "mx-4" : ""}`}>
                     <div className="bulma-card-content">
                         <div className="is-flex is-flex-direction-column is-align-items-center">
