d3b0fc49936a71160890b3a824106d99160df590
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2025-07-26 13:07:48 +0200

    feat: change button colour

--- TokanPages.ClientApp/src/Components/Newsletter/Section/View/newsletterSectionView.tsx
+++ TokanPages.ClientApp/src/Components/Newsletter/Section/View/newsletterSectionView.tsx
@@ -21,7 +21,7 @@ const ActiveButton = (props: NewsletterViewProps): React.ReactElement => (
     <button
         type="submit"
         onClick={props.buttonHandler}
-        className="bulma-button bulma-is-link bulma-is-light bulma-is-fullwidth"
+        className="bulma-button bulma-is-fullwidth"
         disabled={props.progress}
     >
         {!props.progress ? props.buttonText : <ProgressBar size={20} />}
