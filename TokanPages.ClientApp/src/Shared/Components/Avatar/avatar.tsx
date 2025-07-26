23277da054e0d378424bb6c6dda4d663d6d04d06
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2025-07-26 23:34:45 +0200

    fix: resolve found issues w/avatar

--- TokanPages.ClientApp/src/Shared/Components/Avatar/avatar.tsx
+++ TokanPages.ClientApp/src/Shared/Components/Avatar/avatar.tsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-import "./avatar.css";
 
 interface AvatarProps {
     alt: string;
@@ -13,12 +12,12 @@ export const Avatar = (props: AvatarProps): React.ReactElement => {
     const hasSrc = props.src && props.src !== "";
 
     if (!hasSrc && props.children) {
-        return <div className={`avatar-wrapper ${props.className}`}>{props.children}</div>;
+        return <div className={props.className}>{props.children}</div>;
     }
 
     return (
-        <div className={`avatar-wrapper ${props.className}`}>
-            <img alt={props.alt} title={props.title} src={props.src} className="avatar-image" />
-        </div>
+        <figure className={`bulma-image ${props.className ?? ""}`}>
+            <img alt={props.alt} title={props.title} src={props.src} className="bulma-is-rounded" />
+        </figure>
     );
 };
