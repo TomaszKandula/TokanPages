ee732bf2d0e10d6c70d45a52d2d3201d04808842
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2025-07-27 21:36:56 +0200

    fix: add language ID to the commands

--- TokanPages.Backend/TokanPages.Backend.Application/Users/Commands/VerifyUserEmailCommand.cs
+++ TokanPages.Backend/TokanPages.Backend.Application/Users/Commands/VerifyUserEmailCommand.cs
@@ -4,5 +4,7 @@ namespace TokanPages.Backend.Application.Users.Commands;
 
 public class VerifyUserEmailCommand : IRequest<Unit>
 {
+    public string LanguageId { get; set; } = "";
+
     public string EmailAddress { get; set; } = "";
 }
