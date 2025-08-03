84a8fabb5a79bd70c13a5ce1fbcfedc5b4e1e4ad
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2025-08-03 22:00:18 +0200

    feat: update language list

--- TokanPages.ClientApp/src/Components/Layout/Navigation/Components/RenderToolbarSmallScreen/renderToolbarSmallScreen.tsx
+++ TokanPages.ClientApp/src/Components/Layout/Navigation/Components/RenderToolbarSmallScreen/renderToolbarSmallScreen.tsx
@@ -1,15 +1,19 @@
 import * as React from "react";
 import { Link as RouterLink } from "react-router-dom";
-import { NavigationViewProps } from "../../Abstractions";
+import { ListSeparatorProps, NavigationViewProps } from "../../Abstractions";
 import { RenderMenuIcon } from "../RenderMenuIcon";
 import { GET_FLAG_URL, GET_ICONS_URL } from "../../../../../Api";
 import { LanguageItemDto } from "../../../../../Api/Models";
 import { CustomImage, Icon, Link, Media } from "../../../../../Shared/Components";
 import { APP_BAR_HEIGHT_DESKTOP, APP_BAR_HEIGHT_NON_DESKTOP_TOP } from "../../../../../Shared/constants";
 import { RenderBottomSheet } from "../RenderBottomSheet";
-import { RenderLanguageIcon } from "..";
+import { RenderSelectionIcon } from "..";
 import "./renderToolbarSmallScreen.css";
 import { v4 as uuidv4 } from "uuid";
+
+const ListSeparator = (props: ListSeparatorProps): React.ReactElement | null => {
+    return props.length === props.index + 1 ? null : <hr className="mx-0 my-1" />;
+};
 
 const RenderDoubleToolbar = (props: NavigationViewProps) => {
     return (
@@ -63,23 +67,27 @@ const RenderDoubleToolbar = (props: NavigationViewProps) => {
 
 const RenderLanguages = (props: NavigationViewProps): React.ReactElement => (
     <>
-        {props.languages?.languages.map((item: LanguageItemDto, _index: number) => (
-            <a
-                className="bulma-navbar-item is-flex is-align-items-center"
-                key={uuidv4()}
-                onClick={() => props.languagePickHandler(item.id)}
-            >
-                <CustomImage
-                    base={GET_FLAG_URL}
-                    source={`${item.id}.png`}
-                    title="Language flag"
-                    alt={`A flag (${item.name}) symbolizing available language`}
-                    className="bulma-image bulma-is-16x16 mr-4"
-                />
-                <div>{item.name}</div>
-                <RenderLanguageIcon selection={item.id} languageId={props.languageId} />
-            </a>
-        ))}
+        <div className="navbar-top-line"></div>
+        <div className="is-flex is-flex-direction-column m-4">
+            {props.languages?.languages.map((item: LanguageItemDto, index: number) => (
+                <React.Fragment key={uuidv4()}>
+                    <a className="is-flex is-align-items-center" onClick={() => props.languagePickHandler(item.id)}>
+                        <CustomImage
+                            base={GET_FLAG_URL}
+                            source={`${item.id}.png`}
+                            title="Language flag"
+                            alt={`A flag (${item.name}) symbolizing available language`}
+                            className="bulma-image bulma-is-16x16 m-2"
+                        />
+                        <div className="has-text-black m-2">{item.name}</div>
+                        <div className="ml-4">
+                            <RenderSelectionIcon selection={item.id} languageId={props.languageId} />
+                        </div>
+                    </a>
+                    <ListSeparator length={props.languages?.languages.length} index={index} />
+                </React.Fragment>
+            ))}
+        </div>
     </>
 );
 
@@ -87,11 +95,11 @@ export const RenderToolbarSmallScreen = (props: NavigationViewProps): React.Reac
     <>
         <Media.TabletOnly>
             <RenderDoubleToolbar {...props} />
-            <RenderBottomSheet {...props} bottomSheetHeight={250} content={<RenderLanguages {...props} />} />
+            <RenderBottomSheet {...props} bottomSheetHeight={350} content={<RenderLanguages {...props} />} />
         </Media.TabletOnly>
         <Media.MobileOnly>
             <RenderDoubleToolbar {...props} />
-            <RenderBottomSheet {...props} bottomSheetHeight={250} content={<RenderLanguages {...props} />} />
+            <RenderBottomSheet {...props} bottomSheetHeight={350} content={<RenderLanguages {...props} />} />
         </Media.MobileOnly>
     </>
 );
