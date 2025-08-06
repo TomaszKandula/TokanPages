d4f24a35ecc99b7beaeaa8202d4d8c7a61b6cede
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2025-08-06 17:14:01 +0200

    refactor: apply prettier

--- TokanPages.ClientApp/src/Shared/Components/RenderHtml/types.ts
+++ TokanPages.ClientApp/src/Shared/Components/RenderHtml/types.ts
@@ -1,4 +1,4 @@
-export type TagType = "p" | "span" | "div" | "h1"| "h2" | "h3" | "h4" | "h5" | "h6" | "blockquote" | "li";
+export type TagType = "p" | "span" | "div" | "h1" | "h2" | "h3" | "h4" | "h5" | "h6" | "blockquote" | "li";
 
 export interface ProcessTextProps {
     value: string;
