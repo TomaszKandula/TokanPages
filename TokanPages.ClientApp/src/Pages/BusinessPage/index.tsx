64bef6ef87a27b0a5b3c29a2f729bccbdf929409
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2025-08-18 23:08:09 +0200

    feat: display full navbar

--- TokanPages.ClientApp/src/Pages/BusinessPage/index.tsx
+++ TokanPages.ClientApp/src/Pages/BusinessPage/index.tsx
@@ -13,7 +13,7 @@ export const BusinessPage = () => {
 
     return (
         <>
-            <Navigation backNavigationOnly />
+            <Navigation />
             <main>
                 <BusinessForm hasIcon hasShadow />
             </main>
