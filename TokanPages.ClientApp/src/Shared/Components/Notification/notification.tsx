aa59c48590bd53af38379603dae07420a6ce2e84
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2025-08-24 10:23:42 +0200

    feat: use html renderer to handle links

--- TokanPages.ClientApp/src/Shared/Components/Notification/notification.tsx
+++ TokanPages.ClientApp/src/Shared/Components/Notification/notification.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { NotificationProps } from "./types";
 import { Icon } from "../Icon";
+import { RenderHtml } from "../RenderHtml";
 
 export const Notification = (props: NotificationProps): React.ReactElement => (
     <div className="bulma-notification is-flex is-align-items-center">
         <Icon name="Information" size={1.5} className="has-text-link" />
-        <span className="is-size-6 ml-4">{props.text}</span>
+        <RenderHtml value={props.text} tag="span" className="is-size-6 ml-4" />
     </div>
 );
