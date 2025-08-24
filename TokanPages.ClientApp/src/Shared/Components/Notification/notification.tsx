36d1cbd280765d04b09c7cda8400ae0e89413a30
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2025-08-24 10:38:43 +0200

    feat: relocate margin container

--- TokanPages.ClientApp/src/Shared/Components/Notification/notification.tsx
+++ TokanPages.ClientApp/src/Shared/Components/Notification/notification.tsx
@@ -6,6 +6,8 @@ import { ProcessParagraphs } from "../RenderContent/Renderers";
 export const Notification = (props: NotificationProps): React.ReactElement => (
     <div className="bulma-notification is-flex is-align-items-center">
         <Icon name="Information" size={1.5} className="has-text-link" />
-        <ProcessParagraphs tag="span" html={props.text} className="is-size-6 ml-4" />
+        <div className="ml-4">
+            <ProcessParagraphs tag="span" html={props.text} className="is-size-6" />
+        </div>
     </div>
 );
