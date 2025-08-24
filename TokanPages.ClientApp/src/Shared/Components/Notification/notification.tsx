d1a05abce4af9c4a2e1926bd0c335c81d3232f04
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2025-08-24 10:34:13 +0200

    feat: use text processor

--- TokanPages.ClientApp/src/Shared/Components/Notification/notification.tsx
+++ TokanPages.ClientApp/src/Shared/Components/Notification/notification.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { NotificationProps } from "./types";
 import { Icon } from "../Icon";
-import { RenderHtml } from "../RenderHtml";
+import { ProcessParagraphs } from "../RenderContent/Renderers";
 
 export const Notification = (props: NotificationProps): React.ReactElement => (
     <div className="bulma-notification is-flex is-align-items-center">
         <Icon name="Information" size={1.5} className="has-text-link" />
-        <RenderHtml value={props.text} tag="span" className="is-size-6 ml-4" />
+        <ProcessParagraphs tag="span" html={props.text} className="is-size-6 ml-4" />
     </div>
 );
