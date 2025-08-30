fd1a201f532f14542c0553ce6d7db5d1f89c3ee4
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2025-08-30 16:16:16 +0200

    test: update tests

--- TokanPages.ClientApp/src/Components/Articles/ArticleList/View/articleListView.test.tsx
+++ TokanPages.ClientApp/src/Components/Articles/ArticleList/View/articleListView.test.tsx
@@ -44,7 +44,7 @@ describe("test articles group component: ArticleListView", () => {
 
         const html = render(
             <Router>
-                <ArticleListView isLoading={false} isMobile={false} articles={[articles]} title="" content={[""]} />
+                <ArticleListView isLoading={false} isMobile={false} articles={[articles]} title="" content={[""]} placeholder="" />
             </Router>
         );
 
