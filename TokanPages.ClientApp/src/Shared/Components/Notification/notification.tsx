51529ed8ca3b157d4ce185beea883618c53fd107
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2025-08-30 15:12:21 +0200

    refactor: apply prettier

--- TokanPages.ClientApp/src/Shared/Components/Notification/notification.tsx
+++ TokanPages.ClientApp/src/Shared/Components/Notification/notification.tsx
@@ -13,46 +13,48 @@ const RenderIcon = (props: NotificationProps): React.ReactElement => {
         case "link":
         case "primary":
         case "info":
-            iconName = "Information"
+            iconName = "Information";
             break;
         case "success":
-            iconName = "CheckCircle"
+            iconName = "CheckCircle";
             break;
         case "warning":
-            iconName = "Alert"
+            iconName = "Alert";
             break;
         case "danger":
-            iconName = "AlertCircle"
+            iconName = "AlertCircle";
             break;
     }
 
     let style = "has-text-link";
     switch (props.mode) {
         case "link":
-            style = `has-text-link${props.isLight ? "" : "-light"}`
+            style = `has-text-link${props.isLight ? "" : "-light"}`;
             break;
         case "primary":
-            style = `has-text-primary${props.isLight ? "" : "-light"}`
+            style = `has-text-primary${props.isLight ? "" : "-light"}`;
             break;
         case "info":
-            style = `has-text-info${props.isLight ? "" : "-light"}`
+            style = `has-text-info${props.isLight ? "" : "-light"}`;
             break;
         case "success":
-            style = `has-text-success${props.isLight ? "" : "-light"}`
+            style = `has-text-success${props.isLight ? "" : "-light"}`;
             break;
         case "warning":
-            style = `has-text-warning${props.isLight ? "" : "-light"}`
+            style = `has-text-warning${props.isLight ? "" : "-light"}`;
             break;
         case "danger":
-            style = `has-text-danger${props.isLight ? "" : "-light"}`
+            style = `has-text-danger${props.isLight ? "" : "-light"}`;
             break;
     }
 
     return <Icon name={iconName} size={1.5} className={style} />;
-}
+};
 
 export const Notification = (props: NotificationProps): React.ReactElement => (
-    <div className={`bulma-notification ${props.mode ?? ""} ${props.isLight ? "bulma-is-light" : ""} is-flex is-align-items-center ${props.className ?? ""}`}>
+    <div
+        className={`bulma-notification ${props.mode ?? ""} ${props.isLight ? "bulma-is-light" : ""} is-flex is-align-items-center ${props.className ?? ""}`}
+    >
         {props.onClose ? <button className="bulma-delete"></button> : null}
         <RenderIcon {...props} />
         <div className={props.hasIcon ? "ml-4" : ""}>
