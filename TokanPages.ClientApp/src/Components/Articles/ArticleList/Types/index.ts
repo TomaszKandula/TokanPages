68de8cf3c0ca587975f1f12f31a2d8b8a17ee303
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2025-08-31 14:56:45 +0200

    feat: update types

--- TokanPages.ClientApp/src/Components/Articles/ArticleList/Types/index.ts
+++ TokanPages.ClientApp/src/Components/Articles/ArticleList/Types/index.ts
@@ -1,29 +1,48 @@
+import { ReactChangeEvent, ReactKeyboardEvent } from "Shared/types";
 import { ViewProperties } from "../../../../Shared/Abstractions";
 import { ArticleItem } from "../../../../Shared/Components/RenderContent/Models";
+
+export interface SearchInputProps { 
+    searchInput: string 
+}
+
+export interface ArticlesProps {
+    articles: ArticleItem[];
+}
+
+export interface ActionButtonProps {
+    label: string;
+    onClick: () => void;
+}
+
+export interface ButtonsProps {
+    buttonSearch: ActionButtonProps;
+    buttonClear: ActionButtonProps;
+}
+
+export interface TextProps {
+    title: string;
+    text: string[];
+    placeholder: string;
+}
 
 export interface ArticleListProps {
     className?: string;
 }
 
-export interface ArticleListViewProps extends ViewProperties, ArticleListProps {
+export interface ArticleListViewProps extends ViewProperties, ArticlesProps, ArticleListProps, TextProps, ButtonsProps {
     isMobile: boolean;
-    articles: ArticleItem[];
-    title: string;
-    placeholder: string;
-    buttonSearch: string;
-    buttonClear: string;
-    content: string[];
+    onKeyUp?: (event: ReactKeyboardEvent) => void;
+    onChange?: (event: ReactChangeEvent) => void;
+    value: SearchInputProps;   
 }
 
-export interface RenderContentProps {
-    articles: ArticleItem[];
+export interface RenderContentProps extends ArticlesProps {
 }
 
-export interface RenderStaticTextProps {
+export interface RenderStaticTextProps extends TextProps, ButtonsProps {
     isLoading: boolean;
-    title: string;
-    text: string[];
-    placeholder: string;
-    buttonSearch: string;
-    buttonClear: string;
+    onKeyUp?: (event: ReactKeyboardEvent) => void;
+    onChange?: (event: ReactChangeEvent) => void;
+    value: SearchInputProps;   
 }
