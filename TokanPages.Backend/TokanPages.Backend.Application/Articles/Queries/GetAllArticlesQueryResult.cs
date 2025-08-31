d38154a994ceea461573ee0ac8eb4ce8c51777c2
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2025-08-31 09:22:51 +0200

    feat: add new model

---
+++ TokanPages.Backend/TokanPages.Backend.Application/Articles/Queries/GetAllArticlesQueryResult.cs
@@ -0,0 +1,5 @@
+using TokanPages.Backend.Core.Paging;
+
+namespace TokanPages.Backend.Application.Articles.Queries;
+
+public class GetAllArticlesQueryResult : PagingResults<GetArticlesQueryResult> { }
