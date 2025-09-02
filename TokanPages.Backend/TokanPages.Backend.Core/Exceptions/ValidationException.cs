2d669901eeffab5ccf37a06c15d5e117f84792fe
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2025-09-02 19:12:09 +0200

    fix: remove obsolete constructors and unused serialization

--- TokanPages.Backend/TokanPages.Backend.Core/Exceptions/ValidationException.cs
+++ TokanPages.Backend/TokanPages.Backend.Core/Exceptions/ValidationException.cs
@@ -1,17 +1,12 @@
 ﻿using System.Diagnostics.CodeAnalysis;
-using System.Runtime.Serialization;
 using FluentValidation.Results;
 
 namespace TokanPages.Backend.Core.Exceptions;
 
 [ExcludeFromCodeCoverage]
-[Serializable]
 public class ValidationException : BusinessException
 {
     public ValidationResult ValidationResult { get; } = new();
-
-    protected ValidationException(SerializationInfo serializationInfo, 
-        StreamingContext streamingContext) : base(serializationInfo, streamingContext) { }
 
     public ValidationException(ValidationResult validationResult, string errorMessage = "") : base(errorMessage)
         => ValidationResult = validationResult;
