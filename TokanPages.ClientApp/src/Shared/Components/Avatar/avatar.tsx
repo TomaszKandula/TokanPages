f5d2e9fb34b715345ab95abe2b73fdeb3824d800
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2025-09-04 22:06:20 +0200

    feat: introduce new sizes to avatar

--- TokanPages.ClientApp/src/Shared/Components/Avatar/avatar.tsx
+++ TokanPages.ClientApp/src/Shared/Components/Avatar/avatar.tsx
@@ -1,33 +1,48 @@
 import * as React from "react";
+import { FigoureSize } from "../../../Shared/enums";
 import "./avatar.css";
 
 interface AvatarProps {
     alt: string;
     title: string;
+    size: FigoureSize;
     src?: string;
     className?: string;
     children?: React.ReactElement | string;
+}
+
+const ConvertSize = (size: FigoureSize, prefix: string = "bulma"): string => {
+    switch (size) {
+        case FigoureSize.xs:
+            return `${prefix}-is-16x16`;
+        case FigoureSize.s:
+            return `${prefix}-is-24x24`;
+        case FigoureSize.m:
+            return `${prefix}-is-32x32`;
+        case FigoureSize.l:
+            return `${prefix}-is-48x48`;
+        case FigoureSize.xl:
+            return `${prefix}-is-96x96`;
+        case FigoureSize.xxl:
+            return `${prefix}-is-128x128`;
+    }
 }
 
 export const Avatar = (props: AvatarProps): React.ReactElement => {
     const hasSrc = props.src && props.src !== "";
 
     if (!hasSrc && props.children) {
-        const baseClass =
-            "has-background-grey-light has-text-white is-flex is-justify-content-center is-align-items-center";
-        let className;
-        if (props.className?.includes("96x96")) {
-            className = `${baseClass} default-avatar-large`;
-        } else {
-            className = `${baseClass} default-avatar-small`;
-        }
-
+        const baseFlex = "is-flex is-justify-content-center is-align-items-center";
+        const baseClass = `has-background-grey-light has-text-white ${baseFlex}`;
+        const className = `${baseClass} ${ConvertSize(props.size)} ${props.className ?? ""}`;
         return <div className={className}>{props.children}</div>;
     }
 
+    const className = `${ConvertSize(props.size)} ${props.className ?? ""}`;
+
     return (
-        <figure className={`bulma-image ${props.className ?? ""}`}>
-            <img alt={props.alt} title={props.title} src={props.src} className="bulma-is-rounded" />
+        <figure className={`bulma-image ${className}`}>
+            <img alt={props.alt} title={props.title} src={props.src} className={`bulma-is-rounded ${ConvertSize(props.size, "avatar")}`} />
         </figure>
     );
 };
--- TokanPages.ClientApp/src/Shared/Components/UserAvatar/View/userAvatarView.tsx
+++ TokanPages.ClientApp/src/Shared/Components/UserAvatar/View/userAvatarView.tsx
@@ -1,26 +1,23 @@
 import * as React from "react";
 import { Avatar } from "../../../../Shared/Components";
+import { FigoureSize } from "../../../../Shared/enums";
 import Validate from "validate.js";
 
 interface Properties {
-    isLarge: boolean;
+    size: FigoureSize;
     userLetter: string;
     avatarSource: string;
     className?: string;
 }
 
 export const UserAvatarView = (props: Properties): React.ReactElement => {
-    const className = props.isLarge
-        ? `bulma-is-96x96 ${props.className ?? ""}`
-        : `bulma-is-48x48 ${props.className ?? ""}`;
-
     if (Validate.isEmpty(props.avatarSource)) {
         return (
-            <Avatar className={className} alt="User avatar" title="Avatar">
+            <Avatar className={props.className} alt="User avatar" title="Avatar" size={props.size}>
                 <h2 className="is-size-4 has-text-white">{props.userLetter}</h2>
             </Avatar>
         );
     }
 
-    return <Avatar className={className} src={props.avatarSource} alt="User avatar" title="Avatar"></Avatar>;
+    return <Avatar className={props.className} size={props.size} src={props.avatarSource} alt="User avatar" title="Avatar"></Avatar>;
 };
