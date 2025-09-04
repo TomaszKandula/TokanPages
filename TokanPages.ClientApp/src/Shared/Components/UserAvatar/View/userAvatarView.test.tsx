f0ab177c27d59ccca04cce423ec4a0a9fad86e29
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2025-09-04 22:06:29 +0200

    test: update tests

--- TokanPages.ClientApp/src/Shared/Components/UserAvatar/View/userAvatarView.test.tsx
+++ TokanPages.ClientApp/src/Shared/Components/UserAvatar/View/userAvatarView.test.tsx
@@ -1,17 +1,18 @@
 import "../../../../setupTests";
 import React from "react";
 import { render } from "@testing-library/react";
+import { FigoureSize } from "../../../../Shared/enums";
 import { UserAvatarView } from "./userAvatarView";
 
 describe("test user avatar component", () => {
     it("should correctly render large user avatar as user letter.", () => {
-        const html = render(<UserAvatarView isLarge={true} userLetter={"T"} avatarSource={""} />);
+        const html = render(<UserAvatarView size={FigoureSize.l} userLetter={"T"} avatarSource={""} />);
 
         expect(html).toMatchSnapshot();
     });
 
     it("should correctly render small user avatar as user letter.", () => {
-        const html = render(<UserAvatarView isLarge={false} userLetter={"T"} avatarSource={""} />);
+        const html = render(<UserAvatarView size={FigoureSize.s} userLetter={"T"} avatarSource={""} />);
 
         expect(html).toMatchSnapshot();
     });
@@ -19,7 +20,7 @@ describe("test user avatar component", () => {
     it("should correctly render large user avatar image.", () => {
         const html = render(
             <UserAvatarView
-                isLarge={true}
+                size={FigoureSize.l}
                 userLetter={"T"}
                 avatarSource={"http://localhost/api/v1/assets/avatars/example_avatar.jpg"}
             />
@@ -31,7 +32,7 @@ describe("test user avatar component", () => {
     it("should correctly render small user avatar image.", () => {
         const html = render(
             <UserAvatarView
-                isLarge={false}
+                size={FigoureSize.s}
                 userLetter={"T"}
                 avatarSource={"http://localhost/api/v1/assets/avatars/example_avatar.jpg"}
             />
