eb9fb95a8a1ffbe75cc3ad05bd4f7a9293bc3283
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2025-09-04 15:52:22 +0200

    feat: restyle navbar component for desktop

--- TokanPages.ClientApp/src/Components/Layout/Navigation/Components/RenderToolbarLargeScreen/renderToolbarLargeScreen.tsx
+++ TokanPages.ClientApp/src/Components/Layout/Navigation/Components/RenderToolbarLargeScreen/renderToolbarLargeScreen.tsx
@@ -2,35 +2,32 @@ import * as React from "react";
 import { Link } from "react-router-dom";
 import { NavigationViewProps } from "../../Abstractions";
 import { GET_ICONS_URL } from "../../../../../Api";
-import { CustomImage } from "../../../../../Shared/Components";
+import { CustomImage, Media } from "../../../../../Shared/Components";
 import { RenderLanguageSection } from "../RenderLanguageSection";
 import { RenderNavbarMenu } from "../RenderMenu";
 
-export const RenderToolbarLargeScreen = (props: NavigationViewProps): React.ReactElement =>
-    props.media.isDesktop ? (
-        <>
-            <div className="bulma-navbar-start is-flex is-align-self-center">
-                <Link to={`/${props.languageId}`} rel="noopener nofollow">
-                    <CustomImage
-                        base={GET_ICONS_URL}
-                        source={props.navigation?.logo}
-                        title="TomKandula logo"
-                        alt="An application logo"
-                        width={180}
-                        height={30}
-                        className="ml-4"
-                    />
-                </Link>
-            </div>
-            <div className="is-flex is-align-self-center">
-                <RenderNavbarMenu
-                    isAnonymous={props.isAnonymous}
-                    languageId={props.languageId}
-                    items={props.navigation?.menu?.items}
+export const RenderToolbarLargeScreen = (props: NavigationViewProps): React.ReactElement => (
+    <Media.DesktopOnly>
+        <div className="bulma-navbar-start" style={{ minWidth: 220 }}>
+            <Link to={`/${props.languageId}`} rel="noopener nofollow" className="is-flex is-align-self-center">
+                <CustomImage
+                    base={GET_ICONS_URL}
+                    source={props.navigation?.logo}
+                    title="TomKandula logo"
+                    alt="An application logo"
+                    width={180}
+                    height={30}
+                    className="ml-4"
                 />
-            </div>
-            <div className="bulma-navbar-end">{props.isLoading ? null : <RenderLanguageSection {...props} />}</div>
-        </>
-    ) : (
-        <></>
-    );
+            </Link>
+        </div>
+        <RenderNavbarMenu
+            isAnonymous={props.isAnonymous}
+            languageId={props.languageId}
+            items={props.navigation?.menu?.items}
+        />
+        <div className="bulma-navbar-end" style={{ minWidth: 100 }}>
+            {props.isLoading ? null : <RenderLanguageSection {...props} />}
+        </div>
+    </Media.DesktopOnly>
+);
