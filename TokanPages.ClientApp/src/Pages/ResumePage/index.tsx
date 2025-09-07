bc57de184e39b5498cfe66c8dce715ead08ed026
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2025-09-07 19:15:07 +0200

    feat: add breadcrumb

--- TokanPages.ClientApp/src/Pages/ResumePage/index.tsx
+++ TokanPages.ClientApp/src/Pages/ResumePage/index.tsx
@@ -1,18 +1,26 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { ApplicationState } from "../../Store/Configuration";
 import { usePageContent, useSnapshot, useUnhead } from "../../Shared/Hooks";
-import { Footer, Navigation } from "Components/Layout";
+import { CustomBreadcrumb } from "../../Shared/Components";
+import { Footer, Navigation } from "../../Components/Layout";
 
 export const ResumePage = (): React.ReactElement => {
     const heading = useUnhead("ResumePage");
     useSnapshot();
     usePageContent(["layoutNavigation", "layoutFooter", "templates", "sectionCookiesPrompt", "pageResume"], "ResumePage");
 
+    const state = useSelector((state: ApplicationState) => state);
+    const data = state.contentPageData;
+    const isLoading = data?.isLoading ?? false;
+
     return (
         <>
             <Navigation />
             <main>
                 <h1 className="seo-only">{heading}</h1>
-                <div>TEST</div>
+                <CustomBreadcrumb isLoading={isLoading} />
+                <div>EMPTY PAGE</div>
             </main>
             <Footer />
         </>
