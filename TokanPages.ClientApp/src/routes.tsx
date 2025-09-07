c711e7015dde30710e360906e3e070c296676023
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2025-09-07 19:12:25 +0200

    feat: register resume page

--- TokanPages.ClientApp/src/routes.tsx
+++ TokanPages.ClientApp/src/routes.tsx
@@ -29,6 +29,7 @@ import {
     PdfViewerPage,
     BusinessPage,
     UserNotesPage,
+    ResumePage,
 } from "./Pages";
 
 interface PageProps {
@@ -44,6 +45,7 @@ interface RoutesProps {
 
 const pages: PageProps[] = [
     { path: "/", page: <MainPage />, canPrerender: true },
+    { path: "/resume", page: <ResumePage />, canPrerender: true},
     { path: "/showcase", page: <ShowcasePage />, canPrerender: true },
     { path: "/articles", page: <ArticlesPage />, canPrerender: true },
     { path: "/business", page: <BusinessPage />, canPrerender: true },
