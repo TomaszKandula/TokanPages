aebccacc85e98e8d6320ea0cbf80742f86b917bf
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2025-09-23 19:54:27 +0200

    feat: re-write icon button component

--- TokanPages.ClientApp/src/Shared/Components/IconButton/iconButton.tsx
+++ TokanPages.ClientApp/src/Shared/Components/IconButton/iconButton.tsx
@@ -7,21 +7,55 @@ interface IconButtonProps {
     children: React.ReactElement | React.ReactElement[];
     hasNoHoverEffect?: boolean;
     className?: string;
+    isDisabled?: boolean;
+    style?: "grey" | "transparent";
     onClick?: (event: ReactMouseEvent) => void;
     onMouseDown?: (event: ReactMouseEvent) => void;
 }
 
 export const IconButton = (props: IconButtonProps): React.ReactElement => {
     const size = props.size ?? 48;
-    const baseClassName = props.hasNoHoverEffect ? "icon-button-no-hover" : "icon-button";
-    const className = `${baseClassName} ${props.className ?? ""}`;
+    const ref = React.useRef<HTMLButtonElement>(null);
+    const baseClass = "icon-button-base is-flex is-align-self-center";
+
+    React.useLayoutEffect(() => {
+        const classList = ref.current?.classList;
+        if (!classList) {
+            return;
+        }
+
+        if (props.className && props.className !== "") {
+            if (props.className.includes(" ")) {
+                const list = props.className.split(" ");
+                list.forEach(item => {
+                    classList.add(item);
+                });
+            }
+        }
+
+        if (props.hasNoHoverEffect) {
+            classList.add("icon-button-non-hoverable");
+            classList.remove("icon-button-hoverable");
+        } else {
+            classList.remove("icon-button-non-hoverable");
+            classList.add("icon-button-hoverable");
+        }
+
+        if (props.isDisabled) {
+            classList.add("icon-button-disabled");
+        } else {
+            classList.remove("icon-button-disabled");
+        }
+    }, [props.className, props.hasNoHoverEffect, props.isDisabled]);
 
     return (
         <button
-            className={`${className} is-flex is-align-self-center`}
+            ref={ref}
+            className={baseClass}
             onClick={props.onClick}
             onMouseDown={props.onMouseDown}
             style={{ height: size, width: size }}
+            disabled={props.isDisabled}
         >
             {props.children}
         </button>
