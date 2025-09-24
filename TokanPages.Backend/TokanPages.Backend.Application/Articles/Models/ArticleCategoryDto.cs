c0022a5ad781d71e7a39e527371dffc77dc2f736
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2025-09-24 16:42:06 +0200

    feat: exclude from code coverage

--- TokanPages.Backend/TokanPages.Backend.Application/Articles/Models/ArticleCategoryDto.cs
+++ TokanPages.Backend/TokanPages.Backend.Application/Articles/Models/ArticleCategoryDto.cs
@@ -1,8 +1,11 @@
+using System.Diagnostics.CodeAnalysis;
+
 namespace TokanPages.Backend.Application.Articles.Models;
 
 /// <summary>
 /// Gets list of article category IDs.
 /// </summary>
+[ExcludeFromCodeCoverage]
 public class ArticleCategoryDto
 {
     public Guid Id { get; set; }
