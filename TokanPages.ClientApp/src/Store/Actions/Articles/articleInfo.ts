2495d8dcd90fb37b2c8ec8dfe5ad9d1f915101e6
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2025-10-03 23:09:50 +0200

    feat: change redux action

--- TokanPages.ClientApp/src/Store/Actions/Articles/articleInfo.ts
+++ TokanPages.ClientApp/src/Store/Actions/Articles/articleInfo.ts
@@ -1,37 +1,45 @@
 import { ApplicationAction } from "../../Configuration";
-import { ArticleItemBase } from "../../../Shared/Components/RenderContent/Models";
-import { ExecuteStoreActionProps, GET_ARTICLE_INFO } from "../../../Api";
+import { ExecuteStoreActionProps, RETRIEVE_ARTICLE_INFO } from "../../../Api";
+import { RetrieveArticleInfoRequestDto, RetrieveArticleInfoResponseDto } from "../../../Api/Models";
 import { useApiAction } from "../../../Shared/Hooks";
 
 export const REQUEST = "REQUEST_ARTICLE_INFO";
 export const RECEIVE = "RECEIVE_ARTICLE_INFO";
+export const CLEAR = "CLEAR_ARTICLE_INFO";
 interface Request {
     type: typeof REQUEST;
 }
 interface Receive {
     type: typeof RECEIVE;
-    payload: ArticleItemBase;
+    payload: RetrieveArticleInfoResponseDto;
 }
-export type TKnownActions = Request | Receive;
+interface Clear {
+    type: typeof CLEAR;
+}
+export type TKnownActions = Request | Receive | Clear;
 
 export const ArticleInfoAction = {
     get:
-        (id: string): ApplicationAction<TKnownActions> =>
+        (request: RetrieveArticleInfoRequestDto): ApplicationAction<TKnownActions> =>
         (dispatch, getState) => {
             dispatch({ type: REQUEST });
 
             const actions = useApiAction();
             const input: ExecuteStoreActionProps = {
-                url: GET_ARTICLE_INFO.replace("{id}", id),
+                url: RETRIEVE_ARTICLE_INFO,
                 dispatch: dispatch,
                 state: getState,
                 responseType: RECEIVE,
                 configuration: {
-                    method: "GET",
+                    method: "POST",
+                    body: request,
                     hasJsonResponse: true,
                 },
             };
 
             actions.storeAction(input);
         },
+    clear: (): ApplicationAction<TKnownActions> => (dispatch, _getState) => {
+        dispatch({ type: CLEAR });
+    }
 };
