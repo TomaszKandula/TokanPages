7ade5d27f3be9314325f55a496dbb4b292cfda20
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2025-10-03 21:39:48 +0200

    feat: remove unused code

--- TokanPages.Backend/TokanPages.Backend.Application/Articles/Queries/GetArticleInfoQueryResult.cs
+++
@@ -1,8 +0,0 @@
-using TokanPages.Backend.Application.Articles.Models;
-
-namespace TokanPages.Backend.Application.Articles.Queries;
-
-public class GetArticleInfoQueryResult : ArticleDataDto
-{
-    public int UserLikes { get; set; }
-}
