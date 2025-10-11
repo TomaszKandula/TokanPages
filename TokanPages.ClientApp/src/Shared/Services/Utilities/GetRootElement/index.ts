cd0dea317459b17e3776e05b634d8b3c7c3c276d
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2025-10-11 23:21:07 +0200

    feat: add utility function to get root element

---
+++ TokanPages.ClientApp/src/Shared/Services/Utilities/GetRootElement/index.ts
@@ -0,0 +1,8 @@
+export const GetRootElement = (): HTMLElement => {
+    const root = document.getElementById("root");
+    if (root === null) {
+        throw new Error("Unexpected internal error!");
+    }
+
+    return root;
+}
