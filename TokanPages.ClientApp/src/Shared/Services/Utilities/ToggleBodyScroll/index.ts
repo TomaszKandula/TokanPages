7d7a2071bed0e1dc8a963cced5265bf084fddc36
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2025-10-12 00:53:01 +0200

    feat: change utility function for scroll bar locking

--- TokanPages.ClientApp/src/Shared/Services/Utilities/ToggleBodyScroll/index.ts
+++ TokanPages.ClientApp/src/Shared/Services/Utilities/ToggleBodyScroll/index.ts
@@ -1,7 +1,10 @@
 export const ToggleBodyScroll = (isBodyScrollEnabled: boolean): void => {
+    const html = document.getElementsByTagName("html")[0];
     if (isBodyScrollEnabled) {
         window.document.body.style.removeProperty("overflow");
+        html.classList.remove("is-lock-scroll");
     } else {
         window.document.body.style.overflow = "hidden";
+        html.classList.add("is-lock-scroll")
     }
 };
