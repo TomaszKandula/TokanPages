c3f65be3ba90a1bc5bbc634477334153b4bb56dc
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2025-10-14 19:19:17 +0200

    refactor: reorganize, remove unused code

--- TokanPages.ClientApp/src/Shared/Services/Converters/index.ts
+++ TokanPages.ClientApp/src/Shared/Services/Converters/index.ts
@@ -1,3 +1,2 @@
-export { PropsToFields } from "./PropsToFields/propsToFields";
 export { ObjectToText } from "./ObjectToText/objectToText";
 export { formatPhoneNumber } from "./PhoneFormat/phoneFormat";
