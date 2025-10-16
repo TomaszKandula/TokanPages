f3ba16e3f97e12a1e79647b64655bcb2aec9dae9
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2025-10-16 18:40:30 +0200

    feat: restyle top header for PDF printouts

--- TokanPages.ClientApp/src/Components/Resume/Components/RenderTopHeader/LargeHeader/index.tsx
+++ TokanPages.ClientApp/src/Components/Resume/Components/RenderTopHeader/LargeHeader/index.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { GET_IMAGES_URL } from "../../../../../Api";
-import { Skeleton, Image, Link } from "../../../../../Shared/Components";
+import { Skeleton, Image, Link, Icon } from "../../../../../Shared/Components";
 import { ResumeViewProps } from "../../../Types";
+
+const ICON_SIZE = 1.2;
 
 export const HeaderLarge = (props: ResumeViewProps): React.ReactElement => (
     <div className="is-flex is-gap-2.5 mb-4">
-        <div className="bulma-cell is-align-content-center">
-            <Skeleton isLoading={props.isLoading} mode="Circle" width={128} height={128} disableMarginY>
-                <figure className="bulma-image bulma-is-128x128">
+        <div className="bulma-cell is-align-content-center is-align-self-flex-start">
+            <Skeleton isLoading={props.isLoading} mode="Circle" width={96} height={96} disableMarginY>
+                <figure className="bulma-image bulma-is-96x96">
                     <Image
                         base={GET_IMAGES_URL}
                         source={props.page?.photo?.href}
@@ -20,22 +22,41 @@ export const HeaderLarge = (props: ResumeViewProps): React.ReactElement => (
         </div>
         <div className="bulma-cell is-align-content-center">
             <Skeleton isLoading={props.isLoading} mode="Text" width={100}>
-                <p className="is-size-4 has-text-grey-dark has-text-weight-bold is-capitalized">
+                <h2 className="is-size-4 has-text-grey-dark has-text-weight-bold is-capitalized">
                     {props.page?.resume?.header?.fullName}
-                </p>
+                </h2>
             </Skeleton>
             <Skeleton isLoading={props.isLoading} mode="Text" width={100}>
-                <p className="is-size-6 has-text-grey-dark is-lowercase">{props.page?.resume?.header?.mobilePhone}</p>
+                <div className="is-flex is-gap-1.5">
+                    <Icon name="Phone" size={ICON_SIZE} />
+                    <p className="is-size-6 has-text-grey-dark is-lowercase">
+                        {props.page?.resume?.header?.mobilePhone}
+                    </p>
+                </div>
             </Skeleton>
             <Skeleton isLoading={props.isLoading} mode="Text" width={100}>
-                <Link to={`mailto:${props.page?.resume?.header?.email}`} className="is-size-6 is-underlined">
-                    <p className="is-lowercase">{props.page?.resume?.header?.email}</p>
-                </Link>
+                <div className="is-flex is-gap-1.5">
+                    <Icon name="Email" size={ICON_SIZE} />
+                    <Link to={`mailto:${props.page?.resume?.header?.email}`} className="is-size-6 is-underlined">
+                        <p className="is-lowercase">{props.page?.resume?.header?.email}</p>
+                    </Link>
+                </div>
             </Skeleton>
             <Skeleton isLoading={props.isLoading} mode="Text" width={100}>
-                <Link to={props.page?.resume?.header?.github.href} className="is-size-6 is-underlined">
-                    <p className="is-lowercase">{props.page?.resume?.header?.github.text}</p>
-                </Link>
+                <div className="is-flex is-gap-1.5">
+                    <Icon name="Web" size={ICON_SIZE} />
+                    <Link to={props.page?.resume?.header?.www.href} className="is-size-6 is-underlined">
+                        <p className="is-lowercase">{props.page?.resume?.header?.www.text}</p>
+                    </Link>
+                </div>
+            </Skeleton>
+            <Skeleton isLoading={props.isLoading} mode="Text" width={100}>
+                <div className="is-flex is-gap-1.5">
+                    <Icon name="GitHub" size={ICON_SIZE} />
+                    <Link to={props.page?.resume?.header?.github.href} className="is-size-6 is-underlined">
+                        <p className="is-lowercase">{props.page?.resume?.header?.github.text}</p>
+                    </Link>
+                </div>
             </Skeleton>
         </div>
     </div>
--- TokanPages.ClientApp/src/Components/Resume/Components/RenderTopHeader/SmallHeader/index.tsx
+++ TokanPages.ClientApp/src/Components/Resume/Components/RenderTopHeader/SmallHeader/index.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import { GET_IMAGES_URL } from "../../../../../Api";
-import { Skeleton, Image, Link } from "../../../../../Shared/Components";
+import { Skeleton, Image, Link, Icon } from "../../../../../Shared/Components";
 import { ResumeViewProps } from "../../../Types";
+
+const ICON_SIZE = 1.2;
 
 export const HeaderSmall = (props: ResumeViewProps): React.ReactElement => (
     <>
-        <hr className="mb-2" />
+        <Skeleton isLoading={props.isLoading} mode="Text" width={100}>
+            <h2 className="is-size-4 has-text-centered has-text-grey-dark has-text-weight-medium is-capitalized">
+                {props.page?.resume?.header?.fullName}
+            </h2>
+        </Skeleton>
         <div className="is-flex is-gap-3.5 is-align-items-center is-justify-content-center">
             <Skeleton isLoading={props.isLoading} mode="Circle" width={64} height={64} disableMarginY>
                 <figure className="bulma-image bulma-is-64x64">
@@ -21,29 +27,41 @@ export const HeaderSmall = (props: ResumeViewProps): React.ReactElement => (
             </Skeleton>
             <div className="is-flex is-flex-direction-column">
                 <Skeleton isLoading={props.isLoading} mode="Text" width={100}>
-                    <p className="is-size-6 has-text-grey-dark has-text-weight-bold is-capitalized">
-                        {props.page?.resume?.header?.fullName}
-                    </p>
+                    <div className="is-flex is-gap-1.5">
+                        <Icon name="Phone" size={ICON_SIZE} className="has-text-grey" />
+                        <p className="is-size-6 has-text-grey-dark is-lowercase">
+                            {props.page?.resume?.header?.mobilePhone}
+                        </p>
+                    </div>
                 </Skeleton>
                 <Skeleton isLoading={props.isLoading} mode="Text" width={100}>
-                    <p className="is-size-6 has-text-grey-dark is-lowercase">
-                        {props.page?.resume?.header?.mobilePhone}
-                    </p>
+                    <div className="is-flex is-gap-1.5">
+                        <Icon name="Web" size={ICON_SIZE} className="has-text-grey" />
+                        <Link to={props.page?.resume?.header?.www.href} className="is-size-6 is-underlined">
+                            <p className="is-lowercase">{props.page?.resume?.header?.www.text}</p>
+                        </Link>
+                    </div>
                 </Skeleton>
             </div>
             <div className="is-flex is-flex-direction-column">
                 <Skeleton isLoading={props.isLoading} mode="Text" width={100}>
-                    <Link to={`mailto:${props.page?.resume?.header?.email}`} className="is-size-6 is-underlined">
-                        <p className="is-lowercase">{props.page?.resume?.header?.email}</p>
-                    </Link>
+                    <div className="is-flex is-gap-1.5">
+                        <Icon name="Email" size={ICON_SIZE} className="has-text-grey" />
+                        <Link to={`mailto:${props.page?.resume?.header?.email}`} className="is-size-6 is-underlined">
+                            <p className="is-lowercase">{props.page?.resume?.header?.email}</p>
+                        </Link>
+                    </div>
                 </Skeleton>
                 <Skeleton isLoading={props.isLoading} mode="Text" width={100}>
-                    <Link to={props.page?.resume?.header?.github.href} className="is-size-6 is-underlined">
-                        <p className="is-lowercase">{props.page?.resume?.header?.github.text}</p>
-                    </Link>
+                    <div className="is-flex is-gap-1.5">
+                        <Icon name="GitHub" size={ICON_SIZE} className="has-text-grey" />
+                        <Link to={props.page?.resume?.header?.github.href} className="is-size-6 is-underlined">
+                            <p className="is-lowercase">{props.page?.resume?.header?.github.text}</p>
+                        </Link>
+                    </div>
                 </Skeleton>
             </div>
         </div>
-        <hr className="mt-2" />
+        <hr className="mt-4" />
     </>
 );
