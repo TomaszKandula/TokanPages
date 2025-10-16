3c50dc54bb636abaa67b594cc8956328944aaf83
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2025-10-16 18:39:47 +0200

    fix: allow to set icon colour

--- TokanPages.ClientApp/src/Shared/Components/Icon/Icons/github.tsx
+++ TokanPages.ClientApp/src/Shared/Components/Icon/Icons/github.tsx
@@ -5,13 +5,12 @@ import { IconBaseProps } from "../types";
 export const GithubIcon = (props: IconBaseProps): React.ReactElement => (
     <svg
         viewBox="0 0 48 48"
-        fill="none"
         xmlns="http://www.w3.org/2000/svg"
         style={{ width: `${props.size}rem`, height: `${props.size}rem` }}
         className={props.className}
         onClick={props.onClick}
     >
-        <circle cx="24" cy="24" r="20" fill="#181717" />
+        <circle cx="24" cy="24" r="20" style={{ fill: "currentcolor" }} />
         <path d={gitHubPath} fill="white" />
     </svg>
 );
