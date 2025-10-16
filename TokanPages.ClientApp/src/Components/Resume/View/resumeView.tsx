1e37aadd498c5f32b385dc6b0ec2c53cec235554
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2025-10-16 16:40:14 +0200

    feat: restyle top header, use eager loading when shanpshot is enabled

--- TokanPages.ClientApp/src/Components/Resume/View/resumeView.tsx
+++ TokanPages.ClientApp/src/Components/Resume/View/resumeView.tsx
@@ -172,6 +172,7 @@ const RenderTestimonials = (props: ResumeViewProps) => (
                             source={props.section.photo1}
                             title={props.section.name1}
                             alt={props.section.name1}
+                            loading={props.isSnapshot ? "eager" : "lazy"}
                             className="bulma-is-rounded is-round-border-light"
                         />
                     </figure>
@@ -202,6 +203,7 @@ const RenderTestimonials = (props: ResumeViewProps) => (
                             source={props.section.photo2}
                             title={props.section.name2}
                             alt={props.section.name2}
+                            loading={props.isSnapshot ? "eager" : "lazy"}
                             className="bulma-is-rounded is-round-border-light"
                         />
                     </figure>
@@ -232,6 +234,7 @@ const RenderTestimonials = (props: ResumeViewProps) => (
                             source={props.section.photo3}
                             title={props.section.name3}
                             alt={props.section.name3}
+                            loading={props.isSnapshot ? "eager" : "lazy"}
                             className="bulma-is-rounded is-round-border-light"
                         />
                     </figure>
@@ -241,28 +244,25 @@ const RenderTestimonials = (props: ResumeViewProps) => (
     </div>
 );
 
-const RenderResume = (props: ResumeViewProps) => (
+const RenderTopHeaderSmall = (props: ResumeViewProps): React.ReactElement => (
     <>
-        <h2 className="is-size-3 is-uppercase has-text-grey-dark has-text-centered has-text-weight-light m-5">
-            {props.page?.caption}
-        </h2>
-        <div className="is-flex is-gap-2.5 mb-4">
-            <div className="bulma-cell is-align-content-center">
-                <Skeleton isLoading={props.isLoading} mode="Circle" width={98} height={98} disableMarginY>
-                    <figure className="bulma-image bulma-is-128x128">
-                        <Image
-                            base={GET_IMAGES_URL}
-                            source={props.page?.photo?.href}
-                            title={props.page?.photo?.text}
-                            alt={props.page?.photo?.text}
-                            className="bulma-is-rounded is-round-border-light"
-                        />
-                    </figure>
-                </Skeleton>
-            </div>
-            <div className="bulma-cell is-align-content-center">
+        <hr className="mb-2" />
+        <div className="is-flex is-gap-3.5 is-align-items-center is-justify-content-center">
+            <Skeleton isLoading={props.isLoading} mode="Circle" width={64} height={64} disableMarginY>
+                <figure className="bulma-image bulma-is-64x64">
+                    <Image
+                        base={GET_IMAGES_URL}
+                        source={props.page?.photo?.href}
+                        title={props.page?.photo?.text}
+                        alt={props.page?.photo?.text}
+                        loading="eager"
+                        className="bulma-is-rounded is-round-border-light"
+                    />
+                </figure>
+            </Skeleton>
+            <div className="is-flex is-flex-direction-column">
                 <Skeleton isLoading={props.isLoading} mode="Text" width={100}>
-                    <p className="is-size-4 has-text-grey-dark has-text-weight-bold is-capitalized">
+                    <p className="is-size-6 has-text-grey-dark has-text-weight-bold is-capitalized">
                         {props.page?.resume?.header?.fullName}
                     </p>
                 </Skeleton>
@@ -271,6 +271,8 @@ const RenderResume = (props: ResumeViewProps) => (
                         {props.page?.resume?.header?.mobilePhone}
                     </p>
                 </Skeleton>
+            </div>
+            <div className="is-flex is-flex-direction-column">
                 <Skeleton isLoading={props.isLoading} mode="Text" width={100}>
                     <Link to={`mailto:${props.page?.resume?.header?.email}`} className="is-size-6 is-underlined">
                         <p className="is-lowercase">{props.page?.resume?.header?.email}</p>
@@ -283,6 +285,62 @@ const RenderResume = (props: ResumeViewProps) => (
                 </Skeleton>
             </div>
         </div>
+        <hr className="mt-2" />
+    </>
+);
+
+const RenderTopHeaderLarge = (props: ResumeViewProps): React.ReactElement => (
+    <div className="is-flex is-gap-2.5 mb-4">
+        <div className="bulma-cell is-align-content-center">
+            <Skeleton isLoading={props.isLoading} mode="Circle" width={128} height={128} disableMarginY>
+                <figure className="bulma-image bulma-is-128x128">
+                    <Image
+                        base={GET_IMAGES_URL}
+                        source={props.page?.photo?.href}
+                        title={props.page?.photo?.text}
+                        alt={props.page?.photo?.text}
+                        className="bulma-is-rounded is-round-border-light"
+                    />
+                </figure>
+            </Skeleton>
+        </div>
+        <div className="bulma-cell is-align-content-center">
+            <Skeleton isLoading={props.isLoading} mode="Text" width={100}>
+                <p className="is-size-4 has-text-grey-dark has-text-weight-bold is-capitalized">
+                    {props.page?.resume?.header?.fullName}
+                </p>
+            </Skeleton>
+            <Skeleton isLoading={props.isLoading} mode="Text" width={100}>
+                <p className="is-size-6 has-text-grey-dark is-lowercase">{props.page?.resume?.header?.mobilePhone}</p>
+            </Skeleton>
+            <Skeleton isLoading={props.isLoading} mode="Text" width={100}>
+                <Link to={`mailto:${props.page?.resume?.header?.email}`} className="is-size-6 is-underlined">
+                    <p className="is-lowercase">{props.page?.resume?.header?.email}</p>
+                </Link>
+            </Skeleton>
+            <Skeleton isLoading={props.isLoading} mode="Text" width={100}>
+                <Link to={props.page?.resume?.header?.github.href} className="is-size-6 is-underlined">
+                    <p className="is-lowercase">{props.page?.resume?.header?.github.text}</p>
+                </Link>
+            </Skeleton>
+        </div>
+    </div>
+);
+
+const RenderResume = (props: ResumeViewProps): React.ReactElement => (
+    <>
+        <h2 className="is-size-3 is-uppercase has-text-grey-dark has-text-centered has-text-weight-light m-5">
+            {props.page?.caption}
+        </h2>
+        <Media.DesktopOnly>
+            <RenderTopHeaderSmall {...props} />
+        </Media.DesktopOnly>
+        <Media.TabletOnly>
+            <RenderTopHeaderLarge {...props} />
+        </Media.TabletOnly>
+        <Media.MobileOnly>
+            <RenderTopHeaderLarge {...props} />
+        </Media.MobileOnly>
         <RenderCaption isLoading={props.isLoading} text={props.page.resume.summary.caption} tag="h3" />
         <Skeleton isLoading={props.isLoading} mode="Text" height={24} className="my-4">
             <h4 className="is-size-5 has-text-grey-dark has-text-justified line-height-20">
