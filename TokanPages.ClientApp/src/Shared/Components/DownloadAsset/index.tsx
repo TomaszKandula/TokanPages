387b8a06824eb9db1b325649614e000781c8164f
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2025-10-17 16:36:25 +0200

    fix: accomodate to rem values

--- TokanPages.ClientApp/src/Shared/Components/DownloadAsset/index.tsx
+++ TokanPages.ClientApp/src/Shared/Components/DownloadAsset/index.tsx
@@ -5,10 +5,12 @@ import { DownloadAssetProps } from "./Types";
 
 export const DownloadAsset = (props: DownloadAssetProps): React.ReactElement => {
     const url = `${props.url}&canDownload=true`;
+    const iconButtonSize = (props.size ?? 2.0) * 1.5;
+
     return (
-        <IconButton>
+        <IconButton hasGreyBackground size={iconButtonSize}>
             <a href={url}>
-                <Icon name="Download" size={props.size ?? 1.1} className={props.className} />
+                <Icon name="Download" size={props.size ?? 1.5} className={props.className} />
             </a>
         </IconButton>
     );
