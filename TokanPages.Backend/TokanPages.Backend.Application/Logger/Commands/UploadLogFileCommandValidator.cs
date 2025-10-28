4e0b53ee876f100acf7bff1a220f3c2de70dd8ee
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2025-10-28 22:23:03 +0100

    feat: add validator

---
+++ TokanPages.Backend/TokanPages.Backend.Application/Logger/Commands/UploadLogFileCommandValidator.cs
@@ -0,0 +1,23 @@
+using FluentValidation;
+using Microsoft.Extensions.Configuration;
+using TokanPages.Backend.Shared.Resources;
+
+namespace TokanPages.Backend.Application.Logger.Commands;
+
+public class UploadLogFileCommandValidator : AbstractValidator<UploadLogFileCommand>
+{
+    public UploadLogFileCommandValidator(IConfiguration configuration)
+    {
+        var sizeLimit = configuration.GetValue<int>("AZ_Storage_MaxFileSizeUserMedia");
+
+        RuleFor(command => command.Data)
+            .NotEmpty()
+            .WithErrorCode(nameof(ValidationCodes.REQUIRED))
+            .WithMessage(ValidationCodes.REQUIRED);
+
+        RuleFor(command => command.Data)
+            .Must(bytes => bytes!.Length <= sizeLimit)
+            .WithErrorCode(nameof(ValidationCodes.INVALID_FILE_SIZE))
+            .WithMessage(ValidationCodes.INVALID_FILE_SIZE);
+    }
+}
