53bd7d197fd688ade768ecd341e8ed3cee951447
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2025-10-28 22:22:25 +0100

    feat: add DTO model

---
+++ TokanPages.WebApi/TokanPages.Logger.Dto/UploadLogFileDto.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.AspNetCore.Http;
+
+namespace TokanPages.Logger.Dto;
+
+/// <summary>
+/// Use it when you want to upload log file (Redis, etc.).
+/// </summary>
+[ExcludeFromCodeCoverage]
+public class UploadLogFileDto
+{
+    /// <summary>
+    /// Binary data.
+    /// </summary>
+    [DataType(DataType.Upload)]
+    public IFormFile? Data { get; set; }
+}
