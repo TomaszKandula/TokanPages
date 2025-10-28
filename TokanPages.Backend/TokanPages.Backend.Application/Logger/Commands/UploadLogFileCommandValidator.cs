be9684d7818c375e585815f943e11f0a610575ac
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2025-10-28 22:51:34 +0100

    feat: require catalog name to be not longer than 225 chars

--- TokanPages.Backend/TokanPages.Backend.Application/Logger/Commands/UploadLogFileCommandValidator.cs
+++ TokanPages.Backend/TokanPages.Backend.Application/Logger/Commands/UploadLogFileCommandValidator.cs
@@ -10,6 +10,14 @@ public class UploadLogFileCommandValidator : AbstractValidator<UploadLogFileComm
     {
         var sizeLimit = configuration.GetValue<int>("AZ_Storage_MaxFileSizeUserMedia");
 
+        RuleFor(command => command.CatalogName)
+            .NotEmpty()
+            .WithErrorCode(nameof(ValidationCodes.REQUIRED))
+            .WithMessage(ValidationCodes.REQUIRED)
+            .MaximumLength(225)
+            .WithErrorCode(nameof(ValidationCodes.LENGTH_TOO_LONG_225))
+            .WithMessage(ValidationCodes.LENGTH_TOO_LONG_225);
+
         RuleFor(command => command.Data)
             .NotEmpty()
             .WithErrorCode(nameof(ValidationCodes.REQUIRED))
