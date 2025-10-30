96b3c21a1c4255350683f2d433d1270d3ed52a97
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2025-10-30 23:12:01 +0100

    feat: change contract

--- TokanPages.ClientApp/src/Shared/Components/Slider/Types/index.ts
+++ TokanPages.ClientApp/src/Shared/Components/Slider/Types/index.ts
@@ -1,10 +1,17 @@
+import Slider from "react-slick";
 import { ReactElement, ReactElements, ViewProperties } from "../../../../Shared/Types";
 
 export interface ContentProps {
     children: ReactElement | ReactElements;
 }
 
-export interface SliderProps extends ViewProperties, ContentProps {
+export interface SliderOptionsProps {
+    onNextSlideClick?: () => void;
+    onPrevSlideClick?: () => void;
+    onPlayPauseClick?: () => void;
+}
+
+export interface SliderProps extends ViewProperties, ContentProps, SliderOptionsProps {
     autoplay?: boolean;
     autoplaySpeed?: number;
     pauseOnHover?: boolean;
@@ -17,6 +24,8 @@ export interface SliderProps extends ViewProperties, ContentProps {
 }
 
 export interface SliderViewProps extends SliderProps {
+    isPaused: boolean;
+    reference: React.MutableRefObject<Slider | null>;
     selection: number;
     onSlideChange: (current: number, next: number) => void;
 }
