7b235aae463e1b7dce56669d80ab6ff2c845a1e4
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2025-10-31 19:07:51 +0100

    refactor: simplify code

--- TokanPages.ClientApp/src/Shared/Components/RenderContent/Renderers/RenderImages/index.tsx
+++ TokanPages.ClientApp/src/Shared/Components/RenderContent/Renderers/RenderImages/index.tsx
@@ -5,6 +5,35 @@ import { Carousel, Image } from "../../../../../Shared/Components";
 import { ImageItemProps, TextItem } from "../../Models";
 import { v4 as uuidv4 } from "uuid";
 
+interface RenderImageProps extends TextItem {
+    imageItem: ImageItemProps;
+}
+
+const RenderImage = (props: RenderImageProps) => {
+    const value = props.imageItem;
+
+    return (
+        <>
+            <div className="bulma-card-image">
+                <figure className="bulma-image">
+                    <Image
+                        source={`${API_BASE_URI}${value.image}`}
+                        title={props.text}
+                        alt={props.text}
+                        width={value.constraint?.width ?? props.constraint?.width}
+                        height={value.constraint?.height ?? props.constraint?.height}
+                        objectFit={value.constraint?.objectFit ?? props.constraint?.objectFit}
+                        loading={props.loading}
+                    />
+                </figure>
+            </div>
+            <div className="bulma-card-content">
+                <p className="is-size-6 has-text-black">{value.caption}</p>
+            </div>
+        </>
+    );
+};
+
 export const RenderImages = (props: TextItem): ReactElement => {
     const items = props.value as ImageItemProps[];
 
@@ -22,24 +51,7 @@ export const RenderImages = (props: TextItem): ReactElement => {
             className="bulma-card"
         >
             {items.map((value: ImageItemProps, _index: number) => (
-                <React.Fragment key={uuidv4()}>
-                    <div className="bulma-card-image">
-                        <figure className="bulma-image">
-                            <Image
-                                source={`${API_BASE_URI}${value.image}`}
-                                title={props.text}
-                                alt={props.text}
-                                width={value.constraint?.width ?? props.constraint?.width}
-                                height={value.constraint?.height ?? props.constraint?.height}
-                                objectFit={value.constraint?.objectFit ?? props.constraint?.objectFit}
-                                loading={props.loading}
-                            />
-                        </figure>
-                    </div>
-                    <div className="bulma-card-content">
-                        <p className="is-size-6 has-text-black">{value.caption}</p>
-                    </div>
-                </React.Fragment>
+                <RenderImage key={uuidv4()} {...props} imageItem={value} />
             ))}
         </Carousel>
     );
