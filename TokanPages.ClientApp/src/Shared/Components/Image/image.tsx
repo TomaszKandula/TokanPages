2f9fdbd46353f49a73037e9226c3aa389771a949
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2025-11-02 14:32:41 +0100

    feat: extend image component

--- TokanPages.ClientApp/src/Shared/Components/Image/image.tsx
+++ TokanPages.ClientApp/src/Shared/Components/Image/image.tsx
@@ -1,8 +1,15 @@
 import * as React from "react";
+import { Icon } from "../Icon";
 import { RenderImageProps } from "./Types";
 import validate from "validate.js";
+import "./image.css";
 
 export const Image = (props: RenderImageProps): React.ReactElement | null => {
+    const [isMouseOver, setIsMouseOver] = React.useState(false);
+    const onMouseOver = React.useCallback(() => {
+        setIsMouseOver(!isMouseOver);
+    }, [isMouseOver]);
+
     let src = props.source;
     if (!validate.isEmpty(props.base) && !validate.isEmpty(props.source)) {
         src = `${props.base}/${props.source}`;
@@ -13,18 +20,25 @@ export const Image = (props: RenderImageProps): React.ReactElement | null => {
     }
 
     return (
-        <img
-            src={src}
-            loading={props.loading ?? "lazy"}
-            style={{
-                objectFit: props.objectFit,
-                width: props.width,
-                height: props.height,
-            }}
-            className={props.className}
-            alt={props.alt}
-            title={props.title}
-            onClick={props.onClick}
-        />
+        <div onMouseEnter={onMouseOver} onMouseLeave={onMouseOver}>
+            {isMouseOver && props.isPreviewIcon && (
+                <div className="preview-container">
+                    <Icon name="MagnifyPlusOutline" size={5} className="preview-icon" />
+                </div>
+            )}
+            <img
+                src={src}
+                loading={props.loading ?? "lazy"}
+                style={{
+                    objectFit: props.objectFit,
+                    width: props.width,
+                    height: props.height,
+                }}
+                className={props.className}
+                alt={props.alt}
+                title={props.title}
+                onClick={props.onClick}
+            />
+        </div>
     );
 };
