f5b2d63850aebf0b2590529129e1bd8cf304bdf1
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2025-11-11 20:58:21 +0100

    feat: remove preview cover from video component

--- TokanPages.ClientApp/src/Shared/Components/Video/video.tsx
+++ TokanPages.ClientApp/src/Shared/Components/Video/video.tsx
@@ -1,21 +1,12 @@
 import React from "react";
-import { Icon } from "../Icon";
 import { ReactElement } from "../../../Shared/Types";
 import { VideoProps } from "./Types";
 import validate from "validate.js";
 import "./video.css";
 
 export const Video = (props: VideoProps): ReactElement => {
-    const [isMouseOver, setIsMouseOver] = React.useState(false);
     const [videoUrl, setVideoUrl] = React.useState("");
     const [posterUrl, setPosterUrl] = React.useState("");
-
-    const topRadius = props.isPreviewTopRadius ? "preview-top-radius" : "";
-    const bottomRadius = props.isPreviewBottomRadius ? "preview-bottom-radius" : "";
-
-    const onMouseOver = React.useCallback(() => {
-        setIsMouseOver(!isMouseOver);
-    }, [isMouseOver]);
 
     React.useEffect(() => {
         if (!validate.isEmpty(props.base) && !validate.isEmpty(props.source)) {
@@ -34,29 +25,22 @@ export const Video = (props: VideoProps): ReactElement => {
     }, [props.base, props.poster]);
 
     return (
-        <div onMouseEnter={onMouseOver} onMouseLeave={onMouseOver}>
-            {isMouseOver && props.isPreviewIcon && (
-                <div className={`preview-container ${topRadius} ${bottomRadius}`}>
-                    <Icon name="PlayCircleOutline" size={5} className="preview-icon" />
-                </div>
-            )}
-            <video
-                src={videoUrl}
-                poster={posterUrl}
-                preload={props.preload}
-                controls={props.controls}
-                onClick={props.onClick}
-                className={props.className}
-                style={{
-                    objectFit: props.objectFit,
-                    width: props.width,
-                    height: props.height,
-                    maxWidth: props.width,
-                    maxHeight: props.height,
-                    borderTopLeftRadius: "0.75rem",
-                    borderTopRightRadius: "0.75rem",
-                }}
-            />
-        </div>
+        <video
+            src={videoUrl}
+            poster={posterUrl}
+            preload={props.preload}
+            controls={props.controls}
+            onClick={props.onClick}
+            className={props.className}
+            style={{
+                objectFit: props.objectFit,
+                width: props.width,
+                height: props.height,
+                maxWidth: props.width,
+                maxHeight: props.height,
+                borderTopLeftRadius: "0.75rem",
+                borderTopRightRadius: "0.75rem",
+            }}
+        />
     );
 };
