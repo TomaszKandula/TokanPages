c56b42f771b2f47cc2c8dfbcf9a41f48e03bfdca
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2025-11-11 20:04:31 +0100

    feat: reimplement RenderVideo component

--- TokanPages.ClientApp/src/Shared/Components/RenderContent/Renderers/RenderVideo/renderVideo.tsx
+++ TokanPages.ClientApp/src/Shared/Components/RenderContent/Renderers/RenderVideo/renderVideo.tsx
@@ -1,49 +1,78 @@
 import * as React from "react";
+import { useMediaPresenter } from "../../../../../Shared/Hooks";
+import { MediaPresenter } from "../../../../../Shared/Components/MediaPresenter";
 import { TextItem } from "../../Models/TextModel";
-import { API_BASE_URI } from "../../../../../Api";
 import Validate from "validate.js";
+import { Video } from "Shared/Components/Video";
 
-const RenderDescription = (props: { text: string }): React.ReactElement => {
+interface RenderClipProps extends TextItem {
+    posterUrl: string;
+    videoUrl: string;
+    onClick: () => void;
+}
+
+const RenderDescription = (props: { text: string }): React.ReactElement => (
+    <>
+        <hr className="m-0" />
+        <div className="bulma-card-content">
+            <span className="is-size-6">{props.text}</span>
+        </div>
+    </>
+);
+
+const RenderClip = (props: RenderClipProps): React.ReactElement => {
+    const hasText = !Validate.isEmpty(props.text);
+
     return (
         <>
-            <hr className="m-0" />
-            <div className="bulma-card-content">
-                <span className="is-size-6">{props.text}</span>
+            <div className="bulma-card-image">
+                <figure className="bulma-image">
+                    <Video
+                        source={props.videoUrl}
+                        poster={props.posterUrl}
+                        controls={true}
+                        preload="metadata"
+                        width={props.constraint?.width ?? "100%"}
+                        height={props.constraint?.height}
+                    />
+                </figure>
             </div>
+            {hasText ? null : <RenderDescription text={props.text} />}
         </>
     );
 };
 
 export const RenderVideo = (props: TextItem): React.ReactElement => {
-    let valueUrl = props.value as string;
-    if (!valueUrl.includes("https://")) {
-        valueUrl = `${API_BASE_URI}${valueUrl}`;
-    }
+    const presenter = useMediaPresenter();
 
-    let propUrl = props.prop;
-    if (!propUrl.includes("https://")) {
-        propUrl = `${API_BASE_URI}${propUrl}`;
-    }
+    const [posterUrl, setPosterUrl] = React.useState("");
+    const [videoUrl, setVideoUrl] = React.useState("");
+
+    const hasValue = !Validate.isEmpty(props.value);
+    const hasProp = !Validate.isEmpty(props.prop);
+    const hasPropAndValue = hasProp && hasValue;
+
+    const onClickEvent = React.useCallback(() => {
+        presenter.onSelectionClick(0);
+    }, []);
+
+    React.useEffect(() => {
+        if (hasPropAndValue) {
+            setPosterUrl(props.prop);
+            setVideoUrl(props.value as string);
+        }
+    }, [hasPropAndValue]);
 
     return (
         <div className="bulma-card my-6">
-            <div className="bulma-card-image">
-                <figure className="bulma-image">
-                    <video
-                        src={valueUrl}
-                        poster={propUrl}
-                        controls
-                        title="Video related to the presented article text"
-                        width={props.constraint?.width ?? "100%"}
-                        height={props.constraint?.height}
-                        style={{
-                            borderTopLeftRadius: "0.75rem",
-                            borderTopRightRadius: "0.75rem",
-                        }}
-                    />
-                </figure>
-            </div>
-            {Validate.isEmpty(props.text) ? null : <RenderDescription text={props.text} />}
+            <RenderClip {...props} posterUrl={posterUrl} videoUrl={videoUrl} onClick={onClickEvent} />
+            <MediaPresenter
+                isOpen={presenter.isPresenterOpen}
+                presenting={presenter.selection}
+                collection={[videoUrl]}
+                type="video"
+                onTrigger={presenter.onPresenterClick}
+            />
         </div>
     );
 };
