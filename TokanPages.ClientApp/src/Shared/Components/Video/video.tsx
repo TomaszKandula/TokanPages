8678a334b6ce038f30328a6c51852e2b0cfd4bf2
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2025-11-14 16:52:11 +0100

    feat: add loader for video before autoplay hits

--- TokanPages.ClientApp/src/Shared/Components/Video/video.tsx
+++ TokanPages.ClientApp/src/Shared/Components/Video/video.tsx
@@ -2,11 +2,15 @@ import React from "react";
 import { ReactElement } from "../../../Shared/Types";
 import { VideoProps } from "./Types";
 import validate from "validate.js";
+import { ProgressBar } from "..";
 import "./video.css";
 
 export const Video = (props: VideoProps): ReactElement => {
+    const videoRef = React.useRef<HTMLVideoElement | null>(null);
+
     const [videoUrl, setVideoUrl] = React.useState("");
     const [posterUrl, setPosterUrl] = React.useState("");
+    const [isLoading, setIsLoading] = React.useState(true);
 
     React.useEffect(() => {
         if (!validate.isEmpty(props.base) && !validate.isEmpty(props.source)) {
@@ -24,25 +28,42 @@ export const Video = (props: VideoProps): ReactElement => {
         }
     }, [props.base, props.poster]);
 
+    React.useEffect(() => {
+        if (!videoRef.current) {
+            return;
+        }
+
+        videoRef.current?.addEventListener("loadeddata", () => {
+            setIsLoading(false);
+        });
+        return () => videoRef.current?.addEventListener("loadeddata", () => {});
+    }, [videoRef.current]);
+
     return (
-        <video
-            src={videoUrl}
-            poster={posterUrl}
-            preload={props.preload}
-            controls={props.controls}
-            autoPlay={props.autoplay}
-            onClick={props.onClick}
-            className={props.className}
-            webkit-playsinline="true"
-            style={{
-                objectFit: props.objectFit,
-                width: props.width,
-                height: props.height,
-                maxWidth: props.width,
-                maxHeight: props.height,
-                borderTopLeftRadius: "0.75rem",
-                borderTopRightRadius: "0.75rem",
-            }}
-        />
+        <>
+            {isLoading ? (
+                <div className="video-container">
+                    <ProgressBar className="video-loader" size={60} thickness={5} colour="#FFF" />
+                </div>
+            ) : null}
+            <video
+                ref={videoRef}
+                src={videoUrl}
+                poster={posterUrl}
+                preload={props.preload}
+                controls={props.controls}
+                autoPlay={props.autoplay}
+                onClick={props.onClick}
+                className={props.className}
+                playsInline={true}
+                style={{
+                    objectFit: props.objectFit,
+                    width: props.width,
+                    height: props.height,
+                    maxWidth: props.width,
+                    maxHeight: props.height,
+                }}
+            />
+        </>
     );
 };
