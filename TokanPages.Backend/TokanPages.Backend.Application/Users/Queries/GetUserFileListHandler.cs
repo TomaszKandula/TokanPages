511da3b705f141e51074360f0efa401a474c291f
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2026-01-10 11:51:47 +0100

    feat: reimplement, return blob filtered by given file type

--- TokanPages.Backend/TokanPages.Backend.Application/Users/Queries/GetUserFileListHandler.cs
+++ TokanPages.Backend/TokanPages.Backend.Application/Users/Queries/GetUserFileListHandler.cs
@@ -1,6 +1,8 @@
 using TokanPages.Backend.Core.Utilities.LoggerService;
+using TokanPages.Backend.Domain.Enums;
 using TokanPages.Persistence.Database;
 using TokanPages.Services.AzureStorageService.Abstractions;
+using TokanPages.Services.AzureStorageService.Models;
 using TokanPages.Services.UserService.Abstractions;
 
 namespace TokanPages.Backend.Application.Users.Queries;
@@ -21,22 +23,31 @@ public class GetUserFileListHandler : RequestHandler<GetUserFileListQuery, GetUs
 
     public override async Task<GetUserFileListResult> Handle(GetUserFileListQuery request, CancellationToken cancellationToken)
     {
-        var userId = _userService.GetLoggedUserId();
-
+        var userId = _userService.GetLoggedUserId(); 
         var prefixPath = $"content/users/{userId}/files/";
-        var includeByPath = request.IsVideoFile ? "videos/" : string.Empty;
-        var excludeByPath = request.IsVideoFile ? string.Empty : "videos/";
-
-        var azureBlob = _azureBlobStorageFactory.Create(LoggerService);
-        var result = await azureBlob.GetBlobListing(
-            prefix: prefixPath, 
-            includeByPath: includeByPath, 
-            excludeByPath: excludeByPath, 
-            cancellationToken: cancellationToken);
+        var result = await GetBlobListing(request.Type, prefixPath, cancellationToken);
 
         return new GetUserFileListResult
         {
             FileBlobs = result
         };
+    }
+
+    private async Task<List<string>> GetBlobListing(UserFile type, string prefix, CancellationToken cancellationToken)
+    {
+        var azureBlob = _azureBlobStorageFactory.Create(LoggerService);
+        var options = new StorageListingOptions { Prefix = prefix };
+
+        options.IncludeByPath = type switch
+        {
+            UserFile.Image => "images/",
+            UserFile.Audio => "audio/",
+            UserFile.Video => "videos/",
+            UserFile.Document => "documents/",
+            UserFile.Application => "applications/",
+            _ => options.IncludeByPath
+        };
+
+        return await azureBlob.GetBlobListing(options, cancellationToken: cancellationToken);
     }
 }
