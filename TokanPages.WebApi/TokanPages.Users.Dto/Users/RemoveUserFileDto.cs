4f4400f34c9620a655f71e29416235525a9a4a86
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2026-01-10 15:11:12 +0100

    feat: add DTO model

---
+++ TokanPages.WebApi/TokanPages.Users.Dto/Users/RemoveUserFileDto.cs
@@ -0,0 +1,21 @@
+using System.Diagnostics.CodeAnalysis;
+using TokanPages.Backend.Domain.Enums;
+
+namespace TokanPages.Users.Dto.Users;
+
+/// <summary>
+/// Use it when you want to remove user file.
+/// </summary>
+[ExcludeFromCodeCoverage]
+public class RemoveUserFileDto
+{
+    /// <summary>
+    /// User file type (image, audio, video, document, application).
+    /// </summary>
+    public UserFile Type { get; set; }
+
+    /// <summary>
+    /// File name.
+    /// </summary>
+    public string UniqueBlobName { get; set; } = "";
+}
