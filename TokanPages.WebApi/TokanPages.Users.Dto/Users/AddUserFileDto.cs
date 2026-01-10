84dd90b503ae401b339867d47cfeacdeeb293bbc
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2026-01-10 13:16:26 +0100

    feat: DTO model

---
+++ TokanPages.WebApi/TokanPages.Users.Dto/Users/AddUserFileDto.cs
@@ -0,0 +1,24 @@
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.AspNetCore.Http;
+using TokanPages.Backend.Domain.Enums;
+
+namespace TokanPages.Users.Dto.Users;
+
+/// <summary>
+/// Use it when you want to upload user file.
+/// </summary>
+[ExcludeFromCodeCoverage]
+public class AddUserFileDto
+{
+    /// <summary>
+    /// User file type (image, audio, video, document, application). 
+    /// </summary>
+    public UserFile Type { get; set; }
+
+    /// <summary>
+    /// Binary data.
+    /// </summary>
+    [DataType(DataType.Upload)]
+    public IFormFile? BinaryData { get; set; }
+}
