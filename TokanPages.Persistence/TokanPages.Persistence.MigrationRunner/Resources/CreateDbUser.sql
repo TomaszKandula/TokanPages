c55a9c7d5f3ff2bcb3e4a5a93fa552cfc64fcd85
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2026-01-13 13:23:50 +0100

    feat: update sql script for new schema

--- TokanPages.Persistence/TokanPages.Persistence.MigrationRunner/Resources/CreateDbUser.sql
+++ TokanPages.Persistence/TokanPages.Persistence.MigrationRunner/Resources/CreateDbUser.sql
@@ -4,10 +4,10 @@ EXEC sp_addrolemember 'db_datareader', AppAccessDb
 EXEC sp_addrolemember 'db_datawriter', AppAccessDb
 
 GRANT CREATE TABLE TO AppAccessDb
-GRANT ALTER ON SCHEMA::dbo TO AppAccessDb
+GRANT ALTER ON SCHEMA::operation TO AppAccessDb
 
-GRANT DELETE ON SCHEMA::dbo TO AppAccessDb
-GRANT INSERT ON SCHEMA::dbo TO AppAccessDb
-GRANT SELECT ON SCHEMA::dbo TO AppAccessDb
-GRANT UPDATE ON SCHEMA::dbo TO AppAccessDb
-GRANT REFERENCES ON SCHEMA::dbo TO AppAccessDb
+GRANT DELETE ON SCHEMA::operation TO AppAccessDb
+GRANT INSERT ON SCHEMA::operation TO AppAccessDb
+GRANT SELECT ON SCHEMA::operation TO AppAccessDb
+GRANT UPDATE ON SCHEMA::operation TO AppAccessDb
+GRANT REFERENCES ON SCHEMA::operation TO AppAccessDb
