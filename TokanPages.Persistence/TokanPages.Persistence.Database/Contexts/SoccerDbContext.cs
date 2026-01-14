391c918348f0a4cf9423cc5505eb4907a577e355
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2026-01-15 00:48:36 +0100

    feat: register soccer tables

--- TokanPages.Persistence/TokanPages.Persistence.Database/Contexts/SoccerDbContext.cs
+++ TokanPages.Persistence/TokanPages.Persistence.Database/Contexts/SoccerDbContext.cs
@@ -1,6 +1,8 @@
 using System.Diagnostics.CodeAnalysis;
 using System.Reflection;
 using Microsoft.EntityFrameworkCore;
+using TokanPages.Backend.Domain.Entities.Soccer;
+using Attribute = TokanPages.Backend.Domain.Entities.Soccer.Attribute;
 
 namespace TokanPages.Persistence.Database.Contexts;
 
@@ -8,6 +10,34 @@ namespace TokanPages.Persistence.Database.Contexts;
 public class SoccerDbContext : DbContext
 {
     public SoccerDbContext(DbContextOptions<SoccerDbContext> options) : base(options) { }
+
+    public virtual DbSet<Attribute> Attributes { get; set; }
+
+    public virtual DbSet<Feed> Feeds { get; set; }
+
+    public virtual DbSet<FeedImage> FeedImages { get; set; }
+
+    public virtual DbSet<Field> Fields { get; set; }
+    
+    public virtual DbSet<FieldImage> FieldImages { get; set; }
+    
+    public virtual DbSet<Lineup> Lineups { get; set; }
+    
+    public virtual DbSet<Match> Matches { get; set; }
+    
+    public virtual DbSet<Player> Players { get; set; }
+    
+    public virtual DbSet<PlayerAttribute> PlayerAttributes { get; set; }
+    
+    public virtual DbSet<Position> Positions { get; set; }
+    
+    public virtual DbSet<Quality> Qualities { get; set; }
+    
+    public virtual DbSet<Team> Teams { get; set; }
+    
+    public virtual DbSet<TeamInfo> TeamInfo { get; set; }
+
+    public virtual DbSet<View> Views { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
