e1563b3c877728d135f4691023c51f37b695d8bb
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2026-01-17 12:03:10 +0100

    feat: update schema files

--- TokanPages.Persistence/TokanPages.Persistence.SqlDatabase/Soccer/Schema.sql
+++ TokanPages.Persistence/TokanPages.Persistence.SqlDatabase/Soccer/Schema.sql
@@ -1,2 +1,11 @@
-CREATE SCHEMA [soccer]
-    AUTHORIZATION [dbo]
+CREATE SCHEMA [Soccer] AUTHORIZATION [dbo]
+
+IF EXISTS (SELECT * FROM sys.database_principals WHERE NAME = 'AppAccessDb')
+BEGIN
+    GRANT ALTER ON SCHEMA::Soccer TO AppAccessDb
+    GRANT DELETE ON SCHEMA::Soccer TO AppAccessDb
+    GRANT INSERT ON SCHEMA::Soccer TO AppAccessDb
+    GRANT SELECT ON SCHEMA::Soccer TO AppAccessDb
+    GRANT UPDATE ON SCHEMA::Soccer TO AppAccessDb
+    GRANT REFERENCES ON SCHEMA::Soccer TO AppAccessDb
+END
