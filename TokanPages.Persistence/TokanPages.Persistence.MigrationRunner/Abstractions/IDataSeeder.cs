541862279ecd0deaa27d1a890714308f2cfc1ef2
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2026-01-17 17:07:06 +0100

    feat: remove obsolete migration runner

--- TokanPages.Persistence/TokanPages.Persistence.MigrationRunner/Abstractions/IDataSeeder.cs
+++
@@ -1,8 +0,0 @@
-using Microsoft.EntityFrameworkCore;
-
-namespace TokanPages.Persistence.MigrationRunner.Abstractions;
-
-public interface IDataSeeder
-{
-    void Seed<T>(string connectionString) where T : DbContext;
-}
