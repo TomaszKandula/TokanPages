6be4a5bf73521f4c193233daacbbc9e95570e9a7
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2026-01-23 00:36:02 +0100

    feat: use SQL Generator class

--- TokanPages.Persistence/TokanPages.Persistence.DataAccess/Helpers/DapperWrapper.cs
+++ TokanPages.Persistence/TokanPages.Persistence.DataAccess/Helpers/DapperWrapper.cs
@@ -3,7 +3,6 @@ using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Configuration;
 using TokanPages.Backend.Core.Exceptions;
 using TokanPages.Backend.Core.Utilities.LoggerService;
-using TokanPages.Backend.Domain.Attributes;
 using TokanPages.Backend.Shared.Resources;
 
 namespace TokanPages.Persistence.DataAccess.Helpers;
@@ -12,19 +11,22 @@ public class DapperWrapper : IDapperWrapper
 {
     private readonly ILoggerService _loggerService;
 
+    private readonly ISqlGenerator _sqlGenerator;
+
     private readonly IConfiguration _configuration;
 
     private string ConnectionString => _configuration.GetValue<string>("Db_DatabaseContext") ?? "";
 
-    public DapperWrapper(ILoggerService loggerService, IConfiguration configuration)
+    public DapperWrapper(ILoggerService loggerService, IConfiguration configuration, ISqlGenerator sqlGenerator)
     {
         _loggerService = loggerService;
         _configuration = configuration;
+        _sqlGenerator = sqlGenerator;
     }
 
     public async Task Insert<T>(T entity, CancellationToken cancellationToken = default)
     {
-        var sql = GenerateInsertStatement(entity);
+        var sql = _sqlGenerator.GenerateInsertStatement(entity);
         await ExecuteSqlTransaction(sql, cancellationToken);
     }
 
@@ -35,7 +37,7 @@ public class DapperWrapper : IDapperWrapper
 
     public async Task Delete<T>(T entity, CancellationToken cancellationToken = default)
     {
-        var sql = GenerateDeleteStatement(entity);
+        var sql = _sqlGenerator.GenerateDeleteStatement(entity);
         await ExecuteSqlTransaction(sql, cancellationToken);
     }
 
@@ -54,73 +56,5 @@ public class DapperWrapper : IDapperWrapper
             _loggerService.LogFatal($"{exception.Message} {exception.InnerException?.Message}");
             throw new GeneralException(nameof(ErrorCodes.ERROR_UNEXPECTED), ErrorCodes.ERROR_UNEXPECTED);
         }
-    }
-
-    private static string GenerateInsertStatement<T>(T entity)
-    {
-        const string template = "INSERT INTO {0} ({1}) VALUES ({2})";
-
-        var table = GetTableName<T>();
-        var columns = new List<string>();
-        var values = new List<string>();
-
-        var properties = typeof(T).GetProperties();
-        foreach (var property in properties)
-        {
-            var value = property.GetValue(entity)?.ToString();
-            switch (value)
-            {
-                case null:
-                    values.Add("NULL"); 
-                    break;
-                case "False":
-                    values.Add("0");
-                    break;
-                case "True":
-                    values.Add("1");
-                    break;
-                default:
-                    values.Add("'" + value + "'");
-                    break;
-            }
-
-            columns.Add(property.Name);
-        }
-
-        return string.Format(template, table, string.Join(",", columns), string.Join(",", values));
-    }
-
-    private static string GenerateDeleteStatement<T>(T entity)
-    {
-        const string template = "DELETE FROM {0} WHERE {1}";
-
-        var table = GetTableName<T>();
-        var conditions = new List<string>();
-        var properties = typeof(T).GetProperties();
-
-        foreach (var property in properties)
-        {
-            var value = property.GetValue(entity)?.ToString();
-            if (!string.IsNullOrEmpty(value))
-            {
-                conditions.Add($"{property.Name} = '{value}'");
-            }
-        }
-
-        return string.Format(template, table, string.Join(" AND ", conditions));
-    }
-
-    private static string GetTableName<T>()
-    {
-        var table = nameof(T);
-
-        var entityAttributes = (DatabaseTableAttribute[])typeof(T).GetCustomAttributes(typeof(DatabaseTableAttribute), true);
-        if (entityAttributes.Length == 0)
-            return table;
-
-        var attributes = entityAttributes[0];
-        table = $"{attributes.Schema}.{attributes.TableName}";
-
-        return table;
     }
 }
