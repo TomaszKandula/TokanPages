48b1789a94e15c8d363a05d02bf6fa7f2180196d
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2026-01-23 14:56:58 +0100

    test: update test class

--- TokanPages.Tests/TokanPages.Tests.UnitTests/Models/TestPlayerTwo.cs
+++ TokanPages.Tests/TokanPages.Tests.UnitTests/Models/TestPlayerTwo.cs
@@ -3,4 +3,11 @@ using TokanPages.Backend.Domain.Attributes;
 namespace TokanPages.Tests.UnitTests.Models;
 
 [DatabaseTable(Schema = "soccer")]
-internal abstract class TestPlayerTwo { }
+internal class TestPlayerTwo
+{
+    public Guid Id { get; set; }
+
+    public string? Name { get; set; }
+
+    public bool IsPublished { get; set; }
+}
