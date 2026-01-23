399181ff1e05eeb846d60b2275ef07c68d57a372
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2026-01-23 12:03:26 +0100

    test: extend test class

--- TokanPages.Tests/TokanPages.Tests.UnitTests/Models/TestPlayerOne.cs
+++ TokanPages.Tests/TokanPages.Tests.UnitTests/Models/TestPlayerOne.cs
@@ -2,5 +2,16 @@ using TokanPages.Backend.Domain.Attributes;
 
 namespace TokanPages.Tests.UnitTests.Models;
 
-[DatabaseTable(Schema = "soccer", TableName =  "Players")]
-internal abstract class TestPlayerOne { }
+[DatabaseTable(Schema = "soccer", TableName = "Players")]
+internal class TestPlayerOne
+{
+    public Guid Id { get; set; }
+
+    public string? Name { get; set; }
+
+    public bool IsPublished { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+
+    public int Likes { get; set; }
+}
