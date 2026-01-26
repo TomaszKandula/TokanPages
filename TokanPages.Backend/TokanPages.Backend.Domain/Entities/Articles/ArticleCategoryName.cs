160309719443d56b6ab2e71c0675e8f498bdd6e9
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2026-01-27 00:42:31 +0100

    feat: update entity

--- TokanPages.Backend/TokanPages.Backend.Domain/Entities/Articles/ArticleCategoryName.cs
+++ TokanPages.Backend/TokanPages.Backend.Domain/Entities/Articles/ArticleCategoryName.cs
@@ -1,8 +1,10 @@
 using System.Diagnostics.CodeAnalysis;
+using TokanPages.Backend.Domain.Attributes;
 
 namespace TokanPages.Backend.Domain.Entities.Articles;
 
 [ExcludeFromCodeCoverage]
+[DatabaseTable(Schema = "operation", TableName = "ArticleCategoryNames")]
 public class ArticleCategoryName : Entity<Guid>
 {
     public Guid ArticleCategoryId { get; set; }
