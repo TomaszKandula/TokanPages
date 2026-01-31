43a9f83b0d2e3571ad3feaf213894239b99d5139
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2026-01-31 19:21:29 +0100

    refactor: rename

--- TokanPages.Backend/TokanPages.Backend.Configuration/Options/BoundAppSettings.cs
+++ TokanPages.Backend/TokanPages.Backend.Configuration/Options/BoundAppSettings.cs
@@ -2,9 +2,9 @@ using Microsoft.Extensions.Configuration;
 
 namespace TokanPages.Backend.Configuration.Options;
 
-public static class AppSettingsBind
+public static class BoundAppSettings
 {
-    public static AppSettings GetAppSettings(IConfiguration configuration)
+    public static AppSettings GetSettings(IConfiguration configuration)
     {
         var settings = new AppSettings();
         configuration.Bind(AppSettings.SectionName, settings);
