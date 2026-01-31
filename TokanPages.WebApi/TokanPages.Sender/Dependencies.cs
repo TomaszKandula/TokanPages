0a9ee675b1b74a8ab606db52ccef074dc26cfd96
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2026-01-31 19:27:02 +0100

    feat: use 'GetSettings' method

--- TokanPages.WebApi/TokanPages.Sender/Dependencies.cs
+++ TokanPages.WebApi/TokanPages.Sender/Dependencies.cs
@@ -65,7 +65,7 @@ public static class Dependencies
 
 	private static void SetupServices(this IServiceCollection services, IConfiguration configuration) 
 	{
-		services.AddHttpContextAccessor();
+        services.AddHttpContextAccessor();
 
         services.AddSingleton<ILoggerService, LoggerService>();
 		services.AddSingleton<IHttpClientServiceFactory>(_ => new HttpClientServiceFactory());
@@ -82,16 +82,17 @@ public static class Dependencies
 		services.AddScoped<INewslettersCache, NewslettersCache>();
 		services.AddScoped<IRedisDistributedCache, RedisDistributedCache>();
 
+        var settings = BoundAppSettings.GetSettings(configuration);
 		services.AddSingleton<IAzureBusFactory>(_ =>
 		{
-			var connectionString = configuration.GetValue<string>("AZ_Bus_ConnectionString") ?? "";
+			var connectionString = settings.AzBusConnectionString;
 			return new AzureBusFactory(connectionString);
 		});
 
 		services.AddSingleton<IAzureBlobStorageFactory>(_ =>
 		{
-			var containerName = configuration.GetValue<string>("AZ_Storage_ContainerName") ?? "";
-			var connectionString = configuration.GetValue<string>("AZ_Storage_ConnectionString") ?? "";
+			var containerName = settings.AzStorageContainerName;
+			var connectionString = settings.AzStorageConnectionString;
 			return new AzureBlobStorageFactory(connectionString, containerName);
 		});
 	}
