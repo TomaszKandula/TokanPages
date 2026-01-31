a7f13686ef7d2199b3bbec7d4b0a25a843ca5637
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2026-02-01 00:17:48 +0100

    feat: extend test props

--- TokanPages.Tests/TokanPages.Tests.UnitTests/TestBase.cs
+++ TokanPages.Tests/TokanPages.Tests.UnitTests/TestBase.cs
@@ -66,6 +66,7 @@ public abstract class TestBase
             LimitResetMaturity = 30,
             AzStorageBaseUrl = DataUtilityService.GetRandomString(),
             AzStorageMaxFileSizeUserMedia = 4096,
+            AzStorageMaxFileSizeSingleAsset = 4069,
             PathsTemplatesContactForm = DataUtilityService.GetRandomString(),
             PathsTemplatesNewsletter = DataUtilityService.GetRandomString(),
             EmailAddressContact = DataUtilityService.GetRandomString(),
@@ -75,7 +76,8 @@ public abstract class TestBase
             PathsNewsletterRemove = DataUtilityService.GetRandomString(),
             LimitLikesAnonymous = 25,
             LimitLikesUser = 50,
-            UserNoteMaxCount = 10
+            UserNoteMaxCount = 10,
+            UserNoteMaxSize = 1024
         };
 
         mockedOptions.Setup(options => options.Value).Returns(apsSettings);
