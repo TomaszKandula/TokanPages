f0665ae7da235c7577eda2969fdd17266c3e15f1
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2026-02-01 23:58:44 +0100

    feat: add query method for video upload status

--- TokanPages.Persistence/TokanPages.Persistence.DataAccess/Repositories/Content/ContentRepository.cs
+++ TokanPages.Persistence/TokanPages.Persistence.DataAccess/Repositories/Content/ContentRepository.cs
@@ -1,6 +1,8 @@
 using Microsoft.Extensions.Options;
 using TokanPages.Backend.Configuration.Options;
+using TokanPages.Backend.Domain.Entities;
 using TokanPages.Persistence.DataAccess.Abstractions;
+using TokanPages.Persistence.DataAccess.Repositories.Content.Models;
 
 namespace TokanPages.Persistence.DataAccess.Repositories.Content;
 
@@ -8,4 +10,19 @@ public class ContentRepository : RepositoryPattern, IContentRepository
 {
     public ContentRepository(IDbOperations dbOperations, IOptions<AppSettingsModel> appSettings) 
         : base(dbOperations,  appSettings) { }
+
+    public async Task<VideoUploadStatusDto?> GetVideoUploadStatus(Guid ticketId)
+    {
+        var filterBy = new { TicketId = ticketId };
+        var data = (await DbOperations.Retrieve<UploadedVideo>(filterBy)).SingleOrDefault();
+        if (data is null)
+            return null;
+
+        return new VideoUploadStatusDto
+        {
+            Status = data.Status,
+            VideoUri = data.TargetVideoUri,
+            ThumbnailUri = data.TargetThumbnailUri
+        };
+    }
 }
--- TokanPages.Persistence/TokanPages.Persistence.DataAccess/Repositories/Content/IContentRepository.cs
+++ TokanPages.Persistence/TokanPages.Persistence.DataAccess/Repositories/Content/IContentRepository.cs
@@ -1,6 +1,8 @@
+using TokanPages.Persistence.DataAccess.Repositories.Content.Models;
+
 namespace TokanPages.Persistence.DataAccess.Repositories.Content;
 
 public interface IContentRepository
 {
-    
+    Task<VideoUploadStatusDto?> GetVideoUploadStatus(Guid ticketId);
 }
