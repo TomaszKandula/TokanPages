3403af6f29ee722fae04c97987b0000b6d07c982
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2026-02-01 19:49:01 +0100

    feat: add repository pattern abstract class

---
+++ TokanPages.Persistence/TokanPages.Persistence.DataAccess/Repositories/RepositoryPattern.cs
@@ -0,0 +1,17 @@
+using TokanPages.Backend.Configuration.Options;
+using TokanPages.Persistence.DataAccess.Abstractions;
+
+namespace TokanPages.Persistence.DataAccess.Repositories;
+
+public abstract class RepositoryPattern
+{
+    protected readonly IDbOperations DbOperations;
+
+    protected readonly AppSettingsModel AppSettings;
+
+    protected RepositoryPattern(IDbOperations dbOperations, AppSettingsModel appSettings)
+    {
+        DbOperations = dbOperations;
+        AppSettings = appSettings;
+    }
+}
