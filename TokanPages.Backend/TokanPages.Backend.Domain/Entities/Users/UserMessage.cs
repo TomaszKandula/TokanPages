848d51c5a024bc9485de068bc1df7d245dc39a3b
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2026-02-01 16:17:57 +0100

    feat: update entities

--- TokanPages.Backend/TokanPages.Backend.Domain/Entities/Users/UserMessage.cs
+++ TokanPages.Backend/TokanPages.Backend.Domain/Entities/Users/UserMessage.cs
@@ -1,10 +1,12 @@
 using System.ComponentModel.DataAnnotations;
 using System.Diagnostics.CodeAnalysis;
+using TokanPages.Backend.Domain.Attributes;
 using TokanPages.Backend.Domain.Contracts;
 
 namespace TokanPages.Backend.Domain.Entities.Users;
 
 [ExcludeFromCodeCoverage]
+[DatabaseTable(Schema = "operation", TableName = "UserMessages")]
 public class UserMessage : Entity<Guid>, IAuditable
 {
     [Required]
