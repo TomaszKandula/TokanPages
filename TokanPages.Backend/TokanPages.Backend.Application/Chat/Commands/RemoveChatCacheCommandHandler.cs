ad511c665318e238faf14ed1df45073e9d973d9d
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2026-02-01 18:45:38 +0100

    feat: change hanlder to use repository

--- TokanPages.Backend/TokanPages.Backend.Application/Chat/Commands/RemoveChatCacheCommandHandler.cs
+++ TokanPages.Backend/TokanPages.Backend.Application/Chat/Commands/RemoveChatCacheCommandHandler.cs
@@ -1,39 +1,26 @@
 using TokanPages.Backend.Core.Utilities.LoggerService;
 using MediatR;
-using Microsoft.EntityFrameworkCore;
 using TokanPages.Persistence.DataAccess.Contexts;
+using TokanPages.Persistence.DataAccess.Repositories.Chat;
 
 namespace TokanPages.Backend.Application.Chat.Commands;
 
 public class RemoveChatCacheCommandHandler : RequestHandler<RemoveChatCacheCommand, Unit>
 {
-    public RemoveChatCacheCommandHandler(OperationDbContext operationDbContext, ILoggerService loggerService) : base(operationDbContext, loggerService) { }
+    private readonly IChatRepository _chatRepository;
+
+    public RemoveChatCacheCommandHandler(OperationDbContext operationDbContext, ILoggerService loggerService, IChatRepository chatRepository) 
+        : base(operationDbContext, loggerService) => _chatRepository = chatRepository;
 
     public override async Task<Unit> Handle(RemoveChatCacheCommand request, CancellationToken cancellationToken)
     {
         if (request.ChatId is not null)
         {
-            var cache = await OperationDbContext.UserMessagesCache
-                .Where(cache => cache.Id == request.ChatId)
-                .SingleOrDefaultAsync(cancellationToken);
-
-            if (cache is null)
-                return Unit.Value;
-
-            OperationDbContext.Remove(cache);
-            await OperationDbContext.SaveChangesAsync(cancellationToken);
+            await _chatRepository.RemoveChatUserCacheById((Guid)request.ChatId);
         }
         else if (request.ChatKey is not null)
         {
-            var cacheList = await OperationDbContext.UserMessagesCache
-                .Where(cache => cache.ChatKey == request.ChatKey)
-                .ToListAsync(cancellationToken);
-
-            if (cacheList.Count == 0)
-                return Unit.Value;
-
-            OperationDbContext.RemoveRange(cacheList);
-            await OperationDbContext.SaveChangesAsync(cancellationToken);
+            await _chatRepository.RemoveChatUserCacheByKey(request.ChatKey);
         }
 
         return Unit.Value;        
