d148fd1ca743c5fa794ea15cd6fa4ae8b0237b6e
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2026-02-03 00:37:18 +0100

    feat: implement methods

--- TokanPages.Persistence/TokanPages.Persistence.DataAccess/Repositories/Invoicing/IInvoicingRepository.cs
+++ TokanPages.Persistence/TokanPages.Persistence.DataAccess/Repositories/Invoicing/IInvoicingRepository.cs
@@ -1,6 +1,10 @@
+using TokanPages.Backend.Domain.Entities.Invoicing;
+
 namespace TokanPages.Persistence.DataAccess.Repositories.Invoicing;
 
 public interface IInvoicingRepository
 {
-    
+    Task<List<VatNumberPattern>> GetVatNumberPatterns();
+
+    Task<List<InvoiceTemplate>> GetInvoiceTemplates(bool isDeleted);
 }
--- TokanPages.Persistence/TokanPages.Persistence.DataAccess/Repositories/Invoicing/InvoicingRepository.cs
+++ TokanPages.Persistence/TokanPages.Persistence.DataAccess/Repositories/Invoicing/InvoicingRepository.cs
@@ -1,5 +1,6 @@
 using Microsoft.Extensions.Options;
 using TokanPages.Backend.Configuration.Options;
+using TokanPages.Backend.Domain.Entities.Invoicing;
 using TokanPages.Persistence.DataAccess.Abstractions;
 
 namespace TokanPages.Persistence.DataAccess.Repositories.Invoicing;
@@ -8,7 +9,15 @@ public class InvoicingRepository : RepositoryBase, IInvoicingRepository
 {
     public InvoicingRepository(IDbOperations dbOperations, IOptions<AppSettingsModel> appSettings) 
         : base(dbOperations, appSettings) { }
-    
-    
-    
+
+    public async Task<List<VatNumberPattern>> GetVatNumberPatterns()
+    {
+        return (await DbOperations.Retrieve<VatNumberPattern>()).ToList();
+    }
+
+    public async Task<List<InvoiceTemplate>> GetInvoiceTemplates(bool isDeleted)
+    {
+        var filterBy = new { IsDeleted = isDeleted };
+        return (await DbOperations.Retrieve<InvoiceTemplate>(filterBy: filterBy)).ToList();
+    }
 }
