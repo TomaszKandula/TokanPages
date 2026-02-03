c815ccf1c5af8da2d8e0cb640d5e61448e5a55f0
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2026-02-03 18:01:35 +0100

    feat: implement new methods

--- TokanPages.Persistence/TokanPages.Persistence.DataAccess/Repositories/Invoicing/InvoicingRepository.cs
+++ TokanPages.Persistence/TokanPages.Persistence.DataAccess/Repositories/Invoicing/InvoicingRepository.cs
@@ -4,6 +4,7 @@ using TokanPages.Backend.Core.Exceptions;
 using TokanPages.Backend.Domain.Entities.Invoicing;
 using TokanPages.Backend.Shared.Resources;
 using TokanPages.Persistence.DataAccess.Abstractions;
+using TokanPages.Persistence.DataAccess.Repositories.Invoicing.Models;
 
 namespace TokanPages.Persistence.DataAccess.Repositories.Invoicing;
 
@@ -12,27 +13,80 @@ public class InvoicingRepository : RepositoryBase, IInvoicingRepository
     public InvoicingRepository(IDbOperations dbOperations, IOptions<AppSettingsModel> appSettings) 
         : base(dbOperations, appSettings) { }
 
+    private static BusinessException InvalidTemplateId => new(nameof(ErrorCodes.INVALID_TEMPLATE_ID), ErrorCodes.INVALID_TEMPLATE_ID);
+
+    private static BusinessException InvalidContentType => new(nameof(ErrorCodes.INVALID_CONTENT_TYPE), ErrorCodes.INVALID_CONTENT_TYPE);
+    
     public async Task<List<VatNumberPattern>> GetVatNumberPatterns()
     {
         return (await DbOperations.Retrieve<VatNumberPattern>()).ToList();
     }
 
+    /// <inheritdoc/>
     public async Task<List<InvoiceTemplate>> GetInvoiceTemplates(bool isDeleted)
     {
         var filterBy = new { IsDeleted = isDeleted };
         return (await DbOperations.Retrieve<InvoiceTemplate>(filterBy: filterBy)).ToList();
     }
 
+    /// <inheritdoc/>
+    /// <exception cref="BusinessException">Throws an error code INVALID_TEMPLATE_ID.</exception>
     public async Task<InvoiceTemplate> GetInvoiceTemplate(Guid templateId, bool isDeleted)
     {
         var filterBy = new { Id = templateId, IsDeleted = isDeleted };
         var data = (await DbOperations.Retrieve<InvoiceTemplate>(filterBy: filterBy)).SingleOrDefault();
-        if (data is null)
-            throw new BusinessException(nameof(ErrorCodes.INVALID_TEMPLATE_ID), ErrorCodes.INVALID_TEMPLATE_ID);            
-
-        return data;
+        return data ?? throw InvalidTemplateId;
     }
 
+    /// <inheritdoc/>
+    public async Task<Guid> CreateInvoiceTemplate(InvoiceTemplateDto template, DateTime generatedAt)
+    {
+        if (string.IsNullOrEmpty(template.InvoiceTemplateData.ContentType))
+            throw InvalidContentType;
+
+        var entity = new InvoiceTemplate
+        {
+            Id = Guid.NewGuid(),
+            Name = template.TemplateName,
+            Data = template.InvoiceTemplateData.ContentData,
+            ContentType = template.InvoiceTemplateData.ContentType,
+            ShortDescription = template.InvoiceTemplateDescription,
+            GeneratedAt = generatedAt,
+            IsDeleted = false
+        };
+
+        await DbOperations.Insert(entity);
+        return entity.Id;
+    }
+
+    /// <inheritdoc/>
+    /// <exception cref="BusinessException">Throws an error code INVALID_TEMPLATE_ID.</exception>
+    public async Task<bool> ReplaceInvoiceTemplate(Guid templateId, InvoiceTemplateDataDto data)
+    {
+        try
+        {
+            if (string.IsNullOrEmpty(data.ContentType))
+                throw InvalidContentType;
+
+            var filterBy = new { Id = templateId, IsDeleted = false };
+            var updateBy = new
+            {
+                Data =  data.ContentData,
+                ContentType = data.ContentType,
+                ShortDescription = data.Description
+            };
+
+            await DbOperations.Update<InvoiceTemplate>(updateBy, filterBy);
+        }
+        catch
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    /// <inheritdoc/>
     public async Task<bool> RemoveInvoiceTemplate(Guid templateId)
     {
         try
@@ -49,6 +103,4 @@ public class InvoicingRepository : RepositoryBase, IInvoicingRepository
 
         return true;
     }
-    
-    
 }
