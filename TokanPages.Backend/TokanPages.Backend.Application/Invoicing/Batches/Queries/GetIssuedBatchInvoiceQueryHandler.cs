1b88bf70c4ebb32b5468049a66079b1ff0c19b0f
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2026-02-04 12:28:36 +0100

    feat: replace EFCore w/Dapper

--- TokanPages.Backend/TokanPages.Backend.Application/Invoicing/Batches/Queries/GetIssuedBatchInvoiceQueryHandler.cs
+++ TokanPages.Backend/TokanPages.Backend.Application/Invoicing/Batches/Queries/GetIssuedBatchInvoiceQueryHandler.cs
@@ -1,21 +1,26 @@
 using Microsoft.AspNetCore.Mvc;
+using TokanPages.Backend.Core.Exceptions;
 using TokanPages.Backend.Core.Utilities.LoggerService;
+using TokanPages.Backend.Shared.Resources;
 using TokanPages.Persistence.DataAccess.Contexts;
-using TokanPages.Services.BatchService;
+using TokanPages.Persistence.DataAccess.Repositories.Invoicing;
 
 namespace TokanPages.Backend.Application.Invoicing.Batches.Queries;
 
 public class GetIssuedBatchInvoiceQueryHandler : RequestHandler<GetIssuedBatchInvoiceQuery, FileContentResult>
 {
-    private readonly IBatchService _batchService;
+    private readonly IInvoicingRepository _invoicingRepository;
 
-    public GetIssuedBatchInvoiceQueryHandler(OperationDbContext operationDbContext, ILoggerService loggerService, IBatchService batchService) 
-        : base(operationDbContext, loggerService) => _batchService = batchService;
+    public GetIssuedBatchInvoiceQueryHandler(OperationDbContext operationDbContext, ILoggerService loggerService, IInvoicingRepository invoicingRepository) 
+        : base(operationDbContext, loggerService) => _invoicingRepository = invoicingRepository;
 
     public override async Task<FileContentResult> Handle(GetIssuedBatchInvoiceQuery request, CancellationToken cancellationToken)
     {
-        var result = await _batchService.GetIssuedInvoice(request.InvoiceNumber, cancellationToken);
-        LoggerService.LogInformation($"Returned issued invoice. Invoice number: {result.Number}");
-        return new FileContentResult(result.ContentData, result.ContentType);
+        var data = await _invoicingRepository.GetIssuedInvoiceById(request.InvoiceNumber);
+        if (data is null)
+            throw new BusinessException(nameof(ErrorCodes.INVALID_INVOICE_NUMBER), ErrorCodes.INVALID_INVOICE_NUMBER);
+
+        LoggerService.LogInformation($"Returned issued invoice. Invoice number: {data.Number}");
+        return new FileContentResult(data.ContentData, data.ContentType);
     }
 }
