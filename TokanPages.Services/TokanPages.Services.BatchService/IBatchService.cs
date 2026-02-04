37e83640a6f4c32443a257bf8044a11ac5195fe6
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2026-02-04 12:29:31 +0100

    feat: remove unused code from contract

--- TokanPages.Services/TokanPages.Services.BatchService/IBatchService.cs
+++ TokanPages.Services/TokanPages.Services.BatchService/IBatchService.cs
@@ -7,10 +7,4 @@ public interface IBatchService
     Task<Guid> OrderInvoiceBatchProcessing(IEnumerable<OrderDetail> orderDetails);
 
     Task ProcessOutstandingInvoices(CancellationToken cancellationToken = default);
-
-    //TODO: to be removed
-    Task<ProcessingStatus> GetBatchInvoiceProcessingStatus(Guid processBatchKey, CancellationToken cancellationToken = default);
- 
-    //TODO: to be removed
-    Task<InvoiceData> GetIssuedInvoice(string invoiceNumber, CancellationToken cancellationToken = default);
 }
