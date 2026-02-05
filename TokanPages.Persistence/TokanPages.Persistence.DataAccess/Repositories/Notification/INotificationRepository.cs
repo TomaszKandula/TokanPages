a263099e971428704a8c5f1e975547065b9b8996
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2026-02-05 17:25:08 +0100

    feat: add contract

--- TokanPages.Persistence/TokanPages.Persistence.DataAccess/Repositories/Notification/INotificationRepository.cs
+++ TokanPages.Persistence/TokanPages.Persistence.DataAccess/Repositories/Notification/INotificationRepository.cs
@@ -1,6 +1,17 @@
+using TokanPages.Backend.Domain.Entities.Notifications;
+using TokanPages.Persistence.DataAccess.Repositories.Notification.Models;
+
 namespace TokanPages.Persistence.DataAccess.Repositories.Notification;
 
 public interface INotificationRepository
 {
-    
+    Task<PushNotification?> GetPushNotification(string pnsHandle);
+
+    Task CreatePushNotificationEntry(PushNotificationDto data);
+
+    Task UpdatePushNotificationEntry(PushNotificationDto data);
+
+    Task<List<PushNotificationTag>> GetPushNotificationTags(Guid installationId);
+
+    Task CreatePushNotificationTag(PushNotificationTagDto data);
 }
