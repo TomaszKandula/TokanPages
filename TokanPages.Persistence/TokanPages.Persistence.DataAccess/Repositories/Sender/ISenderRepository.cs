0b09e8a745c1d60a3fd5aa50c5de8f04d6ebc649
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2026-02-07 22:34:47 +0100

    feat: extend contract

--- TokanPages.Persistence/TokanPages.Persistence.DataAccess/Repositories/Sender/ISenderRepository.cs
+++ TokanPages.Persistence/TokanPages.Persistence.DataAccess/Repositories/Sender/ISenderRepository.cs
@@ -1,7 +1,23 @@
+using TokanPages.Backend.Domain.Entities;
+
 namespace TokanPages.Persistence.DataAccess.Repositories.Sender;
 
 public interface ISenderRepository
 {
+    /// <summary>
+    /// Returns newsletter entity by the given ID.
+    /// </summary>
+    /// <param name="id">Newsletter ID.</param>
+    /// <returns>If found, returns an entity, otherwise null.</returns>
+    Task<Newsletter?> GetNewsletter(Guid id);
+
+    /// <summary>
+    /// Returns list of newsletter that are active. 
+    /// </summary>
+    /// <param name="isActive">If true, only active newsletters are returned.</param>
+    /// <returns>Lif of active newsletters.</returns>
+    Task<List<Newsletter>> GetNewsletters(bool isActive);
+
     /// <summary>
     /// Creates a new business inquiry entry in the database.
     /// </summary>
