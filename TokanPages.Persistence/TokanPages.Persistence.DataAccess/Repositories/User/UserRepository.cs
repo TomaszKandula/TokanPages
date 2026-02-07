6747dab08cc1bb6452b0a5a10bc379df7780274a
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2026-02-07 11:09:20 +0100

    feat: get user data by ID

--- TokanPages.Persistence/TokanPages.Persistence.DataAccess/Repositories/User/UserRepository.cs
+++ TokanPages.Persistence/TokanPages.Persistence.DataAccess/Repositories/User/UserRepository.cs
@@ -1,6 +1,7 @@
 using Microsoft.Extensions.Options;
 using TokanPages.Backend.Configuration.Options;
 using TokanPages.Persistence.DataAccess.Abstractions;
+using Users = TokanPages.Backend.Domain.Entities.Users;
 
 namespace TokanPages.Persistence.DataAccess.Repositories.User;
 
@@ -8,4 +9,12 @@ public class UserRepository : RepositoryBase, IUserRepository
 {
     public UserRepository(IDbOperations dbOperations, IOptions<AppSettingsModel> appSettings) 
         : base(dbOperations, appSettings) { }
+
+    /// <inheritdoc/>
+    public async Task<Users.User?> GetUserById(Guid userId)
+    {
+        var filterBy = new { Id = userId };
+        var data = await DbOperations.Retrieve<Users.User>(filterBy);
+        return data.SingleOrDefault();
+    }
 }
