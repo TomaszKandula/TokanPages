68d642dd1ce6e4262d096516129007fc5ef6a482
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2026-02-07 11:48:08 +0100

    refactor: reformat for clarity

--- TokanPages.Backend/TokanPages.Backend.Domain/Entities/Articles/Article.cs
+++ TokanPages.Backend/TokanPages.Backend.Domain/Entities/Articles/Article.cs
@@ -9,14 +9,24 @@ namespace TokanPages.Backend.Domain.Entities.Articles;
 public class Article : Entity<Guid>, IAuditable
 {
     public string Title { get; set; }
+
     public string Description { get; set; }
+
     public bool IsPublished { get; set; }
+
     public Guid CreatedBy { get; set; }
+
     public DateTime CreatedAt { get; set; }
+
     public Guid? ModifiedBy { get; set; }
+
     public DateTime? ModifiedAt { get; set; }
+
     public DateTime? UpdatedAt { get; set; }
+
     public Guid? UserId { get; set; }
+
     public Guid? CategoryId {get; set; }
+
     public string LanguageIso { get; set; }
 }
--- TokanPages.Backend/TokanPages.Backend.Domain/Entities/Articles/ArticleCategoryName.cs
+++ TokanPages.Backend/TokanPages.Backend.Domain/Entities/Articles/ArticleCategoryName.cs
@@ -8,6 +8,8 @@ namespace TokanPages.Backend.Domain.Entities.Articles;
 public class ArticleCategoryName : Entity<Guid>
 {
     public Guid ArticleCategoryId { get; set; }
+
     public string Name { get; set; }
+
     public Guid LanguageId { get; set; }
 }
