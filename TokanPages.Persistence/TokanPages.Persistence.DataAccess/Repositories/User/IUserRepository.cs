7c610f8a11a694c3db0b4f5a54fe578ce6719ffb
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2026-02-07 11:09:06 +0100

    feat: extend contract

--- TokanPages.Persistence/TokanPages.Persistence.DataAccess/Repositories/User/IUserRepository.cs
+++ TokanPages.Persistence/TokanPages.Persistence.DataAccess/Repositories/User/IUserRepository.cs
@@ -1,6 +1,16 @@
+using Users = TokanPages.Backend.Domain.Entities.Users;
+
 namespace TokanPages.Persistence.DataAccess.Repositories.User;
 
 public interface IUserRepository
 {
-    
+    /// <summary>
+    /// Returns user entity by the given ID.
+    /// </summary>
+    /// <remarks>
+    /// ID is a primary key of an entity.
+    /// </remarks>
+    /// <param name="userId">A mandatory user ID.</param>
+    /// <returns>If found, returns user data, otherwise null.</returns>
+    Task<Users.User?> GetUserById(Guid userId);
 }
