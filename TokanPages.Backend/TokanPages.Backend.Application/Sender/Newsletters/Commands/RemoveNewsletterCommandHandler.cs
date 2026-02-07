b837b72f87df2d58e2efebd85319b26c500a33dd
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2026-02-07 23:35:16 +0100

    feat: replace EFCore w/Dapper

--- TokanPages.Backend/TokanPages.Backend.Application/Sender/Newsletters/Commands/RemoveNewsletterCommandHandler.cs
+++ TokanPages.Backend/TokanPages.Backend.Application/Sender/Newsletters/Commands/RemoveNewsletterCommandHandler.cs
@@ -1,28 +1,26 @@
 ﻿using MediatR;
-using Microsoft.EntityFrameworkCore;
 using TokanPages.Backend.Core.Exceptions;
 using TokanPages.Backend.Core.Utilities.LoggerService;
 using TokanPages.Backend.Shared.Resources;
 using TokanPages.Persistence.DataAccess.Contexts;
+using TokanPages.Persistence.DataAccess.Repositories.Sender;
 
 namespace TokanPages.Backend.Application.Sender.Newsletters.Commands;
 
 public class RemoveNewsletterCommandHandler : RequestHandler<RemoveNewsletterCommand, Unit>
 {
-    public RemoveNewsletterCommandHandler(OperationDbContext operationDbContext, ILoggerService loggerService) 
-        : base(operationDbContext, loggerService) { }
+    private readonly ISenderRepository _senderRepository;
+    
+    public RemoveNewsletterCommandHandler(OperationDbContext operationDbContext, ILoggerService loggerService, 
+        ISenderRepository senderRepository) : base(operationDbContext, loggerService) => _senderRepository = senderRepository;
 
     public override async Task<Unit> Handle(RemoveNewsletterCommand request, CancellationToken cancellationToken) 
     {
-        var newsletterData = await OperationDbContext.Newsletters
-            .Where(newsletter => newsletter.Id == request.Id)
-            .SingleOrDefaultAsync(cancellationToken);
-            
+        var newsletterData = await _senderRepository.GetNewsletter(request.Id);
         if (newsletterData is null)
             throw new BusinessException(nameof(ErrorCodes.SUBSCRIBER_DOES_NOT_EXISTS), ErrorCodes.SUBSCRIBER_DOES_NOT_EXISTS);
 
-        OperationDbContext.Newsletters.Remove(newsletterData);
-        await OperationDbContext.SaveChangesAsync(cancellationToken);
+        await _senderRepository.RemoveNewsletter(request.Id);
         return Unit.Value;
     }
 }
