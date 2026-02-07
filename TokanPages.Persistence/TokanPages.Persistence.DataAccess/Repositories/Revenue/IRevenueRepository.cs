9afee269d90787482d7c6f3a8e23bb7c71cc6287
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2026-02-07 13:36:58 +0100

    feat: extend contract

--- TokanPages.Persistence/TokanPages.Persistence.DataAccess/Repositories/Revenue/IRevenueRepository.cs
+++ TokanPages.Persistence/TokanPages.Persistence.DataAccess/Repositories/Revenue/IRevenueRepository.cs
@@ -1,6 +1,21 @@
+using TokanPages.Backend.Domain.Entities;
+using TokanPages.Backend.Domain.Entities.Users;
+
 namespace TokanPages.Persistence.DataAccess.Repositories.Revenue;
 
 public interface IRevenueRepository
 {
-    
+    /// <summary>
+    /// Returns list of subscription prices by the given language ISO.
+    /// </summary>
+    /// <param name="languageIso">User language.</param>
+    /// <returns>List of available subscription prices.</returns>
+    Task <List<SubscriptionPricing>> GetSubscriptionPrices(string languageIso);
+
+    /// <summary>
+    /// Returns list of use subscription.
+    /// </summary>
+    /// <param name="userId">A mandatory user ID.</param>
+    /// <returns>If found, returns user subscription details, otherwise null.</returns>
+    Task<UserSubscription?> GetUserSubscriptions(Guid userId);
 }
