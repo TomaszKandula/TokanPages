2ae03230b69e7a3c375d055dc7224dc61c1dab4f
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2026-02-07 23:28:50 +0100

    feat: replace EFCore w/Dapper

--- TokanPages.Backend/TokanPages.Backend.Application/Sender/Newsletters/Commands/UpdateNewsletterCommandHandler.cs
+++ TokanPages.Backend/TokanPages.Backend.Application/Sender/Newsletters/Commands/UpdateNewsletterCommandHandler.cs
@@ -1,52 +1,47 @@
 ﻿using MediatR;
-using Microsoft.EntityFrameworkCore;
 using TokanPages.Backend.Core.Exceptions;
-using TokanPages.Backend.Core.Utilities.DateTimeService;
 using TokanPages.Backend.Core.Utilities.LoggerService;
 using TokanPages.Backend.Shared.Resources;
 using TokanPages.Persistence.DataAccess.Contexts;
+using TokanPages.Persistence.DataAccess.Repositories.Sender;
+using TokanPages.Persistence.DataAccess.Repositories.Sender.Models;
 using TokanPages.Services.UserService.Abstractions;
 
 namespace TokanPages.Backend.Application.Sender.Newsletters.Commands;
 
 public class UpdateNewsletterCommandHandler : RequestHandler<UpdateNewsletterCommand, Unit>
 {
-    private readonly IDateTimeService _dateTimeService;
-
     private readonly IUserService _userService;
 
+    private readonly ISenderRepository _senderRepository;
+
     public UpdateNewsletterCommandHandler(OperationDbContext operationDbContext, ILoggerService loggerService, 
-        IDateTimeService dateTimeService, IUserService userService) : base(operationDbContext, loggerService)
+        IUserService userService, ISenderRepository senderRepository) : base(operationDbContext, loggerService)
     {
-        _dateTimeService = dateTimeService;
         _userService = userService;
+        _senderRepository = senderRepository;
     }
 
     public override async Task<Unit> Handle(UpdateNewsletterCommand request, CancellationToken cancellationToken) 
     {
-        var newsletterData = await OperationDbContext.Newsletters
-            .Where(newsletter => newsletter.Id == request.Id)
-            .SingleOrDefaultAsync(cancellationToken);
-
+        var newsletterData = await _senderRepository.GetNewsletter(request.Id);
         if (newsletterData is null) 
             throw new BusinessException(nameof(ErrorCodes.SUBSCRIBER_DOES_NOT_EXISTS), ErrorCodes.SUBSCRIBER_DOES_NOT_EXISTS);
 
-        var emailCollection = await OperationDbContext.Newsletters
-            .AsNoTracking()
-            .Where(newsletter => newsletter.Email == request.Email)
-            .ToListAsync(cancellationToken);
-
-        if (emailCollection.Count == 1)
+        var emailCollection = await _senderRepository.GetNewsletter(newsletterData.Email);
+        if (emailCollection is not null)
             throw new BusinessException(nameof(ErrorCodes.EMAIL_ADDRESS_ALREADY_EXISTS), ErrorCodes.EMAIL_ADDRESS_ALREADY_EXISTS);
 
         var user = await _userService.GetUser(cancellationToken);
-        newsletterData.Email = request.Email ?? newsletterData.Email;
-        newsletterData.Count = request.Count ?? newsletterData.Count;
-        newsletterData.IsActivated = request.IsActivated ?? newsletterData.IsActivated;
-        newsletterData.ModifiedAt = _dateTimeService.Now;
-        newsletterData.ModifiedBy = user?.UserId ?? Guid.Empty;
+        var updateNewsletter = new UpdateNewsletterDto
+        {
+            Email = request.Email ?? newsletterData.Email,
+            Count = request.Count ?? newsletterData.Count,
+            IsActivated = request.IsActivated ?? newsletterData.IsActivated,
+            ModifiedBy = user?.UserId ?? Guid.Empty
+        };
 
-        await OperationDbContext.SaveChangesAsync(cancellationToken);
+        await _senderRepository.UpdateNewsletter(updateNewsletter);
         return Unit.Value;
     }
 }
