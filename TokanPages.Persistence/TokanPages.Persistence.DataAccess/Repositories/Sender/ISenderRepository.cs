3e058ca2365e52fbc8535a7f200aed3853e2ffa7
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2026-02-07 19:52:14 +0100

    docs: update XML comment

--- TokanPages.Persistence/TokanPages.Persistence.DataAccess/Repositories/Sender/ISenderRepository.cs
+++ TokanPages.Persistence/TokanPages.Persistence.DataAccess/Repositories/Sender/ISenderRepository.cs
@@ -3,9 +3,8 @@ namespace TokanPages.Persistence.DataAccess.Repositories.Sender;
 public interface ISenderRepository
 {
     /// <summary>
-    /// 
+    /// Creates a new business inquiry entry in the database.
     /// </summary>
-    /// <param name="jsonData"></param>
-    /// <returns></returns>
+    /// <param name="jsonData">A stringify business message.</param>
     Task CreateBusinessInquiry(string jsonData);
 }
