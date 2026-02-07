388089aba499c1a8f72ffd0c4aa02260496e4275
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2026-02-07 01:13:28 +0100

    feat: replace EFCore w/Dapper

--- TokanPages.Backend/TokanPages.Backend.Application/Notifications/Mobile/Commands/AddInstallationCommandHandler.cs
+++ TokanPages.Backend/TokanPages.Backend.Application/Notifications/Mobile/Commands/AddInstallationCommandHandler.cs
@@ -1,11 +1,10 @@
-using TokanPages.Backend.Core.Utilities.DateTimeService;
 using TokanPages.Backend.Core.Utilities.LoggerService;
 using TokanPages.Services.PushNotificationService.Abstractions;
 using TokanPages.Services.PushNotificationService.Exceptions;
 using TokanPages.Services.PushNotificationService.Models;
-using Microsoft.EntityFrameworkCore;
-using TokanPages.Backend.Domain.Entities.Notifications;
 using TokanPages.Persistence.DataAccess.Contexts;
+using TokanPages.Persistence.DataAccess.Repositories.Notification;
+using TokanPages.Persistence.DataAccess.Repositories.Notification.Models;
 
 namespace TokanPages.Backend.Application.Notifications.Mobile.Commands;
 
@@ -14,25 +13,25 @@ public class AddInstallationCommandHandler : RequestHandler<AddInstallationComma
     private const int MaxRetryCount = 3;
 
     private const int DelayMilliseconds = 1000;
-    
-    private readonly IDateTimeService _dateTimeService;
 
     private readonly IAzureNotificationHubFactory _azureNotificationHubFactory;
 
+    private readonly INotificationRepository _notificationRepository;
+
     public AddInstallationCommandHandler(OperationDbContext operationDbContext, ILoggerService loggerService, 
-        IAzureNotificationHubFactory azureNotificationHubFactory, IDateTimeService dateTimeService) 
+        IAzureNotificationHubFactory azureNotificationHubFactory, INotificationRepository notificationRepository) 
         : base(operationDbContext, loggerService)
     {
         _azureNotificationHubFactory = azureNotificationHubFactory;
-        _dateTimeService = dateTimeService;
+        _notificationRepository = notificationRepository;
     }
 
     public override async Task<AddInstallationCommandResult> Handle(AddInstallationCommand request, CancellationToken cancellationToken)
     {
         var hub = _azureNotificationHubFactory.Create();
-        var tags = request.Tags.ToArray();
+        var tags = request.Tags;
 
-        var notification = await GetInstallationByHandle(request.PnsHandle, cancellationToken);
+        var notification = await _notificationRepository.GetPushNotification(request.PnsHandle);
         var installationId = notification?.Id ?? Guid.NewGuid();
         (bool isVerified, string registrationId) status;
 
@@ -50,20 +49,27 @@ public class AddInstallationCommandHandler : RequestHandler<AddInstallationComma
             const string successInfo = "The new PNS handle has been registered within the Azure Notification Hub";
             status = await TryVerifyInstallation(hub, installationId, successInfo, cancellationToken);
 
-            var pushNotification = new PushNotification
+            var pushNotification = new PushNotificationDto
             {
                 Id = installationId,
                 Handle = installation.Handle,
                 Platform = installation.Platform,
-                Description = "First PNS handle installation",
-                CreatedAt = _dateTimeService.Now,
-                CreatedBy = Guid.Empty,
                 IsVerified = status.isVerified,
                 RegistrationId = status.registrationId
             };
 
-            await RegisterInstallation(installationId, pushNotification, tags, cancellationToken);
-            await OperationDbContext.SaveChangesAsync(cancellationToken);
+            var notificationTags = new List<PushNotificationTagDto>();
+            foreach (var tag in tags)
+            {
+                notificationTags.Add(new PushNotificationTagDto
+                {
+                    PushNotificationId = installationId,
+                    Tag = tag,
+                });
+            }
+
+            await _notificationRepository.CreatePushNotificationEntry(pushNotification);
+            await _notificationRepository.CreatePushNotificationTags(notificationTags);
             LoggerService.LogInformation("The new PNS handle installation has been saved within the system database");
         }
         else
@@ -80,16 +86,17 @@ public class AddInstallationCommandHandler : RequestHandler<AddInstallationComma
             const string successInfo = "The existing PNS handle has been updated within the Azure Notification Hub";
             status = await TryVerifyInstallation(hub, installationId, successInfo, cancellationToken);
 
-            await ReplaceExistingTags(notification.Id, tags, cancellationToken);
+            await ReplaceExistingTags(notification.Id, tags);
             LoggerService.LogInformation($"The existing tags for installation ID ({notification.Id}) have been replaced");
 
-            notification.ModifiedAt = _dateTimeService.Now;
-            notification.ModifiedBy = Guid.Empty;
-            notification.Description = "PNS handle installation updated";
-            notification.IsVerified = status.isVerified;
-            notification.RegistrationId = status.registrationId;
+            var notificationDto = new PushNotificationDto
+            {
+                ModifiedBy = Guid.Empty,
+                IsVerified = status.isVerified,
+                RegistrationId = status.registrationId
+            };
 
-            await OperationDbContext.SaveChangesAsync(cancellationToken);
+            await _notificationRepository.UpdatePushNotificationEntry(notificationDto);
             LoggerService.LogInformation("The existing PNS handle installation has been saved within the system database");
         }
 
@@ -99,12 +106,6 @@ public class AddInstallationCommandHandler : RequestHandler<AddInstallationComma
             RegistrationId = status.registrationId,
             IsVerified = status.isVerified
         };
-    }
-
-    private async Task<PushNotification?> GetInstallationByHandle(string pnsHandle, CancellationToken cancellationToken)
-    {
-        return await OperationDbContext.PushNotifications
-            .SingleOrDefaultAsync(notifications => notifications.Handle == pnsHandle, cancellationToken);
     }
 
     private async Task<(bool, string)> TryVerifyInstallation(IAzureNotificationHubClient hub, Guid installationId, string onSuccessInfo, CancellationToken cancellationToken)
@@ -121,56 +122,26 @@ public class AddInstallationCommandHandler : RequestHandler<AddInstallationComma
             catch (InstallationException exception)
             {
                 await Task.Delay(DelayMilliseconds, cancellationToken);
-                var wording = retryCount == 1 ? "try" : "tries";
-                LoggerService.LogWarning($"Could not get installation ID after {retryCount} {wording}, latest exception: {exception.Message}");
+                LoggerService.LogWarning($"Could not get installation ID after {retryCount} try/-ies, latest exception: {exception.Message}");
             }
         }
 
         return (false, string.Empty);
     }
 
-    private async Task RegisterInstallation(Guid installationId, PushNotification pushNotification, IEnumerable<string> tags, CancellationToken cancellationToken)
+    private async Task ReplaceExistingTags(Guid installationId, IEnumerable<string> newTags)
     {
-        var notificationTags = new List<PushNotificationTag>();
-        foreach (var tag in tags)
-        {
-            notificationTags.Add(new PushNotificationTag
-            {
-                Id = Guid.NewGuid(),
-                PushNotificationId = installationId,
-                Tag = tag,
-                CreatedAt = pushNotification.CreatedAt,
-                CreatedBy = pushNotification.CreatedBy
-            });
-        }
-
-        await OperationDbContext.PushNotifications.AddAsync(pushNotification, cancellationToken);
-        await OperationDbContext.PushNotificationTags.AddRangeAsync(notificationTags, cancellationToken);
-    }
-
-    private async Task ReplaceExistingTags(Guid installationId, IEnumerable<string> newTags, CancellationToken cancellationToken)
-    {
-        var existingTags = await OperationDbContext.PushNotificationTags
-            .Where(tags => tags.PushNotificationId == installationId)
-            .ToListAsync(cancellationToken);
-
-        var notificationTags = new List<PushNotificationTag>();
-        var createdAt = _dateTimeService.Now;
-        var createdBy = Guid.Empty;
-
+        var notificationTags = new List<PushNotificationTagDto>();
         foreach (var tag in newTags)
         {
-            notificationTags.Add(new PushNotificationTag
+            notificationTags.Add(new PushNotificationTagDto
             {
-                Id = Guid.NewGuid(),
                 PushNotificationId = installationId,
                 Tag = tag,
-                CreatedAt = createdAt,
-                CreatedBy = createdBy
             });
         }
 
-        OperationDbContext.PushNotificationTags.RemoveRange(existingTags);
-        await OperationDbContext.PushNotificationTags.AddRangeAsync(notificationTags, cancellationToken);
+        await _notificationRepository.DeletePushNotificationTag(installationId);
+        await _notificationRepository.CreatePushNotificationTags(notificationTags);
     }
 }
