2e32aa626cee3998b69be6348b09eaf54fa9da33
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2026-02-07 19:04:16 +0100

    fix: add missing fields

--- TokanPages.Persistence/TokanPages.Persistence.DataAccess/Repositories/Revenue/RevenueRepository.cs
+++ TokanPages.Persistence/TokanPages.Persistence.DataAccess/Repositories/Revenue/RevenueRepository.cs
@@ -137,6 +137,8 @@ public class RevenueRepository : RepositoryBase, IRevenueRepository
             PmtToken = data.PmtToken,
             ModifiedAt = _dateTimeService.Now,
             ModifiedBy = data.ModifiedBy,
+            CreatedAt = data.CreatedAt,
+            CreatedBy = data.CreatedBy,
             ExtOrderId = data.ExtOrderId
         };
 
