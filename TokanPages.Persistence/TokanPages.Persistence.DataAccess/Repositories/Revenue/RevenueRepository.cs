4552687130faf6e628d66af70fd98744c5f597b9
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2026-02-07 13:37:21 +0100

    feat: implement gettins prices and user subscription

--- TokanPages.Persistence/TokanPages.Persistence.DataAccess/Repositories/Revenue/RevenueRepository.cs
+++ TokanPages.Persistence/TokanPages.Persistence.DataAccess/Repositories/Revenue/RevenueRepository.cs
@@ -1,5 +1,7 @@
 using Microsoft.Extensions.Options;
 using TokanPages.Backend.Configuration.Options;
+using TokanPages.Backend.Domain.Entities;
+using TokanPages.Backend.Domain.Entities.Users;
 using TokanPages.Persistence.DataAccess.Abstractions;
 
 namespace TokanPages.Persistence.DataAccess.Repositories.Revenue;
@@ -8,4 +10,20 @@ public class RevenueRepository : RepositoryBase, IRevenueRepository
 {
     public RevenueRepository(IDbOperations dbOperations, IOptions<AppSettingsModel> appSettings) 
         : base(dbOperations, appSettings) { }
+
+    /// <inheritdoc/>
+    public async Task<List<SubscriptionPricing>> GetSubscriptionPrices(string languageIso)
+    {
+        var filterBy = new { LanguageIso = languageIso };
+        var data = await DbOperations.Retrieve<SubscriptionPricing>(filterBy);
+        return data.ToList();
+    }
+
+    /// <inheritdoc/>
+    public async Task<UserSubscription?> GetUserSubscriptions(Guid userId)
+    {
+        var filterBy = new { UserId = userId };
+        var data = await DbOperations.Retrieve<UserSubscription>(filterBy);
+        return data.SingleOrDefault();
+    }
 }
