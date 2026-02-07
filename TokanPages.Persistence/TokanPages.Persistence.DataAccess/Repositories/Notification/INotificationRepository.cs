e5a428cd095deb3ea3cf96f962e80cfb8c4ecd6c
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2026-02-07 09:31:26 +0100

    docs: update XML commments

--- TokanPages.Persistence/TokanPages.Persistence.DataAccess/Repositories/Notification/INotificationRepository.cs
+++ TokanPages.Persistence/TokanPages.Persistence.DataAccess/Repositories/Notification/INotificationRepository.cs
@@ -13,45 +13,50 @@ public interface INotificationRepository
     Task<PushNotification?> GetPushNotification(string pnsHandle);
 
     /// <summary>
-    /// Creates push notification details entry in the repository.
+    /// Creates push notification details entry in the database.
     /// </summary>
     /// <param name="data">Notification details.</param>
     Task CreatePushNotification(PushNotificationDto data);
 
     /// <summary>
-    /// Updates existing push notification details in the repository.
+    /// Updates existing push notification details in the database.
     /// </summary>
     /// <param name="data">Notification details.</param>
     Task UpdatePushNotification(PushNotificationDto data);
 
     /// <summary>
-    /// Deletes existing push notification entry from the repository.
+    /// Deletes existing push notification entry by the given ID.
     /// </summary>
-    /// <param name="primaryKey">Push notification table primary key.</param>
+    /// <remarks>
+    /// ID is the primary key of the given entity.
+    /// </remarks>
+    /// <param name="primaryKey">A mandatory ID.</param>
     Task DeletePushNotificationByPk(Guid primaryKey);
 
     /// <summary>
-    /// Deletes existing push notification entries from the repository.
+    /// Deletes existing push notification entries by the given IDs.
     /// </summary>
-    /// <param name="primaryKeys">Push notification table primary keys.</param>
-    /// <returns></returns>
+    /// <remarks>
+    /// ID is the primary key of the given entity.
+    /// </remarks>
+    /// <param name="primaryKeys">A list of primary keys.</param>
     Task DeletePushNotificationsByPks(List<object> primaryKeys);
 
     /// <summary>
-    /// Returns push notification tags by an installation ID.
+    /// Returns push notification tags by the installation ID.
     /// </summary>
-    /// <param name="installationId">Installation ID.</param>
+    /// <param name="installationId">A mandatory installation ID.</param>
     /// <returns>List of tags.</returns>
     Task<List<PushNotificationTag>> GetPushNotificationTags(Guid installationId);
 
     /// <summary>
-    /// Creates push notification tag entry in the repository.
+    /// Creates push notification tag entry in the database.
     /// </summary>
     /// <param name="data">Tag details.</param>
     Task CreatePushNotificationTag(PushNotificationTagDto data);
 
     /// <summary>
-    /// Creates push notification tags in the repository.
+    /// Creates push notification tags in the database.
     /// </summary>
     /// <param name="data">List of tags details to be saved.</param>
     Task CreatePushNotificationTags(List<PushNotificationTagDto> data);
@@ -69,18 +74,18 @@ public interface INotificationRepository
     Task DeletePushNotificationTagsByIds(List<object> ids);
 
     /// <summary>
-    /// Creates push notification log entries in the repository.
+    /// Creates push notification log entries in the database.
     /// </summary>
     /// <param name="data">List of log details.</param>
     Task CreatePushNotificationLogs(List<PushNotificationLogDto> data);
 
     /// <summary>
-    /// Returns web notification entity by a given ID.
+    /// Returns web notification entity by the given ID.
     /// </summary>
     /// <remarks>
-    /// ID is a Primary Key of the given entity.
+    /// ID is the primary key of the given entity.
     /// </remarks>
-    /// <param name="id">A mandatory ID used to update a database entry.</param>
+    /// <param name="id">A mandatory ID used to update an entry.</param>
     /// <returns>An entity if exists, otherwise null.</returns>
     Task<WebNotification?> GetWebNotificationById(Guid id);
 
@@ -91,15 +96,15 @@ public interface INotificationRepository
     /// The primary key is optional. If not passed, then it will be created.
     /// </remarks>
     /// <param name="value">Stringify data to be saved.</param>
-    /// <param name="primaryKey">Optional primary key of the entity.</param>
+    /// <param name="primaryKey">An optional primary key of the entity.</param>
     Task CreateWebNotification(string value, Guid? primaryKey = null);
 
     /// <summary>
-    /// Deletes web notification entry for a given ID.
+    /// Deletes web notification entry for the given ID.
     /// </summary>
     /// <remarks>
-    /// ID is a Primary Key of the given entity.
+    /// ID is the primary key of the given entity.
     /// </remarks>
-    /// <param name="id">A mandatory ID used to delete a database entry.</param>
+    /// <param name="id">A mandatory ID used to delete an entry.</param>
     Task DeleteWebNotificationById(Guid id);
 }
