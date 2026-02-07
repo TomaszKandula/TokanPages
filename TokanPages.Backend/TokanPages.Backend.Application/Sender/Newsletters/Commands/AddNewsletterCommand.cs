89bf5599d84da1da0d7a9a709409840ae6f5731e
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2026-02-07 23:08:18 +0100

    fix: remove nullable

--- TokanPages.Backend/TokanPages.Backend.Application/Sender/Newsletters/Commands/AddNewsletterCommand.cs
+++ TokanPages.Backend/TokanPages.Backend.Application/Sender/Newsletters/Commands/AddNewsletterCommand.cs
@@ -4,5 +4,5 @@ namespace TokanPages.Backend.Application.Sender.Newsletters.Commands;
 
 public class AddNewsletterCommand : IRequest<Guid>
 {
-    public string? Email { get; set; }
+    public string Email { get; set; } = string.Empty;
 }
