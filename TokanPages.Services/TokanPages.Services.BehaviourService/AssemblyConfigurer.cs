bcd7bb955d5e3280c940fe52663ed8d599ec6efa
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2026-02-08 15:53:01 +0100

    feat: add asembly configurer

---
+++ TokanPages.Services/TokanPages.Services.BehaviourService/AssemblyConfigurer.cs
@@ -0,0 +1,17 @@
+using System.Diagnostics.CodeAnalysis;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace TokanPages.Services.BehaviourService;
+
+[ExcludeFromCodeCoverage]
+public static class AssemblyConfigurer
+{
+    public static void AddBehaviourServices(this IServiceCollection services)
+    {
+        services.AddScoped(typeof(IPipelineBehavior<,>), typeof(HttpRequestBehaviour<,>));
+        services.AddScoped(typeof(IPipelineBehavior<,>), typeof(TokenCheckBehaviour<,>));
+        services.AddScoped(typeof(IPipelineBehavior<,>), typeof(LoggingBehaviour<,>));
+        services.AddScoped(typeof(IPipelineBehavior<,>), typeof(FluentValidationBehavior<,>));
+    }
+}
