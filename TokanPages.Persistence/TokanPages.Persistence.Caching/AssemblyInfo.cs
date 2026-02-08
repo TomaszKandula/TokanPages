1d7335cd953ba87b0a857eb05edf7409ba331840
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2026-02-08 13:24:50 +0100

    feat: expose to WebAPIs

---
+++ TokanPages.Persistence/TokanPages.Persistence.Caching/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("TokanPages.Articles")]
+[assembly: InternalsVisibleTo("TokanPages.Content")]
+[assembly: InternalsVisibleTo("TokanPages.Invoicing")]
+[assembly: InternalsVisibleTo("TokanPages.Revenue")]
+[assembly: InternalsVisibleTo("TokanPages.Sender")]
+[assembly: InternalsVisibleTo("TokanPages.Users")]
