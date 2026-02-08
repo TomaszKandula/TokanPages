e01ad534a1fa899af8b7993b04c0de07f3c65215
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2026-02-08 14:01:14 +0100

    feat: expose to caching and APIs

---
+++ TokanPages.Services/TokanPages.Services.RedisCacheService/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("TokanPages.Persistence.Caching")]
+[assembly: InternalsVisibleTo("TokanPages.Articles")]
+[assembly: InternalsVisibleTo("TokanPages.Content")]
+[assembly: InternalsVisibleTo("TokanPages.Invoicing")]
+[assembly: InternalsVisibleTo("TokanPages.Revenue")]
+[assembly: InternalsVisibleTo("TokanPages.Sender")]
+[assembly: InternalsVisibleTo("TokanPages.Users")]
