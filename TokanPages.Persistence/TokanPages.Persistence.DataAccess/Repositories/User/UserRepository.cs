c3aa9c9eb260ff7f14d2eff2a74f07bbc4bd9afe
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2026-02-08 22:07:14 +0100

    feat: implement HTTP request history

--- TokanPages.Persistence/TokanPages.Persistence.DataAccess/Repositories/User/UserRepository.cs
+++ TokanPages.Persistence/TokanPages.Persistence.DataAccess/Repositories/User/UserRepository.cs
@@ -1,5 +1,7 @@
 using Microsoft.Extensions.Options;
+using TokanPages.Backend.Domain.Entities;
 using TokanPages.Backend.Shared.Options;
+using TokanPages.Backend.Utility.Abstractions;
 using TokanPages.Persistence.DataAccess.Abstractions;
 using Users = TokanPages.Backend.Domain.Entities.Users;
 
@@ -7,8 +9,10 @@ namespace TokanPages.Persistence.DataAccess.Repositories.User;
 
 public class UserRepository : RepositoryBase, IUserRepository
 {
-    public UserRepository(IDbOperations dbOperations, IOptions<AppSettingsModel> appSettings) 
-        : base(dbOperations, appSettings) { }
+    private readonly IDateTimeService _dateTimeService;
+
+    public UserRepository(IDbOperations dbOperations, IOptions<AppSettingsModel> appSettings, IDateTimeService dateTimeService) 
+        : base(dbOperations, appSettings) => _dateTimeService = dateTimeService;
 
     /// <inheritdoc/>
     public async Task<Users.User?> GetUserById(Guid userId)
@@ -24,5 +28,19 @@ public class UserRepository : RepositoryBase, IUserRepository
         var filterBy = new { UserId = userId };
         var data = await DbOperations.Retrieve<Users.UserInfo>(filterBy);
         return data.SingleOrDefault();
+    }
+
+    /// <inheritdoc/>
+    public async Task InsertHttpRequest(string ipAddress, string handlerName)
+    {
+        var entity = new HttpRequest
+        {
+            Id = Guid.NewGuid(),
+            SourceAddress = ipAddress,
+            RequestedAt = _dateTimeService.Now,
+            RequestedHandlerName = handlerName
+        };
+
+        await DbOperations.Insert(entity);
     }
 }
