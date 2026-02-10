95515d36e3b1192ff87117736d87d9ab6854fb64
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2026-02-10 14:03:03 +0100

    feat: add DTO model

---
+++ TokanPages.Persistence/TokanPages.Persistence.DataAccess/Repositories/User/Models/GetUserDetailsDto.cs
@@ -0,0 +1,41 @@
+using System.Diagnostics.CodeAnalysis;
+
+namespace TokanPages.Persistence.DataAccess.Repositories.User.Models;
+
+[ExcludeFromCodeCoverage]
+public class GetUserDetailsDto
+{
+    public Guid UserId { get; init; }
+
+    public string EmailAddress { get; init; } = string.Empty;
+
+    public string UserAlias { get; init; } = string.Empty;
+
+    public string FirstName { get; init; } = string.Empty;
+
+    public string LastName { get; init; } = string.Empty;
+
+    public string UserAboutText { get; init; } = string.Empty; 
+
+    public string UserImageName  { get; init; } = string.Empty;
+
+    public required string CryptedPassword { get; init; }
+
+    public required Guid? ResetId { get; init; }
+
+    public DateTime? ResetIdEnds { get; init; }
+
+    public Guid? ActivationId { get; init; }
+
+    public DateTime? ActivationIdEnds { get; init; }
+
+    public required bool IsActivated { get; init; }
+
+    public required bool IsVerified { get; init; }
+
+    public required bool IsDeleted { get; init; }
+
+    public required bool HasBusinessLock { get; init; }
+
+    public required DateTime Registered { get; init; }
+}
