b0335827fc3ac33b88f775a9fe3582aabe461f9d
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2026-02-10 21:20:39 +0100

    feat: extend contract

--- TokanPages.Persistence/TokanPages.Persistence.DataAccess/Repositories/User/IUserRepository.cs
+++ TokanPages.Persistence/TokanPages.Persistence.DataAccess/Repositories/User/IUserRepository.cs
@@ -15,10 +15,14 @@ public interface IUserRepository
 
     Task<List<GetUserPermissionDto>> GetUserPermissions(Guid userId);
 
+    Task<GetUserRefreshTokenDto?> GetUserRefreshToken(string token);
+
     Task<List<GetUserRefreshTokenDto>> GetUserRefreshTokens(Guid userId);
 
     Task InsertUserRefreshToken(Guid userId, string token, DateTime expires, DateTime created, string? createdByIp);
-    
+
+    Task DeleteUserRefreshToken(Guid id);
+
     Task DeleteUserRefreshTokens(HashSet<Guid> ids);
 
     Task InsertHttpRequest(string ipAddress, string handlerName);
