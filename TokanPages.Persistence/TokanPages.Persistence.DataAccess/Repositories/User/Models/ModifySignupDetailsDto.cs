9d8e27569039a98dde6a45af7289864afd4c8ed7
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2026-02-11 18:02:28 +0100

    refactor: rename

--- TokanPages.Persistence/TokanPages.Persistence.DataAccess/Repositories/User/Models/ModifySignupDetailsDto.cs
+++ TokanPages.Persistence/TokanPages.Persistence.DataAccess/Repositories/User/Models/ModifySignupDetailsDto.cs
@@ -3,7 +3,7 @@ using System.Diagnostics.CodeAnalysis;
 namespace TokanPages.Persistence.DataAccess.Repositories.User.Models;
 
 [ExcludeFromCodeCoverage]
-public class ModifyRegistrationDetailsDto
+public class ModifySignupDetailsDto
 {
     public required Guid UserId { get; init; }
 
