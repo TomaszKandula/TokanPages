1fd041eca704bfdff44089986b0ee1815639670f
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2026-02-12 00:39:32 +0100

    feat: update command props

--- TokanPages.Backend/TokanPages.Backend.Application/Users/Commands/ResetUserPasswordCommand.cs
+++ TokanPages.Backend/TokanPages.Backend.Application/Users/Commands/ResetUserPasswordCommand.cs
@@ -4,7 +4,7 @@ namespace TokanPages.Backend.Application.Users.Commands;
 
 public class ResetUserPasswordCommand : IRequest<Unit>
 {
-    public string LanguageId { get; set; } = "";
+    public string LanguageId { get; set; } = string.Empty;
 
-    public string? EmailAddress { get; set; }
+    public string EmailAddress { get; set; } = string.Empty;
 }
