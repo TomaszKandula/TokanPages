d42aad8a35f7ed4f920d22286ecf0a6cfefe53df
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2026-02-11 17:57:46 +0100

    feat: add DTO model

---
+++ TokanPages.Persistence/TokanPages.Persistence.DataAccess/Repositories/User/Models/ModifyRegistrationDetailsDto.cs
@@ -0,0 +1,15 @@
+using System.Diagnostics.CodeAnalysis;
+
+namespace TokanPages.Persistence.DataAccess.Repositories.User.Models;
+
+[ExcludeFromCodeCoverage]
+public class ModifyRegistrationDetailsDto
+{
+    public required Guid UserId { get; init; }
+
+    public required string CryptedPassword { get; init; }
+
+    public required Guid ActivationId { get; init; }
+
+    public required DateTime ActivationIdEnds { get; init; }
+}
