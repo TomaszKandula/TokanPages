0a74720e7c18aafc02cccba0841d662592a6fd7c
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2026-02-12 00:39:16 +0100

    feat: add DTO model

---
+++ TokanPages.Persistence/TokanPages.Persistence.DataAccess/Repositories/User/Models/UpdateUserPasswordDto.cs
@@ -0,0 +1,13 @@
+using System.Diagnostics.CodeAnalysis;
+
+namespace TokanPages.Persistence.DataAccess.Repositories.User.Models;
+
+[ExcludeFromCodeCoverage]
+public class UpdateUserPasswordDto
+{
+    public required Guid UserId { get; init; }
+
+    public required int ResetMaturity { get; init; }
+
+    public required Guid ResetId { get; init; }
+}
