7626562296bc1ba0339264bf3dbfbebb869aa820
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2026-02-11 17:07:18 +0100

    feat: replace EFCore w/Dapper

--- TokanPages.Backend/TokanPages.Backend.Application/Users/Commands/ActivateUserCommandHandler.cs
+++ TokanPages.Backend/TokanPages.Backend.Application/Users/Commands/ActivateUserCommandHandler.cs
@@ -1,46 +1,42 @@
-using Microsoft.EntityFrameworkCore;
 using TokanPages.Backend.Core.Exceptions;
 using TokanPages.Backend.Shared.Resources;
 using TokanPages.Backend.Utility.Abstractions;
 using TokanPages.Persistence.DataAccess.Contexts;
+using TokanPages.Persistence.DataAccess.Repositories.User;
 
 namespace TokanPages.Backend.Application.Users.Commands;
 
 public class ActivateUserCommandHandler : RequestHandler<ActivateUserCommand, ActivateUserCommandResult>
 {
+    private readonly IUserRepository _userRepository;
+
     private readonly IDateTimeService _dateTimeService;
 
     public ActivateUserCommandHandler(OperationDbContext operationDbContext, ILoggerService loggerService, 
-        IDateTimeService dateTimeService) : base(operationDbContext, loggerService) => _dateTimeService = dateTimeService;
+        IDateTimeService dateTimeService, IUserRepository userRepository) : base(operationDbContext, loggerService)
+    {
+        _dateTimeService = dateTimeService;
+        _userRepository = userRepository;
+    }
 
     public override async Task<ActivateUserCommandResult> Handle(ActivateUserCommand request, CancellationToken cancellationToken)
     {
-        var user = await OperationDbContext.Users
-            .Where(users => users.ActivationId == request.ActivationId)
-            .Where(users => !users.IsDeleted)
-            .SingleOrDefaultAsync(cancellationToken);
-
-        if (user is null)
+        var userDetails = await _userRepository.GetUserDetailsByActivationId(request.ActivationId);
+        if (userDetails is null)
             throw new BusinessException(nameof(ErrorCodes.INVALID_ACTIVATION_ID), ErrorCodes.INVALID_ACTIVATION_ID);
 
-        if (user.ActivationIdEnds < _dateTimeService.Now)
+        if (userDetails.ActivationIdEnds < _dateTimeService.Now)
             throw new BusinessException(nameof(ErrorCodes.EXPIRED_ACTIVATION_ID), ErrorCodes.EXPIRED_ACTIVATION_ID);
 
-        user.IsActivated = true;
-        user.IsVerified = true;
-        user.ActivationId = null;
-        user.ActivationIdEnds = null;
-
-        if (user.HasBusinessLock)
+        await _userRepository.ActivateUser(userDetails.UserId);
+        if (userDetails.HasBusinessLock)
             LoggerService.LogWarning("The user is activated but has the business lock and thus will require administrator action.");
 
-        LoggerService.LogInformation($"User account has been activated, user ID: {user.Id}");
-        await OperationDbContext.SaveChangesAsync(cancellationToken);
-
+        LoggerService.LogInformation($"User account has been activated, user ID: {userDetails.UserId}");
         return new ActivateUserCommandResult
         {
-            UserId = user.Id,
-            HasBusinessLock = user.HasBusinessLock
+            UserId = userDetails.UserId,
+            HasBusinessLock = userDetails.HasBusinessLock
         };
     }
 }
