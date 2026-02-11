4808a3be509d40f4283c0050d8d64cf0c5f28f58
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2026-02-11 14:07:55 +0100

    feat: replace EFCore w/Dapper

--- TokanPages.Backend/TokanPages.Backend.Application/Users/Queries/GetUserNotesQueryHandler.cs
+++ TokanPages.Backend/TokanPages.Backend.Application/Users/Queries/GetUserNotesQueryHandler.cs
@@ -1,26 +1,28 @@
-using Microsoft.EntityFrameworkCore;
 using TokanPages.Backend.Core.Extensions;
 using TokanPages.Backend.Utility.Abstractions;
 using TokanPages.Persistence.DataAccess.Contexts;
+using TokanPages.Persistence.DataAccess.Repositories.User;
 using TokanPages.Services.UserService.Abstractions;
 
 namespace TokanPages.Backend.Application.Users.Queries;
 
 public class GetUserNotesQueryHandler : RequestHandler<GetUserNotesQuery, GetUserNotesQueryResult>
 {
+    private readonly IUserRepository _userRepository;
+
     private readonly IUserService _userService;
 
-    public GetUserNotesQueryHandler(OperationDbContext operationDbContext, ILoggerService loggerService, IUserService userService) 
-        : base(operationDbContext, loggerService) => _userService = userService;
+    public GetUserNotesQueryHandler(OperationDbContext operationDbContext, ILoggerService loggerService, 
+        IUserService userService, IUserRepository userRepository) : base(operationDbContext, loggerService)
+    {
+        _userService = userService;
+        _userRepository = userRepository;
+    }
 
     public override async Task<GetUserNotesQueryResult> Handle(GetUserNotesQuery request, CancellationToken cancellationToken)
     {
         var userId = _userService.GetLoggedUserId();
-        var notes = await OperationDbContext.UserNotes
-            .AsNoTracking()
-            .Where(note => note.UserId == userId)
-            .ToListAsync(cancellationToken);
-
+        var notes = await _userRepository.GetUserNotes(userId);
         if (notes.Count == 0)
             return new GetUserNotesQueryResult();
 
