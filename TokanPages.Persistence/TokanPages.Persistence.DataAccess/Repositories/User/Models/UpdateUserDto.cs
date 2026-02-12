b88a66dbe8f006aab95076461d464d752bd962f6
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2026-02-12 18:55:57 +0100

    feat: update properties

--- TokanPages.Persistence/TokanPages.Persistence.DataAccess/Repositories/User/Models/UpdateUserDto.cs
+++ TokanPages.Persistence/TokanPages.Persistence.DataAccess/Repositories/User/Models/UpdateUserDto.cs
@@ -5,13 +5,13 @@ namespace TokanPages.Persistence.DataAccess.Repositories.User.Models;
 [ExcludeFromCodeCoverage]
 public class UpdateUserDto
 {
-    public required Guid UserId { get; set; }
+    public required Guid UserId { get; init; }
 
-    public required string UserAlias { get; set; }
+    public required string UserAlias { get; init; }
 
-    public required string EmailAddress { get; set; }
+    public required string EmailAddress { get; init; }
 
-    public required bool IsActivated { get; set; }
+    public required bool IsActivated { get; init; }
 
-    public required bool IsVerified { get; set; }
+    public required bool IsVerified { get; init; }
 }
