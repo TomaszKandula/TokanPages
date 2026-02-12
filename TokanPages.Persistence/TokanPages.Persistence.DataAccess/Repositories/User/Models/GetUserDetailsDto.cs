93a0bf0056707e403833b4b0c0b7e56a9ac333ad
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2026-02-12 17:24:50 +0100

    fix: add missing property

--- TokanPages.Persistence/TokanPages.Persistence.DataAccess/Repositories/User/Models/GetUserDetailsDto.cs
+++ TokanPages.Persistence/TokanPages.Persistence.DataAccess/Repositories/User/Models/GetUserDetailsDto.cs
@@ -19,6 +19,8 @@ public class GetUserDetailsDto
 
     public required string UserImageName  { get; init; } = string.Empty;
 
+    public required string UserVideoName  { get; init; } = string.Empty;
+
     public required string CryptedPassword { get; init; }
 
     public Guid? ResetId { get; init; }
