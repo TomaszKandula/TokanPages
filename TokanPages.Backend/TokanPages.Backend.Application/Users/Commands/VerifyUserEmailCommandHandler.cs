3c9da81c0b5824d0af4c859ca875a3a488ce1caa
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2026-02-12 18:51:42 +0100

    feat: remove EFCore, use repository service

--- TokanPages.Backend/TokanPages.Backend.Application/Users/Commands/VerifyUserEmailCommandHandler.cs
+++ TokanPages.Backend/TokanPages.Backend.Application/Users/Commands/VerifyUserEmailCommandHandler.cs
@@ -1,4 +1,3 @@
-using TokanPages.Backend.Domain.Entities;
 using TokanPages.Services.EmailSenderService.Abstractions;
 using TokanPages.Services.EmailSenderService.Models;
 using TokanPages.Services.UserService.Abstractions;
@@ -7,6 +6,8 @@ using Microsoft.Extensions.Options;
 using TokanPages.Backend.Shared.Options;
 using TokanPages.Backend.Utility.Abstractions;
 using TokanPages.Persistence.DataAccess.Contexts;
+using TokanPages.Persistence.DataAccess.Repositories.Messaging;
+using TokanPages.Persistence.DataAccess.Repositories.User;
 
 namespace TokanPages.Backend.Application.Users.Commands;
 
@@ -16,66 +17,48 @@ public class VerifyUserEmailCommandHandler : RequestHandler<VerifyUserEmailComma
 
     private readonly IUserService _userService;
 
+    private readonly IUserRepository _userRepository;
+    
+    private readonly IMessagingRepository _messagingRepository;
+
     private readonly IDateTimeService _dateTimeService;
 
     private readonly IEmailSenderService _emailSenderService;
 
     public VerifyUserEmailCommandHandler(OperationDbContext operationDbContext, ILoggerService loggerService, 
         IOptions<AppSettingsModel> options, IUserService userService, IDateTimeService dateTimeService, 
-        IEmailSenderService emailSenderService) : base(operationDbContext, loggerService)
+        IEmailSenderService emailSenderService, IUserRepository userRepository, IMessagingRepository messagingRepository) : base(operationDbContext, loggerService)
     {
         _appSettings = options.Value;
         _userService = userService;
         _dateTimeService = dateTimeService;
         _emailSenderService = emailSenderService;
+        _userRepository = userRepository;
+        _messagingRepository = messagingRepository;
     }
 
     public override async Task<Unit> Handle(VerifyUserEmailCommand request, CancellationToken cancellationToken)
     {
         var user = await _userService.GetActiveUser();
 
+        var messageId = Guid.NewGuid();
         var activationId = Guid.NewGuid();
         var activationMaturity = _appSettings.LimitActivationMaturity;
         var activationIdEnds = _dateTimeService.Now.AddMinutes(activationMaturity);
-
-        user.ActivationId = activationId;
-        user.ActivationIdEnds = activationIdEnds;
-
-        var messageId = await PrepareNotificationUncommitted(cancellationToken);
-        await CommitAllChanges(cancellationToken);
-        await SendNotification(messageId, request.LanguageId, request.EmailAddress, activationId, activationIdEnds, cancellationToken);
-
-        LoggerService.LogInformation($"Sending activation email to verify user email address.");
-        return Unit.Value;
-    }
-
-    private async Task CommitAllChanges(CancellationToken cancellationToken = default) 
-        => await OperationDbContext.SaveChangesAsync(cancellationToken);
-
-    private async Task<Guid> PrepareNotificationUncommitted(CancellationToken cancellationToken)
-    {
-        var messageId = Guid.NewGuid();
-        var serviceBusMessage = new ServiceBusMessage
-        {
-            Id = messageId,
-            IsConsumed = false
-        };
-
-        await OperationDbContext.ServiceBusMessages.AddAsync(serviceBusMessage, cancellationToken);
-        return messageId;
-    }
-
-    private async Task SendNotification(Guid messageId, string languageId, string emailAddress, Guid activationId, DateTime activationIdEnds, CancellationToken cancellationToken)
-    {
         var configuration = new VerifyEmailConfiguration
         {
-            LanguageId = languageId,
+            LanguageId = request.LanguageId,
             MessageId = messageId,
-            EmailAddress = emailAddress,
+            EmailAddress = request.EmailAddress,
             VerificationId = activationId,
             VerificationIdEnds = activationIdEnds
         };
 
+        await _messagingRepository.CreateServiceBusMessage(messageId);
+        await _userRepository.UpdateUserActivation(user.UserId, activationId, activationIdEnds);
         await _emailSenderService.SendToServiceBus(configuration, cancellationToken);
+
+        LoggerService.LogInformation("Sending activation email to verify user email address.");
+        return Unit.Value;
     }
 }
