fb2ec353e60746c04c876c8325679ffa781383f8
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2026-02-14 11:00:03 +0100

    feat: change contract, do not return boolean value

--- TokanPages.Persistence/TokanPages.Persistence.DataAccess/Repositories/Messaging/IMessagingRepository.cs
+++ TokanPages.Persistence/TokanPages.Persistence.DataAccess/Repositories/Messaging/IMessagingRepository.cs
@@ -6,7 +6,7 @@ public interface IMessagingRepository
 {
     Task<ServiceBusMessage?> GetServiceBusMessage(Guid messageId);
 
-    Task<bool> CreateServiceBusMessage(Guid messageId);
+    Task CreateServiceBusMessage(Guid messageId);
 
-    Task<bool> UpdateServiceBusMessage(Guid messageId, bool isConsumed);
+    Task UpdateServiceBusMessage(Guid messageId, bool isConsumed);
 }
