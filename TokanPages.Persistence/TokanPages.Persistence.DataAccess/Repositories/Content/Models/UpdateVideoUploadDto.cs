e09d4cd0d5dece5577d27db6121763549184ef7f
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2026-02-14 14:54:51 +0100

    feat: use init

--- TokanPages.Persistence/TokanPages.Persistence.DataAccess/Repositories/Content/Models/UpdateVideoUploadDto.cs
+++ TokanPages.Persistence/TokanPages.Persistence.DataAccess/Repositories/Content/Models/UpdateVideoUploadDto.cs
@@ -6,15 +6,15 @@ namespace TokanPages.Persistence.DataAccess.Repositories.Content.Models;
 [ExcludeFromCodeCoverage]
 public class UpdateVideoUploadDto
 {
-    public required Guid TicketId { get; set; }
+    public required Guid TicketId { get; init; }
 
-    public required VideoStatus Status { get; set; }
+    public required VideoStatus Status { get; init; }
 
-    public required bool IsSourceDeleted { get; set; }
+    public required bool IsSourceDeleted { get; init; }
 
-    public required string? ProcessingWarning { get; set; }
+    public required string? ProcessingWarning { get; init; }
 
-    public required long InputSizeInBytes { get; set; }
+    public required long InputSizeInBytes { get; init; }
 
-    public required long OutputSizeInBytes { get; set; }
+    public required long OutputSizeInBytes { get; init; }
 }
