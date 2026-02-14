a2193eecfd0fa46fdfd8f9d66f98a23fb83fb92e
Tomasz 'Tom' Kandula <tom@tomkandula.com>
2026-02-14 14:39:05 +0100

    feat: extend contract

--- TokanPages.Persistence/TokanPages.Persistence.DataAccess/Repositories/Content/IContentRepository.cs
+++ TokanPages.Persistence/TokanPages.Persistence.DataAccess/Repositories/Content/IContentRepository.cs
@@ -1,10 +1,13 @@
+using TokanPages.Backend.Domain.Enums;
 using TokanPages.Persistence.DataAccess.Repositories.Content.Models;
 
 namespace TokanPages.Persistence.DataAccess.Repositories.Content;
 
 public interface IContentRepository
 {
-    Task<VideoUploadStatusDto?> GetVideoUploadStatus(Guid ticketId);
+    Task<VideoUploadStatusDto?> GetVideoUploadStatus(Guid ticketId, VideoStatus status);
+
+    Task UpdateVideoUploadStatus(Guid ticketId, VideoStatus status);
 
     Task CreateVideoUpload(Guid userId, Guid ticketId, string sourceBlobUri, string targetVideoUri, string targetThumbnailUri);
 
